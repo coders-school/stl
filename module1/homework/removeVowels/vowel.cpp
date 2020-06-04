10d9d7bbd6184db086b7fd186282b4cd59ffc50c
NG90 <glowka.natalia@gmail.com>
2020-06-04 22:09:50 +0200

    removeVowels - function stub

---
+++ module1/homework/removeVowels/vowel.cpp
@@ -0,0 +1,10 @@
+#include "vowel.hpp"
+#include <array>
+
+namespace {
+constexpr std::array<char, 12> vowels{'a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y'};
+}
+
+void removeVowels(std::vector<std::string>& text) {
+    return;
+}
