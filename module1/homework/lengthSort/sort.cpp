ddacf414c9995677e459527986afb9bb3f9cf5a7
Andrzej Rafalski <anraf1001@gmail.com>
2020-06-05 13:16:37 +0200

    Implementation of lenghtSort

---
+++ module1/homework/lengthSort/sort.cpp
@@ -0,0 +1,23 @@
+#include "sort.hpp"
+
+#include <algorithm>
+
+std::deque<std::string> lengthSort(std::forward_list<std::string> listToSort) {
+    auto sortPred = [](const std::string& lhs, const std::string& rhs) {
+        if (lhs.size() == rhs.size()) {
+            return lhs <= rhs;
+        }
+
+        return lhs.size() < rhs.size();
+    };
+
+    listToSort.sort(sortPred);
+
+    std::deque<std::string> result;
+
+    for (const auto& el : listToSort) {
+        result.push_back(el);
+    }
+
+    return result;
+}
