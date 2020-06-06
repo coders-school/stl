75adf6014bfd9b57d08b3b68aeed8eeea18e9f0d
Marcinati <marcin.kapusta.pk@gmail.com>
2020-06-06 09:35:55 +0200

    implementation of removing vowels

---
+++ module1/homework/removeVowels/vowel.cpp
@@ -0,0 +1,14 @@
+#include "vowel.hpp"
+#include <algorithm>
+#include <array>
+
+bool isVowel(const char stringElement) {
+    constexpr std::array<char, 12> vowels = {'a', 'e', 'o', 'i', 'u', 'y',
+                                             'A', 'E', 'O', 'I', 'U', 'Y'};
+    return std::find(vowels.begin(), vowels.end(), stringElement) != vowels.end();
+}
+void removeVowels(std::vector<std::string>& vector) {
+    for (auto& el : vector) {
+        el.erase(std::remove_if(el.begin(), el.end(), isVowel), el.end());
+    }
+}
---
+++ module1/homework/removeVowels/vowel.hpp
@@ -0,0 +1,6 @@
+#pragma once
+#include <string>
+#include <vector>
+
+bool isVowel(const char stringElement);
+void removeVowels(std::vector<std::string>& vector);
