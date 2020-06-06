dd15b3b09b70b5a0dcf810e3cd3d4425bc764d47
mariomoore <mariusz.murawski84@gmail.com>
2020-06-06 18:53:16 +0200

    Empty build

---
+++ module1/homework/grayscaleImages/compression.cpp
@@ -0,0 +1,13 @@
+#include "compression.hpp"
+
+std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(std::array<std::array<uint8_t, width>, height> gsArr) {
+    std::vector<std::pair<uint8_t, uint8_t>> compressedGS;
+
+    return compressedGS;
+}
+
+std::array<std::array<uint8_t, width>, height> decompressGrayscale(std::vector<std::pair<uint8_t, uint8_t>> gsVec) {
+    std::array<std::array<uint8_t, width>, height> decompressedGS;
+
+    return decompressedGS;
+}
