d36da6d36a758c36d5e1be29a4fc36137f7ab4dc
mariomoore <mariusz.murawski84@gmail.com>
2020-06-06 19:06:35 +0200

    Added const ref

--- module1/homework/grayscaleImages/compression.cpp
+++ module1/homework/grayscaleImages/compression.cpp
@@ -1,12 +1,12 @@
 #include "compression.hpp"
 
-std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(std::array<std::array<uint8_t, width>, height> gsArr) {
+std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(const std::array<std::array<uint8_t, width>, height>& gsArr) {
     std::vector<std::pair<uint8_t, uint8_t>> compressedGS;
 
     return compressedGS;
 }
 
-std::array<std::array<uint8_t, width>, height> decompressGrayscale(std::vector<std::pair<uint8_t, uint8_t>> gsVec) {
+std::array<std::array<uint8_t, width>, height> decompressGrayscale(const std::vector<std::pair<uint8_t, uint8_t>>& gsVec) {
     std::array<std::array<uint8_t, width>, height> decompressedGS;
 
     return decompressedGS;
