5278dd84001d063ce4779873832ec9425bc2bce5
kszytko <krzysiek.szytko@gmail.com>
2020-06-07 23:44:48 +0200

    Added removeVowels function

---
+++ module1/homework/removeVowels/vowel.cpp
@@ -0,0 +1,9 @@
+#include "vowel.hpp"
+#include <algorithm>
+
+void removeVowels(std::vector<std::string>& words){
+    auto isVowel = [](char c){ return std::find(VOWELS.begin(), VOWELS.end(), c) != VOWELS.end(); };
+    for(auto& word: words){
+        word.erase(std::remove_if(word.begin(), word.end(), isVowel), word.end());
+    }
+}
---
+++ module1/homework/removeVowels/vowel.hpp
@@ -0,0 +1,7 @@
+#include <string>
+#include <vector>
+
+const std::string VOWELS{"aeiouyAEIOUY"};
+//const std::vector<char> VOWELS{'a','e','i','o','u','y'};
+
+void removeVowels(std::vector<std::string>&);
