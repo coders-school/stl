7e43c16708e39f8ad229c899f0f3cf3794dc7583
duszek <marcin.dus@gmail.com>
2020-06-07 14:28:07 +0200

    removeVowels - first approach with for loops

---
+++ module1/homework/removeVowels/vowel.cpp
@@ -0,0 +1,19 @@
+
+#include "vowel.hpp"
+#include <algorithm>
+
+void removeVowelsfromWord(std::string& word)
+{
+    for (char i : "aAeEyYuUiIiOo")
+    {
+        word.erase(std::remove(std::begin(word), std::end(word), i), std::end(word));
+    }
+}
+
+void removeVowels(std::vector<std::string>& words)
+{
+    for (auto& s : words)
+    {
+        removeVowelsfromWord(s);
+    }
+}
