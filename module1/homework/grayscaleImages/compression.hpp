223de3349bed0e971e3a8e78f745ee719dbf7f20
Vicx95 <wiktor.zyla31@gmail.com>
2020-06-07 21:28:22 +0200

    Implements compressGrayscale function

--- module1/homework/grayscaleImages/compression.hpp
+++ module1/homework/grayscaleImages/compression.hpp
@@ -1,12 +1,14 @@
 #pragma once
 
 #include <array>
+#include <algorithm>
 #include <vector>
+
 
 constexpr size_t height = 32;
 constexpr size_t width = 32;
 
-std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(std::array<std::array<uint8_t, width>, height>);  //TODO: Name for the parameter
+std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(const std::array<std::array<uint8_t, width>, height>& compress);
 
 std::array<std::array<uint8_t, width>, height> decompressGrayscale(const std::vector<std::pair<uint8_t, uint8_t>>& compressedMap);
 
