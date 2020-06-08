0540c7cdb773c89f59df9476d7eea1bc8bf4f553
Kasper Biegun <kasper@alert.pl>
2020-06-08 09:13:30 +0200

    Refactor

--- module1/homework/lengthSort/sort.cpp
+++ module1/homework/lengthSort/sort.cpp
@@ -5,18 +5,18 @@ bool compareLexicographically(const std::string& first_string,
                               const std::string& second_string) {
     std::size_t i = 0;
     while ((i < first_string.size()) && (i < second_string.size())) {
-        if (first_string[i] < second_string[i]) {
+        if (tolower(first_string[i]) < tolower(second_string[i])) {
             return true;
-        } else if (first_string[i] > second_string[i]) {
+        } else if (tolower(first_string[i]) > tolower(second_string[i])) {
             return false;
         }
-        ++i;
+        i++;
     }
     return false;
 }
 
 bool compareSize(const std::string& first_string, const std::string& second_string) {
-    return (first_string.size() < second_string.size());
+    return first_string.size() < second_string.size();
 }
 
 std::deque<std::string> lengthSort(std::forward_list<std::string> input_list) {
@@ -24,7 +24,7 @@ std::deque<std::string> lengthSort(std::forward_list<std::string> input_list) {
     input_list.sort(compareLexicographically);
     input_list.sort(compareSize);
     for (std::string element : input_list) {
-        result_deque.push_back(element);
+        result_deque.emplace_back(element);
     }
     return result_deque;
 }
