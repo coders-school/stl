e2937ff08a0c55dd4d27633d918fd8fd84cb4654
Andrzej Rafalski <anraf1001@gmail.com>
2020-06-08 16:49:33 +0200

    Suggested changes to lengthSort

--- module1/homework/lengthSort/sort.cpp
+++ module1/homework/lengthSort/sort.cpp
@@ -1,19 +1,13 @@
 #include "sort.hpp"
 
 std::deque<std::string> lengthSort(std::forward_list<std::string> listToSort) {
-    auto sortPred = [](const std::string& lhs, const std::string& rhs) {
+    listToSort.sort([](const std::string& lhs, const std::string& rhs) {
         if (lhs.size() == rhs.size()) {
             return lhs <= rhs;
         }
 
         return lhs.size() <= rhs.size();
-    };
+    });
 
-    listToSort.sort(sortPred);
-
-    std::deque<std::string> result;
-
-    result.insert(result.cbegin(), listToSort.cbegin(), listToSort.cend());
-
-    return result;
+    return std::deque<std::string>(listToSort.cbegin(), listToSort.cend());
 }
