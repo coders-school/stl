31a44fde8396c47cfca2ea2323e5f463d4de218d
duszek <marcin.dus@gmail.com>
2020-06-08 21:57:37 +0200

    compilation fixed

---
+++ module1/homework/grayscaleImages/compression.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <algorithm>
+#include <array>
+#include <utility>
+#include <vector>
+
+constexpr size_t width = 32;
+constexpr size_t height = 32;
+
+std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(std::array<std::array<uint8_t, width>, height> bitmap);
+std::array<std::array<uint8_t, width>, height>
+decompressGrayscale(std::vector<std::pair<uint8_t, uint8_t>> compressedBitmap);
--- module1/homework/grayscaleImages/test.cpp
+++ module1/homework/grayscaleImages/test.cpp
@@ -3,8 +3,9 @@
 #include <utility>  // for std::pair<>
 #include <vector>
 
-// TODO: include
+#include "compression.hpp"
 #include "gtest/gtest.h"
+
 
 void expectBitmap(const std::vector<std::pair<uint8_t, uint8_t>>& bitmap, size_t fraction) {
     for (int j = 0; j < fraction; j++) {
