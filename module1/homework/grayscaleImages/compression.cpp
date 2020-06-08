fb498f4d6c529e805a0eae116628c7929b6ff362
duszek <marcin.dus@gmail.com>
2020-06-08 21:57:37 +0200

    compilation fixed

---
+++ module1/homework/grayscaleImages/compression.cpp
@@ -0,0 +1,14 @@
+#include "compression.hpp"
+
+std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(std::array<std::array<uint8_t, width>, height> bitmap)
+{
+    std::vector<std::pair<uint8_t, uint8_t>> result;
+    return result;
+}
+
+std::array<std::array<uint8_t, width>, height>
+decompressGrayscale(std::vector<std::pair<uint8_t, uint8_t>> compressedBitmap)
+{
+    std::array<std::array<uint8_t, width>, height> result;
+    return result;
+}
