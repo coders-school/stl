8080393084e80f6e713bf0446107335b245ccecf
KrzysztofJ85 <krzysztof.jucha@gmail.com>
2020-06-09 23:50:16 +0200

    creating required files; including header in main/test.cpp

---
+++ module1/homework/grayscaleImages/compression.cpp
@@ -0,0 +1,18 @@
+#include "compression.hpp"
+
+#include <iostream>
+
+std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(const std::array<std::array<uint8_t, width>, height>& dataToCompress) {
+    std::vector<std::pair<uint8_t, uint8_t>> vecCompressed;
+
+    return vecCompressed;
+}
+
+std::array<std::array<uint8_t, width>, height> decompressGrayscale(const std::vector<std::pair<uint8_t, uint8_t>>& dataTodecompress) {
+    std::array<std::array<uint8_t, width>, height> arrayDecompressed;
+
+    return arrayDecompressed;
+}
+
+void printMap(const std::array<std::array<uint8_t, height>, width>& mapToPrint) {
+}
