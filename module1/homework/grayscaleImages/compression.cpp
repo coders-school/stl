526009be02c195413e08d5e5c62e3ef11c7a2164
mdus <marcin.dus@nokia.com>
2020-06-09 11:05:48 +0200

    First implementation od compress [green]

--- module1/homework/grayscaleImages/compression.cpp
+++ module1/homework/grayscaleImages/compression.cpp
@@ -1,12 +1,24 @@
 #include "compression.hpp"
+#include <iostream>
 
-CompressedBitmap compressGrayscale(Bitmap bitmap)
+
+void printBitmap(Bitmap bitmap) {
+    std::cout << "BITMAP!" << "\n";
+    for(const auto & line : bitmap)
+    {
+        for(const auto & pixel : line)
+        {
+            std::cout << static_cast<int>(pixel) << " ";
+        }
+        std::cout << "\n";
+    }
+}
+
+CompressedBitmap compressLine(const std::array<uint8_t,width>& line)
 {
     CompressedBitmap result;
-    std::pair<uint8_t, uint8_t> buffer{bitmap[0][0], 0};
+    std::pair<uint8_t, uint8_t> buffer{line[0], 0};
 
-    for (const auto& line : bitmap)
-    {
         for (const auto& pixel : line)
         {
             if (buffer.first == pixel)
@@ -22,6 +34,23 @@ CompressedBitmap compressGrayscale(Bitmap bitmap)
             }
         }
         result.push_back(buffer);
+    return result;
+}
+
+
+
+CompressedBitmap compressGrayscale(Bitmap bitmap)
+{
+    CompressedBitmap result;
+    CompressedBitmap tmp;
+
+    //printBitmap(bitmap);
+
+    for (const auto& line : bitmap)
+    {
+        tmp = compressLine(line);
+        result.insert(std::end(result), std::begin(tmp), std::end(tmp));
+
     }
     return result;
 }
