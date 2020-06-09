b16a56b576076a2adfb7d9f2a1e2b791ba0441c3
KrzysztofJ85 <krzysztof.jucha@gmail.com>
2020-06-10 00:27:26 +0200

    required files prepared

---
+++ module1/homework/lengthSort/sort.cpp
@@ -0,0 +1,9 @@
+#include "sort.hpp"
+
+#include <algorithm>
+
+std::deque<std::string> lengthSort(const std::forward_list<std::string>& listToSort) {
+    std::deque<std::string> deq;
+
+    return deq;
+}
