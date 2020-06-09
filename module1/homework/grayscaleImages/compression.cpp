3ed0b8766f9d42bf79c718e9fa1bcd489089d894
KrzysztofJ85 <krzysztof.jucha@gmail.com>
2020-06-09 23:52:08 +0200

    compressGrayscale() prepared

--- module1/homework/grayscaleImages/compression.cpp
+++ module1/homework/grayscaleImages/compression.cpp
@@ -6,6 +6,19 @@ constexpr uint8_t ASCIIspace = 32;
 
 std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(const std::array<std::array<uint8_t, width>, height>& dataToCompress) {
     std::vector<std::pair<uint8_t, uint8_t>> vecCompressed;
+    for (const auto& arrayInVec : dataToCompress) {
+        uint8_t counter = 0;
+        for (auto it = arrayInVec.begin(); it != arrayInVec.end(); ++it) {
+            if (*it == *std::next(it) && (std::next(it) != arrayInVec.end())) {
+                counter++;
+            }
+            if (*it != *std::next(it) || (std::next(it) == arrayInVec.end())) {
+                counter++;
+                vecCompressed.push_back({*it, counter});
+                counter = 0;
+            }
+        }
+    }
 
     return vecCompressed;
 }
