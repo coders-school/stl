f7539d2c8ef52b14c4f034608d120cd22186eb38
KrzysztofJ85 <krzysztof.jucha@gmail.com>
2020-06-10 00:28:30 +0200

    lengthSort() prepared

--- module1/homework/lengthSort/sort.cpp
+++ module1/homework/lengthSort/sort.cpp
@@ -5,5 +5,13 @@
 std::deque<std::string> lengthSort(const std::forward_list<std::string>& listToSort) {
     std::deque<std::string> deq;
 
+    std::copy(listToSort.begin(), listToSort.end(), std::back_inserter(deq));
+
+    std::sort(deq.begin(), deq.end(), [](const auto& leftString, const auto& rightString) {
+        if (leftString.size() == rightString.size()) {
+            return leftString < rightString;
+        }
+        return leftString.size() < rightString.size();
+    });
     return deq;
 }
