560b89aab5afd70367a5083285ec5a1256430526
LogarithmDerivative <mateusz.kowalski074@gmail.com>
2020-06-10 00:31:33 +0200

    compression added

--- module1/homework/grayscaleImages/compression.cpp
+++ module1/homework/grayscaleImages/compression.cpp
@@ -1,10 +1,33 @@
 #include "compression.hpp"
 
-std::array<std::array<int, width>, height> decompressGrayscale(const std::vector<std::pair<int, int>>& compressedData)
+std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(const std::array<std::array<uint8_t, width>, height>& bitmap)
 {
-    std::array<std::array<int, width>, height> decompressedData;
+    std::vector<std::pair<uint8_t, uint8_t>> compressedData;
 
-    
+    compressedData.reserve(width * height);
 
-    return decompressedData;
+    size_t i, j, counter = 1;
+
+    for(i = 0; i < height; i++)
+    {
+        for(j = 0; j < width - 1; j++)
+        {
+            if(bitmap[i][j] != bitmap[i][j + 1])
+            {
+                compressedData.emplace_back(bitmap[i][j], counter);
+                counter = 1;
+            }
+            else 
+            {
+                counter++;
+            }
+        }
+        compressedData.emplace_back(bitmap[i][j], counter);
+        counter = 1;
+    }
+
+    compressedData.shrink_to_fit();
+
+    return compressedData;
 }
+
