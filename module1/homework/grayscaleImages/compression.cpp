ad1e2f71c780f0323668f9ebf25d27c4e179dfeb
LogarithmDerivative <mateusz.kowalski074@gmail.com>
2020-06-09 23:00:31 +0200

    added code to make this compile

--- module1/homework/grayscaleImages/compression.cpp
+++ module1/homework/grayscaleImages/compression.cpp
@@ -1,1 +1,10 @@
 #include "compression.hpp"
+
+std::array<std::array<int, width>, height> decompressGrayscale(const std::vector<std::pair<int, int>>& compressedData)
+{
+    std::array<std::array<int, width>, height> decompressedData;
+
+    
+
+    return decompressedData;
+}
--- module1/homework/grayscaleImages/compression.hpp
+++ module1/homework/grayscaleImages/compression.hpp
@@ -1,1 +1,9 @@
 #pragma once
+
+#include <array>
+#include <vector>
+
+constexpr size_t width = 32;
+constexpr size_t height = 32;
+
+std::array<std::array<int, width>, height> decompressGrayscale(const std::vector<std::pair<int, int>>&);
