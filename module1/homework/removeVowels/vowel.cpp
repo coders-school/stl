ab29520c1ff13c416127ceeb772ce7ee11518fd6
KrzysztofJ85 <krzysztof.jucha@gmail.com>
2020-06-10 00:47:58 +0200

    removeVowels() prepared

--- module1/homework/removeVowels/vowel.cpp
+++ module1/homework/removeVowels/vowel.cpp
@@ -3,4 +3,11 @@
 #include <algorithm>
 
 void removeVowels(std::vector<std::string>& vecOfWords) {
+    std::string vowels("aeyioąęuóAEYIOĄĘUÓ");
+
+    for (auto& element : vecOfWords) {
+        for (auto vowel : vowels) {
+            element.erase(std::remove(element.begin(), element.end(), vowel), element.end());
+        }
+    }
 }
