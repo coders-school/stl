58c2d21d0fabeda547faa306f742e2e7e9412847
jakjan95 <jakubjanusz95@gmail.com>
2020-06-09 07:34:33 +0200

    compression upgrade

--- module1/homework/grayscaleImages/compression.cpp
+++ module1/homework/grayscaleImages/compression.cpp
@@ -2,41 +2,45 @@
 
 #include <sstream>
 
-std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(
-    const std::array<std::array<uint8_t, width>, height>& bitmap) {
+std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(const std::array<std::array<uint8_t, width>, height>& bitmap) {
     std::vector<std::pair<uint8_t, uint8_t>> compressed_bitmap;
     for (size_t i = 0; i < height; ++i) {
-        int sequence_begin = 0;
+        uint8_t previous = bitmap[i][0];
+        uint8_t count = 1;
+
         for (size_t j = 1; j < width; ++j) {
-            if (bitmap[i][j] != bitmap[i][j - 1]) {
-                compressed_bitmap.push_back(
-                    std::make_pair(bitmap[i][j - 1], j - sequence_begin));
-                sequence_begin = j;
+            if (bitmap[i][j] == previous) {
+                count++;
+            }
+            if (bitmap[i][j] != previous || j == width - 1) {
+                compressed_bitmap.push_back(std::make_pair(previous, count));
+                previous = bitmap[i][j];
+                count = 1;
             }
         }
-        compressed_bitmap.push_back(
-            std::make_pair(bitmap[i][width - 1], width - sequence_begin));
     }
-
     return compressed_bitmap;
 }
 
 std::array<std::array<uint8_t, width>, height> decompressGrayscale(
     const std::vector<std::pair<uint8_t, uint8_t>>& compressed_bitmap) {
     std::array<std::array<uint8_t, width>, height> decompressed_bitmap;
-    size_t i = 0, j = 0;
-    for (const auto& el : compressed_bitmap) {
-        auto value = el.first;
-        auto quantity = el.second;
+    int i = 0, j = 0;
+    for (auto element : compressed_bitmap) {
+        auto value = element.first;
+        auto quantity = element.second;
+
         do {
             decompressed_bitmap[i][j++] = value;
             quantity--;
-        } while (quantity > 0);
-        if (j >= width) {
-            i++;
-            j = 0;
-        }
+            if (j == width) {
+                i++;
+                j = 0;
+            }
+
+        } while (quantity >= 1);
     }
+
     return decompressed_bitmap;
 }
 
