9c6c3ec7fe1187e9aeab9a2857040487cbb023fe
duszek <marcin.dus@gmail.com>
2020-06-09 23:32:19 +0200

    Refactor parametric compression tests

--- module1/homework/grayscaleImages/compression.hpp
+++ module1/homework/grayscaleImages/compression.hpp
@@ -4,8 +4,8 @@
 #include <utility>
 #include <vector>
 
-constexpr size_t width = 240;
-constexpr size_t height = 160;
+constexpr size_t width = 32;
+constexpr size_t height = 32;
 
 using CompressedBitmap = std::vector<std::pair<uint8_t, uint8_t>>;
 using Line =  std::array<uint8_t, width>;
