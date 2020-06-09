183e935ea3e52c58909b2c64af256b69caa85a65
KrzysztofJ85 <krzysztof.jucha@gmail.com>
2020-06-09 23:51:31 +0200

    printMap() prepared

--- module1/homework/grayscaleImages/compression.cpp
+++ module1/homework/grayscaleImages/compression.cpp
@@ -2,6 +2,8 @@
 
 #include <iostream>
 
+constexpr uint8_t ASCIIspace = 32;
+
 std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(const std::array<std::array<uint8_t, width>, height>& dataToCompress) {
     std::vector<std::pair<uint8_t, uint8_t>> vecCompressed;
 
@@ -15,4 +17,14 @@ std::array<std::array<uint8_t, width>, height> decompressGrayscale(const std::ve
 }
 
 void printMap(const std::array<std::array<uint8_t, height>, width>& mapToPrint) {
+    for (const auto& column : mapToPrint) {
+        for (const auto& rowValue : column) {
+            if (rowValue > ASCIIspace) {
+                std::cout << rowValue;
+            } else {
+                std::cout << ASCIIspace;
+            }
+        }
+        std::cout << '\n';
+    }
 }
