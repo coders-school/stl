8b25aae5003b9a14277667f7c59db2232acdf21b
Kuba W <winczukjakub@gmail.com>
2020-06-09 23:50:23 +0200

    removed const reference to char

--- module1/homework/removeVowels/vowel.cpp
+++ module1/homework/removeVowels/vowel.cpp
@@ -6,7 +6,7 @@ std::vector<std::string>& removeVowels(std::vector<std::string>& vec) {
     std::string vow("aeiouyAEIOUY");
 
     for(auto& string : vec) {
-        for(const auto& letter : vow) {
+        for(auto letter : vow) {
             string.erase(std::remove(string.begin(), string.end(), letter), string.end());
         }
     }
