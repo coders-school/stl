04d01e0ba85d0f90518100926c8e03652f2ac3ec
BreWal <waldemar.brejecki@gmail.com>
2020-06-10 19:32:16 +0200

    made removeVowels simpler

--- module1/homework/removeVowels/vowel.cpp
+++ module1/homework/removeVowels/vowel.cpp
@@ -1,17 +1,16 @@
 #include "vowel.hpp"
 
+#include <bits/stdc++.h>
+
 #include <algorithm>
 #include <array>
 
 void removeVowels(std::vector<std::string>& vec) {
-    constexpr std::array<char, 12> vowels{'a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y'};
+    std::regex vows("[aeiouyAEIOUY]");
 
     for (auto& str : vec) {
-        for (size_t i = 0; i < str.length(); i++) {
-            if (std::find(vowels.begin(), vowels.end(), str[i]) != vowels.end()) {
-                str.erase(i, 1);
-                i -= 1;
-            }
+        if (str.empty() == false) {
+            str = std::regex_replace(str, vows, "");
         }
     }
 }
