02ae6e538a8ec6fec3dfdc5e307948c1d08f372c
Mateusz Adamski <nauka.programowania.ma@gmail.com>
2020-06-10 17:49:05 +0200

    Add module3 for STL

---
+++ module3/homework/inner_product/test.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+
+#include "arithmeticAverage.hpp"
+#include "gtest/gtest.h"
+
+bool cmp(double first, double second, double epsilon = 0.5) {
+    return (fabs(first - second) < epsilon);
+}
+
+TEST(arithmeticAverageTest, ShouldCalculateArithmeticAverage) {
+    std::vector<int> first{-5, 4, 2, -5, 2, 4, 6, 7};
+    std::vector<int> second{3, 5, -2, 6, 7, -3, 6, 9};
+    EXPECT_TRUE(cmp(ArithmeticAverage(first, second), 2.875));
+    // Write more test case
+}
+
+TEST(arithmeticAverageTest, ShouldCalculateDistance) {
+    std::vector<int> first{7, 4, 3};
+    std::vector<int> second{17, 6, 2};
+    EXPECT_TRUE(cmp(Distance(first, second), 10.247));
+    // Write more test case
+}
