216f27d9f57c5637167b6c39d560860debc15cab
kozlofski <kozlofski.dev@gmail.com>
2020-06-11 14:32:50 +0200

    Changes after Code Review

--- module1/homework/removeVowels/vowel.cpp
+++ module1/homework/removeVowels/vowel.cpp
@@ -3,17 +3,18 @@
 #include <algorithm>
 #include <string>
 
-const std::string vowels = "aeiouyAEIOUY";
+std::string vowels = "AEIOUYaeiouy";
 
 void removeVowels(std::vector<std::string>& vectorOfStrings) {
     for (auto& str : vectorOfStrings) {
-        std::remove_if(str.begin(), str.end(), [](char c) {
-            for (auto it = vowels.begin(); it != vowels.end(); it++) {
-                if (*it == c) {
-                    return true;
-                }
-            }
-            return false;
-        });
+        str.erase(std::remove_if(str.begin(), str.end(), [](char c) {
+                      for (auto it = vowels.begin(); it != vowels.end(); it++) {
+                          if (*it == c) {
+                              return true;
+                          }
+                      }
+                      return false;
+                  }),
+                  str.end());
     }
 }
