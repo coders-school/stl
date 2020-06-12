f8bb439dc51dfb8621271e09013f8a67ecfba566
jakjan95 <jakubjanusz95@gmail.com>
2020-06-12 18:47:15 +0200

    removeDuplicateAndTransformToMap works

---
+++ module2/homework/transformContainers/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "transform.hpp"
+
+int main(){
+    return 0;
+}
---
+++ module2/homework/transformContainers/transform.cpp
@@ -0,0 +1,21 @@
+#include "transform.hpp"
+
+#include <algorithm>
+
+std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string> list, std::deque<int> deque) {
+    list.sort();
+    list.erase(std::unique(list.begin(), list.end()), list.end());
+
+    std::sort(deque.begin(), deque.end());
+    deque.erase(std::unique(deque.begin(), deque.end()), deque.end());
+
+    std::map<int, std::string> result;
+
+    std::transform(
+        deque.begin(), deque.end(), list.begin(), deque.begin(), [&result](auto first, auto second) {
+            result[first] = second;
+            return first;
+        });
+
+    return result;
+}
