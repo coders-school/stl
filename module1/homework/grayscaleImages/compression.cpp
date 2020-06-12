a0cfc2ca68ff347dc27153df9d4ae21cabd11770
mdus <marcin.dus@nokia.com>
2020-06-12 10:35:02 +0200

    Fix after review

--- module1/homework/grayscaleImages/compression.cpp
+++ module1/homework/grayscaleImages/compression.cpp
@@ -1,11 +1,11 @@
 #include "compression.hpp"
 #include <iostream>
 
-void printMap(Bitmap bitmap)
+void printMap(const Bitmap& bitmap)
 {
-    for (const auto& line : bitmap)
+    for (const auto line : bitmap)
     {
-        for (const auto& pixel : line)
+        for (const auto pixel : line)
         {
             std::cout << pixel;
         }
@@ -18,12 +18,11 @@ CompressedBitmap compressLine(const std::array<uint8_t, width>& line)
     CompressedBitmap result;
     std::pair<uint8_t, uint8_t> buffer{line[0], 0};
 
-    for (const auto& pixel : line)
+    for (const auto pixel : line)
     {
         if (buffer.first == pixel)
         {
             buffer.second++;
-            buffer.first = pixel;
         }
         else
         {
@@ -36,23 +35,26 @@ CompressedBitmap compressLine(const std::array<uint8_t, width>& line)
     return result;
 }
 
-CompressedBitmap compressGrayscale(Bitmap bitmap)
+CompressedBitmap compressGrayscale(const Bitmap& bitmap)
 {
     CompressedBitmap result;
+    result.reserve(height*width);
     CompressedBitmap tmp;
+    tmp.reserve(width);
 
-    for (const auto& line : bitmap)
+    for (const auto line : bitmap)
     {
         tmp = compressLine(line);
         result.insert(std::end(result), std::begin(tmp), std::end(tmp));
     }
+    result.shrink_to_fit();
     return result;
 }
 
-Bitmap decompressGrayscale(const CompressedBitmap compressedBitmap)
+Bitmap decompressGrayscale(const CompressedBitmap& compressedBitmap)
 {
     Bitmap result;
-    std::array<uint8_t, width>::iterator iter = std::begin(result[0]);
+    auto iter = std::begin(result[0]);
     for (const auto& group : compressedBitmap)
     {
         iter = std::fill_n(iter, group.second, group.first);
