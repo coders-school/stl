ca75797d674f7cda8bac1b051ac9c056001d72fd
MonikaZ <monika.zelechowska@o2.pl>
2020-06-13 00:26:29 +0200

    Add decompress function - longer version

--- module1/homework/grayscaleImages/compression.cpp
+++ module1/homework/grayscaleImages/compression.cpp
@@ -28,6 +28,30 @@ std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(const std::array<std:
     return outputVec;
 }
 
+std::array<std::array<uint8_t, width>, height> decompressGrayscale(const std::vector<std::pair<uint8_t, uint8_t>>& inputVec){
+    std::array<std::array<uint8_t, width>, height> outputArr;
+    auto vecIt = inputVec.begin();
+    size_t repeatingValue = 1;
+    
+    for(size_t rowCounter = 0; rowCounter < height; rowCounter++){
+        repeatingValue = 1;
+        for(size_t columnElement = 0; columnElement < width; columnElement++){
+            if(repeatingValue <= (vecIt->second)){
+                outputArr[rowCounter][columnElement] = (vecIt->first);
+                repeatingValue++;
+            }
+            else{
+                vecIt++;
+                outputArr[rowCounter][columnElement] = (vecIt->first);
+                repeatingValue = 2;
+            }
+        }
+        vecIt++;
+    }
+    return outputArr;
+}
+
+
 void printMap (const std::array<std::array<uint8_t, width>, height>& inputMap){
     for(const auto& mapRow : inputMap){
         for(const auto& rowElement : mapRow){
