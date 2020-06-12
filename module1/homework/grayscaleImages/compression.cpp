e9c1a3f053e0cdd0b196b5bc9cff9d4492094de7
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2020-06-12 13:18:48 +0200

    Review suggestions applied

--- module1/homework/grayscaleImages/compression.cpp
+++ module1/homework/grayscaleImages/compression.cpp
@@ -2,39 +2,39 @@
 
 #include <iostream>
 
-std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(const std::array<std::array<uint8_t, width>, height>& bitmap) {
-    std::vector<std::pair<uint8_t, uint8_t>> compressed;
+CompressedBitmap compressGrayscale(const Bitmap& bitmap) {
+    CompressedBitmap compressed;
     for (const auto& line : bitmap) {
         uint8_t count{};
-        uint8_t pixel = line.at(0);
-        for (const auto& el : line) {
+        uint8_t pixel = line.front();
+        for (auto el : line) {
             if (pixel == el) {
                 ++count;
             } else {
-                compressed.push_back(std::make_pair(pixel, count));
+                compressed.push_back({pixel, count});
                 count = 1;
                 pixel = el;
             }
         }
-        compressed.push_back(std::make_pair(pixel, count));
+        compressed.push_back({pixel, count});
     }
 
     return compressed;
 }
 
-std::array<std::array<uint8_t, width>, height> decompressGrayscale(const std::vector<std::pair<uint8_t, uint8_t>>& compressed) {
-    std::array<std::array<uint8_t, width>, height> bitmap;
+Bitmap decompressGrayscale(const CompressedBitmap& compressed) {
+    Bitmap bitmap;
 
     size_t count{}, line{}, el{};
+
     for (const auto& pair : compressed) {
         if (count >= width) {
             count = 0;
             el = 0;
             ++line;
         }
-        for (int i = 0; i < pair.second; ++i) {
-            bitmap[line][el] = pair.first;
-            ++el;
+        for (size_t i = 0; i < pair.second; ++i) {
+            bitmap[line][el++] = pair.first;
         }
         count += pair.second;
     }
@@ -42,10 +42,10 @@ std::array<std::array<uint8_t, width>, height> decompressGrayscale(const std::ve
     return bitmap;
 }
 
-void printMap(const std::array<std::array<uint8_t, width>, height>& bitmap) {
+void printMap(const Bitmap& bitmap) {
     for (const auto& line : bitmap) {
-        for (const auto& el : line) {
-            if(el <= ' ') {
+        for (auto el : line) {
+            if(el < ' ') {
                 std::cout << ' ';
             } else {
                 std::cout << el;
--- module1/homework/grayscaleImages/compression.hpp
+++ module1/homework/grayscaleImages/compression.hpp
@@ -6,8 +6,11 @@
 constexpr size_t width = 32;
 constexpr size_t height = 32;
 
-std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(const std::array<std::array<uint8_t, width>, height>& bitmap);
+using CompressedBitmap = std::vector<std::pair<uint8_t, uint8_t>>;
+using Bitmap = std::array<std::array<uint8_t, width>, height>;
 
-std::array<std::array<uint8_t, width>, height> decompressGrayscale(const std::vector<std::pair<uint8_t, uint8_t>>& compressed);
+CompressedBitmap compressGrayscale(const Bitmap& bitmap);
 
-void printMap(const std::array<std::array<uint8_t, width>, height>& bitmap);
+Bitmap decompressGrayscale(const CompressedBitmap& compressed);
+
+void printMap(const Bitmap& bitmap);
