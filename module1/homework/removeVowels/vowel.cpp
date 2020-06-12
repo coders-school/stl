8e75b9b4ba8d67de962304b0c0de36fda4da8208
Bartlomiej Gottfried <bargot@o2.pl>
2020-06-12 16:45:59 +0200

    Improved formatting

--- module1/homework/removeVowels/vowel.cpp
+++ module1/homework/removeVowels/vowel.cpp
@@ -1,11 +1,9 @@
 #include "vowel.hpp"
 #include <algorithm>
-#include <iostream>
 
 
 void removeVowels(std::vector<std::string>& someText)
 {
-    //const std::string vowels{"AEIOUYaeiouy"};
     const std::string vowels{"AEIOUY"};
 
     for (std::string& words : someText)
@@ -14,8 +12,8 @@ void removeVowels(std::vector<std::string>& someText)
             remove_if(words.begin(), words.end(), [vowels](char c)
             {
                 return vowels.end() != find(vowels.begin(), vowels.end(), std::toupper(c));
-            }), 
+            }),
             words.end()
-        );         
+        );
     }
 }
