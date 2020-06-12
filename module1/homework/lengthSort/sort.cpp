5f1e12d9aa20759fb865f1ba5485fbece519219d
damixiorek <damixiorek@gmail.com>
2020-06-12 21:02:34 +0200

    sort list works

--- module1/homework/lengthSort/sort.cpp
+++ module1/homework/lengthSort/sort.cpp
@@ -1,9 +1,19 @@
 #include "sort.hpp"
 
-std::deque<std::string> lengthSort(std::forward_list<std::string> fList) {
+#include<algorithm>
 
 
-return {};
+std::deque<std::string> lengthSort(std::forward_list<std::string> takedList) {
+
+takedList.sort();
+
+std::deque<std::string> deq (takedList.begin(), takedList.end());
+
+auto sortSize = [](std::string s1, std::string s2){return s1.size() < s2.size();};
+
+std::sort(deq.begin(),deq.end(), sortSize);
+
+return deq;
 
 }
 
