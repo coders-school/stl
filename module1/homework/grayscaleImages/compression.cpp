ba929229d7f462c11fbd6813875acc3ea7b9e595
damixiorek <damixiorek@gmail.com>
2020-06-13 23:23:47 +0200

    compression grey scale works!

--- module1/homework/grayscaleImages/compression.cpp
+++ module1/homework/grayscaleImages/compression.cpp
@@ -1,15 +1,26 @@
 #include "compression.hpp"
 
 #include <iostream>
+#include <algorithm>
 
 std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(std::array<std::array<uint8_t, width>, height> getGrey) {
 
 std::vector<std::pair<uint8_t, uint8_t>> smallMap;
-//smallMap.reserve(width);
 
+for (auto everyRow : getGrey) {
 
+    std::array<uint8_t, width>::iterator firstIt = everyRow.begin();
 
-return {};
+    while (firstIt != everyRow.end()) {
+
+        auto secondIt = std::find_if_not(firstIt,everyRow.end(), [firstIt](int x){return x == *firstIt;});
+        auto dist = std::distance(firstIt, secondIt);
+        smallMap.emplace_back(std::make_pair(*firstIt,dist));
+        firstIt = secondIt;
+    }
+}
+
+return smallMap;
 
 }
 
@@ -23,8 +34,9 @@ return {};
 void printMap (const std::array<std::array<uint8_t, width>, height>& getMap){
 
     for(const auto& everyRow : getMap){
+
         for(const auto& element : everyRow){
-            std::cout << element;
+            std::cout << (uint16_t)element;
         }
         std::cout << "\n";
     }
