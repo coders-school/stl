b6e1b7c96e82e643ed1ad503134274024986f8e3
Tomonike <pgurtowski1@gmail.com>
2020-06-13 20:44:23 +0200

    Working removeVowels

---
+++ module1/homework/removeVowels/vowel.cpp
@@ -0,0 +1,13 @@
+#include "vowel.hpp"
+
+#include <algorithm>
+
+void removeVowels(std::vector<std::string>& words) {
+    std::string vowels("aąeęiouyAĄEĘIOUY");
+
+    for (auto& element : words) {
+        for (auto vowel : vowels) {
+            element.erase(std::remove(element.begin(), element.end(), vowel), element.end());
+        }
+    }
+}
