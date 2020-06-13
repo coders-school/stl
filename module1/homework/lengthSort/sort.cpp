5208ecd023b147c18ec23708f744e92e399f71bb
NG90 <glowka.natalia@gmail.com>
2020-06-13 21:17:31 +0200

    formating

--- module1/homework/lengthSort/sort.cpp
+++ module1/homework/lengthSort/sort.cpp
@@ -5,12 +5,14 @@
 std::deque<std::string> lengthSort(const std::forward_list<std::string>& listToSort) {
     std::deque<std::string> output{begin(listToSort), end(listToSort)};
 
-    std::sort(begin(output), end(output), [](const std::string& s1, const std::string& s2) {
-        if (s1.length() == s2.length()) {
-            return s1 < s2;
-        }
-        return s1.length() < s2.length();
-    });
+    std::sort(begin(output),
+              end(output),
+              [](const std::string& s1, const std::string& s2) {
+                  if (s1.length() == s2.length()) {
+                      return s1 < s2;
+                  }
+                  return s1.length() < s2.length();
+              });
 
     return output;
 }
--- module1/homework/removeVowels/vowel.cpp
+++ module1/homework/removeVowels/vowel.cpp
@@ -3,15 +3,17 @@
 #include <algorithm>
 #include <array>
 
-namespace {
-constexpr std::array<char, 12> vowels{'a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y'};
-}
-
 void removeVowels(std::vector<std::string>& text) {
+    constexpr std::array<char, 12> vowels{'a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y'};
+
     for (auto& el : text) {
-        el.erase(std::remove_if(std::begin(el), std::end(el), [](unsigned char c) {
-                     return std::find(std::cbegin(vowels), std::cend(vowels), c) != std::cend(vowels);
-                 }),
+        el.erase(std::remove_if(std::begin(el),
+                                std::end(el),
+                                [&vowels](unsigned char c) {
+                                    return std::find(std::cbegin(vowels),
+                                                     std::cend(vowels),
+                                                     c) != std::cend(vowels);
+                                }),
                  std::end(el));
     }
 }
