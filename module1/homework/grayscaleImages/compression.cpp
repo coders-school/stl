475deb61cb2d25cf75d618100140bf35be6a77d4
m-kop <m.kopertowski@epicspace.eu>
2020-06-13 16:53:49 +0200

    add compressGrayscale() implementation

--- module1/homework/grayscaleImages/compression.cpp
+++ module1/homework/grayscaleImages/compression.cpp
@@ -1,8 +1,28 @@
 #include "compression.hpp"
 
 std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(const std::array<std::array<uint8_t, width>, height>& bitmap) {
-    // todo: implement
-    return {};
+    
+    std::vector<std::pair<uint8_t, uint8_t>> vec;
+    uint8_t color;
+    uint8_t count;
+
+    for (size_t i = 0; i < height; i++) {
+        for (size_t j = 0; j < width; j++) {
+            if (j == 0) {
+                color = bitmap[i][j];
+                count = 1;
+            } else if (color == bitmap[i][j]) {
+                count++;
+            } else {
+                vec.push_back(std::make_pair(color, count));
+                color = bitmap[i][j];
+                count = 1;
+            }
+        }
+        vec.push_back(std::make_pair(color, count));
+    }
+
+    return vec;
 }
 
 std::array<std::array<uint8_t, width>, height> decompressGrayscale(const std::vector<std::pair<uint8_t, uint8_t>>& bitmap) {
