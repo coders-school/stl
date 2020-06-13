f1b441b6b5272a70ab347154c6631577b90c03ba
m-kop <m.kopertowski@epicspace.eu>
2020-06-13 14:56:19 +0200

    add lentSort() implementation

--- module1/homework/lengthSort/sort.cpp
+++ module1/homework/lengthSort/sort.cpp
@@ -1,6 +1,32 @@
 #include "sort.hpp"
 
 std::deque<std::string> lengthSort(const std::forward_list<std::string>& list) {
-    // implement me
-    return {};
+
+    std::deque<std::string> deq{};
+    auto listIt = list.begin();
+
+    if (!list.empty()) {
+        deq.insert(deq.begin(), *listIt);
+        listIt++;
+    }
+
+    for (; listIt != list.end(); listIt++) {
+        auto deqIt = deq.begin();
+        for (; deqIt != deq.end(); deqIt++) {
+            if ((*listIt).length() < (*deqIt).length()) {
+                deq.insert(deqIt, *listIt);
+                break;
+            } else if ((*listIt).length() == (*deqIt).length()) {
+                if (*listIt < *deqIt) {
+                    deq.insert(deqIt, *listIt);
+                    break;
+                }
+            }
+        }
+        if (deqIt == deq.end()) {
+            deq.insert(deqIt, *listIt);
+        }
+    }
+
+    return deq;
 }
