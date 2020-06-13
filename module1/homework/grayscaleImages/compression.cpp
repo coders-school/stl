f99f1cb8cd53e38ccfe5b4cad4eb45ab093f55ca
m-kop <m.kopertowski@epicspace.eu>
2020-06-13 15:39:01 +0200

    add comrpess module - initial commit

---
+++ module1/homework/grayscaleImages/compression.cpp
@@ -0,0 +1,12 @@
+#include "compression.hpp"
+
+std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(const std::array<std::array<uint8_t, width>, height>& bitmap) {
+    // todo: implement
+    return {};
+}
+
+std::array<std::array<uint8_t, width>, height> decompressGrayscale(const std::vector<std::pair<uint8_t, uint8_t>>& bitmap) {
+    // todo: implement
+    return {};
+}
+
