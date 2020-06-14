db546c9cb03e64d20aa34bf058983f799faaa090
malinaq3 <malinaq3@gmail.com>
2020-06-14 21:16:51 +0200

    lengthSort workinkg

--- module1/homework/lengthSort/sort.cpp
+++ module1/homework/lengthSort/sort.cpp
@@ -5,12 +5,12 @@
 #include <string>
 
 std::deque<std::string> lengthSort(std::forward_list<std::string>& flist) {
-    flist.sort([](const std::string& a, const std::string& b) { return a.size() < b.size(); });
+    flist.sort([](const std::string& a, const std::string& b) { return a.size() == b.size() ? a < b : a.size() < b.size(); });
+
     std::deque<std::string> dq;
     for (auto el : flist) {
         dq.push_back(el);
     }
-    //std::sort(dq.begin(), dq.end(), [](const auto& a, const auto& b) { return a.size() == b.size(); });
 
     return dq;
 }
