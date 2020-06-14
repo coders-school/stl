2ba25c2f9d045b5bd15502d4635cde788ff4ce49
Marcinati <marcin.kapusta.pk@gmail.com>
2020-06-14 20:12:27 +0200

    one more space

--- module2/homework/transformContainers/uniquemap.cpp
+++ module2/homework/transformContainers/uniquemap.cpp
@@ -6,7 +6,6 @@
 
 std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string>& inputList,
                                                            std::deque<int>& inputDeque) {
-
     inputList.sort();
     inputList.erase(std::unique(inputList.begin(), inputList.end()), inputList.end());
 
@@ -14,13 +13,13 @@ std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string
     inputDeque.erase(std::unique(inputDeque.begin(), inputDeque.end()), inputDeque.end());
 
     std::map<int, std::string> outputMap;
-    std::transform(inputDeque.begin(), 
+    std::transform(inputDeque.begin(),
                    inputDeque.end(),
                    inputList.begin(),
                    std::inserter(outputMap, outputMap.end()),
                    [](const auto& deqElement, const auto& lisElement) {
                        return std::make_pair(deqElement, lisElement);
-                    });
+                   });
 
-return outputMap;
+    return outputMap;
 }
