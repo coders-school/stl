3b23389ede2623e55f2b2fbda0eb70518089724c
Karol Dulemba <kdulemba@gmail.com>
2020-06-15 00:19:30 +0200

    Final_solution for TransformContainersToMap

--- module2/homework/transformContainers/transformContainers.cpp
+++ module2/homework/transformContainers/transformContainers.cpp
@@ -4,7 +4,7 @@
 #include <algorithm>
 #include <map>
 #include <list>
-
+#include <utility>
 std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string> list, std::deque<int>deque){
 
 std::sort(deque.begin(), deque.end());
@@ -14,5 +14,7 @@ list.sort();
 list.erase(std::unique(list.begin(), list.end()), list.end());
 
 std::map<int, std::string> map;
+std::transform(deque.begin(), deque.end(), list.begin(),std::inserter(map, map.end()),[](int el1, std::string el2){return std::pair(el1, el2);});
+
 return map;
 }
