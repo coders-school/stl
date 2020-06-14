d1cd5ec8d9c10a4fcfd90c1d4037ee3923270df7
MonikaZ <monika.zelechowska@o2.pl>
2020-06-14 18:11:25 +0200

    Refactoring of removeVowel function

--- module1/homework/removeVowels/vowel.cpp
+++ module1/homework/removeVowels/vowel.cpp
@@ -1,11 +1,11 @@
 #include "vowel.hpp"
+
 #include <algorithm>
 
-
-void removeVowels(std::vector<std::string>& vec){
-    std::vector<char> vowelVector {'a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U', 'y', 'Y'};
-    for(auto& element : vec){
-        for(const auto& vowel : vowelVector){
+void removeVowels(std::vector<std::string>& vec) {
+    const std::vector<char> vowelVector{'a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U', 'y', 'Y'};
+    for (auto& element : vec) {
+        for (const auto& vowel : vowelVector) {
             element.erase(std::remove(element.begin(), element.end(), vowel), element.end());
         }
     }
