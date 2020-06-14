3f9f717bffa101ddc37c6d28c19abce973eea46f
Tomasz Eichler <tomasz.eichler@wp.pl>
2020-06-14 08:00:58 +0200

    Change deque initialization with copy constructor using forward_list begin, end iterators

--- module1/homework/lengthSort/sort.cpp
+++ module1/homework/lengthSort/sort.cpp
@@ -1,15 +1,12 @@
 #include "sort.hpp"
 
 std::deque<std::string> lengthSort(std::forward_list<std::string>& fList) {
-    std::deque<std::string> deq;
     
     fList.sort([](const std::string& lhs, const std::string& rhs) {
         return lhs.size() == rhs.size() ? lhs < rhs : lhs.size() < rhs.size();
     });
 
-    for (const auto& el : fList) {
-        deq.push_back(el);
-    }
+    std::deque<std::string> result(fList.begin(), fList.end());
 
-    return deq;
+    return result;
 }
