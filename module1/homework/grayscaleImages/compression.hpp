481888ef84ce69fad2ac74a38d81cc104cb622f5
MonikaZ <monika.zelechowska@o2.pl>
2020-06-14 17:20:29 +0200

    Add alisas to vector, and rowElement in loop as const

--- module1/homework/grayscaleImages/compression.hpp
+++ module1/homework/grayscaleImages/compression.hpp
@@ -4,7 +4,8 @@
 
 constexpr size_t width = 32;
 constexpr size_t height = 32;
+using pairVector = std::vector<std::pair<uint8_t, uint8_t>>;
 
-std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(const std::array<std::array<uint8_t, width>, height>&);
-std::array<std::array<uint8_t, width>, height> decompressGrayscale(const std::vector<std::pair<uint8_t, uint8_t>>&);
-void printMap (const std::array<std::array<uint8_t, width>, height>&);
+pairVector compressGrayscale(const std::array<std::array<uint8_t, width>, height>&);
+std::array<std::array<uint8_t, width>, height> decompressGrayscale(const pairVector&);
+void printMap(const std::array<std::array<uint8_t, width>, height>&);
