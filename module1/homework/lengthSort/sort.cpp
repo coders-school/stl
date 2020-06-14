62f4a36c3b1651c02184b795cc67a12238d2d24e
DarkAriesX <skrzys2103@gmail.com>
2020-06-14 22:24:31 +0200

    MY formating

--- module1/homework/lengthSort/sort.cpp
+++ module1/homework/lengthSort/sort.cpp
@@ -10,9 +10,8 @@ std::deque<std::string> lengthSort(const std::forward_list<std::string>& list){
     std::sort(queue.begin(), queue.end(), [](std::string it, std::string it2){
         if(it.size() == it2.size()){
             return it < it2;
-        } else{
-            return it.size() < it2.size();
         }
+        return it.size() < it2.size();
     });
     
     return queue;
