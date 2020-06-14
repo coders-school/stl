f191e493e6ff296c92abb4b964daf312d04f021a
Marcin N <19634288+niemarcin@users.noreply.github.com>
2020-06-14 21:34:18 +0200

    vector used and minor code formatting changes

--- module1/homework/removeVowels/vowel.hpp
+++ module1/homework/removeVowels/vowel.hpp
@@ -3,5 +3,5 @@
 #include <string>
 #include <vector>
 
-const char allVowels[] = {'a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y'};
-void removeVowels (std::vector<std::string>& word);
+const std::vector<char> allVowels = {'a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y'};
+void removeVowels(std::vector<std::string>& word);
