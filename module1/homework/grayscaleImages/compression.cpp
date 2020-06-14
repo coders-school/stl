02c1d609c2a85f4337b44ae6ebfef77f67413387
BreWal <waldemar.brejecki@gmail.com>
2020-06-14 21:34:19 +0200

    compression using algorithms

--- module1/homework/grayscaleImages/compression.cpp
+++ module1/homework/grayscaleImages/compression.cpp
@@ -1,43 +1,40 @@
 #include "compression.hpp"
 
+#include <algorithm>
 #include <iostream>
 
 std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(const std::array<std::array<uint8_t, width>, height>& arr) {
     std::vector<std::pair<uint8_t, uint8_t>> vec;
-    uint8_t counter;
+    vec.reserve(width * height);
     uint8_t buffer;
-    for (const auto& ln : arr) {
-        counter = 0;
-        buffer = ln.at(0);
-        for (const auto& el : ln) {
-            if (buffer == el) {
-                counter++;
-            } else {
-                vec.push_back(std::make_pair(buffer, counter));
-                counter = 1;
-                buffer = el;
-            }
+    std::for_each(arr.begin(), arr.end(), [&vec, &buffer](auto ln) {
+        buffer = ln.front();
+        auto it1 = ln.begin();
+        while (it1 != ln.end()) {
+            auto it2 = std::find_if(it1, ln.end(), [&buffer](const auto a) { return buffer != a; });
+            vec.push_back(std::make_pair(buffer, std::count(it1, it2, buffer)));
+            it1 = it2;
+            buffer = *it1;
         }
-        vec.push_back(std::make_pair(buffer, counter));
-    }
-
+    });
+    vec.shrink_to_fit();
     return vec;
 }
 
 std::array<std::array<uint8_t, width>, height> decompressGrayscale(const std::vector<std::pair<uint8_t, uint8_t>>& vec) {
     std::array<std::array<uint8_t, width>, height> arr;
-    uint8_t counter;
-    uint8_t line = 0;
-    for (const auto& el : vec) {
-        if (counter >= width) {
-            counter = 0;
-            ++line;
+
+    auto it_vec = vec.begin();
+
+    std::for_each(arr.begin(), arr.end(), [&](auto& ln) {
+        auto it_ln = ln.begin();
+
+        while (it_ln != ln.end()) {
+            std::fill(it_ln, it_ln + (*it_vec).second, (*it_vec).first);
+            it_ln += (*it_vec).second;
+            it_vec++;
         }
-        for (size_t i = counter; i < counter + el.second; i++) {
-            arr[line][i] = el.first;
-        }
-        counter += el.second;
-    }
+    });
 
     return arr;
 }
