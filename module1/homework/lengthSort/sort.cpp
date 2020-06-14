44e28ae84e25aafad8b25970b370f4be3eef9d93
kari0036 <57415454+kari0036@users.noreply.github.com>
2020-06-14 20:13:28 +0000

    Update sort.cpp

--- module1/homework/lengthSort/sort.cpp
+++ module1/homework/lengthSort/sort.cpp
@@ -3,17 +3,17 @@
 #include <algorithm>
 
 std::deque<std::string> lengthSort(const std::forward_list<std::string>& list){
-    std::deque<std::string> queue;
+    std::deque<std::string> deq;
     for(auto& el : list){
-        queue.push_back(el);
+        deq.push_back(el);
     }
-    std::sort(queue.begin(), queue.end(), [](std::string it, std::string it2){
-        if(it.size() == it2.size()){
-            return it < it2;
-        } else{
-            return it.size() < it2.size();
-        }
-    });
+    std::sort(deq.begin(), deq.end(), 
+              [](std::string it, std::string it2){
+                 if(it.size() == it2.size()){
+                    return it < it2;
+                 } 
+                 return it.size() < it2.size();
+              });
     
-    return queue;
+    return deq;
 }
