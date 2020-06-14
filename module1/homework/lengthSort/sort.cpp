0b3a1a638fd4063138bfd48e0d3f4b158d6f9094
ArturGubala <gubala-artur@wp.pl>
2020-06-14 21:22:12 +0200

    changed transform for sort

--- module1/homework/lengthSort/sort.cpp
+++ module1/homework/lengthSort/sort.cpp
@@ -3,9 +3,9 @@
 #include <algorithm>
 
 std::deque<std::string> lengthSort(const std::forward_list<std::string>& words) {
-    std::deque<std::string> sortedWords;
+    std::deque<std::string> sortedWords{begin(words), end(words)};
 
-    std::transform(begin(words), end(words), begin(sortedWords), [](const auto& first, const auto& second) {
+    std::sort(begin(sortedWords), end(sortedWords), [](const auto& first, const auto& second) {
         return first.length() == second.length() ? first < second : first.length() < second.length();
     });
 
