33f2326f0a55d25c959899acdbf8bd3bc09f0e18
majkel <michal.sobotka.84@gmail.com>
2020-06-14 22:37:28 +0200

    transformContainer with for loop and adittional container

--- module2/homework/transformContainers/transformContainers.cpp
+++ module2/homework/transformContainers/transformContainers.cpp
@@ -1,7 +1,24 @@
 #include "transformContainers.hpp"
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <utility>
 
-std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string>, std::deque<int>) {
+std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string> myList, std::deque<int> myDeque) {
     std::map<int, std::string> myMap;
+    std::sort(myDeque.begin(), myDeque.end());
+    auto dequeIt = std::unique(myDeque.begin(), myDeque.end());
+    myDeque.erase(dequeIt, myDeque.end());
+
+    myList.sort();
+    auto listIt = std::unique(myList.begin(), myList.end());
+    myList.erase(listIt, myList.end());
+
+    std::deque<std::string> myDequeOfString (myList.begin(), myList.end());
+
+    for (auto it = 0; it < myDeque.size(); it++) {
+        myMap.emplace(std::make_pair(myDeque.at(it), myDequeOfString.at(it)));
+    }
 
     return myMap;
 }
