c47a9261a31294573cd4f48e4f5218600b1d9444
DarkAriesX <skrzys2103@gmail.com>
2020-06-14 22:23:31 +0200

    Please don't format my code without asking

--- module1/homework/lengthSort/sort.cpp
+++ module1/homework/lengthSort/sort.cpp
@@ -3,17 +3,17 @@
 #include <algorithm>
 
 std::deque<std::string> lengthSort(const std::forward_list<std::string>& list){
-    std::deque<std::string> deq;
+    std::deque<std::string> queue;
     for(auto& el : list){
-        deq.push_back(el);
+        queue.push_back(el);
     }
-    std::sort(deq.begin(), deq.end(), 
-              [](std::string it, std::string it2){
-                 if(it.size() == it2.size()){
-                    return it < it2;
-                 } 
-                 return it.size() < it2.size();
-              });
+    std::sort(queue.begin(), queue.end(), [](std::string it, std::string it2){
+        if(it.size() == it2.size()){
+            return it < it2;
+        } else{
+            return it.size() < it2.size();
+        }
+    });
     
-    return deq;
+    return queue;
 }
