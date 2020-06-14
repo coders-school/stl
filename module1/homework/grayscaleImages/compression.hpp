5c7e681d95b5a0d43523a1421c5ea816bee05eaa
Vicx95 <wiktor.zyla31@gmail.com>
2020-06-14 15:18:28 +0200

    Suggested change for compressGrayScale function

--- module1/homework/grayscaleImages/compression.hpp
+++ module1/homework/grayscaleImages/compression.hpp
@@ -3,11 +3,14 @@
 #include <algorithm>
 #include <array>
 #include <vector>
+#include <iterator>
 
 constexpr size_t height = 32;
 constexpr size_t width = 32;
 
-std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(const std::array<std::array<uint8_t, width>, height>& compress);
+using Image = const std::array<std::array<uint8_t, width>, height>&;
+
+std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(Image compress);
 
 std::array<std::array<uint8_t, width>, height> decompressGrayscale(const std::vector<std::pair<uint8_t, uint8_t>>& compressedMap);
 
