b5a369d93069cd04fe1643edbf4fe2ccefcf2deb
LogarithmDerivative <mateusz.kowalski074@gmail.com>
2020-06-14 15:44:05 +0200

    changed to string

--- module1/homework/removeVowels/vowel.cpp
+++ module1/homework/removeVowels/vowel.cpp
@@ -1,18 +1,13 @@
 #include "vowel.hpp"
 
 #include <algorithm>
-#include <array>
 
-void removeVowels(std::vector<std::string>& words)
-{
-    constexpr size_t size = 12;
-    const std::array<char, size> vowels = { 'a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y' };
-    
-    for(auto& word : words)
-    {
-       for(const auto& letter : vowels)
-       {
-           word.erase(std::remove(word.begin(), word.end(), letter), word.end());
-       }
+void removeVowels(std::vector<std::string>& words) {
+    const std::string vowels = "aeiouyAEIOUY";
+
+    for (auto& word : words) {
+        for (const auto& letter : vowels) {
+            word.erase(std::remove(word.begin(), word.end(), letter), word.end());
+        }
     }
 }
