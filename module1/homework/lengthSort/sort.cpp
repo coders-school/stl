4a15dadc01bbdb67860ac38567e136c729bffd1c
MonikaZ <monika.zelechowska@o2.pl>
2020-06-14 18:32:21 +0200

    refactoring of lengthSort function

--- module1/homework/lengthSort/sort.cpp
+++ module1/homework/lengthSort/sort.cpp
@@ -1,23 +1,21 @@
-#include <algorithm>
-#include <forward_list>
-#include <string>
-
 #include "sort.hpp"
 
-std::deque<std::string> lengthSort(std::forward_list<std::string>& list){
+#include <algorithm>
+
+std::deque<std::string> lengthSort(std::forward_list<std::string>& list) {
     list.sort();
-    std::deque<std::string> deq{};
-    for(auto& listElement : list){
-        deq.push_back(listElement);
-    }
+    std::deque<std::string> deq{list.begin(), list.end()};
     std::string tempStr;
-    for(auto element : deq){
-        for(int i =0; i<(deq.size() -1); i++){
-            if(deq[i].size() > deq[i+1].size()){
-                tempStr = deq[i];
-                deq[i] = deq[i+1];
-                deq[i+1] = tempStr;
-            }
+    auto swap = [&tempStr](auto first, auto second) {
+        tempStr = first;
+        first = second;
+        second = tempStr;
+    };
+
+    for (auto element : deq) {
+        for (int i = 0; i < (deq.size() - 1); i++) {
+            if (deq[i].size() > deq[i + 1].size())
+                swap(deq[i], deq[i + 1]);
         }
     }
     return deq;
