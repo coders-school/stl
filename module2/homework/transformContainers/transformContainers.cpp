686f8f26823ae8932658d3593f75cafd376ea8c5
BreWal <waldemar.brejecki@gmail.com>
2020-06-14 13:24:57 +0200

    transform function implemented, header files included

---
+++ module2/homework/transformContainers/transformContainers.cpp
@@ -0,0 +1,15 @@
+#include "transformContainers.hpp"
+
+#include <algorithm>
+#include <map>
+
+void removeDuplicateAndTranformToMap(std::list<std::string> strList, std::deque<int> deq) {
+    std::sort(deq.begin(), deq.end());
+    strList.sort();
+
+    deq.erase(std::unique(deq.begin(), deq.end()), deq.end());
+    strList.erase(std::unique(strList.begin(), strList.end()), strList.end());
+
+    std::map<int, std::string> map;
+    std::transform(deq.begin(), deq.end(), strList.begin(), std::inserter(map, map.end()), [](const int& number, const std::string& str) { return std::make_pair(number, str); });
+}
---
+++ module2/homework/transformContainers/transformContainers.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <deque>
+#include <string>
+
+void removeDuplicateAndTranformToMap(std::list<std::string> strList, std::deque<int> deq);
