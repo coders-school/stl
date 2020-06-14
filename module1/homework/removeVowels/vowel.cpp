1e3b05323ac5047808f769c34a1370cdd93f65ad
lisie-sprawy <magda.jankowska6@tlen.pl>
2020-06-14 21:06:09 +0200

    fixed .hppand changing function's name

--- module1/homework/removeVowels/vowel.cpp
+++ module1/homework/removeVowels/vowel.cpp
@@ -1,15 +1,17 @@
 #include "vowel.hpp"
 #include <algorithm>
 
-void noVowelsInSentence (std::string& sentence){
-    for (char vowel : {'a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y'}){
+void removeVowelsFromString (std::string& sentence){
+    for (auto vowel : allVowels){
     sentence.erase(std::remove(sentence.begin(), sentence.end(), vowel), sentence.end());
     }
 }
 
 void removeVowels (std::vector<std::string>& word){
     for (auto& elem : word){
-        noVowelsInSentence(elem);
+        removeVowelsFromString(elem);
     }
 }
+
+
 
