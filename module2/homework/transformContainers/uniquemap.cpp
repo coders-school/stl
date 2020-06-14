2e8a411df14842e86278eecddcd7796e99340a2b
Marcinati <marcin.kapusta.pk@gmail.com>
2020-06-14 20:06:55 +0200

    better version of transform

--- module2/homework/transformContainers/uniquemap.cpp
+++ module2/homework/transformContainers/uniquemap.cpp
@@ -1,27 +1,26 @@
 #include "uniquemap.hpp"
 
 #include <algorithm>
+#include <iterator>
+#include <utility>
 
 std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string>& inputList,
                                                            std::deque<int>& inputDeque) {
 
-auto readyTotransform = [&](auto input){
-    std::sort(input.begin(), input.end());
-    input.erase(std::unique(input.begin(), input.end()), input.end());
-};
+    inputList.sort();
+    inputList.erase(std::unique(inputList.begin(), inputList.end()), inputList.end());
 
-readyTotransform(inputList);
-readyTotransform(inputDeque);
+    std::sort(inputDeque.begin(), inputDeque.end());
+    inputDeque.erase(std::unique(inputDeque.begin(), inputDeque.end()), inputDeque.end());
 
-
-//std::sort(inputList.begin(), inputList.end());
-//std::sort(inputDeque.begin(), inputDeque.end());
-
-//inputList.erase(std::unique(inputList.begin(), inputList.end()), inputList.end());
-//inputDeque.erase(std::unique(inputDeque.begin(), inputDeque.end()), inputDeque.end());
-
-std::map<int, std::string> outputMap;
-std::transform(inputDeque.begin(), inputDeque.end(), inputList.begin(), outputMap.begin());
+    std::map<int, std::string> outputMap;
+    std::transform(inputDeque.begin(), 
+                   inputDeque.end(),
+                   inputList.begin(),
+                   std::inserter(outputMap, outputMap.end()),
+                   [](const auto& deqElement, const auto& lisElement) {
+    return std::make_pair(deqElement, lisElement);
+    });
 
 return outputMap;
 }
