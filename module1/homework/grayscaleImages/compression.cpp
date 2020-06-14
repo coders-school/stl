5a3696dddae40a5a3ce539cea0358bf01f3b6529
Michal Muszynski <zgredek2318@gmail.com>
2020-06-14 23:36:39 +0200

    decompress added

--- module1/homework/grayscaleImages/compression.cpp
+++ module1/homework/grayscaleImages/compression.cpp
@@ -3,23 +3,22 @@
 #include <cstdint>
 
 std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(const std::array<std::array<uint8_t, width>, height>& bitmap) {
-   
     uint8_t counter = 1;
     std::vector<std::pair<uint8_t, uint8_t>> compressed_bitmap;
     for (size_t row = 0; row < height; ++row) {
         for (size_t col = 0; col < width - 1; ++col) {
             if (col == width - 2) {
-                if(bitmap[row][col] == bitmap[row][col + 1]) {
-                compressed_bitmap.push_back(std::pair<uint8_t, uint8_t>(bitmap[row][col], counter + 1));
-                counter = 1;
+                if (bitmap[row][col] == bitmap[row][col + 1]) {
+                    compressed_bitmap.push_back(std::pair<uint8_t, uint8_t>(bitmap[row][col], counter + 1));
+                    counter = 1;
                 } else {
                     compressed_bitmap.push_back(std::pair<uint8_t, uint8_t>(bitmap[row][col], counter));
                     counter = 1;
                     compressed_bitmap.push_back(std::pair<uint8_t, uint8_t>(bitmap[row][col + 1], counter));
                 }
-                continue; 
+                continue;
             }
-            if(bitmap[row][col] == bitmap[row][col + 1]) {
+            if (bitmap[row][col] == bitmap[row][col + 1]) {
                 counter++;
             } else {
                 compressed_bitmap.push_back(std::pair<uint8_t, uint8_t>(bitmap[row][col], counter));
@@ -30,4 +29,21 @@ std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(const std::array<std:
     return compressed_bitmap;
 }
 
-std::array<std::array<uint8_t, width>, height> decompressGrayscale(std::vector<std::pair<uint8_t, uint8_t>>&) {};
+std::array<std::array<uint8_t, width>, height> decompressGrayscale(std::vector<std::pair<uint8_t, uint8_t>>& comp_b) {
+    std::array<std::array<uint8_t, width>, height> decompressed_bitmap;
+    int shade_counter = 0;
+    int row = 0;
+
+    for (auto el : comp_b) {
+        for (size_t i = shade_counter; i < shade_counter + el.second; ++i) {
+            decompressed_bitmap[row][i] = el.first;
+        }
+        shade_counter += el.second;
+        if (shade_counter == width) {
+            shade_counter = 0;
+            ++row;
+        }
+    }
+
+    return decompressed_bitmap;
+};
