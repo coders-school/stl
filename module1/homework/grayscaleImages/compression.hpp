9e107314c105e42a52b6b71ae8b5f3f8dc6e6dd9
MonikaZ <monika.zelechowska@o2.pl>
2020-06-15 09:55:05 +0200

    compression/decompresion function refactoring

--- module1/homework/grayscaleImages/compression.hpp
+++ module1/homework/grayscaleImages/compression.hpp
@@ -2,9 +2,11 @@
 #include <array>
 #include <vector>
 
+using pairVector = std::vector<std::pair<uint8_t, uint8_t>>;
+
 constexpr size_t width = 32;
 constexpr size_t height = 32;
 
-std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(const std::array<std::array<uint8_t, width>, height>&);
-std::array<std::array<uint8_t, width>, height> decompressGrayscale(const std::vector<std::pair<uint8_t, uint8_t>>&);
-void printMap (const std::array<std::array<uint8_t, width>, height>&);
+pairVector compressGrayscale(const std::array<std::array<uint8_t, width>, height>&);
+std::array<std::array<uint8_t, width>, height> decompressGrayscale(const pairVector&);
+void printMap(const std::array<std::array<uint8_t, width>, height>&);
