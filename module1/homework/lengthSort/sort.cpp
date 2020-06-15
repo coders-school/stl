70885105684c7ec1ec6e61c534e979202a2ca5c4
kali-03 <mkaliszczuk03@wp.pl>
2020-06-15 19:12:27 +0200

    Improved lengthSort

--- module1/homework/lengthSort/sort.cpp
+++ module1/homework/lengthSort/sort.cpp
@@ -3,10 +3,9 @@
 #include <functional>
 
 std::deque<std::string> lengthSort(std::forward_list<std::string> mail) {
-    std::deque<std::string> vec;
     mail.sort(std::greater<std::string>());
-    for (const auto& el : mail) {
-        vec.push_front(el);
-    }
+    mail.reverse();
+    std::deque<std::string> vec{mail.begin(), mail.end()};
+
     return vec;
 }
