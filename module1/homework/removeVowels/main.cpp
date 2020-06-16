6996b1087ef3a7546dc3f1d2c3db6863dc97605f
Marcinati <marcin.kapusta.pk@gmail.com>
2020-06-16 20:50:25 +0200

    restore originals

---
+++ module1/homework/removeVowels/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include "vowel.hpp"
+
+template <typename T>
+void printVector(const std::vector<T>& collection) {
+    for (const auto& element : collection) {
+        std::cout << element << '\n';
+    }
+    std::cout << '\n';
+}
+
+int main() {
+    std::vector<std::string> words = {
+        "Somebody once told me the world is gonna roll me",
+        "I ain't the sharpest tool in the shed",
+        "She was looking kind of dumb with her finger and her thumb",
+        "In the shape of an L on her forehead"
+    };
+    printVector(words);
+    removeVowels(words);
+    printVector(words);
+
+    return 0;
+}
