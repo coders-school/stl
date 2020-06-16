d5839106e6025fe0c9121ce6cd72f1150937d0e1
Marcinati <marcin.kapusta.pk@gmail.com>
2020-06-16 20:46:51 +0200

    old tasks removed

--- module1/homework/lengthSort/sort.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <deque>
-#include <forward_list>
-#include <string>
-
-bool compare(const std::string&, const std::string&);
-std::deque<std::string> lengthSort(std::forward_list<std::string>);
