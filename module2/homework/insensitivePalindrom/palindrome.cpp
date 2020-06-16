7729f88ed08e7d878a6f2d39fa2874c476e99cf0
ArturGubala <gubala-artur@wp.pl>
2020-06-16 11:47:49 +0200

    implementation for task 2 - insensitive palindrome

--- module2/homework/insensitivePalindrom/palindrome.cpp
+++ module2/homework/insensitivePalindrom/palindrome.cpp
@@ -1,7 +1,20 @@
 #include "palindrome.hpp"
 
 #include <algorithm>
+#include <cctype>
 
-bool is_palindrome(std::string str) {
-    return true;
+std::string deleteSpecialChar(std::string& str) {
+    str.erase(std::remove_if(begin(str), end(str), [](const auto& character) { return !(std::isalpha(character)); }), end(str));
+    return str;
+}
+
+std::string doLowerCase(std::string& str) {
+    std::transform(begin(str), end(str), begin(str), [](auto& letter) { return std::tolower(letter); });
+    return str;
+}
+
+bool is_palindrome(std::string& str) {
+    std::string strToCheck = deleteSpecialChar(str);
+    doLowerCase(strToCheck);
+    return std::equal(begin(strToCheck), begin(strToCheck) + strToCheck.size() / 2, rbegin(strToCheck));
 }
