6e8ef1758c2cf13b0d8a0e246bfb4ef941764b64
lisie-sprawy <magda.jankowska6@tlen.pl>
2020-06-17 00:26:59 +0200

    implementation for task 3 - transformContainers

---
+++ module2/homework/transformContainers/transformContainers.cpp
@@ -0,0 +1,14 @@
+#include "transformContainers.hpp"
+#include <algorithm>
+
+std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string> listWords, std::deque<int> dequeNumbers){
+    listWords.sort();
+    std::sort(dequeNumbers.begin(), dequeNumbers.end());
+    listWords.erase(std::unique(listWords.begin(), listWords.end()), listWords.end());
+    dequeNumbers.erase(std::unique(dequeNumbers.begin(), dequeNumbers.end()), dequeNumbers.end());
+
+std::map<int, std::string> map;
+std::transform(listWords.begin(), listWords.end(), dequeNumbers.begin(), std::inserter(map, map.end()), [](const auto& str, const int num) {return std::make_pair(num, str);});
+
+return map;
+}
