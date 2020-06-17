0a9bd102c778060c750abbea6600211c3c918ab4
Miroslaw Kopertowski <miroslaw.kopertowski@tieto.com>
2020-06-17 11:02:31 +0200

    emplace_back called once

--- module1/homework/grayscaleImages/compression.cpp
+++ module1/homework/grayscaleImages/compression.cpp
@@ -11,16 +11,18 @@ CompressedBitmap compressGrayscale(const Bitmap& bitmap) {
     for (const auto& row : bitmap) {
         color = row.front();
         count = 1;
-        for (auto pixelIt = std::next(row.begin()); pixelIt != row.end(); pixelIt++) {
-            if (color != *pixelIt) {
+        for (auto pixelIt = row.begin(); pixelIt != row.end(); pixelIt++) {
+            auto nextIt = std::next(pixelIt);
+            if ((nextIt == row.end()) or (color != *nextIt)) {
                 compressed.emplace_back(std::make_pair(color, count));
-                color = *pixelIt;
-                count = 1;
+                if (nextIt != row.end()) {
+                    color = *nextIt;
+                    count = 1;
+                }
             } else {
                 count++;
             }
         }
-        compressed.emplace_back(std::make_pair(color, count));
     }
     compressed.shrink_to_fit();
     return compressed;
