266afb8046d0092f9c2bd528d5dba8d6d758c0ba
jakjan95 <jakubjanusz95@gmail.com>
2020-06-17 19:29:18 +0200

    decompressGrayscale works

--- module2/homework/grayscaleImages/compression.cpp
+++ module2/homework/grayscaleImages/compression.cpp
@@ -9,7 +9,6 @@ std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(
     std::vector<std::pair<uint8_t, uint8_t>> compressed_bitmap;
     for (const auto& element : bitmap) {
         for (auto it = element.begin(); it != element.end();) {
-            
             auto itDifferent = std::find_if_not(it, element.end(), [it](auto recent) {
                 return *it == recent;
             });
@@ -24,20 +23,10 @@ std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(
 std::array<std::array<uint8_t, width>, height> decompressGrayscale(
     const std::vector<std::pair<uint8_t, uint8_t>>& compressed_bitmap) {
     std::array<std::array<uint8_t, width>, height> decompressed_bitmap;
-    int i = 0, j = 0;
-    for (auto element : compressed_bitmap) {
-        auto value = element.first;
-        auto quantity = element.second;
-
-        do {
-            decompressed_bitmap[i][j++] = value;
-            quantity--;
-            if (j == width) {
-                i++;
-                j = 0;
-            }
-        } while (quantity >= 1);
-    }
+    auto it = decompressed_bitmap.front().begin();
+    std::for_each(compressed_bitmap.begin(), compressed_bitmap.end(), [&it](auto pair) {
+        it = std::fill_n(it, pair.second, pair.first);
+    });
 
     return decompressed_bitmap;
 }
