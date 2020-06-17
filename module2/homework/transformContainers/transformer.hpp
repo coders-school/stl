6db8d9009b2cd963f34b98898e600a4335900a22
duszek <marcin.dus@gmail.com>
2020-06-17 23:34:04 +0200

    transformContainer compilation fixed

---
+++ module2/homework/transformContainers/transformer.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <list>
+#include <deque>
+#include <string>
+#include <map>
+
+
+std::map<int, std::string> removeDuplicateAndTranformToMap(const std::list<std::string>& list, const std::deque<int>& deque)
+{
+    std::map<int, std::string> p{{1, "ddd"}};
+
+return p; 
+
+}
