a6245dfe5d221e03ef9e956c7a3c46f639e35ed3
duszek <marcin.dus@gmail.com>
2020-06-18 00:36:38 +0200

    transforme ideas

--- module2/homework/transformContainers/transformer.hpp
+++ module2/homework/transformContainers/transformer.hpp
@@ -3,12 +3,26 @@
 #include <deque>
 #include <string>
 #include <map>
+#include <algorithm>
 
-
-std::map<int, std::string> removeDuplicateAndTranformToMap(const std::list<std::string>& list, const std::deque<int>& deque)
+std::map<int,std::string> zip(std::list<std::string> values, std::deque<int> keys)
 {
-    std::map<int, std::string> p{{1, "ddd"}};
+    std::map<int,std::string> result; 
+    auto keysIter = std::begin(keys);
+    auto valuesIter = std::begin(values);
 
-return p; 
+     //for ( ; keysIter != std::end(keys), valuesIter != std::end(values); keysIter++, valuesIter++ )
+     //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!danger!
+     for ( ; keysIter != std::end(keys); keysIter++, valuesIter++ )
+     {
+        result.insert(std::make_pair(*keysIter, *valuesIter));
+     }
+    return result; 
+}
 
+std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string>& list, std::deque<int>& deque)
+{
+    std::unique(std::begin(deque), std::end(deque));
+    std::unique(std::begin(list), std::end(list));
+    return zip(list, deque) ; 
 }
