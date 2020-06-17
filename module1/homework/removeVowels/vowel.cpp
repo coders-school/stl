78414109a99e517dc819d7812609f6cb0f288dda
kari0036 <57415454+kari0036@users.noreply.github.com>
2020-06-17 10:06:37 +0000

    Update vowel.cpp

--- module1/homework/removeVowels/vowel.cpp
+++ module1/homework/removeVowels/vowel.cpp
@@ -3,15 +3,14 @@
 #include <algorithm>
 #include <array>
 
-const std::array<char, 12> vowels {'a','A','e','E','i','I','o','O','u','U','y','Y'};
+constexpr std::array<char, 12> vowels {'a','A','e','E','i','I','o','O','u','U','y','Y'};
 
 void removeVowels(std::vector<std::string>& vec){
    for (auto& it : vec) {
-      it.erase(std::remove_if(it.begin(),
-                              it.end(),
-                              [](auto c) {
-                                 return std::any_of(vowels.begin(), vowels.end(), [c](auto vow) {
-                                    return vow == c;
+      it.erase(std::remove_if(it.begin(),it.end(),
+                                 [](auto c) {
+                                    return std::any_of(vowels.begin(), vowels.end(), [c](auto vow) {
+                                       return vow == c;
                                  });
                               }),
                it.end());
