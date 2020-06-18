1b7929901ac3e982bc8d74cd6a7ac83268d08139
malinaq3 <62904542+malinaq3@users.noreply.github.com>
2020-06-18 11:18:39 +0200

    references added

--- module2/homework/transformContainers/transformContainers.hpp
+++ module2/homework/transformContainers/transformContainers.hpp
@@ -5,5 +5,5 @@
 #include <map>
 
 std::map<int, std::string> removeDuplicateAndTranformToMap(
-    std::list<std::string> str,
-    std::deque<int> numbers);
+    std::list<std::string>& str,
+    std::deque<int>& numbers);
