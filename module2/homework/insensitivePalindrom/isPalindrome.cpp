9089c58d2025f9877c2d5e3a98d16aa299bb9652
kozlofski <kozlofski.dev@gmail.com>
2020-06-18 14:54:26 +0200

    isPalindrome code formatting

--- module2/homework/insensitivePalindrom/isPalindrome.cpp
+++ module2/homework/insensitivePalindrom/isPalindrome.cpp
@@ -4,15 +4,22 @@
 #include <cctype>
 
 bool is_palindrome(std::string testedString) {
-    testedString.erase(std::remove_if(testedString.begin(), testedString.end(), [](unsigned char c) {
-        return !std::isalpha(c);
-    }));
+    testedString.erase(std::remove_if(testedString.begin(),
+                                      testedString.end(),
+                                      [](unsigned char c) {
+                                          return !std::isalpha(c);
+                                      }));
 
-    std::transform(testedString.begin(), testedString.end(), testedString.begin(), [](unsigned char c) {
-        return std::tolower(c);
-    });
+    std::transform(testedString.begin(),
+                   testedString.end(),
+                   testedString.begin(),
+                   [](unsigned char c) {
+                       return std::tolower(c);
+                   });
 
-    return equal(testedString.begin(), testedString.begin() + testedString.size() / 2, testedString.rbegin());
+    return equal(testedString.begin(),
+                 testedString.begin() + testedString.size() / 2,
+                 testedString.rbegin());
 }
 
 int main() {
