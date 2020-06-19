49008f7f1769e62fbf2e4cb6bf9219ff6e410768
NG90 <glowka.natalia@gmail.com>
2020-06-20 00:30:16 +0200

    compressGrayscale - 1st attempt

--- module1/homework/grayscaleImages/compression.cpp
+++ module1/homework/grayscaleImages/compression.cpp
@@ -8,14 +8,17 @@ std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(
     const std::array<std::array<uint8_t, width>, height>& bitmap) {
     std::vector<std::pair<uint8_t, uint8_t>> compressedBitmap{};
 
-    for (const auto& el : bitmap) {
-        compressedBitmap.push_back({*begin(el), 1});
-        for (auto iter = std::next(begin(el)); iter != end(el); ++iter) {
-            if (*iter == compressedBitmap.back().first) {
-                compressedBitmap.back().second++;
-            } else {
-                compressedBitmap.push_back({*iter, 1});
-            }
+    for (const auto& row : bitmap) {
+        auto iterStart = begin(row);
+        auto iterFound = begin(row);
+        while (iterFound != end(row)) {
+            iterFound = std::find_if(iterStart,
+                                     end(row),
+                                     [&iterStart](const auto el) {
+                                         return el != *iterStart;
+                                     });
+            compressedBitmap.push_back({*iterStart, std::distance(iterStart, iterFound)});
+            iterStart = iterFound;
         }
     }
 
@@ -29,8 +32,8 @@ std::array<std::array<uint8_t, width>, height> decompressGrayscale(
     auto iterRow = begin(decompressedBitmap);
     auto iterEl = begin(*iterRow);
 
-    std::for_each(begin(compressedBitmap),
-                  end(compressedBitmap),
+    std::for_each(cbegin(compressedBitmap),
+                  cend(compressedBitmap),
                   [&iterRow, &iterEl](const auto& el) {
                       iterEl = std::fill_n(iterEl, el.second, el.first);
                       if (iterEl == end(*iterRow)) {
