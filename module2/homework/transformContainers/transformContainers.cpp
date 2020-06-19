8cf14854392d0bc2cb33eaf0a5d966c61da753a5
lisie-sprawy <magda.jankowska6@tlen.pl>
2020-06-19 22:57:48 +0200

    adding code identation, divide long line into 2 shorter ones

--- module2/homework/transformContainers/transformContainers.cpp
+++ module2/homework/transformContainers/transformContainers.cpp
@@ -7,8 +7,12 @@ std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string
     listWords.erase(std::unique(listWords.begin(), listWords.end()), listWords.end());
     dequeNumbers.erase(std::unique(dequeNumbers.begin(), dequeNumbers.end()), dequeNumbers.end());
 
-std::map<int, std::string> map;
-std::transform(listWords.begin(), listWords.end(), dequeNumbers.begin(), std::inserter(map, map.end()), [](const auto& str, const int num) {return std::make_pair(num, str);});
+    std::map<int, std::string> map;
+    std::transform(listWords.begin(), listWords.end(), dequeNumbers.begin(), 
+                    std::inserter(map, map.end()), 
+                    [](const auto& str, const int num) {
+                        return std::make_pair(num, str);
+                    });
 
-return map;
+    return map;
 }
