7e7d12b88a72dd459d364b3c6c76d5b2ba5266ff
Kasperice <kasper@alert.pl>
2020-06-19 11:22:46 +0200

    Sort changed

--- module1/homework/lengthSort/sort.cpp
+++ module1/homework/lengthSort/sort.cpp
@@ -3,11 +3,13 @@
 #include <algorithm>
 
 bool compare(const std::string& first_string, const std::string& second_string) {
+    if (first_string.size() == second_string.size()) {
+        return first_string < second_string;
+    }
     return first_string.size() < second_string.size();
 }
 
 std::deque<std::string> lengthSort(std::forward_list<std::string> input_list) {
-    input_list.sort();
     std::deque<std::string> result_deque{input_list.begin(), input_list.end()};
     std::sort(result_deque.begin(), result_deque.end(), compare);
     return result_deque;
