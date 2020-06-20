7b5bc64154b7a005222fa4052eeb2e482346b487
Mateusz Jaworski <mathew.jaworski@gmail.com>
2020-06-20 14:28:31 +0200

    sort update

--- module1/homework/lengthSort/sort.cpp
+++ module1/homework/lengthSort/sort.cpp
@@ -3,7 +3,13 @@
 #include <algorithm>
 
 std::deque<std::string> lengthSort(std::forward_list<std::string> wordsList) {
-    wordsList.sort(std::less<std::string>());
+    wordsList.sort([](std::string a, std::string b) {
+        if (a.size() == b.size()) {
+            return std::lexicographical_compare(a.begin(), a.end(), b.begin(), b.end());
+        } else {
+            return a.size() < b.size();
+        }
+    });
     std::deque<std::string> sortedWords(wordsList.begin(), wordsList.end());
     return sortedWords;
 }
