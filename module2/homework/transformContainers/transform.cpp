29018fcd71eae686137b25f0c82d45ea1396762e
kyrtaq <kyrtaq13@gmail.com>
2020-06-20 13:40:15 +0200

    added const ref

--- module2/homework/transformContainers/transform.cpp
+++ module2/homework/transformContainers/transform.cpp
@@ -4,14 +4,10 @@
 #include "tranform.hpp"
 
 std::map<int, std::string> removeDuplicateAndTranformToMap(
-    std::list<std::string> uniqueList,
-    std::deque<int> uniqueDeque) {
+    std::list<std::string>& uniqueList,
+    std::deque<int>& uniqueDeque) {
     uniqueList.sort();
     uniqueList.erase(std::unique(uniqueList.begin(), uniqueList.end()), uniqueList.end());
-
-    for (const auto el : uniqueDeque) {
-        std::cout << el << ' ';
-    }
 
     std::sort(uniqueDeque.begin(), uniqueDeque.end());
     uniqueDeque.erase(std::unique(uniqueDeque.begin(), uniqueDeque.end()), uniqueDeque.end());
