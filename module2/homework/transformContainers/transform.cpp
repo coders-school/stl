e8fd167f2a6b2ac5eb5202fe50741b2989cdb7bc
jakjan95 <jakubjanusz95@gmail.com>
2020-06-20 14:32:52 +0200

    little cleaning

--- module2/homework/transformContainers/transform.cpp
+++ module2/homework/transformContainers/transform.cpp
@@ -5,17 +5,14 @@
 std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string> list, std::deque<int> deque) {
     list.sort();
     list.erase(std::unique(list.begin(), list.end()), list.end());
-
     std::sort(deque.begin(), deque.end());
     deque.erase(std::unique(deque.begin(), deque.end()), deque.end());
 
-    
-
     std::map<int, std::string> result;
-    if(deque.size() != list.size()){
+    if (deque.size() != list.size()) {
         return result;
     }
-    
+
     std::transform(
         deque.begin(), deque.end(), list.begin(), deque.begin(), [&result](auto first, auto second) {
             result[first] = second;
