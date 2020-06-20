638c25df8c02d94aa50af3741185e1cc463a1307
jakjan95 <jakubjanusz95@gmail.com>
2020-06-20 11:37:26 +0200

    removing previous exercise

--- module1/homework/lengthSort/sort.hpp
+++
@@ -1,6 +0,0 @@
-#include <deque>
-#include <forward_list>
-#include <string>
-
-bool compare(const std::string& s1, const std::string& s2);
-std::deque<std::string> lengthSort(const std::forward_list<std::string>& lst);
--- module1/homework/removeVowels/vowel.hpp
+++
@@ -1,4 +0,0 @@
-#include <string>
-#include <vector>
-
-void removeVowels(std::vector<std::string>& vec);
