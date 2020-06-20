fad60039733971a0a5f24b94c364cfe0fabae6cd
m-kop <m.kopertowski@epicspace.eu>
2020-06-21 00:59:17 +0200

    add is_palindrom() implementation

--- module2/homework/insensitivePalindrom/palindrom.cpp
+++ module2/homework/insensitivePalindrom/palindrom.cpp
@@ -1,6 +1,18 @@
 #include "palindrom.hpp"
 
-bool is_palindrome(const std::string&) {
-    // ToDo: implement
-    return false;
+#include <algorithm>
+#include <cctype>
+
+bool is_palindrome(const std::string& sentence) {
+    std::string toCheck = sentence;
+
+    toCheck.erase(std::remove_if(toCheck.begin(), toCheck.end(), [](auto character) {
+        return !isalnum(character);
+    }), toCheck.end());
+
+    std::transform(toCheck.begin(), toCheck.end(), toCheck.begin(), [](auto character) {
+        return tolower(character);
+    });
+
+    return std::equal(toCheck.begin(), toCheck.begin() + toCheck.size()/2, toCheck.rbegin());       
 }
