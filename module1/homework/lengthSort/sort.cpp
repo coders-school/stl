4a546ded342414966e8f671be7ac0002db61c81c
m-kop <m.kopertowski@epicspace.eu>
2020-06-20 21:10:03 +0200

    fix for equal length sorting

--- module1/homework/lengthSort/sort.cpp
+++ module1/homework/lengthSort/sort.cpp
@@ -1,15 +1,17 @@
 #include "sort.hpp"
 
 #include <algorithm>
+#include <iterator>
 
 bool comparator(const std::string& firstString, const std::string& secondString) {
-    return firstString.length() < secondString.length();
+    return (firstString.size() == secondString.size() ? firstString < secondString
+                                                      : (firstString.size() < secondString.size()));
 }
 
 std::deque<std::string> lengthSort(std::forward_list<std::string> list) {
-    list.sort();
-    std::deque<std::string> deq{list.begin(), list.end()};
-    std::sort(deq.begin(), deq.end(), comparator);
+    list.sort(comparator);
+    std::deque<std::string> deq;
+    std::copy(list.begin(), list.end(), std::back_inserter(deq));
 
     return deq;
 }
