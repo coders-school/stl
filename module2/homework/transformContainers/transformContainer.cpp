3b9a2a7231693e0e3e0dd6379edc926384f18738
m-kop <m.kopertowski@epicspace.eu>
2020-06-20 22:30:21 +0200

    add removeDuplicateAndTranformToMap() declaration

---
+++ module2/homework/transformContainers/transformContainer.cpp
@@ -0,0 +1,7 @@
+#include "transformContainer.hpp"
+
+std::map<int, std::string> removeDuplicateAndTranformToMap(const std::list<std::string>&,
+                                                           const std::deque<int> &) {
+
+    return {};   
+}
