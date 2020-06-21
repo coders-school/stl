6c9df47de5d96bd2765f7f661514a421f616dc98
LogarithmDerivative <mateusz.kowalski074@gmail.com>
2020-06-21 13:28:12 +0200

    added starting files

---
+++ module2/homework/transformContainers/transform.cpp
@@ -0,0 +1,10 @@
+#include "transform.hpp"
+
+#include <algorithm>
+
+std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string>& list, std::deque<int>& dek)
+{
+    std::map<int, std::string> result;
+
+    return {};
+}
---
+++ module2/homework/transformContainers/transform.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <deque>
+#include <map>
+#include <list>
+#include <string>
+
+std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string>&, std::deque<int>&);
+
