4453956bf61972720396cd51918e9ac08b7454d8
Marcin N <19634288+niemarcin@users.noreply.github.com>
2020-06-21 13:45:26 +0200

    removed unnecessary files from homework1

--- module1/homework/lengthSort/sort.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <deque>
-#include <forward_list>
-#include <string>
-
-std::deque<std::string> lengthSort(const std::forward_list<std::string>& words);
--- module1/homework/removeVowels/vowel.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <string>
-#include <vector>
-
-const std::vector<char> allVowels = {'a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y'};
-void removeVowels(std::vector<std::string>& word);
