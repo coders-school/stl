248a2b3b69f1c06412b13b158110dd68249dd54f
kali-03 <mkaliszczuk03@wp.pl>
2020-06-21 22:14:43 +0200

    transformContainers

---
+++ module2/homework/transformContainers/transformContainers.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include <deque>
+#include <list>
+#include <map>
+#include <string>
+
+std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string> word1, std::deque<int> num1);
