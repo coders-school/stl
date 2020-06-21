8efb2c314d805194ae43472841ff6dc8d78750af
Nessy90 <nessy0991@gmail.com>
2020-06-21 12:08:57 +0200

    check if equal

--- module2/homework/transformContainers/transformContainers.cpp
+++ module2/homework/transformContainers/transformContainers.cpp
@@ -1,9 +1,10 @@
 #include "transformContainers.hpp"
 
 #include <algorithm>
+#include <iostream>
 
 std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string>& listOfWords, std::deque<int>& dequeOfNumbers) {
-    std::map<int, std::string> transformedMap;
+    std::map<int, std::string> transformedMap{};
 
     listOfWords.sort();
     listOfWords.erase(std::unique(begin(listOfWords), end(listOfWords)), end(listOfWords));
@@ -11,11 +12,13 @@ std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string
     std::sort(dequeOfNumbers.begin(), dequeOfNumbers.end());
     dequeOfNumbers.erase(std::unique(begin(dequeOfNumbers), end(dequeOfNumbers)), end(dequeOfNumbers));
 
-    std::transform(dequeOfNumbers.begin(), dequeOfNumbers.end(), listOfWords.begin(),
-                   std::inserter(transformedMap, transformedMap.end()),
-                   [](const int number, const std::string& word) {
-                       return std::make_pair(number, word);
-                   });
+    if (listOfWords.size() == dequeOfNumbers.size()) {
+        std::transform(dequeOfNumbers.begin(), dequeOfNumbers.end(), listOfWords.begin(),
+                       std::inserter(transformedMap, transformedMap.end()),
+                       [](const int number, const std::string& word) {
+                           return std::make_pair(number, word);
+                       });
+    }
 
     return transformedMap;
 }
