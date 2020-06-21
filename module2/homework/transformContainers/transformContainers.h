12e6c1f5375464c3bb074547a2d29faeb7ca523e
Tal <natalia.nowacka.it@gmail.com>
2020-06-21 13:36:19 +0200

    adding transformContainers method + changing CMake in insensitivePalindrom

---
+++ module2/homework/transformContainers/transformContainers.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <string>
+#include <deque>
+#include <algorithm>
+#include <map>
+#include <list>
+#include <utility>
+
+std::map<int, std::string>& removeDuplicateAndTranformToMap(std::list<std::string> texts, std::deque<int> numbers);
