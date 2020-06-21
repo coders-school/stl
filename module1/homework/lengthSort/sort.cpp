f28f39862b6b12fbd2269a9f8b88c1d5a982bf1d
MonikaZ <monika.zelechowska@o2.pl>
2020-06-21 13:37:02 +0200

    Change sort function - use sort with predicate

--- module1/homework/lengthSort/sort.cpp
+++ module1/homework/lengthSort/sort.cpp
@@ -3,10 +3,17 @@
 #include <algorithm>
 
 std::deque<std::string> lengthSort(std::forward_list<std::string>& list) {
-    auto sortByLength = [](auto first, auto second) { return first.size() < second.size(); };
+    struct Greater {
+        bool operator()(const std::string& first, const std::string& second) {
+            if (first.size() == second.size()) {
+                return first <= second;
+            }
 
-    list.sort(sortByLength);
-    list.sort();
+            return first.size() <= second.size();
+        }
+    };
+
+    list.sort(Greater());
     std::deque<std::string> deq{list.begin(), list.end()};
 
     return deq;
