a175bc7b02347f2b631c79d884c2b0f9be9d05e9
LogarithmDerivative <mateusz.kowalski074@gmail.com>
2020-06-21 14:17:08 +0200

    transformContainers done

--- module2/homework/transformContainers/transform.cpp
+++ module2/homework/transformContainers/transform.cpp
@@ -2,9 +2,20 @@
 
 #include <algorithm>
 
-std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string>& list, std::deque<int>& dek)
-{
-    std::map<int, std::string> result;
+std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string>& words, std::deque<int>& numbers) {
+    std::map<int, std::string> resultMap;
 
-    return {};
+    words.sort();
+    words.unique();
+
+    std::sort(numbers.begin(), numbers.end());
+    auto last = std::unique(numbers.begin(), numbers.end());
+    numbers.erase(last, numbers.end());
+
+    std::transform(words.begin(), words.end(), numbers.begin(), std::inserter(resultMap, resultMap.end()),
+                   [](const auto& str, const auto num) {
+                       return std::make_pair(num, str);
+                   });
+
+    return resultMap;
 }
--- module2/homework/transformContainers/transform.hpp
+++ module2/homework/transformContainers/transform.hpp
@@ -1,9 +1,8 @@
 #pragma once
 
 #include <deque>
-#include <map>
 #include <list>
+#include <map>
 #include <string>
 
-std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string>&, std::deque<int>&);
-
+std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string>& words, std::deque<int>& numbers);
