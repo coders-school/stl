489cd12a3ca0291e7ecfd8d72defffa278bd80a0
Adrian Baranowicz <adrbaranowicz@gmail.com>
2020-06-21 19:47:55 +0200

    compression/decompression algorithm refactored

--- module1/homework/grayscaleImages/compression.cpp
+++ module1/homework/grayscaleImages/compression.cpp
@@ -6,21 +6,15 @@ CompressedBitmap compressGrayscale(const Bitmap& bitmap) {
     CompressedBitmap compressedBitmap;
     compressedBitmap.reserve(height * width);
     for (const auto& row : bitmap) {
-        uint8_t color{row.front()};
-        std::size_t consecutiveCounter{1};
-        for (auto it = row.begin(); it != row.end(); ++it) {
-            auto nextBitColor = std::next(it);
-            if (color != *nextBitColor || nextBitColor == row.end())
-            {
-                compressedBitmap.emplace_back(color, consecutiveCounter);
-                consecutiveCounter = 1;
-                color = *nextBitColor;
-            }
-            else
-            {
-                ++consecutiveCounter;
-            }
-        }
+        for (auto currentColorIt = row.begin(); currentColorIt != row.end();) {
+            const auto diffColorIt = std::find_if(currentColorIt, 
+                                                  row.end(), 
+                                                  [&](const auto& color) { 
+                                                      return color != *currentColorIt; 
+                                                  });
+            compressedBitmap.emplace_back(*currentColorIt, std::distance(currentColorIt, diffColorIt));
+            currentColorIt = diffColorIt;
+        }    
     }
     compressedBitmap.shrink_to_fit();
     return compressedBitmap;
@@ -28,19 +22,9 @@ CompressedBitmap compressGrayscale(const Bitmap& bitmap) {
 
 Bitmap decompressGrayscale(const CompressedBitmap& compressedBitmap) {
     Bitmap decompressedBitmap;
-    size_t currentRow{0};
-    size_t currentColumn{0};
+    auto it = (decompressedBitmap.front()).begin();
     for (const auto& colorChunk : compressedBitmap) {
-        for(size_t consecutiveCounter = 0; consecutiveCounter < colorChunk.second; ++consecutiveCounter){
-            decompressedBitmap[currentRow][currentColumn] = colorChunk.first;
-            if (currentColumn < width - 1) {
-                currentColumn++;
-            }
-            else {
-                currentRow++;
-                currentColumn = 0;
-            } 
-        }
+        it = std::fill_n(it, colorChunk.second, colorChunk.first);
     }
     return decompressedBitmap;
 }
