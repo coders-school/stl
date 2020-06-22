f3080efa5fb6e6c06ba91074e0ddeba78abb99b9
MPrzem <przemyslaw.malec1997@gmail.com>
2020-06-22 22:18:48 +0200

    Strings sorting works

---
+++ module1/homework/lengthSort/sort.cpp
@@ -0,0 +1,11 @@
+#include "sort.hpp"
+
+std::deque<std::string> lengthSort(std::forward_list<std::string> words){
+    words.sort();
+
+    std::deque<std::string> result(words.begin(), words.end());
+
+    return result;
+
+
+}
---
+++ module1/homework/lengthSort/sort.hpp
@@ -0,0 +1,8 @@
+
+#pragma once
+
+#include <deque>
+#include <forward_list>
+#include <string>
+
+std::deque<std::string> lengthSort(std::forward_list<std::string> data);
