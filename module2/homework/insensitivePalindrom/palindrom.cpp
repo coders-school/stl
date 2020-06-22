71ab83004542583205c7057979dfab17856e5d3e
McKucia <macikucia@onet.pl>
2020-06-22 11:17:12 +0200

    Aliases

--- module2/homework/insensitivePalindrom/palindrom.cpp
+++ module2/homework/insensitivePalindrom/palindrom.cpp
@@ -2,6 +2,7 @@
 #include <algorithm>
 
 bool is_palindrome(std::string sequence){
+
     std::transform(sequence.begin(), sequence.end(), sequence.begin(), [](auto str){ return std::tolower(str); });
     sequence.erase( (remove_if(sequence.begin(), sequence.end(), [](auto el){ return !isalpha(el); })), sequence.end());
     return std::equal(sequence.begin(), sequence.begin() + sequence.size()/2, sequence.rbegin());
