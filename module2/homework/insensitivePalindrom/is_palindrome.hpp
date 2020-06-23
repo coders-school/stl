6f5f5bf8954c3c06f0058624b9c1a7ad144d7a08
duszek <marcin.dus@gmail.com>
2020-06-23 23:59:18 +0200

    Zadanie 2 - insensitivePalindrom solved

--- module2/homework/insensitivePalindrom/is_palindrome.hpp
+++ module2/homework/insensitivePalindrom/is_palindrome.hpp
@@ -1,9 +1,12 @@
+#include <algorithm>
+#include <cctype>
 #include <string>
-bool is_palindrome(const std::string & text)
+
+bool is_palindrome(std::string text)
 {
+    text.erase(std::remove_if(text.begin(), text.end(), [](unsigned char x) { return !std::isalpha(x); }), text.end());
+    std::transform(text.begin(), text.end(), text.begin(), [](unsigned char c) { return std::tolower(c); });
     std::string text_cp = text;
     reverse(text_cp.begin(), text_cp.end());
     return text == text_cp;
 }
-
-
