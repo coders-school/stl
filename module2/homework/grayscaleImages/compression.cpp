73b104d66913943bea84c1422128dcd92d34c4a8
duszek <marcin.dus@gmail.com>
2020-06-23 22:13:28 +0200

    Grayscaleimages compression with for_each

--- module2/homework/grayscaleImages/compression.cpp
+++ module2/homework/grayscaleImages/compression.cpp
@@ -16,36 +16,18 @@ void printMap(const Bitmap& bitmap)
 CompressedBitmap compressLine(const std::array<uint8_t, width>& line)
 {
     CompressedBitmap result;
-    result.reserve(width);
-/*   some ideas....
-   std::array<int, 10> arr{1,1,1,2,2,2,3,3,1,1};
+    result.reserve(width * height);
 
-auto p = std::equal_range(std::begin(arr), std::end(arr), arr.front());
- 
-    for ( auto i = p.first; i != p.second; ++i )
-        std::cout << *i << "\n"; 
-//std::cout << std::distance(p.first, p.second) << "\n";
-
-auto q = std::equal_range(p.second, std::end(arr), *p.second);
- 
-    for ( auto i = q.first; i != q.second; ++i )
-        std::cout << *i << "\n"; *
-        */
-
-    std::transform(std::begin(line), std::end(line),
-    [&](uint8_t pixel)
-    {
-        if(!result.empty() and result.back().first == pixel)
+    std::for_each(std::cbegin(line), std::cend(line), [&result, &line](uint8_t pixel) {
+        if (!result.empty() and result.back().first == pixel)
         {
             result.back().second++;
         }
         else
         {
-            result.push_back(std::make_pair(pixel,1));
+            result.push_back(std::make_pair(pixel, 1));
         }
-    }
-    );
-
+    });
 
     result.shrink_to_fit();
     return result;
@@ -58,11 +40,11 @@ CompressedBitmap compressGrayscale(const Bitmap& bitmap)
     CompressedBitmap tmp;
     tmp.reserve(width);
 
-    for (const auto& line : bitmap)
+    std::for_each(std::begin(bitmap), std::end(bitmap), [&tmp, &result](const auto& line)
     {
         tmp = compressLine(line);
         result.insert(std::end(result), std::begin(tmp), std::end(tmp));
-    }
+    });
     result.shrink_to_fit();
     return result;
 }
@@ -71,9 +53,9 @@ Bitmap decompressGrayscale(const CompressedBitmap& compressedBitmap)
 {
     Bitmap result;
     auto iter = std::begin(result.front());
-    for (const auto& group : compressedBitmap)
+    std::for_each(std::begin(compressedBitmap), std::end(compressedBitmap), [&result, &iter] (const auto& group)
     {
         iter = std::fill_n(iter, group.second, group.first);
-    }
+    });
     return result;
 }
