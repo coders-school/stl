ca26e4f82bef4c59256efb7abfb52b8d6d91b8a8
Wojciech Matusiak <253035@student.pwr.edu.pl>
2020-06-23 16:11:35 +0200

    refactoring compression.cpp

--- module1/homework/grayscaleImages/compression.cpp
+++ module1/homework/grayscaleImages/compression.cpp
@@ -1,33 +1,26 @@
 #include "compression.hpp"
 
+#include <algorithm>
 #include <cstdint>
 
 std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(const std::array<std::array<uint8_t, width>, height>& bitmap) {
-   
-    uint8_t counter = 1;
     std::vector<std::pair<uint8_t, uint8_t>> compressed_bitmap;
-    for (size_t row = 0; row < height; ++row) {
-        for (size_t col = 0; col < width - 1; ++col) {
-            if (col == width - 2) {
-                if(bitmap[row][col] == bitmap[row][col + 1]) {
-                compressed_bitmap.push_back(std::pair<uint8_t, uint8_t>(bitmap[row][col], counter + 1));
-                counter = 1;
-                } else {
-                    compressed_bitmap.push_back(std::pair<uint8_t, uint8_t>(bitmap[row][col], counter));
-                    counter = 1;
-                    compressed_bitmap.push_back(std::pair<uint8_t, uint8_t>(bitmap[row][col + 1], counter));
-                }
-                continue; 
-            }
-            if(bitmap[row][col] == bitmap[row][col + 1]) {
-                counter++;
-            } else {
-                compressed_bitmap.push_back(std::pair<uint8_t, uint8_t>(bitmap[row][col], counter));
-                counter = 1;
-            }
+    compressed_bitmap.reserve(width * height);
+    uint8_t counter = 1;
+    for (const auto& row : bitmap) {
+        const uint8_t* beginning = row.begin();
+        const uint8_t* end = row.begin();
+        while (end != row.end()) {
+            end = std::find_if(end, row.end(), [end](auto elem) {
+                return *end != elem;
+            });
+            counter = std::distance(beginning, end);
+            compressed_bitmap.push_back(std::make_pair(*beginning, counter));
+            beginning = end;
         }
     }
+    compressed_bitmap.shrink_to_fit();
     return compressed_bitmap;
 }
 
-std::array<std::array<uint8_t, width>, height> decompressGrayscale(std::vector<std::pair<uint8_t, uint8_t>>&) {};
+std::array<std::array<uint8_t, width>, height> decompressGrayscale(std::vector<std::pair<uint8_t, uint8_t>>&){};
