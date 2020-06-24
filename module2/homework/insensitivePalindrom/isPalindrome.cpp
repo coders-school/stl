1bff2747e360b79f2c00784eade07a14a4e27e0b
Wojciech Matusiak <253035@student.pwr.edu.pl>
2020-06-24 20:08:27 +0200

    intial commit + editing CMakelists and text.cpp

---
+++ module2/homework/insensitivePalindrom/isPalindrome.cpp
@@ -0,0 +1,13 @@
+#include "isPalindrome.hpp"
+
+#include <algorithm>
+
+bool is_palindrome(const std::string& text) {
+    std::string pure_text;
+    std::copy_if(text.begin(), text.end(), pure_text.begin(), 
+    [](char letter)
+    {
+        
+    });
+    
+}
