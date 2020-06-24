e9957eb9485b68edb4c3f8efe9bac3df59219893
Wojciech Matusiak <253035@student.pwr.edu.pl>
2020-06-24 20:55:43 +0200

    insensitivePalindrome added

--- module2/homework/insensitivePalindrom/isPalindrome.cpp
+++ module2/homework/insensitivePalindrom/isPalindrome.cpp
@@ -1,13 +1,18 @@
 #include "isPalindrome.hpp"
 
 #include <algorithm>
+#include <cctype>
 
 bool is_palindrome(const std::string& text) {
     std::string pure_text;
-    std::copy_if(text.begin(), text.end(), pure_text.begin(), 
-    [](char letter)
-    {
-        
-    });
-    
+    pure_text.reserve(text.size());
+    std::for_each(text.begin(), text.end(),
+                  [&pure_text](unsigned char letter) {
+                      if (isalnum(letter)) {
+                          pure_text += tolower(letter);
+                      }
+                  });
+    pure_text.shrink_to_fit();
+
+    return std::equal(pure_text.begin(), pure_text.begin() + (pure_text.size() / 2), pure_text.rbegin());
 }
