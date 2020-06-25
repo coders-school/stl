ce99c5816ec5f682c2ff2656f0d6e6cde7957c69
McKucia <macikucia@onet.pl>
2020-06-25 17:32:27 +0200

    Using stl algorithm in lengthSort

--- module1/homework/lengthSort/sort.cpp
+++ module1/homework/lengthSort/sort.cpp
@@ -1,14 +1,20 @@
 #include "sort.hpp"
+#include <algorithm>
 
 std::deque<std::string> lengthSort(std::forward_list<std::string>& forwardList) {
-    std::deque<std::string> dq;
-    auto it = dq.begin();
 
-    for (const auto& word : forwardList) {
-        for (it = dq.begin(); it != dq.end(); it++)
-            if (it->size() > word.size() || it->size() == word.size() && *it > word)
-                break;
-        dq.insert(it, word);
+    std::deque<std::string> dq;
+    
+    forwardList.sort( [](const auto str1, const auto str2){
+        if(str1.size() == str2.size()){
+            return str1 < str2;
+        }
+        return str1.size() < str2.size();
+    } );
+    
+    for(const auto el : forwardList){
+        dq.push_back(el);
     }
+
     return dq;
 }
