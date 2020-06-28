162d30ba040e98b5d7440c44d95ce42377be8646
damixiorek <damixiorek@gmail.com>
2020-06-28 11:17:26 +0200

    palindrome.cpp modyfied

--- module2/homework/insensitivePalindrom/palindrome.cpp
+++ module2/homework/insensitivePalindrom/palindrome.cpp
@@ -4,7 +4,7 @@
 
 bool is_palindrome(std::string str){
 
-    str.erase(std::remove_if(str.begin(), str.end(), [](char letter) { return std::isalpha(letter) == 0; }), str.end());
+    str.erase(std::remove_if(str.begin(), str.end(), [](char letter) { return !(std::isalpha(letter)); }), str.end());
     std::transform(str.begin(), str.end(), str.begin(), [](auto letter){ return std::tolower(letter); });
 
     return str == std::string(str.rbegin(), str.rend());
