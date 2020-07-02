9691009b52856113eb8044476883b65e5a48523d
g00fer <bargot@o2.pl>
2020-07-02 22:07:07 +0200

    Run formatting on file

--- module2/homework/grayscaleImages/compression.cpp
+++ module2/homework/grayscaleImages/compression.cpp
@@ -7,46 +7,48 @@
 using Bitmap = std::array<std::array<uint8_t, width>, height>;
 using CompressedBitmap = std::vector<std::pair<uint8_t, uint8_t>>;
 
-void printMap (const Bitmap &bitmap)
+void printMap(const Bitmap &bitmap)
 {
-    for(const auto& line : bitmap)
+    for (const auto &line : bitmap)
     {
         for (const auto c : line)
         {
-            if(c < ' '){
-                std::cout<<' ';  
-            }else{
-                std::cout<<c;
+            if (c < ' ')
+            {
+                std::cout << ' ';
+            }
+            else
+            {
+                std::cout << c;
             }
         }
-        std::cout<<'\n';
+        std::cout << '\n';
     }
 }
 
-CompressedBitmap compressGrayscale (const Bitmap& bitmap)
+CompressedBitmap compressGrayscale(const Bitmap &bitmap)
 {
     CompressedBitmap compressed;
 
-    for (const auto& line : bitmap)
+    for (const auto &line : bitmap)
     {
         auto firstElemIter = line.cbegin();
         auto lastElemIter = firstElemIter;
 
         do
         {
-            lastElemIter = std::find_if( firstElemIter, line.cend(), [firstElemIter](uint8_t i) {
+            lastElemIter = std::find_if(firstElemIter, line.cend(), [firstElemIter](uint8_t i) {
                 return i != *firstElemIter;
             });
             compressed.push_back({*firstElemIter, std::distance(firstElemIter, lastElemIter)});
             firstElemIter = lastElemIter;
 
-        } while(firstElemIter != line.cend());
+        } while (firstElemIter != line.cend());
     }
     return compressed;
 }
 
-
-Bitmap decompressGrayscale(const CompressedBitmap& compressed)
+Bitmap decompressGrayscale(const CompressedBitmap &compressed)
 {
     Bitmap decompressed;
     auto row = 0;
@@ -56,7 +58,7 @@ Bitmap decompressGrayscale(const CompressedBitmap& compressed)
     {
         std::fill_n(&decompressed[row][column], chunk.second, chunk.first);
         column += chunk.second;
-        if(column >= width )
+        if (column >= width)
         {
             row++;
             column = 0;
