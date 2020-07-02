5b57e82783d5db2c92271b3e592cd5faa637806d
MPrzem <przemyslaw.malec1997@gmail.com>
2020-07-02 19:00:50 +0200

    Checking ranges

--- module2/homework/insensitivePalindrom/palindroms.cpp
+++ module2/homework/insensitivePalindrom/palindroms.cpp
@@ -1,6 +1,14 @@
 #include "palindroms.hpp"
 
-bool is_palindrome(std::string word){
-
+bool is_palindrome(std::string word)
+{
     return true;
+}
+bool isOutOfRange(const char c,const int upperBound1,const int upperBound2)
+{
+    const int index = (const int ) c;
+    if(index<upperBound1||index>upperBound2)
+        return true;
+
+
 }
--- module2/homework/insensitivePalindrom/palindroms.hpp
+++ module2/homework/insensitivePalindrom/palindroms.hpp
@@ -1,3 +1,12 @@
 #include <string>
 
+inline constexpr uint8_t ASCII_digits_range_start = 48;
+inline constexpr uint8_t ASCII_digits_range_end = 57;
+
+inline constexpr uint8_t ASCII_small_letters_range_start = 97;
+inline constexpr uint8_t ASCII_small_letters_range_end = 122;
+
+inline constexpr uint8_t ASCII_Capital_letters_range_start = 65;
+inline constexpr uint8_t ASCII_Capital_letters_range_end = 90;
+
 bool is_palindrome(std::string word);
