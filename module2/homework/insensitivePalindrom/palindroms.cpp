20546116720cc65629ccf3732b3fcb31452e466b
MPrzem <przemyslaw.malec1997@gmail.com>
2020-07-04 16:14:07 +0200

    Palindromes program has passed all tests

--- module2/homework/insensitivePalindrom/palindroms.cpp
+++ module2/homework/insensitivePalindrom/palindroms.cpp
@@ -1,14 +1,80 @@
 #include "palindroms.hpp"
+#include <iostream>
+#include <algorithm>
+
+bool isInRanges(const char c);
+bool isInRange(const char c, const int upperBound1, const int upperBound2);
+bool insensitiveCapital(const int first, const int second);
 
 bool is_palindrome(std::string word)
 {
-    return true;
+    bool itCouldBePalindrome;
+    auto start_it = word.begin();
+    auto end_it = word.end();
+    auto iterators = std::make_pair(word.begin(), word.rbegin());
+    auto middle = start_it + word.size() / 2;
+
+    do
+    {
+
+        itCouldBePalindrome = false;
+
+        iterators = std::mismatch(iterators.first, middle, iterators.second);
+        if (iterators.first == middle)
+            return true;
+        if (!isInRanges(*iterators.first))
+        {
+            iterators.first++;
+            itCouldBePalindrome = true;
+        }
+        if (!isInRanges(*iterators.second))
+        {
+            itCouldBePalindrome = true;
+            iterators.second++;
+        }
+        if (insensitiveCapital(*iterators.first, *iterators.second) && isInRanges(*iterators.first) && isInRanges(*iterators.second))
+        {
+
+            itCouldBePalindrome = true;
+            iterators.first++;
+            iterators.second++;
+        }
+
+        if (iterators.first >= end_it || iterators.second.base()< start_it)
+            if (itCouldBePalindrome)
+                return true;
+            else
+                return false;
+
+    } while (itCouldBePalindrome);
+    std::cout<<std::string(iterators.first,iterators.second.base());
+    return false;
 }
-bool isOutOfRange(const char c,const int upperBound1,const int upperBound2)
+bool isInRange(const char c, const uint8_t lowerBound, const uint8_t upperBound2)
 {
-    const int index = (const int ) c;
-    if(index<upperBound1||index>upperBound2)
+    const int index = (const int)c;
+    if (index >= lowerBound && index <= upperBound2)
         return true;
+    else
+        return false;
+}
 
-
+///Jesli jest poza kazdym z zakresow to jest poza zakresem
+bool isInRanges(const char c)
+{
+    if (isInRange(c, ASCII_digits_range_start, ASCII_digits_range_end))
+        return true;
+    if (isInRange(c, ASCII_small_letters_range_start, ASCII_small_letters_range_end))
+        return true;
+    if (isInRange(c, ASCII_Capital_letters_range_start, ASCII_Capital_letters_range_end))
+        return true;
+    return false;
+}
+bool insensitiveCapital(const int first, const int second)
+{
+    int diffrence = first - second;
+    if (diffrence == 32 || diffrence == -32)
+        return true;
+    else
+        return false;
 }
--- module2/homework/insensitivePalindrom/palindroms.hpp
+++ module2/homework/insensitivePalindrom/palindroms.hpp
@@ -1,12 +1,14 @@
+#pragma once
+
 #include <string>
 
-inline constexpr uint8_t ASCII_digits_range_start = 48;
-inline constexpr uint8_t ASCII_digits_range_end = 57;
+ const uint8_t ASCII_digits_range_start = 48;
+ const uint8_t ASCII_digits_range_end = 57;
 
-inline constexpr uint8_t ASCII_small_letters_range_start = 97;
-inline constexpr uint8_t ASCII_small_letters_range_end = 122;
+ const uint8_t ASCII_small_letters_range_start = 97;
+ const uint8_t ASCII_small_letters_range_end = 122;
 
-inline constexpr uint8_t ASCII_Capital_letters_range_start = 65;
-inline constexpr uint8_t ASCII_Capital_letters_range_end = 90;
+ const uint8_t ASCII_Capital_letters_range_start = 65;
+ const uint8_t ASCII_Capital_letters_range_end = 90;
 
 bool is_palindrome(std::string word);
