c3482f063fd6341ced491181e75f4034e5233009
MPrzem <przemyslaw.malec1997@gmail.com>
2020-07-07 20:59:34 +0200

    Fixed sorting

--- module1/homework/lengthSort/sort.cpp
+++ module1/homework/lengthSort/sort.cpp
@@ -1,18 +1,16 @@
 #include "sort.hpp"
 
-#include<algorithm>
+#include <algorithm>
 
-std::deque<std::string> lengthSort(std::forward_list<std::string>& words)
+std::deque<std::string> lengthSort(std::forward_list<std::string> &words)
 {
-    
     std::deque<std::string> result(words.begin(), words.end());
 
-    std::sort(result.begin(),result.end(),[](const std::string& str1, const std::string& str2){
-        if(str1.size()==str2.size())
-            return str1<str2;
-        else 
-            return str1.size()<str2.size();
+    std::sort(result.begin(), result.end(), [](const std::string &str1, const std::string &str2) {
+        if (str1.size() == str2.size())
+            return str1 < str2;
+        else
+            return str1.size() < str2.size();
     });
-
     return result;
 }
