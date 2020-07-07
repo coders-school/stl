ead834b71ad130aff9c5c196d23e0693c420d1b2
PiotrHCpp <piotr.hoffner.wroc@gmail.com>
2020-07-07 16:48:22 +0200

    added transform.hpp

---
+++ module2/homework/transformContainers/transform.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <algorithm>
+#include <deque>
+#include <list>
+#include <map>
+#include <string>
+
+using intStrMap = std::map<int, std::string>;
+using strList = std::list<std::string>;
+using intDeque = std::deque<int>;
+
+intStrMap removeDuplicateAndTranformToMap(strList strList, intDeque intDeq);
