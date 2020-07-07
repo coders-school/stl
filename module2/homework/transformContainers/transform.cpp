87907743ec458c283cc468f332c167ac987a8816
PiotrHCpp <piotr.hoffner.wroc@gmail.com>
2020-07-07 16:49:41 +0200

    introduced removeDuplicateAndTranformToMap() in transform.cpp

---
+++ module2/homework/transformContainers/transform.cpp
@@ -0,0 +1,20 @@
+#include "transform.hpp"
+
+intStrMap removeDuplicateAndTranformToMap(strList strList, intDeque intDeq) {
+    intStrMap returnValue{};
+    if(strList.empty() || intDeq.empty()) { 
+        return returnValue;
+    }
+    strList.sort();
+    std::sort(begin(intDeq), end(intDeq));
+    strList.erase(std::unique(begin(strList), end(strList)), end(strList));
+    intDeq.erase(std::unique(begin(intDeq), end(intDeq)), end(intDeq));
+    std::transform(begin(intDeq), 
+                   end(intDeq), 
+                   begin(strList),
+                   std::inserter(returnValue, end(returnValue)),
+                   [](int num, std::string str){
+                        return std::make_pair(num, str);
+                   });
+    return returnValue;
+}
