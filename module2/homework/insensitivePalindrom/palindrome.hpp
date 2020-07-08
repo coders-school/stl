787ed81648dbaa31af8b3594aad367737a639778
kawapa <kawapa@outlook.com>
2020-07-08 20:00:41 +0200

    Fix palindrome

--- module2/homework/insensitivePalindrom/palindrome.hpp
+++ module2/homework/insensitivePalindrom/palindrome.hpp
@@ -1,15 +1,5 @@
 #pragma once
 
-#include <algorithm>
-#include <cctype>
+#include <string>
 
-bool is_palindrome(const std::string &text)
-{
-    std::string temp = text;
-
-    temp.erase(std::remove_if(begin(temp), end(temp), [](char c){ return !std::isalpha(c); }), end(temp));
-    std::transform(begin(temp), end(temp), begin(temp), [](unsigned char c) {
-        return std::tolower(c); });
-
-    return std::equal(begin(temp), begin(temp) + temp.size() / 2, rbegin(temp));
-}
+bool is_palindrome(const std::string& text);
