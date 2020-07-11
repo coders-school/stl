713ab15ad1cffb1e42dbf0f7b6b9a97d3e3b8a89
Michal <mcamromer@gmail.com>
2020-07-11 19:26:05 +0200

    Replace in the vowel.hpp file definition of the removeVowels function with it's declaration.

--- module1/homework/removeVowels/vowel.hpp
+++ module1/homework/removeVowels/vowel.hpp
@@ -3,18 +3,4 @@
 #include <vector>
 #include <string>
 
-const std::string vovels = "aeioyAEIOY";
-
-void removeVowels(std::vector<std::string>& lines)
-{
-    for(auto& line : lines)
-    {
-        for (int i = 0; i < line.length(); i++)
-        {
-            if(vovels.find(line[i]) != std::string::npos)
-            {
-                line.erase(i, 1);
-            }
-        }
-    }
-}
+void removeVowels(std::vector<std::string>& lines);
