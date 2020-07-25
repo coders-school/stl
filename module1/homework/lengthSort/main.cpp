e502b89a97035f311e0999fcc83860d703be93df
PiotrD <dywel321@gmail.com>
2020-07-25 13:03:26 +0200

    lengthSort()

--- module1/homework/lengthSort/main.cpp
+++ module1/homework/lengthSort/main.cpp
@@ -1,8 +1,19 @@
 #include <forward_list>
 #include <iostream>
 #include <string>
+#include <deque>
+#include <forward_list>
+#include <functional>
 
-#include "sort.hpp"
+using deq = std::deque<std::string>;
+using flist = std::forward_list<std::string>;
+
+auto cmp = [](const std::string str1, const std::string str2) {
+    if (str1.length() == str2.length()) {
+        return str1 < str2;
+    } else
+        return (str1.length() < str2.length());
+};
 
 template <typename Container>
 void printCollection(const Container& collection) {
@@ -12,9 +23,18 @@ void printCollection(const Container& collection) {
     std::cout << '\n';
 }
 
+deq lengthSort(const flist& flist) {
+    auto tmpList = flist;
+    tmpList.sort(cmp);
+    deq deque(tmpList.begin(), tmpList.end());
+    return deque;
+}
+
 int main() {
-    std::forward_list<std::string> words = {
-        "Somebody", "once", "told", "me", "the", "world", "is", "gonna", "roll", "me"};
+    std::forward_list<std::string> words = {"Somebody", "once",  "told", "me",
+                                              "the",      "world", "is",   "gonna",
+                                              "roll",     "me"};
+
     printCollection(words);
     auto sorted = lengthSort(words);
     printCollection(sorted);
