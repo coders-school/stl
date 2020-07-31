511196c4e64ded76e870af3cee3bec57a25a367b
WojtekMs <wojtek.atmo@gmail.com>
2020-07-31 12:20:15 +0200

    adding header file

---
+++ module2/homework/transformContainers/transformContainers.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include <deque>
+#include <list>
+#include <string>
+#include <map>
+
+std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string>&, std::deque<int>&);
