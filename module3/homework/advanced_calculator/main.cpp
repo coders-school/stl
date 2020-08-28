a0ca0ffcc4f964a0bc37a1afae25ec11ca61531e
Andrzej Rafalski <anraf1001@gmail.com>
2020-08-28 15:53:07 +0200

    Added main loop

--- module3/homework/advanced_calculator/main.cpp
+++ module3/homework/advanced_calculator/main.cpp
@@ -1,3 +1,13 @@
+#include <iostream>
+#include <string>
+
+#include "advancedCalculator.hpp"
+
 int main() {
+    std::string input;
+    do {
+        std::cout << "Enter expression to calculate (q to exit): ";
+        std::getline(std::cin, input);
+    } while (input != "q");
     return 0;
 }
