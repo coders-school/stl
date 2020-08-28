33790efa246841b6133662b0f028f45b27e2de27
Andrzej Rafalski <anraf1001@gmail.com>
2020-08-28 14:08:23 +0200

    Moved commands map outside process function

--- module3/homework/advanced_calculator/advancedCalculator.cpp
+++ module3/homework/advanced_calculator/advancedCalculator.cpp
@@ -4,16 +4,15 @@
 #include <functional>
 #include <map>
 
-#include <iostream>
+std::map<char, std::function<double(double, double)>> commands{{'+', std::plus<double>()},
+                                                               {'-', std::minus<double>()},
+                                                               {'*', std::multiplies<double>()},
+                                                               {'/', std::divides<double>()},
+                                                               {'%', std::modulus<int>()},
+                                                               {'^', [](double lhs, double rhs) { return pow(lhs, rhs); }},
+                                                               {'$', [](double lhs, double rhs) { return pow(lhs, 1.0 / rhs); }},
+                                                               {'!', [](double lhs, double rhs = 0.0) { return tgamma(lhs + 1.0); }}};
 
 ErrorCode process(const std::string& input, double* out) {
-    std::map<char, std::function<double(double, double)>> commands{{'+', std::plus<double>()},
-                                                                   {'-', std::minus<double>()},
-                                                                   {'*', std::multiplies<double>()},
-                                                                   {'/', std::divides<double>()},
-                                                                   {'%', std::modulus<int>()},
-                                                                   {'^', [](double lhs, double rhs) { return pow(lhs, rhs); }},
-                                                                   {'$', [](double lhs, double rhs) { return pow(lhs, 1.0 / rhs); }},
-                                                                   {'!', [](double lhs, double rhs = 0.0) { return tgamma(lhs + 1.0); }}};
     return ErrorCode::OK;
 }
