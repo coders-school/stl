47091a3bef7e22dfcebc2344044422874b6dbe3a
Andrzej Rafalski <anraf1001@gmail.com>
2020-08-28 16:03:12 +0200

    Added error codes to main loop

--- module3/homework/advanced_calculator/main.cpp
+++ module3/homework/advanced_calculator/main.cpp
@@ -5,9 +5,32 @@
 
 int main() {
     std::string input;
+    double result{};
+
     do {
         std::cout << "Enter expression to calculate (q to exit): ";
         std::getline(std::cin, input);
+
+        switch (process(input, &result)) {
+        case ErrorCode::OK:
+            std::cout << "Result: " << result << '\n';
+            break;
+        case ErrorCode::BadCharacter:
+            std::cout << "Error!!! Bad character\n";
+            break;
+        case ErrorCode::BadFormat:
+            std::cout << "Error!!! Bad format\n";
+            break;
+        case ErrorCode::DivideBy0:
+            std::cout << "Error!!! Divide by 0\n";
+            break;
+        case ErrorCode::SqrtOfNagativeNumber:
+            std::cout << "Error!!! Square root of negative number\n";
+            break;
+        case ErrorCode::ModuleOfNonIntegerValue:
+            std::cout << "Error!!! Modulo of non-integer value\n";
+            break;
+        }
     } while (input != "q");
     return 0;
 }
