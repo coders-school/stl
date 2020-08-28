eb362dc34f19df544a75c10cc7e70874b2ec5432
Andrzej Rafalski <anraf1001@gmail.com>
2020-08-28 14:24:13 +0200

    Changed names of variables

--- module3/homework/advanced_calculator/advancedCalculator.cpp
+++ module3/homework/advanced_calculator/advancedCalculator.cpp
@@ -9,9 +9,9 @@ std::map<char, std::function<double(double, double)>> commands{{'+', std::plus<d
                                                                {'*', std::multiplies<double>()},
                                                                {'/', std::divides<double>()},
                                                                {'%', std::modulus<int>()},
-                                                               {'^', [](double lhs, double rhs) { return pow(lhs, rhs); }},
-                                                               {'$', [](double lhs, double rhs) { return pow(lhs, 1.0 / rhs); }},
-                                                               {'!', [](double lhs, double rhs = 0.0) { return tgamma(lhs + 1.0); }}};
+                                                               {'^', [](double base, double exponent) { return pow(base, exponent); }},
+                                                               {'$', [](double num, double root) { return pow(num, 1.0 / root); }},
+                                                               {'!', [](double num, double whatever = 0.0) { return tgamma(num + 1.0); }}};
 
 ErrorCode process(const std::string& input, double* out) {
     return ErrorCode::OK;
