000641d864199cff21e7691ab1daae64554a9c37
Andrzej Rafalski <anraf1001@gmail.com>
2020-08-28 16:09:18 +0200

    Errors handling moved to another function

--- module3/homework/advanced_calculator/main.cpp
+++ module3/homework/advanced_calculator/main.cpp
@@ -3,34 +3,45 @@
 
 #include "advancedCalculator.hpp"
 
-int main() {
-    std::string input;
+void calculate(const std::string& input) {
     double result{};
 
-    do {
+    switch (process(input, &result)) {
+    case ErrorCode::OK:
+        std::cout << "Result: " << result << '\n';
+        break;
+    case ErrorCode::BadCharacter:
+        std::cout << "Error!!! Bad character\n";
+        break;
+    case ErrorCode::BadFormat:
+        std::cout << "Error!!! Bad format\n";
+        break;
+    case ErrorCode::DivideBy0:
+        std::cout << "Error!!! Divide by 0\n";
+        break;
+    case ErrorCode::SqrtOfNagativeNumber:
+        std::cout << "Error!!! Square root of negative number\n";
+        break;
+    case ErrorCode::ModuleOfNonIntegerValue:
+        std::cout << "Error!!! Modulo of non-integer value\n";
+        break;
+    }
+}
+
+int main() {
+    std::string input;
+
+    while (true) {
         std::cout << "Enter expression to calculate (q to exit): ";
         std::getline(std::cin, input);
 
-        switch (process(input, &result)) {
-        case ErrorCode::OK:
-            std::cout << "Result: " << result << '\n';
-            break;
-        case ErrorCode::BadCharacter:
-            std::cout << "Error!!! Bad character\n";
-            break;
-        case ErrorCode::BadFormat:
-            std::cout << "Error!!! Bad format\n";
-            break;
-        case ErrorCode::DivideBy0:
-            std::cout << "Error!!! Divide by 0\n";
-            break;
-        case ErrorCode::SqrtOfNagativeNumber:
-            std::cout << "Error!!! Square root of negative number\n";
-            break;
-        case ErrorCode::ModuleOfNonIntegerValue:
-            std::cout << "Error!!! Modulo of non-integer value\n";
+        if (input == "q") {
             break;
         }
-    } while (input != "q");
+
+        calculate(input);
+
+        std::cout << "--------------\n";
+    }
     return 0;
 }
