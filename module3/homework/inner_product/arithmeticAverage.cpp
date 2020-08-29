bec2f0067415d3db41c4b751eae87ab5993b77b4
MonikaZ <monika.zelechowska@o2.pl>
2020-08-29 22:17:58 +0200

    Add AritmetricAvarage and Distance functions implemantation

---
+++ module3/homework/inner_product/arithmeticAverage.cpp
@@ -0,0 +1,28 @@
+#include "arithmeticAverage.hpp"
+#include <cmath>
+#include <functional>
+#include <numeric>
+
+double ArithmeticAverage(const std::vector<int>& firstVec, const std::vector<int>& secondVec) {
+    const int divider = 1 / (firstVec.size() + secondVec.size());
+    return std::inner_product(firstVec.begin(),
+                              firstVec.end(),
+                              secondVec.begin(),
+                              divider,
+                              std::plus<>(),
+                              std::plus<>());
+}
+
+double Distance(const std::vector<int>& firstVec, const std::vector<int>& secondVec) {
+    const int initValue = 0;
+    return std::inner_product(secondVec.begin(),
+                              secondVec.end(),
+                              firstVec.begin(),
+                              initValue,
+                              std::plus<>(),
+                              [&](int firstVal, int secondVal) { return std::pow((firstVal - secondVal), 2); });
+}
+
+int main() {
+    return 0;
+}
---
+++ module3/homework/inner_product/arithmeticAverage.hpp
@@ -0,0 +1,4 @@
+#include <vector>
+
+double ArithmeticAverage(const std::vector<int>& firstVec, const std::vector<int>& secondVec);
+double Distance(const std::vector<int>& firstVec, const std::vector<int>& secondVec);
