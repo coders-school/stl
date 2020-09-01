1f4ddd60a2350d48701a8862d04289986bb0656c
Andrzej Rafalski <anraf1001@gmail.com>
2020-09-01 10:26:08 +0200

    Added essential files

---
+++ module4/homework/appendNewRecipe/AppendNewRecipe.cpp
@@ -0,0 +1,6 @@
+#include "AppendNewRecipe.hpp"
+
+bool AppendNewRecipe(std::vector<std::string> steps,
+                     const std::list<std::string>& ingredients,
+                     const std::deque<std::pair<size_t, char>>& amount) {
+}
