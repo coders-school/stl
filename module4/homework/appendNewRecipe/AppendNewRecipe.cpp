920760ed54170b17681461f1e9e2cf639713c87c
Andrzej Rafalski <anraf1001@gmail.com>
2020-09-01 10:36:17 +0200

    Added Format functions

--- module4/homework/appendNewRecipe/AppendNewRecipe.cpp
+++ module4/homework/appendNewRecipe/AppendNewRecipe.cpp
@@ -3,4 +3,21 @@
 bool AppendNewRecipe(std::vector<std::string> steps,
                      const std::list<std::string>& ingredients,
                      const std::deque<std::pair<size_t, char>>& amount) {
+    std::fstream recipes("recipes.txt", std::ios::app);
+
+    if (!recipes.is_open()) {
+        return false;
+    }
+
+    recipes.close();
+    return true;
+}
+
+std::vector<std::string> FormatIngredients(const std::list<std::string>& ingredients,
+                                           const std::deque<std::pair<size_t, char>>& amount) {
+}
+
+std::stringstream FormatRecipit(std::vector<std::string> steps,
+                                const std::list<std::string>& ingredients,
+                                const std::deque<std::pair<size_t, char>>& amount) {
 }
