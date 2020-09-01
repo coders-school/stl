1ed3462afe9c46b70b68f1d2d2caf1f6f8c95796
Andrzej Rafalski <anraf1001@gmail.com>
2020-09-01 12:11:20 +0200

    Solution

--- module4/homework/appendNewRecipe/AppendNewRecipe.cpp
+++ module4/homework/appendNewRecipe/AppendNewRecipe.cpp
@@ -5,7 +5,7 @@
 #include <iterator>
 #include <map>
 
-std::map<char, std::string> quantities{{'g', " gram"}, {'s', " szklanka(i)"}, {'m', "ml"}};
+std::map<char, std::string> quantities{{'g', "gram"}, {'s', "szklanka(i)"}, {'m', "mililitrów"}};
 
 bool AppendNewRecipe(std::vector<std::string> steps,
                      const std::list<std::string>& ingredients,
@@ -30,7 +30,7 @@ std::vector<std::string> FormatIngredients(const std::list<std::string>& ingredi
                    std::back_inserter(vecIngredients),
                    [&](const auto& ingredient, const auto& amount) {
                        std::stringstream sstr;
-                       sstr << amount.first << quantities[amount.second] << ' ' << ingredient;
+                       sstr << amount.first << ' ' << quantities[amount.second] << ' ' << ingredient;
                        return sstr.str();
                    });
 
@@ -40,4 +40,20 @@ std::vector<std::string> FormatIngredients(const std::list<std::string>& ingredi
 std::stringstream FormatRecipit(std::vector<std::string> steps,
                                 const std::list<std::string>& ingredients,
                                 const std::deque<std::pair<size_t, char>>& amount) {
+    std::stringstream recipit;
+    std::vector<std::string> vecIngedients = FormatIngredients(ingredients, amount);
+
+    recipit << "Składniki:\n";
+    std::copy(vecIngedients.cbegin(), vecIngedients.cend(), std::ostream_iterator<std::string>(recipit, ",\n"));
+    recipit << "\nKroki:\n";
+    std::transform(steps.cbegin(), steps.cend(),
+                   std::ostream_iterator<std::string>(recipit, "\n"),
+                   [counter{1}](const auto& step) mutable {
+                       std::stringstream sstr;
+                       sstr << counter++ << ") " << step << '.';
+                       return sstr.str();
+                   });
+    recipit << "___________________________________\n";
+
+    return recipit;
 }
