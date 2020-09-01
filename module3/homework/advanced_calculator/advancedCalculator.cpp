3061df8d78ad41eb7ac117fcc881a95e76ea360a
majkel84 <michal.sobotka.84@gmail.com>
2020-09-01 23:08:46 +0200

    STL homework3 - map<char, function>

--- module3/homework/advanced_calculator/advancedCalculator.cpp
+++ module3/homework/advanced_calculator/advancedCalculator.cpp
@@ -4,12 +4,16 @@
 #include <functional>
 #include <map>
 
+std::function<int(int)> factorial;
+factorial = [&factorial](int n)
+{ return n < 2 ? 1 : n * factorial(n - 1); };
+
 const std::map<char, std::function<double(double, double)>> equation {
     {'+', std::plus<double>()},
     {'-', std::minus<double>()},
     {'*', std::multiplies<double>()},
     {'/', std::divides<double>()},
-    //{'!', },
+    //{'!',  },
     {'^', [](double base, double exp) { return std::pow(base, exp); } },
     {'$', [](double base, double exp) { return std::pow(base, 1/exp); } }
 };
