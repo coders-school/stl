a5a4d379d51b6c6ee1cfedcaba19a8e3a60c5597
Andrzej Rafalski <anraf1001@gmail.com>
2020-09-01 11:25:30 +0200

    Added ingredients formatting

--- module4/homework/appendNewRecipe/AppendNewRecipe.cpp
+++ module4/homework/appendNewRecipe/AppendNewRecipe.cpp
@@ -1,13 +1,22 @@
 #include "AppendNewRecipe.hpp"
 
+#include <algorithm>
+#include <fstream>
+#include <iterator>
+#include <map>
+
+std::map<char, std::string> quantities{{'g', " gram"}, {'s', " szklanka(i)"}, {'m', "ml"}};
+
 bool AppendNewRecipe(std::vector<std::string> steps,
                      const std::list<std::string>& ingredients,
                      const std::deque<std::pair<size_t, char>>& amount) {
-    std::fstream recipes("recipes.txt", std::ios::app);
+    std::fstream recipes("recipes.txt", recipes.out | recipes.app);
 
     if (!recipes.is_open()) {
         return false;
     }
+
+    recipes << FormatRecipit(steps, ingredients, amount).str();
 
     recipes.close();
     return true;
@@ -15,6 +24,17 @@ bool AppendNewRecipe(std::vector<std::string> steps,
 
 std::vector<std::string> FormatIngredients(const std::list<std::string>& ingredients,
                                            const std::deque<std::pair<size_t, char>>& amount) {
+    std::vector<std::string> vecIngredients;
+
+    std::transform(ingredients.cbegin(), ingredients.cend(), amount.cbegin(),
+                   std::back_inserter(vecIngredients),
+                   [&](const auto& ingredient, const auto& amount) {
+                       std::stringstream sstr;
+                       sstr << amount.first << quantities[amount.second] << ' ' << ingredient;
+                       return sstr.str();
+                   });
+
+    return vecIngredients;
 }
 
 std::stringstream FormatRecipit(std::vector<std::string> steps,
