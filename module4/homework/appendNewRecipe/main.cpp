a4d2033dec4149d37348a6446ce1046270dc3c77
Andrzej Rafalski <anraf1001@gmail.com>
2020-09-01 12:11:30 +0200

    Main

--- module4/homework/appendNewRecipe/main.cpp
+++ module4/homework/appendNewRecipe/main.cpp
@@ -1,3 +1,20 @@
+#include "AppendNewRecipe.hpp"
+#include <fstream>
+#include <iostream>
+
 int main() {
+    std::vector<std::string> steps{"Wsypać do miski 20 gram cukru",
+                                   "Dorzucić 1 szklankę mąki",
+                                   "Dokładnie wymieszać",
+                                   "Nalać 40ml rumu do kieliszka",
+                                   "Wypić kieliszek",
+                                   "Wysypać zawartość miski"};
+    std::list<std::string> ingredients{"cukru", "mąki", "rumu"};
+    std::deque<std::pair<size_t, char>> amount{
+        {20, 'g'},
+        {1, 's'},
+        {40, 'm'}};
+
+    AppendNewRecipe(steps, ingredients, amount);
     return 0;
 }
