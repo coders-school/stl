aa58385299d270827a12b8c0ea91baaaa6dfebf9
WojtekMs <wojtek.atmo@gmail.com>
2020-09-02 20:55:20 +0200

    task done

--- module3/homework/inner_product/arithmeticAverage.cpp
+++ module3/homework/inner_product/arithmeticAverage.cpp
@@ -1,9 +1,22 @@
 #include "arithmeticAverage.hpp"
 
-double ArithmeticAverage(std::vector<int> lhs, std::vector<int> rhs) {
+#include <numeric>
+#include <cmath>
 
+double ArithmeticAverage(std::vector<int> lhs, std::vector<int> rhs) {
+    if (lhs.size() != rhs.size()) {
+        return 0;
+    }
+    auto sum = std::inner_product(begin(lhs), end(lhs), begin(rhs), 0, std::plus<int>(), std::plus<int>());
+    return static_cast<double>(sum) / (lhs.size() + rhs.size());
 }
 
 double Distance(std::vector<int> lhs, std::vector<int> rhs) {
-
+    if (lhs.size() != rhs.size()) {
+        return -1;
+    }
+    auto sumOfPowers = std::inner_product(begin(lhs), end(lhs), begin(rhs), 0, std::plus<int>(), [](int lhs, int rhs) {
+        return (rhs - lhs) * (rhs - lhs);
+    });
+    return sqrt(sumOfPowers);
 }
