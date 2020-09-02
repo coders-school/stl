173e714cc2d2bbb81867cff4c02d94e6c234535c
WojtekMs <wojtek.atmo@gmail.com>
2020-09-02 22:06:40 +0200

    more test cases for arithmetic average

--- module3/homework/inner_product/test.cpp
+++ module3/homework/inner_product/test.cpp
@@ -1,5 +1,6 @@
-#include <vector>
 #include <cmath>
+#include <tuple>
+#include <vector>
 
 #include "arithmeticAverage.hpp"
 #include "gtest/gtest.h"
@@ -8,12 +9,33 @@ bool cmp(double first, double second, double epsilon = 0.5) {
     return (fabs(first - second) < epsilon);
 }
 
-TEST(arithmeticAverageTest, ShouldCalculateArithmeticAverage) {
-    std::vector<int> first{-5, 4, 2, -5, 2, 4, 6, 7};
-    std::vector<int> second{3, 5, -2, 6, 7, -3, 6, 9};
-    EXPECT_TRUE(cmp(ArithmeticAverage(first, second), 2.875));
-    // Write more test case
-}
+class ArithmeticAverageTests
+    : public ::testing::TestWithParam<
+          std::tuple<std::vector<int>, std::vector<int>, double>> {};
+
+TEST_P(ArithmeticAverageTests, ShouldCalculateArithmethicAverage) {
+    auto [first, second, expectedAverage] = GetParam();
+    EXPECT_TRUE(cmp(ArithmeticAverage(first, second), expectedAverage))
+        << ArithmeticAverage(first, second) << ", " << expectedAverage;
+};
+
+INSTANTIATE_TEST_SUITE_P(
+    ShouldCalculateArithmeticAverage,
+    ArithmeticAverageTests,
+    testing::Values(
+        std::tuple<std::vector<int>, std::vector<int>, double>{
+            {-5, 4, 2, -5, 2, 4, 6, 7},
+            {3, 5, -2, 6, 7, -3, 6, 9},
+            2.875},
+        std::tuple<std::vector<int>, std::vector<int>, double>{{1, 2, 3, 4, 5},
+                                                               {1, 2, 3, 4, 5},
+                                                               3},
+
+        std::tuple<std::vector<int>, std::vector<int>, double>{{1, 2, 3},
+                                                               {1, 2, 3},
+                                                               2},
+        std::tuple<std::vector<int>, std::vector<int>, double>{{0}, {0}, 0}));
+
 
 TEST(arithmeticAverageTest, ShouldCalculateDistance) {
     std::vector<int> first{7, 4, 3};
