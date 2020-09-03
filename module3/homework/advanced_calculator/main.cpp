c936114f621252db1ef11ae5632268b7a96c324d
majkel84 <michal.sobotka.84@gmail.com>
2020-09-03 21:49:32 +0200

    advancedCalculator - while loop in main.cpp

--- module3/homework/advanced_calculator/main.cpp
+++ module3/homework/advanced_calculator/main.cpp
@@ -1,3 +1,39 @@
+#include <iostream>
+#include <string>
+
+#include "advancedCalculator.hpp"
+
+void printErrorCode(std::string s) {
+    double result;
+    switch (process(s, &result))
+    {
+    case ErrorCode::OK:
+        std::cout << "OK, result = " << result << '\n';
+        break;
+    case ErrorCode::BadCharacter:
+        std::cout << "Bad character" << '\n';
+    case ErrorCode::BadFormat:
+        std::cout << "Bad format" << '\n';
+    case ErrorCode::DivideBy0:
+        std::cout << "Divide by 0" << '\n';
+    case ErrorCode::ModuleOfNonIntegerValue:
+        std::cout << "Module of non integer value" << '\n';
+    case ErrorCode::SqrtOfNegativeNumber:
+        std::cout << "Sqrt on negative value" << '\n';
+    default:
+        break;
+    }
+}
+
 int main() {
+    std::string input {};
+    while (input != "q")
+    {
+        std::cout << "\033[2J\033[1;1H";
+        std::cout << "Enter equation: ";
+        std::getline(std::cin, input);
+        printErrorCode(input);
+    }
+    
     return 1;
 }
