dfe4c810b95d9b4eae71d0fb2a96057d42554899
majkel84 <michal.sobotka.84@gmail.com>
2020-09-03 23:13:08 +0200

    STL homework 4 - convert enum to string

--- module4/homework/appendNewRecipe/AppendNewRecipe.cpp
+++ module4/homework/appendNewRecipe/AppendNewRecipe.cpp
@@ -9,6 +9,16 @@ bool AppendNewRecipeconst(std::vector<std::string> steps,
 std::vector<std::string> FormatIngredients(const std::list<std::string>& ingredients,
                                            const std::deque<std::pair<size_t, char>>& amount) {
     std::vector<std::string> result {};
+    for (auto it = 0; it < ingredients.size(); it++) {
+        result[it] += amount[it].first;
+        result[it] += " ";
+        result[it] += convertEnumToString(amount[it].second);
+        result[it] += " ";
+        auto itIngredients = ingredients.begin();
+        std::advance(itIngredients, it);
+        result[it] += *itIngredients;
+        result[it] += ",";
+    }
     return result;
 }
 
@@ -17,4 +27,22 @@ std::stringstream FormatRecipit(std::vector<std::string> steps,
                                 const std::deque<std::pair<size_t, char>>& amount) {
     std::stringstream ss;
     return ss;
+}
+
+std::string convertEnumToString(char c) {
+    switch (c)
+    {
+    case 'g':
+        return "gram";
+        break;
+    case 'm':
+        return "mililitry";
+        break;
+    case 's':
+        return "szklanki";
+        break;
+    default:
+        break;
+    }
+    return "error";
 }
--- module4/homework/appendNewRecipe/AppendNewRecipe.hpp
+++ module4/homework/appendNewRecipe/AppendNewRecipe.hpp
@@ -1,7 +1,37 @@
+#pragma once
+
 #include <deque>
 #include <list>
+#include <map>
 #include <sstream>
+#include <string>
 #include <vector>
+
+/*struct VolumeClass
+{
+    enum class Volume : char {
+        g,
+        m,
+        s
+    };
+};
+
+const char* enumToString (VolumeClass::Volume e)
+{
+    const std::map<VolumeClass::Volume,const char*> volume {
+        { VolumeClass::Volume::g, "gram" },
+        { VolumeClass::Volume::m, "mililitry" },
+        { VolumeClass::Volume::s, "szklanki" }
+    };
+    auto it = volume.find(e);
+    return it == volume.end() ? "Out of range" : it->second;
+};*/
+
+enum class Volume {
+    g,
+    m,
+    s
+};
 
 bool AppendNewRecipeconst(std::vector<std::string> steps,
                      const std::list<std::string>& ingredients,
@@ -13,3 +43,5 @@ std::vector<std::string> FormatIngredients(const std::list<std::string>& ingredi
 std::stringstream FormatRecipit(std::vector<std::string> steps,
                                 const std::list<std::string>& ingredients,
                                 const std::deque<std::pair<size_t, char>>& amount);
+
+std::string convertEnumToString(char c);
