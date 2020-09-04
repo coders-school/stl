dc344bbbe6723b153dc4e037514e9f820d84ec3c
jakjan95 <jakubjanusz95@gmail.com>
2020-09-04 09:54:00 +0200

    Added CLI for Advanced Calculator

--- module3/homework/advanced_calculator/main.cpp
+++ module3/homework/advanced_calculator/main.cpp
@@ -1,13 +1,53 @@
 #include <iostream>
 
+#include <iomanip>
+#include <map>
+
 #include "advancedCalculator.hpp"
 
+const std::map<ErrorCode, std::string> errorNames{
+    {ErrorCode::OK, "ErrorCode::OK"},
+    {ErrorCode::BadCharacter, "ErrorCode::BadCharacter"},
+    {ErrorCode::BadFormat, "ErrorCode::BadFormat"},
+    {ErrorCode::DivideBy0, "ErrorCode::DivideBy0"},
+    {ErrorCode::SqrtOfNegativeNumber, "ErrorCode::SqrtOfNegativeNumber"},
+    {ErrorCode::ModuleOfNonIntegerValue, "ErrorCode::ModuleOfNonIntegerValue"}};
+
+void calculate(const std::string& input) {
+    double result = 0;
+    auto error = process(input, &result);
+    if (error != ErrorCode::OK) {
+        std::cout << "You did it wrong!!! - " << errorNames.at(error) << '\n';
+    } else {
+        std::cout << "Result of " << input << " is: " << result << '\n';
+    }
+}
+
+void informations() {
+    std::cout << "Valid oprations:\n"
+              << "Add [+]\n"
+              << "Multiply [*]\n"
+              << "Divide [/]\n"
+              << "Substract [-]\n"
+              << "Modulo [%] - Only intigers!\n"
+              << "Factorial [!]\n"
+              << "Power [^]\n"
+              << "Root [$]\n";
+    std::cout << "Sample valid command: 5 + 5 \n";
+    std::cout << "To quit type: quit\n";
+}
+
 int main() {
-    double a = 1;
-    ErrorCode err = process("5+5", &a);
-    ErrorCode err2 = process("3!", &a);
-    ErrorCode err3 = process("15*2!", &a);
-    ErrorCode err4 = process("15%2!", &a);
-    std::cout <<factorial(5)<<'\n';
+    std::cout << "*** Advanced calculator ****\n";
+    std::cout << "For more information type: help\n";
+    for (std::string line; std::cout << ">>", std::getline(std::cin, line);) {
+        if (line == "quit") {
+            break;
+        } else if (line == "help") {
+            informations();
+        } else {
+            calculate(line);
+        }
+    }
     return 0;
 }
