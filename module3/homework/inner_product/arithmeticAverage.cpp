8184779a3947b78cb5e223d30d6f3a28c847af9f
majkel84 <michal.sobotka.84@gmail.com>
2020-09-06 13:08:40 +0200

    homework3 - Inner Product - const iterators

--- module3/homework/inner_product/arithmeticAverage.cpp
+++ module3/homework/inner_product/arithmeticAverage.cpp
@@ -11,8 +11,8 @@ double ArithmeticAverage(std::vector<int> first, std::vector<int> second) {
 }
 
 double Distance(std::vector<int> first, std::vector<int> second) {
-    return sqrt(std::inner_product(first.begin(), first.end(), second.begin(), 0.0, std::plus<>(),
-                                   [](auto& el1, auto& el2) {
+    return sqrt(std::inner_product(first.cbegin(), first.cend(), second.cbegin(), 0.0, std::plus<>(),
+                                   [](const auto& el1, const auto& el2) {
                                        return pow(el1 - el2, 2);
                                        }));
 }
