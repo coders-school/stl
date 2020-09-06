f4079ddfd4d1ed14bdccc6249e20ed2eeb1b6c6b
Kuba C <jakub.cieplinski@outlook.com>
2020-09-06 10:16:57 +0200

    corrected a bug related to vector sizes

--- module3/homework/inner_product/arithmeticAverage.cpp
+++ module3/homework/inner_product/arithmeticAverage.cpp
@@ -5,6 +5,10 @@
 
 double ArithmeticAverage(std::vector<int>& v1, std::vector<int>& v2) {
     double size = v1.size() + v2.size();
+    if(v1.size() < v2.size()){
+        return std::inner_product(v2.begin(), v2.end(), v1.begin(), 0, std::plus<>(), std::plus<>()) / size;
+    }
+
     return std::inner_product(v1.begin(), v1.end(), v2.begin(), 0, std::plus<>(), std::plus<>()) / size;
 }
 
