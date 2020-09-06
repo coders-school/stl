008b352a9e886314236e39b3b11bfb11b7317a2d
WojtekMs <wojtek.atmo@gmail.com>
2020-09-06 12:53:07 +0200

    math operations added

--- module3/homework/advanced_calculator/advancedCalculator.cpp
+++ module3/homework/advanced_calculator/advancedCalculator.cpp
@@ -1,5 +1,20 @@
 #include "advancedCalculator.hpp"
 
+#include <cmath>
+#include <functional>
+#include <map>
+#include <variant>
+
 ErrorCode process(std::string input, double* out) {
-    return ErrorCode::OK;
+    std::variant<std::function<double(double, double)>, std::function<int(int, int)>, std::function<double(double)>>
+        functions;
+    std::map<char, decltype(functions)> basicMathOperations{
+        {'+', std::function<double(double, double)>([](double lhs, double rhs) { return lhs + rhs; })},
+        {'-', std::function<double(double, double)>([](double lhs, double rhs) { return lhs - rhs; })},
+        {'*', std::function<double(double, double)>([](double lhs, double rhs) { return lhs * rhs; })},
+        {'/', std::function<double(double, double)>([](double lhs, double rhs) { return lhs / rhs; })},
+        {'%', std::function<int(int, int)>([](int lhs, int rhs){ return lhs % rhs; })}, 
+        {'!', std::function<double(double)>([](double lhs){ return std::tgamma(lhs + 1); })}, 
+        {'^', std::function<double(double,double)>([](double lhs, double rhs){ return std::pow(lhs, rhs); })}, 
+        {'$', std::function<double(double,double)>([](double lhs, double rhs){ return std::pow(lhs, 1/rhs); })}};
 }
