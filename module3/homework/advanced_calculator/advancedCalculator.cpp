2dfd805ae2180c6f8e0fe331a050a394f4e7ef9c
LogarithmDerivative <mateusz.kowalski074@gmail.com>
2020-09-07 20:16:28 +0200

    moved include<functional> to .cpp file

--- module3/homework/advanced_calculator/advancedCalculator.cpp
+++ module3/homework/advanced_calculator/advancedCalculator.cpp
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <cmath>
+#include <functional>
 #include <iostream>
 #include <regex>
 
--- module3/homework/advanced_calculator/advancedCalculator.hpp
+++ module3/homework/advanced_calculator/advancedCalculator.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <functional>
 #include <map>
 #include <string>
 #include <vector>
