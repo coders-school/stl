e0bfb77aa25f88a11bef1356b17806b177a0f636
WojtekMs <wojtek.atmo@gmail.com>
2020-09-08 23:02:11 +0200

    isBadCharacter function added

--- module3/homework/advanced_calculator/advancedCalculator.cpp
+++ module3/homework/advanced_calculator/advancedCalculator.cpp
@@ -2,8 +2,16 @@
 
 #include <cmath>
 #include <functional>
+#include <iostream>
 #include <map>
+#include <regex>
 #include <variant>
+
+bool isBadCharacter(const std::string& input) {
+    std::string badCharacters{R"([^0-9\+\-\*\/\^\%\!\$\ ])"};
+    std::regex pattern(badCharacters);
+    return std::regex_search(input, pattern);
+}
 
 ErrorCode process(std::string input, double* out) {
     std::variant<std::function<double(double, double)>, std::function<int(int, int)>, std::function<double(double)>>
@@ -13,8 +21,10 @@ ErrorCode process(std::string input, double* out) {
         {'-', std::function<double(double, double)>([](double lhs, double rhs) { return lhs - rhs; })},
         {'*', std::function<double(double, double)>([](double lhs, double rhs) { return lhs * rhs; })},
         {'/', std::function<double(double, double)>([](double lhs, double rhs) { return lhs / rhs; })},
-        {'%', std::function<int(int, int)>([](int lhs, int rhs){ return lhs % rhs; })}, 
-        {'!', std::function<double(double)>([](double lhs){ return std::tgamma(lhs + 1); })}, 
-        {'^', std::function<double(double,double)>([](double lhs, double rhs){ return std::pow(lhs, rhs); })}, 
-        {'$', std::function<double(double,double)>([](double lhs, double rhs){ return std::pow(lhs, 1/rhs); })}};
+        {'%', std::function<int(int, int)>([](int lhs, int rhs) { return lhs % rhs; })},
+        {'!', std::function<double(double)>([](double lhs) { return std::tgamma(lhs + 1); })},
+        {'^', std::function<double(double, double)>([](double lhs, double rhs) { return std::pow(lhs, rhs); })},
+        {'$', std::function<double(double, double)>([](double lhs, double rhs) { return std::pow(lhs, 1 / rhs); })}};
+
+    return ErrorCode::OK;
 }
--- module3/homework/advanced_calculator/main.cpp
+++ module3/homework/advanced_calculator/main.cpp
@@ -1,3 +1,13 @@
+#include <cmath>
+#include <iostream>
+#include <string>
+
+#include "advancedCalculator.hpp"
+
 int main() {
-    
+    std::string text;
+    std::getline(std::cin, text);
+    double result = 0;
+    process(text, &result);
+    std::cout << result << '\n';    
 }
