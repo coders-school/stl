04be306800a4652be429de40261830b34e68def3
Vicx95 <wiktor.zyla31@gmail.com>
2020-09-08 18:01:43 +0200

    Implement distance function

--- module3/homework/inner_product/arithmeticAverage.cpp
+++ module3/homework/inner_product/arithmeticAverage.cpp
@@ -1,11 +1,20 @@
 #include "arithmeticAverage.hpp"
+#include <cmath>
 #include <functional>
 #include <numeric>
 
 double ArithmeticAverage(const std::vector<int>& first, const std::vector<int>& second) {
-    int sumOfElements = first.size() + second.size();
+    double sumOfElements = static_cast<double>(first.size() + second.size());
     auto average = std::inner_product(first.begin(), first.end(),
                                       second.begin(), 0,
                                       std::plus<int>(), std::plus<int>()) / sumOfElements;
     return average;
+}
+
+double Distance(const std::vector<int>& first, const std::vector<int>& second) {
+    auto distance = std::sqrt(std::inner_product(first.begin(), first.end(), second.begin(),
+                    0, std::plus<int>(),
+                    [](const auto& first, const auto& second) {
+                        return std::pow(std::abs(first - second), 2);
+                    }));
 }
