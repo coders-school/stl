ccc7922333159b003995dfc106b0c0def11dd5f3
LogarithmDerivative <mateusz.kowalski074@gmail.com>
2020-09-09 20:59:05 +0200

    main added

---
+++ module4/homework/appendNewRecipe/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#include "AppendNewRecipe.hpp"
+
+int main() {
+    std::vector<std::string> steps{"Wsypac do miski 20 gram cukru",
+                                   "Dorzucic 1 szklanke maki",
+                                   "Dokladnie wymieszac",
+                                   "Nalac 40ml rumu do kieliszka",
+                                   "Wypic kieliszek",
+                                   "Wysypac zawartosc miski"};
+    std::list<std::string> ingredients{"cukru", "maki", "rumu"};
+    std::deque<std::pair<size_t, char>> amount{
+        {20, 'g'},
+        {1, 's'},
+        {40, 'm'}};
+
+    std::cout << std::boolalpha << AppendNewRecipe(steps, ingredients, amount) << '\n';
+
+    return 0;
+}
