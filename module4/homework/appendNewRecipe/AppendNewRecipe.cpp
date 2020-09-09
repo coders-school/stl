e9c12e8a5ad614b94be6666678f951007afb1c32
LogarithmDerivative <mateusz.kowalski074@gmail.com>
2020-09-09 20:47:56 +0200

    AppendNewRecipe done

--- module4/homework/appendNewRecipe/AppendNewRecipe.cpp
+++ module4/homework/appendNewRecipe/AppendNewRecipe.cpp
@@ -1,6 +1,7 @@
 #include "AppendNewRecipe.hpp"
 
 #include <algorithm>
+#include <fstream>
 #include <iostream>
 #include <map>
 
@@ -10,20 +11,43 @@ std::map<char, std::string> units{
     {'m', "mililitrow"}};
 
 bool AppendNewRecipe(VecStr steps, const ListStr& ingredients, const DequeSizetChar& amount) {
+    auto newRecipe = (FormatRecipit(steps, ingredients, amount)).str();
+
+    std::fstream file("recipes.txt", file.app);
+    if (file.is_open()) {
+        file << newRecipe;
+        file.close();
+        return true;
+    }
+    
     return false;
 }
 
 std::stringstream FormatRecipit(VecStr steps, const ListStr& ingredients, const DequeSizetChar& amount) {
-    return {};
+    auto formatedIngredients = FormatIngredients(ingredients, amount);
+    std::stringstream result{};
+    result << "Skladniki:\n";
+    for(const auto& ingredient : formatedIngredients) {
+        result << ingredient << ',' << '\n';
+    }
+    result << "\nKroki:";
+    size_t i = 1;
+    for(const auto& step : steps) {
+        result << '\n' << i << ") " << step << '.';
+        i++;
+    }
+    result << "\n___________________________________\n";
+    
+    return result;
 }
 
 VecStr FormatIngredients(const ListStr& ingredients, const DequeSizetChar& amount) {
     VecStr result{};
-    size_t  i = 0;
+    size_t i = 0;
     for (const auto& ingredient : ingredients) {
-        std::ostringstream oss{};
-        oss << amount[i].first << ' ' << units[amount[i].second] << ' ' << ingredient;
-        result.push_back(oss.str());
+        std::stringstream ss{};
+        ss << amount[i].first << ' ' << units[amount[i].second] << ' ' << ingredient;
+        result.push_back(ss.str());
         i++;
     }
     
