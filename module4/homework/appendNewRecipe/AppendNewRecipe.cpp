648c033ccc34cc615be6ee44d83209ff99c22d91
WojtekMs <wojtek.atmo@gmail.com>
2020-09-11 23:08:52 +0200

    file opening mode changed to app

--- module4/homework/appendNewRecipe/AppendNewRecipe.cpp
+++ module4/homework/appendNewRecipe/AppendNewRecipe.cpp
@@ -1,10 +1,18 @@
 #include "AppendNewRecipe.hpp"
 
 #include <algorithm>
+#include <fstream>
 
 bool AppendNewRecipe(std::vector<std::string> steps,
                      const std::list<std::string>& ingredients,
                      const std::deque<std::pair<size_t, char>>& amount) {
+    std::ofstream file("recipes.txt", std::ios::app);
+    if (file.is_open()) {
+        auto text = FormatRecipit(steps, ingredients, amount).str();
+        file << text;
+        file.close();
+        return true;
+    }
     return false;
 }
 
