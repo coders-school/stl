d01889dc8698cc856e235c46d83cc8a43b99bb08
WojtekMs <wojtek.atmo@gmail.com>
2020-09-11 21:08:32 +0200

    configuring task

---
+++ module4/homework/appendNewRecipe/AppendNewRecipe.cpp
@@ -0,0 +1,18 @@
+#include "AppendNewRecipe.hpp"
+
+bool AppendNewRecipe(std::vector<std::string> steps,
+                     const std::list<std::string>& ingredients,
+                     const std::deque<std::pair<size_t, char>>& amount) {
+    return false;
+}
+
+std::vector<std::string> FormatIngredients(const std::list<std::string>& ingredients,
+                                           const std::deque<std::pair<size_t, char>>& amount) {
+    return {};
+}
+
+std::stringstream FormatRecipit(std::vector<std::string> steps,
+                                const std::list<std::string>& ingredients,
+                                const std::deque<std::pair<size_t, char>>& amount) {
+    return {};
+}
