7d5466369ce1dfa86edb6473be005e97ad2fed6c
KS <krzysiek.szytko@gmail.com>
2020-09-11 12:14:38 +0200

    Main created

---
+++ module3/homework/advanced_calculator/main.cpp
@@ -0,0 +1,11 @@
+#include "advancedCalculator.hpp"
+#include <string>
+
+
+int main(){
+    double out;
+    //process("5 + 10", &out);
+    std::string s("5");
+    out = std::stod(s);
+    return 0;
+}
