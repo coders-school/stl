68709a5b901ef8c18628a3aacb35276103e4c37d
WojtekMs <wojtek.atmo@gmail.com>
2020-09-11 21:19:35 +0200

    FormatIngredients definition added

--- module4/homework/appendNewRecipe/AppendNewRecipe.cpp
+++ module4/homework/appendNewRecipe/AppendNewRecipe.cpp
@@ -1,5 +1,7 @@
 #include "AppendNewRecipe.hpp"
 
+#include <algorithm>
+
 bool AppendNewRecipe(std::vector<std::string> steps,
                      const std::list<std::string>& ingredients,
                      const std::deque<std::pair<size_t, char>>& amount) {
@@ -8,7 +10,20 @@ bool AppendNewRecipe(std::vector<std::string> steps,
 
 std::vector<std::string> FormatIngredients(const std::list<std::string>& ingredients,
                                            const std::deque<std::pair<size_t, char>>& amount) {
-    return {};
+    if (ingredients.size() != amount.size()) {
+        return {};
+    }
+    std::vector<std::string> formattedIngredients(ingredients.size());
+    std::transform(begin(ingredients),
+                   end(ingredients),
+                   begin(amount),
+                   begin(formattedIngredients),
+                   [](const auto& ingredient, const auto& pair) 
+                   {
+                       auto [amount, unit] = pair;
+                       return std::to_string(amount) + unit + ' ' + ingredient;
+                   });
+    return formattedIngredients;
 }
 
 std::stringstream FormatRecipit(std::vector<std::string> steps,
