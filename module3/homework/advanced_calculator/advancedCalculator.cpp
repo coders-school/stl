d8e402aec1429d58f837dfa1084ac43a6f1d3287
KS <krzysiek.szytko@gmail.com>
2020-09-11 13:54:12 +0200

    Clean advencedCalculator file

--- module3/homework/advanced_calculator/advancedCalculator.cpp
+++ module3/homework/advanced_calculator/advancedCalculator.cpp
@@ -1,79 +1,9 @@
 #include "advancedCalculator.hpp"
-#include <algorithm>
-#include <stdexcept>
-#include <regex>
+#include "formulaParser.hpp"
+#include "calculatorExceptions.hpp"
+
 #include <iostream>
 
-
-void TextParser::parse(){
-    searchElements();
-    readElements();
-    validateElements();
-}
-
-void TextParser::searchElements(){
-    std::regex formulaPattern(NUMBER_PATTERN + SYMBOL_PATTERN + NUMBER_PATTERN);
-    bool foundMatch = std::regex_search(text_, elements_, formulaPattern);
-
-    if(!foundMatch)
-        throw BadFormatException();
-}
-
-void TextParser::readElements(){
-    match = getElement(0);
-    firstNumber = getElement(1);
-    symbol = getElement(2);
-    secondNumber = getElement(3);
-}
-
-std::string TextParser::getElement(size_t number){
-    return elements_[number].str();
-}
-
-bool TextParser::validateElements(){
-    if(match != text_ || firstNumber.empty() || symbol.empty())
-        throw BadFormatException();
-}
-
-void TextParser::castToFormula(){
-    formula_.first = std::stod(firstNumber);
-    formula_.second = std::stod(secondNumber);
-    formula_.symbol = symbol[0]; 
-}
-
-
-
-void FormulaParser::validateCharacters(){
-    if(!hasValidCharacters())
-        throw BadCharacterException();
-}
-
-bool FormulaParser::hasValidCharacters(){
-    return std::all_of(text_.begin(), text_.end(), 
-        [&](char symbol){ return symbolIsValid(symbol); });
-};
-
-bool FormulaParser::symbolIsValid(char symbol){
-    return VALID_CHARACTERS.find(symbol) != std::string::npos;
-}
-
-void FormulaParser::parseText(){
-    trimSpaces();
-    parseTextIntoFormula();
-
-    if(formula.second != 0 && formula.symbol == '!')
-        throw BadFormatException();
-
-}
-
-void FormulaParser::trimSpaces(){
-    text_.erase(std::remove(text_.begin(), text_.end(), ' '), text_.end());
-}
-
-void FormulaParser::parseTextIntoFormula(){
-    TextParser tp(text_);
-    formula = tp.getFormula();
-}
 
 ErrorCode process(std::string input, double* out){
     try{
--- module3/homework/advanced_calculator/advancedCalculator.hpp
+++ module3/homework/advanced_calculator/advancedCalculator.hpp
@@ -1,13 +1,6 @@
+#pragma once
+
 #include <string>
-#include <regex>
-
-struct BadFormatException : public std::invalid_argument{
-	BadFormatException() : std::invalid_argument("BadFormat Exception"){};
-};
-
-struct BadCharacterException : public std::invalid_argument{
-	BadCharacterException() : std::invalid_argument("BadCharacter Exception"){};
-};
 
 enum class ErrorCode{
     OK,
@@ -20,74 +13,6 @@ enum class ErrorCode{
 
 ErrorCode process(std::string input, double* out);
 
-
-
-
-struct Formula{
-    double first;
-    double second;
-    char symbol;
-};
-
-class TextParser{
-public:
-    TextParser(std::string text): text_(text){
-        parse();
-    }
-
-    Formula getFormula() const { return formula_; }
-
-private:
-    void parse();
-    void searchElements();
-    void readElements();
-    std::string getElement(size_t number);
-    bool validateElements();
-
-    void castToFormula();
-
-
-private:
-    std::string match;
-    std::string symbol;
-    std::string firstNumber;
-    std::string secondNumber;
-
-    Formula formula_;
-
-    std::string text_;
-    std::smatch elements_;
-
-    const std::string VALID_SYMBOLS = "-+/*%!^$";
-    const std::string NUMBER_PATTERN = "(-?\\d*\\.?\\d*)";
-    const std::string SYMBOL_PATTERN = "([" + VALID_SYMBOLS + "])";
-};
-
-
-class FormulaParser{
-public:
-    Formula formula;
-
-    FormulaParser(std::string text) : text_(text){
-        validateCharacters();
-        parseText();
-    }
-
-private:
-    void validateCharacters();
-        bool hasValidCharacters();
-        bool symbolIsValid(char symbol);
-
-    void parseText();
-        void trimSpaces();
-        void parseTextIntoFormula();
-  
-
-
-private:
-    std::string text_;
-    const std::string VALID_CHARACTERS = "-+/*%!^$., 0123456789";
-};
 
 class FormulaProcess{
 
