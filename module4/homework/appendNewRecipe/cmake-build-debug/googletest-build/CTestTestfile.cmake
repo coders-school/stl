949bbc43b1f34703ff0dfcac490b504b2078e02f
McKucia <macikucia@onet.pl>
2020-09-13 17:44:01 +0200

    Delete cmake from clion, fix test, add two func to format

--- module4/homework/appendNewRecipe/cmake-build-debug/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dell/stl/module4/homework/appendNewRecipe/cmake-build-debug/googletest-src
-# Build directory: /home/dell/stl/module4/homework/appendNewRecipe/cmake-build-debug/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
