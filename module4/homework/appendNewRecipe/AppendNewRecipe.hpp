ba380ceff2e4d476f232f79513ace8e0fb2433b9
Karol Dulemba <kdulemba@gmail.com>
2020-09-13 11:28:36 +0200

    new lines added

--- module4/homework/appendNewRecipe/AppendNewRecipe.hpp
+++ module4/homework/appendNewRecipe/AppendNewRecipe.hpp
@@ -17,3 +17,4 @@ std::stringstream
 FormatRecipit(std::vector<std::string> steps,
               const std::list<std::string> &ingredients,
               const std::deque<std::pair<size_t, char>> &amount);
+              
