0b05968c423d9c6320aa9066bbd39bf15fb664e8
Marcin N <19634288+niemarcin@users.noreply.github.com>
2020-09-16 01:37:09 +0200

    more test cases covered for arithmeticAverage

--- module3/homework/inner_product/arithmeticAverage.cpp
+++ module3/homework/inner_product/arithmeticAverage.cpp
@@ -3,12 +3,45 @@
 #include <algorithm>
 #include <functional>
 #include <numeric>
+#include <stdexcept>
 #include <vector>
 
 double ArithmeticAverage(std::vector<int> first, std::vector<int> second) {
-    auto sum = std::inner_product(first.begin(), first.end(), second.begin(), 0,
-                       std::plus<>(), std::plus<>());
-    return static_cast<double>(sum) / (first.size() + second.size());
+    std::vector<int> longer;
+    std::vector<int> shorterOrEqual;
+
+    if (first.size() == 0 && second.size() == 0) {
+        throw std::invalid_argument("At least one vector should not be empty");
+    }
+    else if (first.size() == 0) {
+        longer.reserve(second.size());
+        longer = second;
+        shorterOrEqual.reserve(second.size());
+        shorterOrEqual = second;
+    }
+    else if (second.size() == 0) {
+        longer.reserve(first.size());
+        longer = first;
+        shorterOrEqual.reserve(first.size());
+        shorterOrEqual = first;
+    }
+    else if (first.size() >= second.size()) {
+        longer.reserve(first.size());
+        longer = first;
+        shorterOrEqual.reserve(second.size());
+        shorterOrEqual = second;
+    }
+    else {
+        longer.reserve(second.size());
+        longer = second;
+        shorterOrEqual.reserve(first.size());
+        shorterOrEqual = first;
+    }
+
+    auto sum = std::inner_product(longer.begin(), longer.end(), shorterOrEqual.begin(), 0,
+                                 std::plus<>(), std::plus<>());
+
+    return static_cast<double>(sum) / (longer.size() + shorterOrEqual.size());
 }
 
 double Distance(std::vector<int> first, std::vector<int> second) {
--- module3/homework/inner_product/test.cpp
+++ module3/homework/inner_product/test.cpp
@@ -11,9 +11,24 @@ bool cmp(double first, double second, double epsilon = 0.5) {
 TEST(arithmeticAverageTest, ShouldCalculateArithmeticAverage) {
     std::vector<int> first{-5, 4, 2, -5, 2, 4, 6, 7};
     std::vector<int> second{3, 5, -2, 6, 7, -3, 6, 9};
-    // EXPECT_TRUE(cmp(ArithmeticAverage(first, second), 2.875));
-    EXPECT_EQ(ArithmeticAverage(first, second), 2.875);
-    // Write more test case
+    EXPECT_TRUE(cmp(ArithmeticAverage(first, second), 2.875));
+}
+
+TEST(arithmeticAverageTest, ShouldCalculateArithmeticAverageWhenFirstVectorIsLonger) {
+    std::vector<int> first{2, 3, 4};
+    std::vector<int> second{5, 6};
+    EXPECT_TRUE(cmp(ArithmeticAverage(first, second), 4));
+}
+
+TEST(arithmeticAverageTest, ShouldCalculateArithmeticAverageWhenFirstVectorIsShorter) {
+    std::vector<int> first{2, 3};
+    std::vector<int> second{4, 5, 6};
+    EXPECT_TRUE(cmp(ArithmeticAverage(first, second), 4));
+}
+TEST(arithmeticAverageTest, ShouldCalculateArithmeticAverageWhenFirstVectorIsEmpty) {
+    std::vector<int> first{};
+    std::vector<int> second{6, 10};
+    EXPECT_TRUE(cmp(ArithmeticAverage(first, second), 8));
 }
 
 // TEST(arithmeticAverageTest, ShouldCalculateDistance) {
