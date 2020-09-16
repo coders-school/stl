4cb7fa5ebbd3793b1e846ab56137a27141e0ae4e
Marcin N <19634288+niemarcin@users.noreply.github.com>
2020-09-16 22:48:53 +0200

    both vectors need to be the same size

--- module3/homework/inner_product/arithmeticAverage.cpp
+++ module3/homework/inner_product/arithmeticAverage.cpp
@@ -8,46 +8,22 @@
 #include <vector>
 
 double ArithmeticAverage(std::vector<int> first, std::vector<int> second) {
-    std::vector<int> longer;
-    std::vector<int> shorterOrEqual;
-
-    if (first.size() == 0 && second.size() == 0) {
-        throw std::invalid_argument("At least one vector should not be empty");
+    if (first.size() == 0 || second.size() == 0) {
+        throw std::invalid_argument("Vectors cannot be empty");
     }
-    else if (first.size() == 0) {
-        longer.reserve(second.size());
-        longer = second;
-        shorterOrEqual.reserve(second.size());
-        shorterOrEqual = second;
+    else if (first.size() != second.size()) {
+        throw std::invalid_argument("Both vectors should be the same size");
     }
-    else if (second.size() == 0) {
-        longer.reserve(first.size());
-        longer = first;
-        shorterOrEqual.reserve(first.size());
-        shorterOrEqual = first;
-    }
-    else if (first.size() >= second.size()) {
-        longer.reserve(first.size());
-        longer = first;
-        shorterOrEqual.reserve(second.size());
-        shorterOrEqual = second;
-    }
-    else {
-        longer.reserve(second.size());
-        longer = second;
-        shorterOrEqual.reserve(first.size());
-        shorterOrEqual = first;
-    }
-
-    auto sum = std::inner_product(longer.begin(), longer.end(), shorterOrEqual.begin(), 0,
+    
+    auto sum = std::inner_product(first.begin(), first.end(), second.begin(), 0,
                                  std::plus<>(), std::plus<>());
 
-    return static_cast<double>(sum) / (longer.size() + shorterOrEqual.size());
+    return static_cast<double>(sum) / (first.size() + second.size());
 }
 
 double Distance(std::vector<int> first, std::vector<int> second) {
     if (first.size() == 0 || second.size() == 0) {
-        throw std::invalid_argument("At least one vector should not be empty");
+        throw std::invalid_argument("Vectors cannot be empty");
     }
     else if (first.size() != second.size()) {
         throw std::invalid_argument("Both vectors should be the same size");
--- module3/homework/inner_product/test.cpp
+++ module3/homework/inner_product/test.cpp
@@ -28,8 +28,7 @@ class distanceExceptionTestFixture : public ::testing::TestWithParam<ExceptionTe
 
 TEST_P(averageCalculationTestFixture, ShouldCalculateArithmeticAverage) {
     auto [first, second, result] = GetParam();
-    EXPECT_EQ(ArithmeticAverage(first, second), result);
-    // EXPECT_TRUE(cmp(ArithmeticAverage(first, second), result));
+    EXPECT_TRUE(cmp(ArithmeticAverage(first, second), result));
 }
 
 TEST_P(averageExceptionTestFixture, ArithmeticAverageShouldThrowInvalidArgument) {
@@ -51,29 +50,30 @@ INSTANTIATE_TEST_CASE_P(
         averageCalculationTest,
         averageCalculationTestFixture,
         ::testing::Values(CalculationTuple{{-5, 4, 2, -5, 2, 4, 6, 7} , {3, 5, -2, 6, 7, -3, 6, 9} , 2.875},
-                          CalculationTuple{{2, 3, 4}                  , {5, 6}                     , 4},
-                          CalculationTuple{{2, 3}                     , {4, 5, 6}                  , 4},
-                          CalculationTuple{{}                         , {6, 10}                    , 8},
-                          CalculationTuple{{6, 10}                    , {}                         , 8}
+                          CalculationTuple{{2, 0, 3}                  , {1, 10, -2}                , 2.333}
         ));
 
 INSTANTIATE_TEST_CASE_P(
         distanceCalculationTest,
         distanceCalculationTestFixture,
-        ::testing::Values(CalculationTuple{{7, 4, 3} , {17, 6, 2} , 10.247},
-                          CalculationTuple{{1, 2, 3, 4, 5} , {-1, 2, -3, 4, -5} , 11.832}
+        ::testing::Values(CalculationTuple{{7, 4, 3}        , {17, 6, 2}            , 10.247},
+                          CalculationTuple{{1, 2, 3, 4, 5}  , {-1, 2, -3, 4, -5}    , 11.832}
         ));
 
 INSTANTIATE_TEST_CASE_P(
         averageExceptionTest,
         averageExceptionTestFixture,
-        ::testing::Values(ExceptionTestTuple{{}, {}}
+        ::testing::Values(ExceptionTestTuple{{}             , {}             },
+                          ExceptionTestTuple{{2, 3, 4}      , {5, 6}         },
+                          ExceptionTestTuple{{2, 3}         , {4, 5, 6}      },
+                          ExceptionTestTuple{{}             , {6, 10}        },
+                          ExceptionTestTuple{{6, 10}        , {}             }
         ));
 
 INSTANTIATE_TEST_CASE_P(
         distanceExceptionTest,
         distanceExceptionTestFixture,
-        ::testing::Values(ExceptionTestTuple{{}, {}},
-                          ExceptionTestTuple{{}, {1, 2, 3}},
-                          ExceptionTestTuple{{4, 5}, {1, 2, 3}}
+        ::testing::Values(ExceptionTestTuple{{}         , {}       },
+                          ExceptionTestTuple{{}         , {1, 2, 3}},
+                          ExceptionTestTuple{{4, 5}     , {1, 2, 3}}
         ));
