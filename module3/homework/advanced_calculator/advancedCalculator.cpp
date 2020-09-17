2c11210fa64060070917915ded2d4536582f422d
m-kop <m.kopertowski@epicspace.eu>
2020-09-17 13:29:26 +0200

    add const map with operations

--- module3/homework/advanced_calculator/advancedCalculator.cpp
+++ module3/homework/advanced_calculator/advancedCalculator.cpp
@@ -1,7 +1,38 @@
 #include "advancedCalculator.hpp"
 
+#include <functional>
+#include <map>
+#include <cmath>
+#include <iostream>
+
+const std::map<char, std::function<double(double,double)>> operations {
+    {'+', std::plus<>()},
+    {'-', std::minus<>()},
+    {'*', std::multiplies<>()},
+    {'/', std::divides<>()},
+    {'%', std::modulus<int>()},
+    {'^', [](auto base, auto exponent){ return pow(base, exponent); }},
+    {'$', [](auto value, auto root){ return pow(value, 1.0 / root); }},
+    {'!', [](auto value, auto notUsed){ return value <=0 ? 1.0 : std::tgamma(value + 1.0); }}
+};
+
 ErrorCode process(const std::string& input, double* result)
 {
-    // ToDo
-    return ErrorCode::OK;
+    char operation;
+
+
+    auto lhs = 7;
+    auto rhs = 3;
+
+    if ((operation == '/') and (rhs == 0)) {
+        return ErrorCode::DivideBy0;
+    } else if ((operation == '$') and (lhs < 0)) {
+        return ErrorCode::SqrtOfNagativeNumber;
+    }
+    else if (auto it = operations.find(operation); it != operations.end()) {
+        *result = it->second(lhs, rhs);
+        return ErrorCode::OK;
+    } else {
+        return ErrorCode::BadCharacter;
+    }
 }
