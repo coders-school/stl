79ebad7e3e678e1489598618d1a2b617bc3b6999
m-kop <m.kopertowski@epicspace.eu>
2020-09-17 14:57:10 +0200

    add main() implementation

--- module3/homework/advanced_calculator/main.cpp
+++ module3/homework/advanced_calculator/main.cpp
@@ -1,6 +1,23 @@
 #include "advancedCalculator.hpp"
 
+#include <iostream>
+
 int main()
 {
+    std::string line;
+    double result;
+
+    std::cout << "Advanced Calculator." << std::endl;
+
+    while (1) {
+        std::cout << "Enter expression to calculate (or q to exit): ";
+        std::getline(std::cin, line);
+        if (line == "q") {
+            return 0;
+        }
+        process(line, &result);
+        std::cout << "Result: " << result << std::endl;
+    }
+
     return 0;   
 }
