53ab12b53f4f06171ca25a93663d0f08741c16c7
Marcinati <marcin.kapusta.pk@gmail.com>
2020-09-18 20:29:45 +0200

    error code

--- module3/homework/advanced_calculator/advancedCalculator.hpp
+++ module3/homework/advanced_calculator/advancedCalculator.hpp
@@ -1,3 +1,12 @@
 #pragma once
 
+enum class ErrorCode {
+    Ok,
+    BadCharacter,
+    BadFormat,
+    DivideBy0,
+    SqrtOfNegativeNumber,
+    ModuleOfNonIntegerValue,
+};
+
 void startCalculate();
