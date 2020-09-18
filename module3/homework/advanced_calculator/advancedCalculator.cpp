f15a62833487156578e68784dacba0d9091126ac
Marcinati <marcin.kapusta.pk@gmail.com>
2020-09-18 22:22:39 +0200

    operations

--- module3/homework/advanced_calculator/advancedCalculator.cpp
+++ module3/homework/advanced_calculator/advancedCalculator.cpp
@@ -1,4 +1,5 @@
 #include "advancedCalculator.hpp"
+#include <cmath>
 #include <iostream>
 
 void startCalculate() {
@@ -16,9 +17,20 @@ void calculate() {
 }
 
 void printGoodByeScreen() {
-    std::cout << "Thank you for using our application. \n";
+    std::cout << "Thank you for using our services. \n";
 }
 
 ErrorCode process(std::string input, double* out) {
     return ErrorCode::OK;
 }
+
+const std::map<std::string, std::function<double(double, double)>> operations{
+    {"+", std::plus<double>()},
+    {"*", std::multiplies<double>()},
+    {"/", std::divides<double>()},
+    {"-", std::minus<double>()},
+    {"%", std::modulus<int>()},
+    {"!", [](double firstNum, double secNum) { return firstNum <= 0 ? 1.0 : std::tgamma(firstNum + 1.0 + secNum - secNum); }},
+    {"^", [](auto firstNum, auto secNum) { return std::pow(firstNum, secNum); }},
+    {"$", [](auto firstNum, auto secNum) { return pow(firstNum, 1.0 / secNum); }}
+};
--- module3/homework/advanced_calculator/advancedCalculator.hpp
+++ module3/homework/advanced_calculator/advancedCalculator.hpp
@@ -1,4 +1,6 @@
 #pragma once
+#include <functional>
+#include <map>
 #include <string>
 
 enum class ErrorCode {
