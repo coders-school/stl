6224c9e21db17d2ba4a83dee842398398a859228
KS <krzysiek.szytko@gmail.com>
2020-09-18 21:31:54 +0200

    Add average and distance

---
+++ module3/homework/inner_product/arithmeticAverage.cpp
@@ -0,0 +1,18 @@
+#include "arithmeticAverage.hpp"
+
+#include <math.h>
+#include <numeric>
+
+double ArithmeticAverage(std::vector<int> & first, std::vector<int>  & second){
+    double result = std::inner_product(first.begin(), first.end() ,second.begin(),0
+                                    , std::plus<>(), std::plus<>());
+    return result / (first.size() * 2);
+}
+
+double Distance(std::vector<int> & first, std::vector<int>  & second){
+    double result = std::inner_product(first.begin(), first.end() ,second.begin(),0,
+                                    std::plus<>(),
+                                    [](double a, double b){ return pow(a - b, 2);}
+    );
+    return sqrt(result);
+}
---
+++ module3/homework/inner_product/arithmeticAverage.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <vector>
+
+double ArithmeticAverage(std::vector<int> & first, std::vector<int>  & second);
+double Distance(std::vector<int> & first, std::vector<int>  & second);
