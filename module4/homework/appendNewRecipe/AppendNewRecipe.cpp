4a5811b6ec3c6345f5e79dde7bac69954bf88341
m-kop <m.kopertowski@epicspace.eu>
2020-09-18 21:47:42 +0200

    add initial commit

---
+++ module4/homework/appendNewRecipe/AppendNewRecipe.cpp
@@ -0,0 +1,24 @@
+#include "AppendNewRecipe.hpp"
+
+bool AppendNewRecipe(std::vector<std::string> steps,
+                     const std::list<std::string>& ingredients,
+                     const std::deque<std::pair<size_t, char>>& amount)
+{
+    // ToDo
+    return false;
+}
+
+std::vector<std::string> FormatIngredients(const std::list<std::string>& ingredients,
+                                           const std::deque<std::pair<size_t, char>>& amount)
+{
+    // ToDo
+    return {};
+}
+
+std::stringstream FormatRecipit(std::vector<std::string> steps,
+                                const std::list<std::string>& ingredients,
+                                const std::deque<std::pair<size_t, char>>& amount)
+{
+    // ToDo
+    return {};
+}
---
+++ module4/homework/appendNewRecipe/AppendNewRecipe.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <deque>
+#include <list>
+#include <sstream>
+#include <string>
+#include <vector>
+
+bool AppendNewRecipe(std::vector<std::string> steps,
+                     const std::list<std::string>& ingredients,
+                     const std::deque<std::pair<size_t, char>>& amount);
+std::vector<std::string> FormatIngredients(const std::list<std::string>& ingredients,
+                                           const std::deque<std::pair<size_t, char>>& amount);
+std::stringstream FormatRecipit(std::vector<std::string> steps,
+                                const std::list<std::string>& ingredients,
+                                const std::deque<std::pair<size_t, char>>& amount);
