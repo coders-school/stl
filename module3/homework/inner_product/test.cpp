7c97323b24d962e346320f0e03225594a29dc560
Vicx95 <wiktor.zyla31@gmail.com>
2020-09-19 19:56:00 +0200

    add more test case

--- module3/homework/inner_product/test.cpp
+++ module3/homework/inner_product/test.cpp
@@ -12,12 +12,31 @@ TEST(arithmeticAverageTest, ShouldCalculateArithmeticAverage) {
     std::vector<int> first{-5, 4, 2, -5, 2, 4, 6, 7};
     std::vector<int> second{3, 5, -2, 6, 7, -3, 6, 9};
     EXPECT_TRUE(cmp(ArithmeticAverage(first, second), 2.875));
-    // Write more test case
+    first = {1, 2, 3, 4};
+    second = {1, 2, 3, 4};
+    ASSERT_EQ(ArithmeticAverage(first, second), 2.5);
+    EXPECT_TRUE(cmp(ArithmeticAverage(first, second), 2.875));
+
+    first = {-5, 4, 2, -5, 2, 4, 6, 7};
+    second = {3, 5, -2, 6, 7, -3, 6, 9};
+    ASSERT_EQ(ArithmeticAverage(first, second), 2.875);
+    EXPECT_TRUE(cmp(ArithmeticAverage(first, second), 2.875));
 }
 
 TEST(arithmeticAverageTest, ShouldCalculateDistance) {
     std::vector<int> first{7, 4, 3};
     std::vector<int> second{17, 6, 2};
     EXPECT_TRUE(cmp(Distance(first, second), 10.247));
-    // Write more test case
+
+    first = {3, 2};
+    second = {2, 9};
+    EXPECT_TRUE(cmp(Distance(first, second), 7.280));
+
+    first = {0, 0, 0};
+    second = {1, 1, 1};
+    EXPECT_TRUE(cmp(Distance(first, second), 1.732));
+
+    first = {2, 3, 1};
+    second = {3, 2, 4};
+    EXPECT_TRUE(cmp(Distance(first, second), 3.316));
 }
