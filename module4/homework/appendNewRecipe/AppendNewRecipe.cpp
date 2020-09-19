4563adc91b44a90fa2d52fa4ef0bf1e06815c47e
m-kop <m.kopertowski@epicspace.eu>
2020-09-19 13:04:57 +0200

    add AppendNewRecipe implementation

--- module4/homework/appendNewRecipe/AppendNewRecipe.cpp
+++ module4/homework/appendNewRecipe/AppendNewRecipe.cpp
@@ -1,10 +1,29 @@
 #include "AppendNewRecipe.hpp"
 
+#include <fstream>
+#include <iostream>
+
+constexpr char filename[] = "recipes.txt";
+constexpr char ingredientsHeader[] = "Skladniki:";
+constexpr char stepsHeader[] = "Kroki:";
+constexpr char gram[] = "gram";
+constexpr char glass[] = "szklanka(i)";
+constexpr char ml[] = "mililitrow";
+
 bool AppendNewRecipe(std::vector<std::string> steps,
                      const std::list<std::string>& ingredients,
                      const std::deque<std::pair<size_t, char>>& amount)
 {
-    // ToDo
+    std::fstream file(filename, file.out | file.app);
+    try {
+        if (file.is_open()) {
+            file << FormatRecipit(steps, ingredients, amount).str();
+            file.close();
+            return true;
+        }
+    } catch (const std::fstream::failure& e) {
+        std::cerr << "Cannot open file: " << e.what() << std::endl;
+    }
     return false;
 }
 
