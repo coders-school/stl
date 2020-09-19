c4ea260f067dba3f181d46bccfbd6540e874c0df
Marcinati <65040642+Marcinati@users.noreply.github.com>
2020-09-19 13:16:43 +0200

    Update advancedCalculator.cpp

--- module3/homework/advanced_calculator/advancedCalculator.cpp
+++ module3/homework/advanced_calculator/advancedCalculator.cpp
@@ -61,7 +61,7 @@ void printGoodByeScreen() {
 
 bool isBadCharacter(std::string input) {
     return (std::any_of(input.begin(), input.end(), [](auto el) { 
-        return !(std::isdigit(el) || std::iscomma(el) || operations.find(el) != operations.end(); }));
+        return !(std::isdigit(el) || std::isnormal(el) || operations.find(el) != operations.end()); }));
 }
 
 bool isBadFormat(std::string input) {
@@ -87,13 +87,13 @@ ErrorCode process(std::string input, double* out) {
     return ErrorCode::OK;
 }
 
-const std::map<std::string, std::function<double(double, double)>> operations {
-    {"+", std::plus<double>()},
-    {"*", std::multiplies<double>()},
-    {"/", std::divides<double>()},
-    {"-", std::minus<double>()},
-    {"%", std::modulus<int>()},
-    {"!", [](double firstNum, double secNum) { return firstNum <= 0 ? 1.0 : std::tgamma(firstNum + 1.0 + secNum - secNum); }},
-    {"^", [](auto firstNum, auto secNum) { return std::pow(firstNum, secNum); }},
-    {"$", [](auto firstNum, auto secNum) { return pow(firstNum, 1.0 / secNum); }}
+const std::map<char, std::function<double(double, double)>> operations {
+    {'+', std::plus<double>()},
+    {'*', std::multiplies<double>()},
+    {'/', std::divides<double>()},
+    {'-', std::minus<double>()},
+    {'%', std::modulus<int>()},
+    {'!', [](double firstNum, double secNum) { return firstNum <= 0 ? 1.0 : std::tgamma(firstNum + 1.0 + secNum - secNum); }},
+    {'^', [](auto firstNum, auto secNum) { return std::pow(firstNum, secNum); }},
+    {'$', [](auto firstNum, auto secNum) { return pow(firstNum, 1.0 / secNum); }}
 };
