7bfbd4735e4747af92d37fa0c945140b69ffd403
Marcinati <marcin.kapusta.pk@gmail.com>
2020-09-19 21:03:28 +0200

    const iterators

--- module3/homework/inner_product/arithmeticAverage.cpp
+++ module3/homework/inner_product/arithmeticAverage.cpp
@@ -5,13 +5,13 @@
 
 double ArithmeticAverage(const std::vector<int> & firstVec, const std::vector<int> & secVec) {
     double init = 0.0;
-    return std::inner_product(firstVec.begin(), firstVec.end(), secVec.begin(), init,
+    return std::inner_product(firstVec.cbegin(), firstVec.cend(), secVec.cbegin(), init,
                               std::plus<int>(), std::plus<int>()) / (firstVec.size() + secVec.size());
 }
 
 double Distance(const std::vector<int> & pointA, const std::vector<int> & pointB) {
     double init = 0.0;
-    return std::sqrt(std::inner_product(pointA.begin(), pointA.end(), pointB.begin(), init,
+    return std::sqrt(std::inner_product(pointA.cbegin(), pointA.cend(), pointB.cbegin(), init,
                                         std::plus<int>(), [](int first, int second){
                                             return std::pow(first - second, 2);}));
 }
