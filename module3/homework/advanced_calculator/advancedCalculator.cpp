5de1f9564d75617afba57300cf246c937fa2bf34
Marcinati <65040642+Marcinati@users.noreply.github.com>
2020-09-19 13:22:42 +0200

    move up operations

--- module3/homework/advanced_calculator/advancedCalculator.cpp
+++ module3/homework/advanced_calculator/advancedCalculator.cpp
@@ -4,6 +4,17 @@
 #include <cmath>
 #include <iostream>
 
+const std::map<char, std::function<double(double, double)>> operations {
+    {'+', std::plus<double>()},
+    {'*', std::multiplies<double>()},
+    {'/', std::divides<double>()},
+    {'-', std::minus<double>()},
+    {'%', std::modulus<int>()},
+    {'!', [](double firstNum, double secNum) { return firstNum <= 0 ? 1.0 : std::tgamma(firstNum + 1.0 + secNum - secNum); }},
+    {'^', [](auto firstNum, auto secNum) { return std::pow(firstNum, secNum); }},
+    {'$', [](auto firstNum, auto secNum) { return pow(firstNum, 1.0 / secNum); }}
+};
+
 void startCalculate() {
     printWelcomeScreen();
     mainLoop();
@@ -87,14 +98,3 @@ ErrorCode process(std::string input, double* out) {
     }
     return ErrorCode::OK;
 }
-
-const std::map<char, std::function<double(double, double)>> operations {
-    {'+', std::plus<double>()},
-    {'*', std::multiplies<double>()},
-    {'/', std::divides<double>()},
-    {'-', std::minus<double>()},
-    {'%', std::modulus<int>()},
-    {'!', [](double firstNum, double secNum) { return firstNum <= 0 ? 1.0 : std::tgamma(firstNum + 1.0 + secNum - secNum); }},
-    {'^', [](auto firstNum, auto secNum) { return std::pow(firstNum, secNum); }},
-    {'$', [](auto firstNum, auto secNum) { return pow(firstNum, 1.0 / secNum); }}
-};
