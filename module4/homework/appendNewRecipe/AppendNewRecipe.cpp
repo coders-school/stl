53ca733fc95b662102e28467dda3092f61722d71
m-kop <m.kopertowski@epicspace.eu>
2020-09-20 13:20:33 +0200

    add implementation of steps and ingredients

--- module4/homework/appendNewRecipe/AppendNewRecipe.cpp
+++ module4/homework/appendNewRecipe/AppendNewRecipe.cpp
@@ -2,6 +2,9 @@
 
 #include <fstream>
 #include <iostream>
+#include <sstream>
+#include <iterator>
+#include <algorithm>
 
 constexpr char filename[] = "recipes.txt";
 constexpr char ingredientsHeader[] = "Skladniki:";
@@ -27,17 +30,62 @@ bool AppendNewRecipe(std::vector<std::string> steps,
     return false;
 }
 
+std::string FormatUnits(char unit)
+{
+    switch (unit) {
+        case 'g':
+            return gram;
+        case 's':
+            return glass;
+        case 'm':
+            return ml;
+        default:
+            return {};
+    }
+}
+
 std::vector<std::string> FormatIngredients(const std::list<std::string>& ingredients,
                                            const std::deque<std::pair<size_t, char>>& amount)
 {
-    // ToDo
-    return {};
+    std::vector<std::string> ingredientsVec(ingredients.size());    
+    auto itIngredients = ingredients.begin();
+    auto itAmount = amount.begin();
+
+    std::generate(ingredientsVec.begin(), ingredientsVec.end(), [itIngredients, itAmount]() mutable {
+        std::stringstream ingredient;
+        
+        ingredient << std::to_string((itAmount)->first); 
+        ingredient << " ";
+        ingredient << FormatUnits(itAmount->second);
+        ingredient << " ";
+        ingredient << *itIngredients;
+        itAmount++;
+        itIngredients++;
+
+        return ingredient.str();
+    });
+
+    return ingredientsVec;
 }
+
 
 std::stringstream FormatRecipit(std::vector<std::string> steps,
                                 const std::list<std::string>& ingredients,
                                 const std::deque<std::pair<size_t, char>>& amount)
 {
-    // ToDo
-    return {};
+    std::stringstream recipe;
+    auto ingredientsVec = FormatIngredients(ingredients, amount);
+
+    recipe << ingredientsHeader << "\n";
+    std::copy(ingredientsVec.begin(), ingredientsVec.end(), std::ostream_iterator<std::string>(recipe, ",\n"));
+    recipe << std::endl;
+    recipe << stepsHeader << "\n";
+
+    for (auto index = 1; const auto& step : steps) {
+        recipe << index << ") " << step << ".\n";
+        index ++;
+    }
+    recipe << "___________________________________\n";
+
+    return recipe;
 }
