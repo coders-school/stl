75980276578ec16533ba19c9036932c01e41d948
damixiorek <damixiorek@gmail.com>
2020-09-20 12:25:27 +0200

    format recipit functionality works

--- module4/homework/appendNewRecipe/AppendNewRecipe.cpp
+++ module4/homework/appendNewRecipe/AppendNewRecipe.cpp
@@ -35,5 +35,24 @@ std::stringstream FormatRecipit(std::vector<std::string> steps,
                                 const std::list<std::string>& ingredients,
                                 const std::deque<std::pair<size_t, char>>& amount)
 {
-    return {};
+    std::stringstream ss;
+    std::vector<std::string> separatedIngredients = FormatIngredients(ingredients, amount);
+
+    ss << "Skladniki:\n";
+    std::for_each(separatedIngredients.begin(), separatedIngredients.end(),
+                  [&ss](std::string singleIngredient) {
+                      ss << singleIngredient << ",\n";
+                  });
+    ss << '\n';
+
+    ss << "Kroki:\n";
+    std::for_each(steps.begin(), steps.end(),
+                  [&ss](std::string singleStep) {
+                      static int whichStep = 1;
+                      ss << whichStep << ") " << singleStep << ".\n";
+                      whichStep++;
+                  });
+    ss << "___________________________________\n";
+
+    return ss;
 }
