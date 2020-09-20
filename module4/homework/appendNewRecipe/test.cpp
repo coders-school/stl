bf442564abf13f5844c7ba2dc0cbb7865845c502
damixiorek <damixiorek@gmail.com>
2020-09-20 10:24:23 +0200

    initialize homework4

--- module4/homework/appendNewRecipe/test.cpp
+++ module4/homework/appendNewRecipe/test.cpp
@@ -13,12 +13,6 @@
 #include "AppendNewRecipe.hpp"
 #include "gtest/gtest.h"
 
-<<<<<<< HEAD
-const std::string expected "Składniki:\n20 gram cukru,\n1 szklanka(i) mąki,\n40 mililitrów rumu,\n\nKroki:\n1) Wsypać do miski 20 gram cukru.\n2) Dorzucić 1 szklanke mąki.\n3) Dokładnie wymieszać.\n4) Nalać 40ml rumu do kieliszka.\n5) Wypić kieliszek.\n6) Wysypac zawartośc miski.\n___________________________________\n";
-
-TEST(AppendNewRecipe, ShoudlFormatIngredients) {
-    std::list<std::string> ingredients{"cukru", "mąki", "rumu"};
-=======
 constexpr const char kExpected[] =
     "Skladniki:\n20 gram cukru,\n1 szklanka(i) maki,"
     "\n40 mililitrow rumu,\n\nKroki:\n1) Wsypac do miski"
@@ -38,7 +32,6 @@ public:
 
 TEST_F(Test, ShoudlFormatIngredients) {
     std::list<std::string> ingredients{"cukru", "maki", "rumu"};
->>>>>>> 1d66207fc7ef740be07bd53cc49682d57a83c218
     std::deque<std::pair<size_t, char>> amount{
         {20, 'g'},
         {1, 's'},
@@ -47,27 +40,12 @@ TEST_F(Test, ShoudlFormatIngredients) {
     std::vector<std::string> result = FormatIngredients(ingredients, amount);
     const std::vector<std::string> expected{
         "20 gram cukru",
-<<<<<<< HEAD
-        "1 szklanka(i) mąki",
-        "40ml rumu"};
-=======
         "1 szklanka(i) maki",
         "40 mililitrow rumu"};
->>>>>>> 1d66207fc7ef740be07bd53cc49682d57a83c218
 
     EXPECT_EQ(expected, result);
 }
 
-<<<<<<< HEAD
-TEST(AppendNewRecipe, ShouldFormatWholeRecipe) {
-    std::vector<std::string> steps{"Wsypać do miski 20 gram cukru",
-                                   "Dorzucić 1 szklanke mąki",
-                                   "Dokładnie wymieszać",
-                                   "Nalać 40ml rumu do kieliszka",
-                                   "Wypić kieliszek",
-                                   "Wysypac zawartośc miski"};
-    std::list<std::string> ingredients{"cukru", "mąki", "rumu"};
-=======
 TEST_F(Test, ShouldFormatWholeRecipe) {
     std::vector<std::string> steps{"Wsypac do miski 20 gram cukru",
                                    "Dorzucic 1 szklanke maki",
@@ -76,25 +54,11 @@ TEST_F(Test, ShouldFormatWholeRecipe) {
                                    "Wypic kieliszek",
                                    "Wysypac zawartosc miski"};
     std::list<std::string> ingredients{"cukru", "maki", "rumu"};
->>>>>>> 1d66207fc7ef740be07bd53cc49682d57a83c218
     std::deque<std::pair<size_t, char>> amount{
         {20, 'g'},
         {1, 's'},
         {40, 'm'}};
 
-<<<<<<< HEAD
-    EXPECT_EQ(FormatRecipit(steps, ingredients, amount).str(), expected);
-}
-
-TEST(AppendNewRecipe, ShouldWriteFile) {
-    std::vector<std::string> steps{"Wsypać do miski 20 gram cukru",
-                                   "Dorzucić 1 szklanke mąki",
-                                   "Dokładnie wymieszać",
-                                   "Nalać 40ml rumu do kieliszka",
-                                   "Wypić kieliszek",
-                                   "Wysypac zawartośc miski"};
-    std::list<std::string> ingredients{"cukru", "mąki", "rumu"};
-=======
     EXPECT_EQ(FormatRecipit(steps, ingredients, amount).str(), kExpected);
 }
 
@@ -106,47 +70,24 @@ TEST_F(Test, ShouldWriteFile) {
                                    "Wypic kieliszek",
                                    "Wysypac zawartosc miski"};
     std::list<std::string> ingredients{"cukru", "maki", "rumu"};
->>>>>>> 1d66207fc7ef740be07bd53cc49682d57a83c218
     std::deque<std::pair<size_t, char>> amount{
         {20, 'g'},
         {1, 's'},
         {40, 'm'}};
 
-<<<<<<< HEAD
-    AppendNewRecipeconst(steps, ingredients, amount);
-=======
     AppendNewRecipe(steps, ingredients, amount);
->>>>>>> 1d66207fc7ef740be07bd53cc49682d57a83c218
 
     std::ifstream file("recipes.txt");
     ASSERT_TRUE(file.is_open());
 
     std::string str;
     std::stringstream ss;
-<<<<<<< HEAD
-    while (file >> str) {
-        ss << str;
-=======
     while (!getline(file, str, '\n').eof()) {
         ss << str << '\n';
->>>>>>> 1d66207fc7ef740be07bd53cc49682d57a83c218
     }
     file.close();
 
     str = ss.str();
-<<<<<<< HEAD
-    EXPECT_EQ(str, expected);
-}
-
-TEST(AppendNewRecipe, ShouldAppendNewRecipit) {
-    std::vector<std::string> steps{"Wsypać do miski 20 gram cukru",
-                                   "Dorzucić 1 szklanke mąki",
-                                   "Dokładnie wymieszać",
-                                   "Nalać 40ml rumu do kieliszka",
-                                   "Wypić kieliszek",
-                                   "Wysypac zawartośc miski"};
-    std::list<std::string> ingredients{"cukru", "mąki", "rumu"};
-=======
     EXPECT_EQ(str, kExpected);
 }
 
@@ -158,41 +99,25 @@ TEST_F(Test, ShouldAppendNewRecipit) {
                                    "Wypic kieliszek",
                                    "Wysypac zawartosc miski"};
     std::list<std::string> ingredients{"cukru", "maki", "rumu"};
->>>>>>> 1d66207fc7ef740be07bd53cc49682d57a83c218
     std::deque<std::pair<size_t, char>> amount{
         {20, 'g'},
         {1, 's'},
         {40, 'm'}};
 
-<<<<<<< HEAD
-    AppendNewRecipeconst(steps, ingredients, amount);
-    AppendNewRecipeconst(steps, ingredients, amount);
-=======
     AppendNewRecipe(steps, ingredients, amount);
     AppendNewRecipe(steps, ingredients, amount);
->>>>>>> 1d66207fc7ef740be07bd53cc49682d57a83c218
 
     std::ifstream file("recipes.txt");
     ASSERT_TRUE(file.is_open());
 
     std::string str;
     std::stringstream ss;
-<<<<<<< HEAD
-    while (file >> str) {
-        ss << str;
-=======
     while (!getline(file, str, '\n').eof()) {
         ss << str << '\n';
->>>>>>> 1d66207fc7ef740be07bd53cc49682d57a83c218
     }
     file.close();
 
     str = ss.str();
-<<<<<<< HEAD
-    auto double_expected = expected + expected;
-    EXPECT_EQ(str, expected);
-=======
     auto double_expected = std::string(kExpected) + std::string(kExpected);
     EXPECT_EQ(str, double_expected);
->>>>>>> 1d66207fc7ef740be07bd53cc49682d57a83c218
 }
