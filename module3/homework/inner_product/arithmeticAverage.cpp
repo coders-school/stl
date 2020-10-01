3d11a9c5d87a6df62dad47edd09d019571fbc43b
Vicx95 <wiktor.zyla31@gmail.com>
2020-10-01 21:43:30 +0200

    Implement suggested changes

--- module3/homework/inner_product/arithmeticAverage.cpp
+++ module3/homework/inner_product/arithmeticAverage.cpp
@@ -4,17 +4,18 @@
 #include <numeric>
 
 double ArithmeticAverage(const std::vector<int>& first, const std::vector<int>& second) {
-    double sumOfElements = static_cast<double>(first.size() + second.size());
-    auto average = std::inner_product(first.begin(), first.end(),
-                                      second.begin(), 0,
+    const double sumOfElements = static_cast<double>(first.size() + second.size());
+    const auto average = std::inner_product(first.cbegin(), first.cend(),
+                                      second.cbegin(), 0,
                                       std::plus<int>(), std::plus<int>()) / sumOfElements;
     return average;
 }
 
 double Distance(const std::vector<int>& first, const std::vector<int>& second) {
-    auto distance = std::sqrt(std::inner_product(first.begin(), first.end(), second.begin(),
+    const auto distance = std::sqrt(std::inner_product(first.cbegin(), first.cend(), second.cbegin(),
                     0, std::plus<int>(),
                     [](const auto& first, const auto& second) {
                         return std::pow(std::abs(first - second), 2);
                     }));
+    return distance;
 }
