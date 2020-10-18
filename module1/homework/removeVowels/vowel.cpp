055fb6c88e3f8f50bb6fb68ea2b271e2ca1262ec
Michal <mcamromer@gmail.com>
2020-10-18 12:37:19 +0200

    Refactor the coding style in the removeVowels function.

--- module1/homework/removeVowels/vowel.cpp
+++ module1/homework/removeVowels/vowel.cpp
@@ -5,17 +5,19 @@ const std::string vovels = "aeiouyAEIOYU";
 
 void removeVowels(std::vector<std::string>& lines)
 {
-    int i;
     for(std::string& line : lines)
     {
-        i = 0;
+        int i = 0;
         while (i < line.length())
         {
             if(vovels.find(line[i]) != std::string::npos)
             {
                 line.erase(i, 1);
             }
-            else i++;
+            else
+            {
+                i++;
+            }
         }
     }
 }
