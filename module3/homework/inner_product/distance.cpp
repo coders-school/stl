1ce7872e57d2a6d3cd732131c2b97c216176fad0
damixiorek <damixiorek@gmail.com>
2020-10-21 18:59:22 +0200

    some changes in distance.cpp

--- module3/homework/inner_product/distance.cpp
+++ module3/homework/inner_product/distance.cpp
@@ -8,7 +8,7 @@
 float Distance::operator()(std::vector<int>& lhs, std::vector<int>& rhs) const
 {
     const int initNum = 0;
-    auto sum = std::inner_product(lhs.begin(), lhs.end(), rhs.begin(), initNum,
+    const auto sum = std::inner_product(lhs.cbegin(), lhs.cend(), rhs.cbegin(), initNum,
                                             std::plus<>(), 
                                             [](int num1, int num2){
                                                 float result = std::pow(num1 - num2, 2);
