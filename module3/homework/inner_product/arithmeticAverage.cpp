1cada492daaa11314b3b0dfa6d4c08d4fadf6c7d
damixiorek <damixiorek@gmail.com>
2020-10-21 19:01:25 +0200

    some changes in arithmeteicAverage.cpp

--- module3/homework/inner_product/arithmeticAverage.cpp
+++ module3/homework/inner_product/arithmeticAverage.cpp
@@ -8,7 +8,7 @@
 float ArithmeticAverage::operator()(std::vector<int>& lhs, std::vector<int>& rhs) const
 {
     const int initNum = 0;
-    int fullSize = lhs.size() + rhs.size();
+    const int fullSize = lhs.size() + rhs.size();
     const auto sum = std::fabs(std::inner_product(lhs.cbegin(), lhs.cend(), rhs.cbegin(), initNum, std::plus<>(), std::plus<>()));
     return sum / fullSize;
 }
