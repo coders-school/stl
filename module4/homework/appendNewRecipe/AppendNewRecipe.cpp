ce54fe187eb9485cf5e7f914334b08a9a7cc39e3
WojtekMs <wojtek.atmo@gmail.com>
2020-11-03 10:12:30 +0100

    using sstream to avoid creating temp string objects

--- module4/homework/appendNewRecipe/AppendNewRecipe.cpp
+++ module4/homework/appendNewRecipe/AppendNewRecipe.cpp
@@ -5,7 +5,8 @@
 
 bool AppendNewRecipe(const std::vector<std::string>& steps,
                      const std::list<std::string>& ingredients,
-                     const std::deque<std::pair<size_t, char>>& amount) {
+                     const std::deque<std::pair<size_t, char>>& amount)
+{
     std::string fileName{"recipes.txt"};
     std::ofstream file(fileName, std::ios::app);
     if (file.is_open()) {
@@ -18,7 +19,8 @@ bool AppendNewRecipe(const std::vector<std::string>& steps,
 }
 
 std::vector<std::string> FormatIngredients(const std::list<std::string>& ingredients,
-                                           const std::deque<std::pair<size_t, char>>& amount) {
+                                           const std::deque<std::pair<size_t, char>>& amount)
+{
     if (ingredients.size() != amount.size()) {
         return {};
     }
@@ -30,30 +32,32 @@ std::vector<std::string> FormatIngredients(const std::list<std::string>& ingredi
                    [](const auto& ingredient, const auto& pair) {
                        const auto [amount, unit] = pair;
                        std::string formattedUnit{};
+                       std::stringstream stream;
                        switch (unit) {
                            case 'g':
-                           formattedUnit = "gram";
-                           break;
+                               formattedUnit = "gram";
+                               break;
                            case 's':
-                           formattedUnit = "szklanka(i)";
-                           break;
+                               formattedUnit = "szklanka(i)";
+                               break;
                            case 'm':
-                           formattedUnit = "mililitrow";
-                           break;
+                               formattedUnit = "mililitrow";
+                               break;
                        }
-                       std::stringstream temp_stream(std::to_string(amount) + ' ' + formattedUnit + ' ' + ingredient);
-                       return temp_stream.str();
+                       stream << amount << ' ' << formattedUnit << ' ' << ingredient;
+                       return stream.str();
                    });
     return formattedIngredients;
 }
 std::stringstream FormatRecipit(const std::vector<std::string>& steps,
                                 const std::list<std::string>& ingredients,
-                                const std::deque<std::pair<size_t, char>>& amount) {
+                                const std::deque<std::pair<size_t, char>>& amount)
+{
     std::stringstream stream;
     const std::string ingredientsMessage{"Skladniki:\n"};
     stream << ingredientsMessage;
     auto formattedIngredients = FormatIngredients(ingredients, amount);
-    for (auto line : formattedIngredients) {
+    for (const auto& line : formattedIngredients) {
         stream << line << ",\n";
     }
     const std::string stepsMessage{"\nKroki:\n"};
