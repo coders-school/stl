ae12a338e7897cf7ccbcae7f9a31f23c61688dbd
Warp-12 <pl.walery@gmail.com>
2021-02-11 10:21:04 +0100

    Small probleb solved with remove vowel homework

--- module1/homework/removeVowels/vowel.cpp
+++ module1/homework/removeVowels/vowel.cpp
@@ -1,7 +1,7 @@
 #include "vowel.hpp"
 
 void removeVowels(std::vector<std::string>& vec) {
-    bool isVowel = [](char c) { return std::find(VOWELS.begin(), VOWELS.end(), c) != VOWELS.end(); };
+    auto isVowel = [](char c) { return std::find(VOWELS.begin(), VOWELS.end(), c) != VOWELS.end(); };
     for (auto& word : vec) {
         word.erase(std::remove_if(word.begin(), word.end(), isVowel), word.end());
     }
