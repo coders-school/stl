d7baf731432f30db7f91e55dc4688af53033ad0c
lukasz-kukulka <qqla83@gmail.com>
2021-03-02 06:20:05 +0000

    task2 vector

--- module1/tasks/stl_1/vector.cpp
+++ module1/tasks/stl_1/vector.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 
 int main(){
-    std::cout << "Vector: \n";
+    std::cout << "Vector 1: \n";
     std::vector<int> vec {1, 2, 4, 5, 6};
     vec.erase(vec.begin());
     vec.insert(vec.end(), 5);
@@ -13,5 +13,15 @@ int main(){
     }
     vec.clear();
     std::cout <<"\nSize: " << vec.size() << "\nCapacity: " << vec.capacity() << "\n";
+
+    std::cout << "\n\nVector 2: \n";
+    std::vector<int>vec2{};
+    std::cout <<"\nSize: " << vec2.size() << "\nCapacity: " << vec2.capacity() << "\n";
+    vec2.insert(vec2.begin(),10, 5);
+    std::cout <<"\nSize: " << vec2.size() << "\nCapacity: " << vec2.capacity() << "\n";
+    vec2.reserve(20);
+    std::cout <<"\nSize: " << vec2.size() << "\nCapacity: " << vec2.capacity() << "\n";
+    vec2.shrink_to_fit();
+    std::cout <<"\nSize: " << vec2.size() << "\nCapacity: " << vec2.capacity() << "\n";
     return 0;
 }
