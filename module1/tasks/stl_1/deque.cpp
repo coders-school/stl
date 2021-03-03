5f90ef27a9dcc8455aba49180d8a84426d136e5e
lukasz-kukulka <qqla83@gmail.com>
2021-03-03 04:50:07 +0000

    task deque

---
+++ module1/tasks/stl_1/deque.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <deque>
+
+void print(const std::deque<int>& contener){
+    for (const auto& it : contener){
+        std::cout << it << " ";
+    }
+    std::cout << "\n";
+}
+
+int main (){
+    std::cout << "Deque: \n";
+    std::deque<int>deq;
+    deq.push_front(5);
+    deq.push_front(4);
+    deq.push_front(3);
+    deq.push_front(2);
+    deq.push_front(1);
+    print(deq);
+    deq.erase(deq.begin() + 1);
+    deq.erase(deq.begin() + 2);
+    print(deq);
+    deq.push_front(30);
+    deq.push_back(30);
+    print(deq);
+    auto it = deq.begin();
+    std::advance(it, 3);
+    deq.insert(it, 20);
+    print(deq);
+    return 0;
+}
