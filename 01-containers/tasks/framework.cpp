6a5c669f79a27c23877f41a7a238bf189ecb518d
Łukasz Ziobroń <lukasz@coders.school>
2021-05-20 13:23:30 +0200

    Tasks solved during the training

--- 01-containers/tasks/framework.cpp
+++ 01-containers/tasks/framework.cpp
@@ -7,7 +7,7 @@
 #include <vector>
 
 template <typename Function, typename ... Args>
-auto measure(std::string const& description, Function&& f, Args... args) {
+auto measure(std::string const& description, Function&& f, Args &&... args) {
     auto start = std::chrono::high_resolution_clock::now();
     auto result = f(std::forward<Args>(args)...);
     auto stop = std::chrono::high_resolution_clock::now();
@@ -17,31 +17,34 @@ auto measure(std::string const& description, Function&& f, Args... args) {
 }
 
 template <typename Container>
-auto access(Container const& c, size_t size) {
+typename Container::const_iterator access(Container const& c, size_t size) {
     auto it = c.begin();
     std::advance(it, size/2);
     return it;
 }
 
-// template <typename Container>
-// auto insert(Container const& c, size_t size) {
-//     auto it = c.begin();
-//     std::advance(it, size/2);
-//     return *it;
-// }
+template <typename Container>
+typename Container::iterator insert(Container & c, typename Container::const_iterator pos, typename Container::value_type value) {
+    return c.insert(pos, value);
+}
 
 template <typename ContainerA, typename ContainerB>
-void measureBoth(std::string const& description, ContainerA const& c1, ContainerB const& c2) {
+void measureBoth(std::string const& description, ContainerA & c1, ContainerB & c2) {
     auto c1Size = c1.size();
     auto c2Size = c2.size();
 
     std::cout << "sizeof c1 " + description << " " << c1Size << ": " << sizeof(c1) << '\n';
     std::cout << "sizeof c2 " + description << " " << c2Size << ": " << sizeof(c2) << '\n';
 
-    measure(description + " accessing " + std::to_string(c1Size/2),
+    auto it = measure(description + " accessing " + std::to_string(c1Size/2),
             access<ContainerA>, c1, c1Size);
-    measure(description + " accessing " + std::to_string(c2Size/2),
+    measure(description + " inserting " + std::to_string(c1Size/2),
+            insert<ContainerA>, c1, it, 42);
+    it = measure(description + " accessing " + std::to_string(c2Size/2),
             access<ContainerB>, c2, c2Size);
+    measure(description + " inserting " + std::to_string(c2Size/2),
+            insert<ContainerB>, c2, it, 42);
+
 }
 
 int main() {
@@ -60,45 +63,18 @@ int main() {
     std::deque<int> d(size, value);
     std::deque<int> d2(size2, value);
 
+    auto it = access(v, size/2);
+    insert(v, it, 42);
+
     measure("Vector accessing with [] " + std::to_string(size/2),
             [&]{ return v[size/2]; });
     measure("Vector accessing with advance " + std::to_string(size/2),
             access<std::vector<int>>, v, size);
 
     measureBoth("vector", v, v2);
-    measureBoth("array", a, a2);
+//    measureBoth("array", a, a2);
     measureBoth("list", l, l2);
     measureBoth("deque", d, d2);
 
     return 0;
 }
-
-// 1. Write a measure function that takes a description and any other function (without parameters so far).
-// It should measure the execution time of the given function. Use `std::chrono::high_resolution_clock()`.
-// After the measurement it should print results + description.
-
-// 2. Measure the time of accessing the middle element of the vectors of size 5M and 50M.
-
-// 3. Measure the time of accessing the middle element of the lists of size 5M and 50M.
-
-// 4. Refactor the code - write an `access` function. It should take a container and a size as parameters.
-// You need to extend the functionality of `measure` function to take any number of parameters.
-// Use variadic templates and perfect_forwarding.
-
-// 5. Measure if accessing vector via index is faster than accessing it via advancing the begin iterator.
-
-// 6. Add measurement for std::deque of size 5M and 50M.
-
-// 7. Refactor the code. Write measureBoth
-// ```cpp
-// template <typename Container>
-// void measureBoth(std::string const& description, Container const& c1, Container const& c2) {
-// ```
-
-// 8. Print sizeof c1 and c2 in measureBoth
-
-// 9. Refactor measure to return the value returned from Function.
-
-// 10. Add measuring the time of inserting an element at the middle position = problematic
-
-// 11. Try compilation with -O3
