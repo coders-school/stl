2b97b4c7e668619880087e1ee5c31fc6d0368db1
Warp-12 <pl.walery@gmail.com>
2021-06-08 18:46:50 +0200

    header file to remove-vowels

---
+++ homework/remove-vowels/vowel.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+const std::string VOWELS = "aeiouyAEIOUY";
+void removeVowels(std::vector<std::string>&);
