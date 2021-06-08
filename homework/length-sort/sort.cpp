bb967d7603d240019573df6d9b3e0a4a671028cc
Piotr <piotrku91@github.com>
2021-06-08 18:30:11 +0200

    std::sort, pass words by const ref

--- homework/length-sort/sort.cpp
+++ homework/length-sort/sort.cpp
@@ -1,27 +1,27 @@
 #include "sort.hpp"
+#include <algorithm>
 
-std::deque<std::string> lengthSort(std::forward_list<std::string> &words)
+std::deque<std::string> lengthSort(const std::forward_list<std::string> &words)
 {
     std::deque<std::string> sorttmp;
-
-    words.sort([](std::string a, std::string b)
-               {
-                   if (a.length() == b.length())
-                   {
-                       if (std::lexicographical_compare(a.begin(), a.end(), b.begin(), b.end()))
-                       {
-                           return false;
-                       }
-                       return true;
-                   }
-                   if (a.length() < b.length())
-                   {
-                       return false;
-                   }
-                   return true;
-               });
-
     std::copy(words.begin(), words.end(), std::front_inserter(sorttmp));
+
+    std::sort(sorttmp.begin(), sorttmp.end(), [](std::string a, std::string b)
+              {
+                  if (a.length() == b.length())
+                  {
+                      if (std::lexicographical_compare(a.begin(), a.end(), b.begin(), b.end()))
+                      {
+                          return true;
+                      }
+                      return false;
+                  }
+                  if (a.length() < b.length())
+                  {
+                      return true;
+                  }
+                  return false;
+              });
 
     return sorttmp;
 };
--- homework/length-sort/sort.hpp
+++ homework/length-sort/sort.hpp
@@ -3,4 +3,4 @@
 #include <forward_list>
 #include <string>
 
-std::deque<std::string> lengthSort(std::forward_list<std::string>& words);
+std::deque<std::string> lengthSort(const std::forward_list<std::string>& words);
