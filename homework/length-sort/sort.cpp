860bee7acbfa8ae92b533fbd4d3dc17a2907dcdf
Łukasz Nitka <lukasnit@wp.pl>
2021-06-08 10:42:57 +0200

    sort.cpp,sort.hpp

---
+++ homework/length-sort/sort.cpp
@@ -0,0 +1,13 @@
+#include "sort.hpp"
+
+
+std::deque<std::string> lengthSort(const std::forward_list<std::string>& list)
+{
+   
+  std::deque<std::string>round(list.begin(),list.end());
+    
+
+
+
+    return round;
+}
