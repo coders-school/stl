c2265e6e6b7c1b110cf18ee29a36bb874adcbd79
Łukasz Nitka <lukasnit@wp.pl>
2021-06-08 11:17:12 +0200

    project_f1

--- homework/length-sort/sort.cpp
+++ homework/length-sort/sort.cpp
@@ -1,13 +1,17 @@
 #include "sort.hpp"
 
+bool round_sort(std::string a, std::string b) {
+    if (a.size() < b.size())
+        return true;
+    if ((a.size() == b.size()) && (a < b))
+        return true;
+    else
+        return false;
+}
 
-std::deque<std::string> lengthSort(const std::forward_list<std::string>& list)
-{
-   
-  std::deque<std::string>round(list.begin(),list.end());
-    
-
-
+std::deque<std::string> lengthSort(const std::forward_list<std::string>& list) {
+    std::deque<std::string> round(list.begin(), list.end());
+    sort(round.begin(), round.end(), round_sort);
 
     return round;
 }
--- homework/length-sort/sort.hpp
+++ homework/length-sort/sort.hpp
@@ -1,7 +1,7 @@
 #pragma once
+#include <algorithm>
+#include <deque>
 #include <forward_list>
 #include <string>
-#include <deque>
 
 std::deque<std::string> lengthSort(const std::forward_list<std::string>& list);
-
