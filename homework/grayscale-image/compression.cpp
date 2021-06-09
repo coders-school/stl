545f6a15a6cc4dd41a0bb99d5ebbc0baf2dd92aa
Jakub Stępień <macista.bukaj@gmail.com>
2021-06-09 20:00:53 +0200

    fixup! flatten array

--- homework/grayscale-image/compression.cpp
+++ homework/grayscale-image/compression.cpp
@@ -6,7 +6,7 @@ compressedGrayscaleImage compressGrayscale(grayscaleImage bitmap) {
 	for (auto line: bitmap) {
 		uint8_t counter = 1;
 		for (auto it = line.begin(); it != line.end(); it++) {
-			if (*it != *std::next(it) || it == line.end() - 1 ) {
+			if (*it != *std::next(it) || it == line.end() - 1) {
 				compressed.push_back(std::make_pair(*it, counter));
 				counter = 1;
 			}
@@ -20,10 +20,10 @@ compressedGrayscaleImage compressGrayscale(grayscaleImage bitmap) {
 
 grayscaleImage decompressGrayscale(compressedGrayscaleImage compressed) {
     grayscaleImage decompressed;
-    size_t i=0;
-    for(auto it : compressed){
-        for(size_t j=0; j<it.second; ++j, ++i){
-            decompressed[0][i]=it.first;
+    size_t i = 0;
+    for (auto it : compressed){
+        for (size_t j = 0; j < it.second; ++j, ++i){
+            decompressed[0][i] = it.first;
         }
     }
     return decompressed;
