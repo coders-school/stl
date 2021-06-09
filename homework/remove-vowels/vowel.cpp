14a23de7864ccc64d0cf2b12caa7825ccc26be89
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2021-06-09 10:12:51 +0200

    Update the regex expression with the uppercase vowels

--- homework/remove-vowels/vowel.cpp
+++ homework/remove-vowels/vowel.cpp
@@ -4,7 +4,7 @@
 #include <regex>
 
 void removeVowels(std::vector<std::string>& strings) {
-    std::regex vowels("a|e|i|o|u|y");
+    std::regex vowels("a|e|i|o|u|y|A|E|I|O|U|Y");
     std::for_each(strings.begin(), strings.end(),
                   [&vowels](std::string& str) {
                       str = std::regex_replace(str, vowels, "");
