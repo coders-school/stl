48ca3ddc8d7b3a6262a2b2e704bbc17d7326ff99
lukasz-kukulka <qqla83@gmail.com>
2021-06-09 21:42:17 +0100

    Complete homework length-sort Lukasz & Mariusz

--- homework/length-sort/sort.cpp
+++ homework/length-sort/sort.cpp
@@ -2,12 +2,10 @@
 #include <algorithm>
 
 std::deque<std::string> lengthSort(const std::forward_list<std::string>& strings) {
-    std::deque<std::string> deq{ strings.begin(), strings.end() };
+    std::deque<std::string> deq{strings.begin(), strings.end()};
     std::sort(deq.begin(), deq.end(),
               [](const std::string& lhs, const std::string& rhs) {
-                  return lhs.size() == rhs.size() ?
-                         lhs < rhs :
-                         lhs.size() < rhs.size();
+                  return lhs.size() == rhs.size() ? lhs < rhs : lhs.size() < rhs.size();
               });
 
     return deq;
