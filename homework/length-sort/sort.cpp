2f9966cce7ce92535bddd9219776de4191246fa8
Piotr Kupczyk <60675900+piotrku91@users.noreply.github.com>
2021-06-09 06:16:17 +0200

    Ready to coop

--- homework/length-sort/sort.cpp
+++ homework/length-sort/sort.cpp
@@ -4,24 +4,7 @@
 std::deque<std::string> lengthSort(const std::forward_list<std::string> &words)
 {
     std::deque<std::string> sorttmp;
-    std::copy(words.begin(), words.end(), std::front_inserter(sorttmp));
-
-    std::sort(sorttmp.begin(), sorttmp.end(), [](std::string a, std::string b)
-              {
-                  if (a.length() == b.length())
-                  {
-                      if (std::lexicographical_compare(a.begin(), a.end(), b.begin(), b.end()))
-                      {
-                          return true;
-                      }
-                      return false;
-                  }
-                  if (a.length() < b.length())
-                  {
-                      return true;
-                  }
-                  return false;
-              });
+   
 
     return sorttmp;
 };
