e7d304afe222d63fabaa65753eff594cc9b37248
BarTes8 <barbara.tessema@gmail.com>
2021-06-09 14:05:06 +0200

    initial commit

--- homework/grayscale-image/compression.cpp
+++ homework/grayscale-image/compression.cpp
@@ -1,21 +1,17 @@
 #include "compression.hpp"
 #include <algorithm>
+#include <iostream>
+#include <iterator>
 #include <utility>
 
 std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(std::array<std::array<uint8_t, width>, height> bitmap) {
     std::vector<std::pair <uint8_t, uint8_t>> compressed;
-	int rows = sizeof(bitmap) / sizeof(bitmap[0]);
-	int cols = sizeof(bitmap[0]) / sizeof(bitmap[0][0]);
-	uint8_t countNumber = 0;
-	std::cout << rows << std::endl;
-	std::cout << cols << std::endl;
-
-	int counter = 1;
-	for (int i = 0; i < rows; i++) {
-		for (int j = 0; j < cols - 1; j++) {
-			
-			if (bitmap[i][j] != bitmap[i][j + 1]) {
-				compressed.push_back(std::make_pair(bitmap[i][j], counter));
+	compressed.reserve(width * height);
+	for (auto& line: bitmap) {
+		uint8_t counter = 1;
+		for (auto it = line.begin(); it != line.end(); it++) {
+			if (it == line.end() || *it != *std::next(it)) {
+				compressed.push_back(std::make_pair(*it, counter));
 				counter = 1;
 			}
 			else {
@@ -24,6 +20,13 @@ std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(std::array<std::array
 		}
 	}
 	return compressed;
+}
+
+void printVecPair(const std::vector<std::pair<uint8_t, uint8_t>>& vec) {
+	for (const auto& el : vec) {
+		std::cout << el.first << " | " << el.second << ' ';
+	}
+	std::cout << '\n';
 }
 
 std::array<std::array<uint8_t, width>, height> decompressGrayscale(std::vector<std::pair<uint8_t, uint8_t>> compressed) {
