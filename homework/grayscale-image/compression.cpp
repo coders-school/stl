3ed7f7ebaaa5d40a16bf8cc613fb73fbfa4f5352
pwiatr3 <pwiatr3@yahoo.pl>
2021-06-10 14:14:44 +0200

    szkielet programu

---
+++ homework/grayscale-image/compression.cpp
@@ -0,0 +1,11 @@
+#include "compression.hpp"
+
+std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(std::array<std::array<uint8_t, width>, height>) {
+    std::vector<std::pair<uint8_t, uint8_t>> temp;
+    return temp;
+}
+std::array<std::array<uint8_t, width>, height> decompressGrayscale(std::vector<std::pair<uint8_t, uint8_t>>) {
+    std::array<std::array<uint8_t, width>, height> temp;
+    return temp;
+}
+
