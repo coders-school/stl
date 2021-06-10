d2a8187755feea4980c050a7d65b7ccefef16cab
Piotr Rogalski <progalski89@gmail.com>
2021-06-11 00:57:12 +0200

    Minor fix

--- homework/remove-vowels/vowel.cpp
+++ homework/remove-vowels/vowel.cpp
@@ -6,7 +6,7 @@ void removeVowels(std::vector<std::string>& stringsVector) {
     // I'm using array of chars instead of C-style string to avoid having null terminator in my array
     constexpr std::array vowels{'a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y'};
     for(auto& string : stringsVector) {
-        for(auto& character : vowels) {
+        for(const auto& character : vowels) {
             string.erase(std::remove(string.begin(), string.end(), character), string.end());
         }
     }
