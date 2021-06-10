89bd152927b634a3e7a98b59f6ef6cf191753a5d
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2021-06-10 13:33:07 +0200

    Declare the compress function in a header

---
+++ homework/grayscale-image/compression.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <array>
+#include <vector>
+
+constexpr size_t width{ 32 };
+constexpr size_t height{ 32 };
+
+using CompressedBitmap = std::vector<std::pair<uint8_t, uint8_t>>;
+using Bitmap = std::array<std::array<uint8_t, width>, height>;
+
+CompressedBitmap compressGrayscale(const Bitmap& bitmap);
