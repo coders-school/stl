124af19bbcc677744c5a941df74e6ee82e59deac
Maciej3206 <heilmanmaciej@gmail.com>
2021-06-10 17:50:30 +0200

    all tests passed

---
+++ homework/length-sort/sort.cpp
@@ -0,0 +1,13 @@
+#include <algorithm>
+#include "sort.hpp"
+
+std::deque<std::string> lengthSort (std::forward_list <std::string> words) {
+    words.sort([](const std::string& lhs, const std::string& rhs){
+        if (lhs.size() == rhs.size()) {
+            return lhs < rhs;
+        }
+        return lhs.size() < rhs.size();
+    });
+    std::deque<std::string> WordsInDeque (words.begin(), words.end());
+    return WordsInDeque;
+}
