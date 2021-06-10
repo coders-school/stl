cafc1d0e73274f2975389c75fac8cb4fb6c04129
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2021-06-10 14:08:37 +0200

    Change the initialization of the ascii range to the uniform itializatation

--- homework/grayscale-image/compression.hpp
+++ homework/grayscale-image/compression.hpp
@@ -5,8 +5,8 @@
 
 constexpr size_t width{ 32 };
 constexpr size_t height{ 32 };
-constexpr uint8_t printable_ascii_min = 32;
-constexpr uint8_t printable_ascii_max = 126;
+constexpr uint8_t printable_ascii_min{ 32 };
+constexpr uint8_t printable_ascii_max{ 126 };
 
 using CompressedBitmap = std::vector<std::pair<uint8_t, uint8_t>>;
 using Bitmap = std::array<std::array<uint8_t, width>, height>;
