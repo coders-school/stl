1a43cbb6a0983c7b960a410483c8c9e9f784e50f
Wojtek Płóciennik <wojtek.plociennik@gmail.com>
2021-06-10 23:39:11 +0200

    Homwork stl length implemented

--- homework/length-sort/sort.cpp
+++ homework/length-sort/sort.cpp
@@ -1,17 +1,15 @@
 #include "sort.hpp"
-#include <algorithm>
-#include <deque>
-#include <forward_list>
-#include <string>
 
-bool compareLength(std::string word, std::string next_word) {
-    if (word.length() < next_word.length())
+
+bool compare_length(std::string& str1, std::string& str2) {
+    if (str1.length() < str2.length() || str1.length() == str2.length() && str1 < str2) {
         return true;
-    if ((word.length() == next_word.length()) && (word < next_word))
-        return true;
+    }
+    return false;
 }
-std::deque<std::string> lengthSort(std::forward_list<std::string> container_forward_list) {
-    container_forward_list.sort(compareLength);
-    std::deque<std::string> container_deque(container_forward_list.begin(), container_forward_list.end());
-    return container_deque;
+
+std::deque<std::string> lengthSort(std::forward_list<std::string> flist) {
+    std::deque<std::string> deq(flist.begin(), flist.end());
+    std::sort(deq.begin(), deq.end(), compare_length);
+    return deq;
 }
--- homework/length-sort/sort.hpp
+++ homework/length-sort/sort.hpp
@@ -1,7 +1,9 @@
 #pragma once
-
+#include <algorithm>
 #include <deque>
 #include <forward_list>
 #include <iostream>
 
 std::deque<std::string> lengthSort(std::forward_list<std::string>);
+
+bool compare_length(std::string& str1, std::string& str2);
