14df179ffe3593d21b9b1b787d43bd71878248ba
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2021-06-10 14:02:50 +0200

    Declare the print function in a header and initialize the range of printable ascii codes

--- homework/grayscale-image/compression.hpp
+++ homework/grayscale-image/compression.hpp
@@ -5,9 +5,12 @@
 
 constexpr size_t width{ 32 };
 constexpr size_t height{ 32 };
+constexpr uint8_t printable_ascii_min = 32;
+constexpr uint8_t printable_ascii_max = 126;
 
 using CompressedBitmap = std::vector<std::pair<uint8_t, uint8_t>>;
 using Bitmap = std::array<std::array<uint8_t, width>, height>;
 
 CompressedBitmap compressGrayscale(const Bitmap& bitmap);
 Bitmap decompressGrayscale(const CompressedBitmap& compressed);
+void printMap(const Bitmap& bitmap);
