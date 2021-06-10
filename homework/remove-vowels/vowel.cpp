fde7e795a017a4fe450590f649f3a701e4bc2aa3
rizzet97 <nit.pawel@gmail.com>
2021-06-10 19:11:12 +0200

    Initial commit

---
+++ homework/remove-vowels/vowel.cpp
@@ -0,0 +1,15 @@
+#include "vowel.hpp"
+
+void removeVowelsString(std::string& input) {
+  auto to_erase = std::remove_if(input.begin(), input.end(), &isVowel);
+  input.erase(to_erase, input.end());
+}
+
+bool isVowel(const char p_char) {
+    constexpr char vowels[] = {'a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y'};
+    return std::find(std::begin(vowels), std::end(vowels), p_char) != std::end(vowels);
+}
+
+void removeVowels(std::vector<std::string>& inputVect) {
+  std::for_each(inputVect.begin(), inputVect.end(), &removeVowelsString);
+}
---
+++ homework/remove-vowels/vowel.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <algorithm>
+
+void removeVowels(std::vector<std::string>&);
+void removeVowelsString(std::string&);
+bool isVowel(const char p_char);
