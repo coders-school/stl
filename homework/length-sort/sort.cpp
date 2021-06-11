940faa2e9ad9db3489d86aaee1fd1b283eb08b25
Barbara Tessema <74462021+BarTes8@users.noreply.github.com>
2021-06-11 14:05:55 +0200

    Update sort.cpp

--- homework/length-sort/sort.cpp
+++ homework/length-sort/sort.cpp
@@ -1,7 +1,7 @@
 #include "sort.hpp"
 
 std::deque<std::string> lengthSort(std::forward_list<std::string>& words) {
-    words.sort([](const std::string& lhs, const std::string& rhs){
+    words.sort([](const std::string& lhs, const std::string& rhs) {
         if (lhs.size() == rhs.size()) {
             return lhs < rhs;
         }
