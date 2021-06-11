58c6857e30354f678715cea3b4b72aa509ec0fba
Gyrydymptum <gyrydymptum@gmail.com>
2021-06-11 12:26:58 +0200

    adding information about erase()

--- homework/remove-vowels/vowel.cpp
+++ homework/remove-vowels/vowel.cpp
@@ -1,13 +1,13 @@
 #include "vowel.hpp"
 #include <algorithm>
 #include <iostream>
-#include <string>
 
 
 void removeVowels(std::vector<std::string>& words) {
     const std::string vowels = "aeiouyAEIOUY";
     for (int i = 0; i < vowels.length(); i++) {
         for (auto& el : words) {
+            //std:: erase(words, vowels[i]); c++ 20
             el.erase(std::remove(el.begin(), el.end(), vowels[i]), el.end());
         }
     }
