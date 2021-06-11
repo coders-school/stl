1eee072ca626a88625e8af3d4cb486e44445ae41
Piotr <piotrku91@github.com>
2021-06-11 08:28:06 +0200

    header

--- homework/grayscale-image/compression.cpp
+++ homework/grayscale-image/compression.cpp
@@ -1,1 +1,12 @@
 #include "compression.hpp"
+
+
+std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(std::array<std::array<uint8_t, width>, height> bitMap)
+{
+std::vector<std::pair<uint8_t, uint8_t>> tmpCompVec;
+
+
+
+
+
+};
