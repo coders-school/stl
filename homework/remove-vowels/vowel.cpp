635096258a7422a9ab6fa30fd5e10a710138bd4c
raxonpc <tomeczekorlowski1@gmail.com>
2021-06-11 18:29:25 +0200

    Replace C-arr with std::arr + std::find

--- homework/remove-vowels/vowel.cpp
+++ homework/remove-vowels/vowel.cpp
@@ -1,17 +1,18 @@
 #include "vowel.hpp"
+#include <array>
  
-bool isVowel(const char& letter) {
-    char vowels[12] = {'a','A','e','E','i','I','o','O','u','U','y','Y'};
-    for (const auto& el : vowels) {
-       if (letter == el){
-          return true;     
-       }
-    }
-    return false;
+bool isVowel(const char letter) 
+{
+    static constexpr std::array vowelArr
+    {'a', 'A', 'e', 'E', 'i', 'I', 'u', 'U', 'o', 'O', 'y', 'Y'};
+    
+    return std::find(vowelArr.cbegin(), vowelArr.cend(), letter) != vowelArr.cend();
 }
  
-void removeVowels(std::vector<std::string>& words) {
-    for (auto& el : words) {
+void removeVowels(std::vector<std::string>& words) 
+{
+    for (auto& el : words) 
+    {
         el.erase(std::remove_if(el.begin(), el.end(), isVowel), el.end());
     }
 }
