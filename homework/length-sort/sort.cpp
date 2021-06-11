8e78342b47323c73b0e46a32b56d45ba0e0d09b1
Barbara Tessema <74462021+BarTes8@users.noreply.github.com>
2021-06-11 14:04:19 +0200

    Update sort.cpp

--- homework/length-sort/sort.cpp
+++ homework/length-sort/sort.cpp
@@ -1,12 +1,12 @@
 #include "sort.hpp"
 
-std::deque<std::string> lengthSort (std::forward_list <std::string> words) {
+std::deque<std::string> lengthSort(std::forward_list<std::string>& words) {
     words.sort([](const std::string& lhs, const std::string& rhs){
         if (lhs.size() == rhs.size()) {
             return lhs < rhs;
         }
         return lhs.size() < rhs.size();
     });
-    std::deque<std::string> WordsInDeque (words.begin(), words.end());
+    std::deque<std::string> WordsInDeque(words.begin(), words.end());
     return WordsInDeque;
 }
