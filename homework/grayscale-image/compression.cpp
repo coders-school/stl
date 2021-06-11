d3f4453adeafded8e45d702669e495d95647166f
adrianz261 <adrian.zuchowski@outlook.com>
2021-06-11 02:19:13 +0200

    Version 1

--- homework/grayscale-image/compression.cpp
+++ homework/grayscale-image/compression.cpp
@@ -1,6 +1,30 @@
 #include "compression.hpp"
 
-compressed_bitmap compressGreyscale(bitmap)
+std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(std::array<std::array<uint8_t, 240>, 160> image)
+{
+    std::vector<std::pair<uint8_t, uint8_t>> result(256);
+
+    for (size_t i = 0; i < image.size(); ++i)
+    {
+        result.push_back(std::pair<uint8_t, uint8_t>(image[i][0], 1));
+        for (size_t j = 1;j < image.at(i).size(); ++j)
+        {
+            if (image[i][j] == result.back().first)
+            {
+                result.back().second++;
+            }
+            else
+            {
+                result.push_back(std::pair<uint8_t, uint8_t>(image[i][j], 1));
+            }
+        }
+        
+    }
+    return result;
+}
+
+
+std::array<std::array<uint8_t, 240>, 160>decompressGrayscale(std::vector<std::pair<uint8_t, uint8_t>>)
 {
     return {};
 }
--- homework/grayscale-image/compression.hpp
+++ homework/grayscale-image/compression.hpp
@@ -5,8 +5,11 @@
 #include <utility>
 
 using uint8_t = unsigned char;
-using bitmap = std::array<std::array<uint8_t, 240>, 160>;
-using compressed_bitmap = std::vector<std::pair<uint8_t, uint8_t>> ;
+constexpr size_t width = 240;
+constexpr size_t height = 160;
 
-compressed_bitmap compressGreyscale(bitmap);
 
+std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(std::array<std::array<uint8_t, 240>, 160>);
+
+
+std::array<std::array<uint8_t, 240>, 160>decompressGrayscale(std::vector<std::pair<uint8_t, uint8_t>>);
