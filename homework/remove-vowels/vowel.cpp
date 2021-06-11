8a12e0417566e97f20390abfbfa15fbe0f0435c8
Gyrydymptum <gyrydymptum@gmail.com>
2021-06-11 10:41:00 +0200

    adding const

--- homework/remove-vowels/vowel.cpp
+++ homework/remove-vowels/vowel.cpp
@@ -1,7 +1,11 @@
 #include "vowel.hpp"
+#include <algorithm>
+#include <iostream>
+#include <string>
+
 
 void removeVowels(std::vector<std::string>& words) {
-    std::string vowels = "aeiouyAEIOUY";
+    const std::string vowels = "aeiouyAEIOUY";
     for (int i = 0; i < vowels.length(); i++) {
         for (auto& el : words) {
             el.erase(std::remove(el.begin(), el.end(), vowels[i]), el.end());
