fb3e1007c393907eef99c2eb12dcbfac45292479
Mikhael <michal.grzelczak@gmail.com>
2021-06-11 22:30:37 +0200

    WIP

---
+++ homework/length-sort/sort.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <algorithm>
+#include <deque>
+#include <forward_list>
+#include <iostream>
+#include <list>
+#include <string>
+#include <iterator>
+
+std::deque<std::string> lengthSort(std::forward_list<std::string>& list);
