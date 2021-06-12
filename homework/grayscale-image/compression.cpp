0877cd430b1f423e9e08957a1d36634142f168c6
Łukasz Nitka <lukasnit@wp.pl>
2021-06-12 17:18:14 +0200

    f_comit

---
+++ homework/grayscale-image/compression.cpp
@@ -0,0 +1,65 @@
+#include "compression.hpp"
+
+std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(std::array<std::array<uint8_t, width>, height> array_1) {
+    std::vector<std::pair<uint8_t, uint8_t>> compres_vec;
+    uint8_t p = 0;
+    uint8_t number = 0;
+
+    if (array_1.empty()) {
+        return compres_vec;
+    } else {
+        p = array_1[0][0];
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (array_1[i][j] == p) {
+                    number++;
+                } else {
+                    compres_vec.push_back(std::make_pair(p, number));
+                    p = array_1[i][j];
+                    number = 1;
+                }
+            }
+        }
+
+        if (p == array_1[height - 1][width - 1]) {
+            compres_vec.push_back(std::make_pair(p, number));
+        }
+        return compres_vec;
+    }
+}
+
+std::array<std::array<uint8_t, width>, height> decompressGrayscale(std::vector<std::pair<uint8_t, uint8_t>> array_2) {
+    std::vector<uint8_t> tab;
+    tab.reserve(width * height);
+    std::array<std::array<uint8_t, width>, height> tab2;
+
+    for (int i = 0; i < array_2.size(); i++) {
+        int k = array_2[i].second;
+        for (int j = 0; j < k; j++) {
+            tab.push_back(array_2[i].first);
+        }
+    }
+
+    auto it = tab.cbegin();
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            tab2[i][j] = *it;
+            //std::cout<<int(*it);
+            it++;
+        }
+    }
+
+    return tab2;
+}
+
+void printMap(std::array<std::array<uint8_t, width>, height> map) {
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            std::cout << int(map[i][j]) << ' ';
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << std::endl;
+}
--- homework/grayscale-image/compression.hpp
+++ homework/grayscale-image/compression.hpp
@@ -1,11 +1,10 @@
 #pragma once
 #include <array>
-#include <vector>
 #include <iostream>
-constexpr size_t width = 5;
-constexpr size_t height = 2;
-
+#include <vector>
+constexpr size_t width = 0;
+constexpr size_t height = 0;
 
 std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(std::array<std::array<uint8_t, width>, height> array_1);
-std::array<std::array<uint8_t, width>, height> decompressGrayscale(std::vector<std::pair<uint8_t, uint8_t>>array_2);
+std::array<std::array<uint8_t, width>, height> decompressGrayscale(std::vector<std::pair<uint8_t, uint8_t>> array_2);
 void printMap(std::array<std::array<uint8_t, width>, height>);
