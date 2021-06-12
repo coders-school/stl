2794135ccc56a2c4296e7209c767a70cf3cb2d68
Krzysztof Kryczka <kw.kryczka@gmail.com>
2021-06-12 15:35:09 +0200

    compression.cpp empty function

---
+++ homework/grayscale-image/compression.cpp
@@ -0,0 +1,16 @@
+#include "compression.hpp"
+
+std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(const std::array<std::array<uint8_t, width>, height>& arr) {
+    std::vector<std::pair<uint8_t, uint8_t>> vec;
+
+    return vec;
+}
+
+std::array<std::array<uint8_t, width>, height> decompressGrayscale(const std::vector<std::pair<uint8_t, uint8_t>>& vec) {
+    std::array<std::array<uint8_t, width>, height> arr;
+
+    return arr;
+}
+
+void printMap(const std::array<std::array<uint8_t, width>, height>& arr) {
+}
