1f9c2145fb62d0da0f64814c9a5d87c62f5d5d2e
Krzysztof Kryczka <kw.kryczka@gmail.com>
2021-06-12 17:48:31 +0200

    Implement decompress bitmap

--- homework/grayscale-image/compression.cpp
+++ homework/grayscale-image/compression.cpp
@@ -9,6 +9,18 @@ std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(const std::array<std:
 std::array<std::array<uint8_t, width>, height> decompressGrayscale(const std::vector<std::pair<uint8_t, uint8_t>>& vec) {
     std::array<std::array<uint8_t, width>, height> arr;
 
+    auto x = 0;
+    auto y = 0;
+
+    for (auto el : vec) {
+        std::fill(&arr[x][y], &arr[x][y + el.second], el.first);
+        y += el.second;
+        if (y >= width) {
+            x++;
+            y = 0;
+        }
+    }
+
     return arr;
 }
 
