34bd9805f94328b9782bf53a288df933bef4c6c7
pZk <58830203+P-Z-K@users.noreply.github.com>
2021-06-12 14:15:58 +0200

    Adjust width and height property

--- homework/grayscale-image/compression.hpp
+++ homework/grayscale-image/compression.hpp
@@ -4,8 +4,8 @@
 #include <array>
 #include <utility>
 
-constexpr size_t width = 240;
-constexpr size_t height = 160;
+constexpr size_t width = 32;
+constexpr size_t height = 32;
 
 std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(
 	const std::array<std::array<uint8_t, width>, height>& bitmap);
