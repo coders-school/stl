e55f2751b23bac739b10e8b3e76fbf380d5a8596
Warp-12 <pl.walery@gmail.com>
2021-06-13 15:37:07 +0200

    Add decompresion function

--- homework/grayscale-image/compression.cpp
+++ homework/grayscale-image/compression.cpp
@@ -10,3 +10,25 @@ std::vector<std::pair<uint8_t, uint8_t>> tmpCompVec;
 
 
 };
+ std::array<std::array<uint8_t, width>, height> decompressGrayscale(std::vector<std::pair<uint8_t, uint8_t>> vectorData){ 
+  std::array<std::array<uint8_t, width>, height> bitmap;
+  size_t rowCounter = 0;
+  size_t columnCounter = 0;
+
+  for (const auto& pair : vectorData) {
+        if (rowCounter < height) {
+          for (size_t pairValue = 0; pairValue < pair.second; ++pairValue) {
+            bitmap[rowCounter][columnCounter] = pair.first;
+              if (columnCounter < width - 1) {
+                columnCounter++;
+              } else {
+                columnCounter = 0;
+                rowCounter++;
+              }
+          }
+        }
+ }
+
+    return bitmap
+}
+ 
