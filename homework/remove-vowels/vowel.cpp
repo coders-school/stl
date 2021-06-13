63bf7c51655ae18b5e5d43c3954264d3c0ca7891
Pawel Bieniasz <pawel.bieniasz@protonmail.com>
2021-06-13 11:09:15 +0200

    const vowels

--- homework/remove-vowels/vowel.cpp
+++ homework/remove-vowels/vowel.cpp
@@ -2,7 +2,7 @@
 
 void removeVowels(std::vector<std::string>& vector)
 {
-	std::string vowels{ "aAeEiIoOuUyY" };
+	const std::string vowels{ "aAeEiIoOuUyY" };
 	for (auto& str : vector) {
 		for (const auto& vow : vowels) {
 			str.erase(std::remove(str.begin(), str.end(), vow), str.end());
