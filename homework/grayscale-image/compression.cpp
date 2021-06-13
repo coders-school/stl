ff83b02167f840098e7a2dc0cc38475ce6f1e38a
Arek Ogiewa <arkadiusz.ogiewa@gmail.com>
2021-06-13 21:19:57 +0200

    compression.hpp and .cpp added, .hpp included in main.cpp

---
+++ homework/grayscale-image/compression.cpp
@@ -0,0 +1,15 @@
+#include "compression.hpp"
+#include <vector>
+#include <array>
+#include <utility>
+
+std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(std::array<std::array<uint8_t, 240>, 160>){
+
+    return 0;
+}
+
+std::array<std::array<uint8_t, 240>, 160> decompressGrayscale(std::vector<std::pair<uint8_t, uint8_t>>){
+
+    return 0;
+}
+
---
+++ homework/grayscale-image/compression.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <vector>
+#include <array>
+#include <utility>
+
+constexpr size_t width = 32;
+constexpr size_t height = 32;
+
+std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(std::array<std::array<uint8_t, 240>, 160>);
+
+std::array<std::array<uint8_t, 240>, 160> decompressGrayscale(std::vector<std::pair<uint8_t, uint8_t>>);
