87b49b8f98ad4a01001548028acb2484ed247931
Sebastian302653 <82955713+Sebastian302653@users.noreply.github.com>
2021-06-13 18:46:52 +0200

    Update vowel.cpp

--- homework/remove-vowels/vowel.cpp
+++ homework/remove-vowels/vowel.cpp
@@ -1,23 +1,9 @@
-#include <iostream>
-#include <string>
-#include "vowel.hpp"
-#include <vector>
-#include <algorithm>
-
-std::vector<std::string> removeVowels(std::vector<std::string> strvec)
-{
-    std::vector<char> vowels = {'a', 'e', 'i', 'o', 'u', 'y', 'A','E','I','O','U','Y'};
-
-    for(auto& j : strvec)
-    {
-        for (int i = 0; i < j.length(); i++)
-        {
-            if (std::find(vowels.begin(), vowels.end(), j[i]) != vowels.end())
-            {
-                j = j.replace(i, 1, "");
-                i -= 1;
-            }
+void removeVowels(std::vector<std::string>& words) {
+    const std::string vowels = "aeiouyAEIOUY";
+    for (int i = 0; i < vowels.length(); i++) {
+        for (auto& el : words) {
+            //std:: erase(words, vowels[i]); c++ 20
+            el.erase(std::remove(el.begin(), el.end(), vowels[i]), el.end());
         }
     }
-    return strvec;
 }
