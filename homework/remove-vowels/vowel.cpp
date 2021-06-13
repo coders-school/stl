f21e6986c8e0c2309e620b44a2bf41abfe66d892
Crazyayane07 <crazyayane07@gmail.com>
2021-06-13 15:34:06 +0200

    tweaks and fixes: - formated document - added forgotten 'y' and 'Y' to vowels vector

--- homework/remove-vowels/vowel.cpp
+++ homework/remove-vowels/vowel.cpp
@@ -1,16 +1,17 @@
 #include "vowel.hpp"
+
 #include <algorithm>
 
-void removeVowels(std::vector<std::string>& vec){
-	for (std::string& str : vec) {
-       removeVowelsFromString(str);
+void removeVowels(std::vector<std::string>& vec) {
+    for (std::string& str : vec) {
+        removeVowelsFromString(str);
     }
 }
 
-void removeVowelsFromString(std::string& str){
-	std::vector<char> vowels {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
-	
-	for(const char& vowel : vowels){
-		str.erase(remove(str.begin(), str.end(), vowel), str.end());
-	}
+void removeVowelsFromString(std::string& str) {
+    std::vector<char> vowels{'a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y'};
+
+    for (const char& vowel : vowels) {
+        str.erase(remove(str.begin(), str.end(), vowel), str.end());
+    }
 }
