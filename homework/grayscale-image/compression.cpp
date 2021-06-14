781013d6fd9163f36ac9085f826da2bf8fc9b45f
Katarzyna Strączyńska <katarzyna.a.straczynska@gmail.com>
2021-06-14 21:02:02 +0200

    Adding decompression & buggs fixed

--- homework/grayscale-image/compression.cpp
+++ homework/grayscale-image/compression.cpp
@@ -1,31 +1,61 @@
 #include "compression.hpp"
 #include <iostream>
 
-
-std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(std::array<std::array<uint8_t, width>, height> & compress_array)
+std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(std::array<std::array<uint8_t, width>, height> &compress_array)
 {
-    auto tmp = compress_array[0][0];
-
-    uint8_t count = 0;
 
     std::vector<std::pair<uint8_t, uint8_t>> vec;
     vec.reserve(width * height);
-
-    for (auto i = 0; i < height; i++)
+    
+    for (const auto &el : compress_array)
     {
-        for (auto j = 0; j < width; j++)
+        uint8_t tmp = el.front();
+        uint8_t count = 0;
+
+        for (auto j = el.cbegin(); j < el.cend(); j++)
         {
-            if (tmp == compress_array[j][i])
+            if (tmp != *j)
             {
-                count++;
+               vec.push_back(std::make_pair(tmp, count));
+                tmp = *j;
+                count = 1;
             }
             else
             {
-                vec.push_back(std::make_pair(tmp, count));
-                count = 0;
-                tmp = compress_array[j][i];
+                count++;
             }
+            if (j + 1 == el.cend())
+            {
+                vec.push_back(std::make_pair(tmp, count));
+            }
+            
         }
     }
+    vec.shrink_to_fit();
     return vec;
+}
+
+std::array<std::array<uint8_t, width>, height> decompressGrayscale(std::vector<std::pair<uint8_t, uint8_t>> &decompress)
+{
+
+    std::array<std::array<uint8_t, width>, height> arr;
+
+    auto column = 0;
+    auto row = 0;
+
+    for (const auto &pair : decompress)
+    {
+        for (uint8_t i = 0; i < pair.second; i++)
+        {
+            arr.at(row).at(column) = pair.first;
+            column++;
+        }
+        if (column == width)
+        {
+            row++;
+            column = 0;
+        }
+    }
+
+    return arr;
 }
