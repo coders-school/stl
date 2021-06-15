c7d7357724a517d4d8ac7dd771dc9165763c407c
Marcin$ <marcin.s10335@gmail.com>
2021-06-15 22:52:42 +0200

    Final solution function

--- homework/remove-vowels/vowel.cpp
+++ homework/remove-vowels/vowel.cpp
@@ -8,22 +8,8 @@ bool isVowel(char c) {
     return (c == 'a' || c == 'e' || c == 'o' || c == 'u' || c == 'i' || c == 'y');
 }
 
-// void removeVowels(std::vector<std::string>& words) {
-//     std::vector<char> vowels = {'a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y'};
-//     std::string temp = "";
-//     for (auto& a : words) {
-//         for (int i = 0; i < a.size(); ++i) {
-//             if (!(std::find(vowels.begin(),vowels.end(),a[i]) != vowels.end())) {
-//                 temp.append(1,a[i]);
-//             }
-//         }
-//         a = temp;
-//         temp = "";
-//     }
-// }
-
 void removeVowels(std::vector<std::string>& words) {
     for (auto& a : words) {
-        a.erase(std::remove_if(a.begin(), a.end(), isVowel),a.end());  
+        a.erase(std::remove_if(a.begin(), a.end(), isVowel), a.end());
     }
 }
