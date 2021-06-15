6f71938de61ae28bbc225fac798aa746bacbe827
Paweł Głowacki <glowacki.pawel89@gmail.com>
2021-06-15 17:47:46 +0200

    initial commit

---
+++ homework/length-sort/sort.cpp
@@ -0,0 +1,17 @@
+#include "sort.hpp"
+
+std::deque<std::string> lengthSort(std::forward_list<std::string>& list) {
+    std::deque<std::string> temp;
+
+    list.sort();
+    for(auto it = list.begin(); it != list.end(); ++it) {
+        temp.push_back(*it);
+    }
+
+    std::sort(temp.begin(), temp.end(), []
+    (const std::string& first, const std::string& second) {
+        return first.size() < second.size();
+    });
+
+    return temp;
+} 
---
+++ homework/length-sort/sort.hpp
@@ -0,0 +1,7 @@
+#include <deque>
+#include <string>
+#include <forward_list>
+#include <iostream>
+#include <algorithm>
+
+std::deque<std::string> lengthSort(std::forward_list<std::string>&); 
