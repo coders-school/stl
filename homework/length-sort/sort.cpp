b7d15724abad51f60782547318d2a3b409ddafec
kondorski <thegreatvaultofknowledge@gmail.com>
2021-06-16 16:58:16 +0200

    sortlength finished

--- homework/length-sort/sort.cpp
+++ homework/length-sort/sort.cpp
@@ -1,1 +1,21 @@
 #include "sort.hpp"
+
+bool compareSize(const std::string &a, const std::string &b)
+{
+    if (a.size() == b.size())
+    {
+        return std::lexicographical_compare(a.begin(), a.end(), b.begin(), b.end());
+    }
+    return a.size() < b.size();
+}
+
+std::deque<std::string> lengthSort(std::forward_list<std::string> &inputList)
+{
+    std::deque<std::string> sortedList;
+    for (auto &el : inputList)
+    {
+        sortedList.push_back(el);
+    }
+    std::sort(sortedList.begin(), sortedList.end(), compareSize);
+    return sortedList;
+}
--- homework/length-sort/sort.hpp
+++ homework/length-sort/sort.hpp
@@ -2,5 +2,7 @@
 #include <deque>
 #include <string>
 #include <forward_list>
+#include <algorithm>
 
-std::deque<std::string> lengthSort (std::forward_list<std::string>);
+bool compareSize (const std::string&, const std::string&);
+std::deque<std::string> lengthSort (std::forward_list<std::string>&);
