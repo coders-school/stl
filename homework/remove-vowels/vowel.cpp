708d635ae913b4deadb4d5e43dd541aac63e955e
bartosz-gruszczyk <bartosz.gruszczyk@gmail.com>
2021-06-16 20:52:00 +0200

    corrected and cleaned

--- homework/remove-vowels/vowel.cpp
+++ homework/remove-vowels/vowel.cpp
@@ -1,30 +1,18 @@
 #include "vowel.hpp"
-#include <iostream>
 
 void removeVowels(std::string& text) {
-    // for(size_t i = 0; i < text.size(); ++i) {
-    //     if(text.at(i) == 'a')
-    // }
-    auto it = text.begin();
-    // for(; it != text.end(); it++) {
-    for(; it != text.end();) {
-        // std::cout << *it << " ";
+    for(auto it = text.begin(); it != text.end();) {
         if(*it == 'a' || *it == 'e' || *it == 'i' || *it == 'o' || *it == 'u' || *it == 'y'||
            *it == 'A' || *it == 'E' || *it == 'I' || *it == 'O' || *it == 'U' || *it == 'Y') {
             text.erase(it);
-        } 
-        else {
-            it++;
+        } else {
+            ++it;
         }
     }
 }
 
 void removeVowels(std::vector<std::string>& words) {
-    
     for(std::string& singleString : words) {
-        // std::cout << singleString << std::endl;
-        // std::cout << "przynajmnie tu jestem" << std::endl;
         removeVowels(singleString);
     }
-
 }
