4dad7e929bd38b622f85d83d6d0dc939de95af8d
bartosz-gruszczyk <bartosz.gruszczyk@gmail.com>
2021-06-16 22:57:55 +0200

    some improvements and code cleaning

--- homework/length-sort/sort.cpp
+++ homework/length-sort/sort.cpp
@@ -1,22 +1,16 @@
-#include <iostream>
 #include "sort.hpp"
 
 std::deque<std::string> lengthSort(std::forward_list<std::string> inputList) {
-
-    std::deque<std::string> sortedDeque; //resize?
+    std::deque<std::string> sortedDeque;
     
     while(!inputList.empty()) {
         auto mainIt = inputList.begin();
         auto mainItBefore = inputList.before_begin();
         auto comparedValueIt = inputList.begin();
-        auto comparedValueItBefore = inputList.before_begin();
-
+        auto comparedValueItBefore = inputList.before_begin();       
         for (; mainIt != inputList.end(); ++mainIt, ++mainItBefore) {
-            if ((*mainIt).length() < (*comparedValueIt).length()) {
-                comparedValueIt = mainIt;
-                comparedValueItBefore = mainItBefore;
-            }
-            else if ((*mainIt).length() == (*comparedValueIt).length() && *mainIt < *comparedValueIt) {
+            if ((*mainIt).length() < (*comparedValueIt).length()
+                || ((*mainIt).length() == (*comparedValueIt).length() && *mainIt < *comparedValueIt)) {
                 comparedValueIt = mainIt;
                 comparedValueItBefore = mainItBefore;
             }
