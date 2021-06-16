58ed1ed93cacb828168d5d811f92b8c888c67515
bartosz-gruszczyk <bartosz.gruszczyk@gmail.com>
2021-06-16 20:36:11 +0200

    first working solution... with shitloads of rubbish

---
+++ homework/grayscale-image/compression.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <array>
+#include <cstdint>
+#include <vector>
+#include <utility>
+
+constexpr size_t width = 32;
+constexpr size_t height = 32;
+
+std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(const std::array<std::array<uint8_t, width>, height>& image);
+std::array<std::array<uint8_t, width>, height> decompressGrayscale(const std::vector<std::pair<uint8_t, uint8_t>>& compressedImage);
+void printMap(std::array<std::array<uint8_t, width>, height> map);
