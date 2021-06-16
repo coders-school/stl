e800873c7be5240f32b65752868ac715d5c9bf66
kondorski <thegreatvaultofknowledge@gmail.com>
2021-06-16 17:42:09 +0200

    new files and includes

---
+++ homework/grayscale-image/compression.cpp
@@ -0,0 +1,9 @@
+#include "compression.hpp"
+
+std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale (std::array<std::array<uint8_t, width>, height>){
+
+}
+
+std::array<std::array<uint8_t, width>, height> decompressGrayscale (std::vector<std::pair<uint8_t, uint8_t>>){
+
+}
