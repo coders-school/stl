b27a3f0155fe8e241bab60b9b815776be09b3b66
kondorski <thegreatvaultofknowledge@gmail.com>
2021-06-20 16:45:49 +0200

    working compression

--- homework/grayscale-image/compression.cpp
+++ homework/grayscale-image/compression.cpp
@@ -1,46 +1,59 @@
 #include "compression.hpp"
-#include <iostream>
 
-void DebugPrint(std::vector<std::pair<uint8_t, uint8_t>> &vec)
+std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(std::array<std::array<uint8_t, width>, height> &uncompressed)
 {
-    for (auto &el : vec)
+    std::vector<std::pair<uint8_t, uint8_t>> compressed;
+    compressed.reserve(width * height);
+    for (const auto &row : uncompressed)
     {
-        std::cout << el.first << " | " << el.second << "\n";
-    }
-}
-
-std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale (std::array<std::array<uint8_t, width>, height> &bitmap)
-{
-    std::vector<std::pair<uint8_t, uint8_t>> compressedBitmap;
-    compressedBitmap.reserve(width*height);
-    for(const auto &el : bitmap)
-    {
-        uint8_t colour = el.front();
+        auto colour = row.front();
         uint8_t counter = 0;
-        std::cout << colour;
-        for (auto i = el.begin(); i < el.end(); i++)
+        auto it = row.cbegin();
+        //for (auto it = row.cbegin(); it < row.cend(); it++) 
+        for(size_t i {0}; i < width; i++)
         {
-            if (colour != *i)
+            if(colour != *it)
             {
-                compressedBitmap.push_back(std::make_pair(colour, counter));
-                colour = *i;
+                compressed.push_back(std::make_pair(colour, counter));
+                colour = *it;
                 counter = 1;
             }
             else
             {
                 counter++;
             }
-            if (i + 1 == el.end())
+            if(it + 1 == row.cend())
             {
-                compressedBitmap.push_back(std::make_pair(colour, counter));
+                compressed.push_back(std::make_pair(colour, counter));
             }
-            
+            std::advance(it, 1);
         }
     }
-    compressedBitmap.shrink_to_fit();
-    return compressedBitmap;
+    compressed.shrink_to_fit();
+    return compressed;
 }
 
-std::array<std::array<uint8_t, width>, height> decompressGrayscale (std::vector<std::pair<uint8_t, uint8_t>>&){
-    
+
+std::array<std::array<uint8_t, width>, height> decompressGrayscale(std::vector<std::pair<uint8_t, uint8_t>> &compressed)
+{
+    std::array<std::array<uint8_t, width>, height> uncompressed;
+
+    auto column = 0;
+    auto row = 0;
+
+    for (const auto &pair : compressed)
+    {
+        for (auto i = 0; i < pair.second; i++)
+        {
+            uncompressed[row][column] = pair.first;
+            column++;
+        }
+        if (column == width)
+        {
+            row++;
+            column = 0;
+        }
+    }
+
+    return uncompressed;
 }
