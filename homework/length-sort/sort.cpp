fdf3aa436ea68776d8d271589543cebb4045fedb
PawelYak <pawelstojak@gmail.com>
2021-07-04 18:06:30 +0200

    lengthSort implementation

--- homework/length-sort/sort.cpp
+++ homework/length-sort/sort.cpp
@@ -8,23 +8,14 @@
 #include <vector>
 
 std::deque<std::string> lengthSort(std::forward_list<std::string> list) {
-
-list.sort();
-  std::vector<std::string> vec{};
+  list.sort();
   std::deque<std::string> deq{};
+
   for (const auto &it : list) {
-    // std::cout << it << "\n";
-    vec.push_back(it);
-  };
-
-  std::sort(vec.begin(), vec.end(),[](const auto& a, const auto&b){return a.size()>b.size();});
-
-  for (const auto &it : vec) {
-
-    // std::cout << it << "\n";
     deq.push_back(it);
   };
 
-  // std::deque<std::string> deq{"dupa"};
+  std::sort(deq.begin(), deq.end(),
+            [](const auto &a, const auto &b) { return a.size() < b.size(); });
   return deq;
 };
