5c1d508d60acabca70ff331435ec8aab1b953a48
LechWilk <lech.wilk@gmail.com>
2021-07-13 10:26:13 +0200

    compilled - used lexicographical_compare oparation right after lengt comparison

--- homework/length-sort/sort.cpp
+++ homework/length-sort/sort.cpp
@@ -2,35 +2,24 @@
 #include<cstring>
 
 
-bool compare(std::string str1, std::string str2)    {
-    int len1 = str1.length();
-    int len2 = str2.length();
-    if(len1<len2)   return true;
-    if(len1>len2)   return false;
-    if(len1==len2)  {
-        if(str1.compare(str2)<0) return true;
-        return false;
+bool compare(std::string& str1, std::string& str2)    {
 
-        /*auto letter2=str2.begin();
-        for(char& letter:str1)  {
-            //jak porównać chary z uwzględnieniem wielkości litery??
-            
-            if(letter<letter2)  return true;
-            if(letter>letter2)  return false;
-            letter2++;
-        }   */
-    }
+    if(str1.length()<str2.length())   return true;
+    if(str1.length()==str2.length())
+        if(std::lexicographical_compare(str1.begin(),str1.end(),str2.begin(),str2.end()))
+            return true;
+
+    return false;
 }
 
 std::deque<std::string> lengthSort(std::forward_list<std::string>& input) {
 
 std::deque<std::string> output;
 //sortowanie podobno działa na forward_liście
+input.sort(compare);
 
-output = input.sort(compare);
-
-
-
+//przepisanie do deque
+for(auto&el:input)  output.emplace_back(el);
 return output;
 }
 
