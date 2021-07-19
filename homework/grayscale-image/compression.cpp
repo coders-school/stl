6748e5c54ea2a7ef1a938c42baca0061f9b4aefa
LechWilk <lech.wilk@gmail.com>
2021-07-19 09:44:30 +0200

    decompression done

--- homework/grayscale-image/compression.cpp
+++ homework/grayscale-image/compression.cpp
@@ -16,17 +16,17 @@ for(auto& el_h:bitmap)  {               //przeglądanie po wierszach
                 p.second++;
             else    {
                 out.push_back(p);
-                // std::cout<<'{'<<(int)p.first<<", "<<(int)p.second<<"} ";
                 p.first=el_v;
                 p.second=1;
                 }
             }
        }
         out.push_back(p);               //załadowanie ostatniego elementu
-        //std::cout<<'{'<<(int)p.first<<", "<<(int)p.second<<"} \n";
 
     }
 
+//.first -  wartość pixela
+//.secound - ilość pixeli o danej wartości
 return out;
 }
 
@@ -35,5 +35,28 @@ return out;
 std::array<std::array<uint8_t,width>,height>  decompressGrayscale(std::vector<std::pair<uint8_t,uint8_t>>  compressed)   {
 std::array<std::array<uint8_t,width>,height> out;
 
+auto input_it = compressed.begin();
+
+for(auto& row:out)  {
+    uint8_t ix = 0;         //index do zliczania elementów o danej wartości, które już są przepisane do bitmapy
+    for(auto& el:row)    {
+        el=input_it->first;
+        if(++ix==input_it->second)  {
+            input_it++;
+            ix=0;
+        }        
+    }
+}
+
 return out;
+}
+
+void printMap(std::array<std::array<uint8_t,width>,height> bitmap)  {
+    for(auto& el_h:bitmap)  {               //przeglądanie po wierszach
+        for(auto& el_v:el_h)    {           //przeglądanie po kolumnach (el. w wierszu)
+            std::cout<<(char)(el_v+32);
+        }
+        std::cout<<'\n';
+    }
+    std::cout<<'\n';
 }
