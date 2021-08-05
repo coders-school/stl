c81d08a97f1c579fdb259f25e4611650a78dfdfc
Piotr Rogalski <progalski89@gmail.com>
2021-08-05 22:04:38 +0200

    Another minor fix

--- homework/insensitive-palindrom/palindrome.cpp
+++ homework/insensitive-palindrom/palindrome.cpp
@@ -2,8 +2,8 @@
 #include <string>
 
 bool is_palindrome(std::string str) {
-    std::transform(str.begin(), str.end(), str.begin(), [](const auto& c) { return std::toupper(c); });
-    auto it = std::remove_if(str.begin(), str.end(), [](const auto& c) {
+    std::transform(str.begin(), str.end(), str.begin(), [](const auto c) { return std::toupper(c); });
+    auto it = std::remove_if(str.begin(), str.end(), [](const auto c) {
         if (c < 65 || c > 90) {
             return true;
         }
