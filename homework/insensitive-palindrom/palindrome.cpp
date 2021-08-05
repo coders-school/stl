210e80655a6c97939a63b670d2df80ad505be9a5
Piotr Rogalski <progalski89@gmail.com>
2021-08-05 21:48:40 +0200

    First working iteration of excercise

---
+++ homework/insensitive-palindrom/palindrome.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <string>
+
+bool is_palindrome(std::string str) {
+    std::transform(str.begin(), str.end(), str.begin(), [](const auto& c) { return std::toupper(c); });
+    auto it = std::remove_if(str.begin(), str.end(), [&](const auto& c) {
+        if (c < 65 || c > 90) {
+            return true;
+        }
+        return false;
+    });
+    str.erase(it, str.end());
+    std::string result = str;
+    std::reverse(result.begin(), result.end());
+    return result == str;
+}
