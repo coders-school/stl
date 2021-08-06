a0e23fede7273c06a044f9a8221d9c4b09953e1c
Piotr Rogalski <progalski89@gmail.com>
2021-08-06 17:03:05 +0200

    Suggestion applied

--- homework/insensitive-palindrom/palindrome.cpp
+++ homework/insensitive-palindrom/palindrome.cpp
@@ -4,7 +4,7 @@
 bool is_palindrome(std::string str) {
     std::transform(str.begin(), str.end(), str.begin(), [](const auto c) { return std::toupper(c); });
     auto it = std::remove_if(str.begin(), str.end(), [](const auto c) {
-        if (c < 65 || c > 90) {
+        if (c < 'A' || c > 'Z') {
             return true;
         }
         return false;
