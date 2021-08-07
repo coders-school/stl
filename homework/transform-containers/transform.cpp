daf823fec187355d09ef27ef545e4493f639be9f
Mateusz Serafin <mateusz97.serafin@gmail.com>
2021-08-07 17:23:57 +0200

    added list and queue comparison before std::transform

--- homework/transform-containers/transform.cpp
+++ homework/transform-containers/transform.cpp
@@ -7,13 +7,16 @@ std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string
     dq.erase(std::unique(dq.begin(), dq.end()),dq.end());
     li.sort();
     li.unique();
-    std::vector<std::pair<int, std::string>>tempMap;
+    std::vector<std::pair<int, std::string>>tempMap(dq.size());
     std::map<int, std::string> map;
+    if(li.size() < dq.size()){
+        return map;
+    }
 
     std::transform(dq.begin(), 
                    dq.end(), 
-                   li.begin(), 
-                   std::back_inserter(tempMap),
+                   li.begin(),
+                   tempMap.begin(),
                    [&map](auto first, auto second){
             std::pair<int, std::string> temp(first, second);
             map.insert(temp);
