37f485c1a421c92647100296455d8d94b2482a75
Gyrydymptum <gyrydymptum@gmail.com>
2021-08-08 18:39:42 +0200

    solution passed test

--- homework/transform-containers/transform.cpp
+++ homework/transform-containers/transform.cpp
@@ -1,16 +1,4 @@
-// Napisz funkcję transformContainers, która przyjmie std::list<std::string> oraz std::deque<int>
-// Usunie duplikaty z obu kontenerów
-// Na koniec skonwertuje to na std::map<int, std::string> i ją zwróci. Użyj std::transform.
-// Zajrzyj do pliku z test.cpp i na jego podstawie wywnioskuj jak się ma nazywać funkcja, którą musisz zaimplementować.
 
-// Plik nagłówkowy oraz źródłowy z implementacją funkcji nazwij tak, jak Ci się wydaje, że będzie pasować :)
-
-// Dodaj odpowiednie #include do test.cpp
-
-// Dodaj swój plik źródłowy cpp do pliku CMakeLists.txt, aby również był on kompilowany. Znajdziesz tam odpowiednie sekcje pod koniec:
-
-// add_executable(${PROJECT_NAME}-ut test.cpp) # add your cpp file here after test.cpp
-// # it this is problematic take a look into CMakeLists.txt files in other exercises
 #include "transform.hpp"
 #include <algorithm>
 #include <iostream>
@@ -21,5 +9,12 @@ std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string
     std::sort(deq.begin(), deq.end());
     deq.erase(std::unique(deq.begin(), deq.end()), deq.end());
     std::map<int, std::string> map;
+    if (lis.size() != deq.size()) {
+        std::cerr << "Containers must be of the same size";
+    } else {
+        //https://stackoverflow.com/questions/46948042/using-stdtransform-to-convert-a-stdvectorstructkey-val-into-a-stdmap
+        std::transform(deq.begin(), deq.end(), lis.begin(), std::inserter(map, map.end()),
+                       [](auto key, auto value) { return std::make_pair(key, value); });
+    }
     return map;
 }
