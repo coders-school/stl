b94b282752d175a6efbc70a42ecff7798f97177a
Marcin Mazurek <marcinmazurek06@gmail.com>
2021-08-09 15:30:46 +0200

    Replace loops by algorithms

--- homework/grayscale-image/compression.cpp
+++ homework/grayscale-image/compression.cpp
@@ -1,51 +1,55 @@
 #include "compression.hpp"
 
-std::vector<std::pair<uint8_t, uint8_t>> 
+#include <algorithm>
+
+std::vector<std::pair<uint8_t, uint8_t>>
 compressGrayscale(std::array<std::array<uint8_t, width>, height> arr)
 {
-    std::vector<std::pair<uint8_t, uint8_t>> vec {};
-    size_t cnt{1};
+    std::vector<std::pair<uint8_t, uint8_t> > vecP{};
+    std::vector<uint8_t> vec{};
 
-    for(auto it = arr.begin(); it != arr.end(); ++it)
-    {
-        for(auto jt = it->begin(); jt != it->end(); ++jt)
-        {
-            if(jt == it->end()-1 || *jt != *(jt+1))
-            {
-                vec.push_back(std::make_pair(*jt, cnt));
-                cnt = 1;
-            }
-            else
-            {
-                ++cnt;
-            }
+    std::transform(begin(arr), end(arr), std::back_inserter(vec), [&vec](auto a) {
+        return std::copy(begin(a), end(a), std::back_inserter(vec));
+    });
+
+    auto m = vec.begin();
+    auto inc{ 1 };
+    uint8_t cnt{ 1 };
+
+    std::transform(begin(vec), end(vec), std::back_inserter(vecP), [&](auto a) {
+        if ((inc++ % width == 0) || m != std::adjacent_find(m, vec.end())) {
+            auto cnt_temp = cnt;
+            cnt = 1;
+            ++m;
+            return (std::make_pair(a, cnt_temp));
+        } else {
+            ++cnt;
+            ++m;
+            return (std::make_pair(static_cast<uint8_t>(0), static_cast<uint8_t>(0)));
         }
-    }
+    });
 
-    return vec;
+    vecP.erase(std::remove(begin(vecP), end(vecP),
+                   std::make_pair(static_cast<uint8_t>(0), static_cast<uint8_t>(0))),
+        end(vecP));
+
+    return vecP;
 }
 
 std::array<std::array<uint8_t, width>, height>
-decompressGrayscale(std::vector<std::pair<uint8_t, uint8_t>> vec)
+decompressGrayscale(std::vector<std::pair<uint8_t, uint8_t>> vecP)
 {
-    std::array<std::array<uint8_t, width>, height> out_arr {};
+    std::array<std::array<uint8_t, width>, height> out_arr{};
+    std::vector<int> vec{};
+    auto cnt{ 1 };
 
-    size_t sum{}, row{}, col{};
-    for(auto const& el : vec)
-    {
-        sum += el.second;
-        for(uint8_t cnt = 0; cnt<el.second; ++cnt)
-        {
-            out_arr[row][col++] = el.first;
-        }
+    std::transform(begin(vecP), end(vecP), std::back_inserter(vec), [&](auto p) {
+        ++cnt;
+        return (std::move(vec.insert(end(vec), p.second, p.first)),
+            end(vec), std::back_inserter(vec));
+    });
 
-        if(sum == width)
-        {
-            sum = 0;
-            col = 0;
-            if(++row > height) break;
-        }
-    }
+    std::copy(begin(vec), end(vec), out_arr.front().begin());
 
     return out_arr;
 }
