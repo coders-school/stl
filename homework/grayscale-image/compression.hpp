774d4a36de0cb7991774b28e5cf5c538fc7e4407
Marcin Mazurek <marcinmazurek06@gmail.com>
2021-08-09 15:30:46 +0200

    Replace loops by algorithms

--- homework/grayscale-image/compression.hpp
+++ homework/grayscale-image/compression.hpp
@@ -1,13 +1,13 @@
 #pragma once
 
 #include <array>
-#include <vector>
 #include <iostream>
+#include <vector>
 
 constexpr size_t width = 32;
 constexpr size_t height = 32;
 
-std::vector<std::pair<uint8_t, uint8_t>> 
+std::vector<std::pair<uint8_t, uint8_t>>
 compressGrayscale(std::array<std::array<uint8_t, width>, height> arr);
 
 std::array<std::array<uint8_t, width>, height>
