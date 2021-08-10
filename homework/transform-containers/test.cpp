fcd1080813b80621670eb0e243a1f88b07b5d296
Piotr <piotrku91@github.com>
2021-08-10 13:11:26 +0200

    working copy

--- homework/transform-containers/test.cpp
+++ homework/transform-containers/test.cpp
@@ -1,6 +1,5 @@
 #include "gtest/gtest.h"
-
-// TODO: add proper includes
+#include "transformation.hpp"
 
 TEST(transformContainerTests, ShouldReturnUniqueMap) {
     std::map<int, std::string> expected_result{
---
+++ homework/transform-containers/transformation.cpp
@@ -0,0 +1,15 @@
+#include "transformation.hpp"
+
+
+std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string> listCont,std::deque<int> deqCont)
+{
+listCont.sort();
+listCont.erase(std::unique(listCont.begin(),listCont.end()),listCont.end());
+std::sort(deqCont.begin(),deqCont.end());
+deqCont.erase(std::unique(deqCont.begin(),deqCont.end()),deqCont.end());
+    std::map<int, std::string> newMap;
+    std::transform(deqCont.begin(), deqCont.end(),
+    listCont.begin(), std::inserter(newMap,newMap.end()),
+    [](auto k, auto& v) { return std::make_pair(k, v); });
+return newMap;
+};
