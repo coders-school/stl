34350ef43c793554bf592ac7f487ab1512e9364c
Łukasz Meisnerowski <lukasz.meisnerowski@gmail.com>
2021-08-11 20:23:49 +0200

    Adding const to for loop and lambda parameters

--- homework/length-sort/sort.cpp
+++ homework/length-sort/sort.cpp
@@ -7,10 +7,10 @@
 std::deque<std::string> lengthSort(std::forward_list<std::string> & f_list){
     f_list.sort();
     std::deque<std::string> d_list;
-    for(auto & el : f_list) {
+    for(const auto & el : f_list) {
         d_list.push_back(el);
     }
 
-    std::sort(d_list.begin(), d_list.end(), [](std::string & str1, std::string & str2){return str1.size() < str2.size();});
+    std::sort(d_list.begin(), d_list.end(), [](const std::string & str1,const std::string & str2){return str1.size() < str2.size();});
     return d_list;
 }
