f6e764cc2626e0d262a67ad8854963651006988b
Łukasz Meisnerowski <lukasz.meisnerowski@gmail.com>
2021-08-11 20:14:28 +0200

    added references to lambda in sort algorithm

--- homework/length-sort/sort.cpp
+++ homework/length-sort/sort.cpp
@@ -11,6 +11,6 @@ std::deque<std::string> lengthSort(std::forward_list<std::string> & f_list){
         d_list.push_back(el);
     }
 
-    std::sort(d_list.begin(), d_list.end(), [](std::string str1, std::string str2){return str1.size() < str2.size();});
+    std::sort(d_list.begin(), d_list.end(), [](std::string & str1, std::string & str2){return str1.size() < str2.size();});
     return d_list;
 }
