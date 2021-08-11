33b31f98dc371dbb689d71a0e532c5eabc37ce5e
BarTes8 <barbara.tessema@gmail.com>
2021-08-11 18:51:57 +0200

    decompression made with all_of

--- homework/grayscale-image/compression.cpp
+++ homework/grayscale-image/compression.cpp
@@ -14,41 +14,15 @@ compressedGrayscaleImage compressGrayscale(const grayscaleImage& bitmap) {
         }
         return true;
     });
-
-    //     std::transform(row.begin(), row.end(), std::back_inserter(compressed), [&](auto number) -> int {
-    //         auto fountIt = std::adjacent_find(it, row.end(), std::not_equal_to<int>{});
-    //         if (fountIt != row.end()) {
-    //             fountIt++;
-    //         }
-    //         compressed.emplace_back(*it, std::distance(it, fountIt));
-    //         it = fountIt;
-    //     });
-    // });
-    // for (const auto& row : bitmap) {
-    //     for (auto it = row.begin(); it != row.end(); ) {
-    //         auto fountIt = std::adjacent_find(it, row.end(), std::not_equal_to<int>{});
-    //         if (fountIt != row.end()) {
-    //             fountIt++;
-    //         }
-    //         compressed.emplace_back(*it, std::distance(it, fountIt));
-    //         it = fountIt;
-    //     }
-    // }
     return compressed;
 }
 
 grayscaleImage decompressGrayscale(const compressedGrayscaleImage& compressed) {
     grayscaleImage decompressed{};
     auto it = decompressed.front().begin();
-    auto itEnd = decompressed.back().end();
-    std::generate (it, itEnd, [i = 0, j = 0, compressed] () mutable{
-            if ( j < compressed[i].second ){
-                ++j;
-            } else {
-                ++i;
-                j = 1;
-            }
-            return (compressed[i].first);
+    std::all_of(compressed.begin(), compressed.end(), [it](const auto& pair) mutable {
+        it = std::fill_n(it, pair.second, pair.first);
+        return true;
     });
     return decompressed;
 }
