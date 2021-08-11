c16c3d6795c0ee2c73a4f749be406243b9518d1d
BarTes8 <barbara.tessema@gmail.com>
2021-08-11 19:00:41 +0200

    changed for to while

--- homework/grayscale-image/compression.cpp
+++ homework/grayscale-image/compression.cpp
@@ -4,7 +4,9 @@ compressedGrayscaleImage compressGrayscale(const grayscaleImage& bitmap) {
     compressedGrayscaleImage compressed;
     compressed.reserve(width * height);
     std::all_of(bitmap.begin(), bitmap.end(), [&compressed](auto row) {
-        for (auto it = row.begin(); it != row.end(); ) {
+        // for (auto it = row.begin(); it != row.end(); ) 
+        auto it = row.begin();
+        while (it != row.end()) {
             auto fountIt = std::adjacent_find(it, row.end(), std::not_equal_to<int>{});
             if (fountIt != row.end()) {
                 fountIt++;
