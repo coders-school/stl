38850b77f8db64e5d0608f03524fcde5a7522e87
Łukasz Meisnerowski <lukasz.meisnerowski@gmail.com>
2021-08-11 20:09:38 +0200

    Added sort forweard list before adding items from it to deque

---
+++ homework/length-sort/sort.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <forward_list>
+#include <string>
+#include <deque>
+#include <algorithm>
+
+std::deque<std::string> lengthSort(std::forward_list<std::string> & f_list){
+    f_list.sort();
+    std::deque<std::string> d_list;
+    for(auto & el : f_list) {
+        d_list.push_back(el);
+    }
+
+    std::sort(d_list.begin(), d_list.end(), [](std::string str1, std::string str2){return str1.size() < str2.size();});
+    return d_list;
+}
---
+++ homework/length-sort/sort.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+#include <forward_list>
+#include <string>
+#include <deque>
+#include <algorithm>
+
+std::deque<std::string> lengthSort(std::forward_list<std::string> &);
