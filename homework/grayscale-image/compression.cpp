61505eb5ba1b3d7d0ff8aeef1ec27c23cc3c318c
Kacu <kacper.kaleta@scythe-studio.com>
2021-08-12 02:32:35 +0200

    removed every for and while keyword

--- homework/grayscale-image/compression.cpp
+++ homework/grayscale-image/compression.cpp
@@ -1,34 +1,57 @@
 #include "compression.hpp"
 
 #include <algorithm>
+#include <functional>
 
 Compressed compressGrayscale(const Decompressed& input) {
     Compressed result;
 
-    auto compression = [&result](const auto& value) { 
-        if (value == result.back().first) {
+    auto rowIterator = input.begin();
+    const uint8_t* colIterator; // = rowIterator->begin();
+
+    std::function<void()> compression;
+    compression = [&]() { 
+        if (*colIterator == result.back().first) {
             ++(result.back().second);
-            return;
         }
-        result.emplace_back(value, 1);
+        else {
+            result.emplace_back(*colIterator, 1);
+        }
+        if (++colIterator != rowIterator->end()) {
+            std::invoke(compression);
+        }
     };
-    auto processRows = [&result, compression](const auto& row) {
-        result.emplace_back(row.front(), 0);
-        std::for_each(row.begin(), row.end(), compression);
+    std::function<void()> processRows;
+    processRows = [&]() {
+        result.emplace_back(rowIterator->front(), 0);
+
+        colIterator = rowIterator->begin();
+        std::invoke(compression);
+
+        if (++rowIterator != input.end()) {
+            std::invoke(processRows);
+        }
     };
-    std::for_each(input.begin(), input.end(), processRows);
+    std::invoke(processRows);
+
     return result;
 }
 
 Decompressed decompressGrayscale(const Compressed& input) {
     Decompressed result;
+    std::function<void()> decompression;
+    auto it = input.begin();
     auto position = result.begin()->begin();
     
-    auto decompression = [&position](const auto& pair) {
-        const auto& [value, number] = pair;
+    decompression = [&position, &decompression, &it, &input]() {
+        const auto& [value, number] = *it;
         std::fill_n(position, number, value);
         position += number;        
+        
+        if (++it != input.end()) {
+            std::invoke(decompression);
+        }
     };
-    std::for_each(input.begin(), input.end(), decompression);
+    std::invoke(decompression);
     return result;
 }
