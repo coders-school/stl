fa41e3cb62b8a3666cb75ea6cd2b69227ce9eae6
Piotr Rogalski <progalski89@gmail.com>
2021-08-13 21:22:33 +0200

    Basic structure

---
+++ homework/inner-product/arithmeticAverage.cpp
@@ -0,0 +1,10 @@
+#include "arithmeticAverage.hpp"
+#include <vector>
+
+double ArithmeticAverage(const std::vector<int>& vec1, const std::vector<int>& vec2) {
+    return 0.00;
+}
+
+double Distance(const std::vector<int>& vec1, const std::vector<int>& vec2) {
+    return 0.00;
+}
---
+++ homework/inner-product/arithmeticAverage.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <vector>
+
+double ArithmeticAverage(const std::vector<int>& vec1, const std::vector<int>& vec2);
+double Distance(const std::vector<int>& vec1, const std::vector<int>& vec2);
