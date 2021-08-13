ad24a955048242766bdf36314de28d4dcd26dbe7
Piotr Rogalski <progalski89@gmail.com>
2021-08-13 22:53:10 +0200

    First working version

--- homework/inner-product/arithmeticAverage.cpp
+++ homework/inner-product/arithmeticAverage.cpp
@@ -1,10 +1,25 @@
 #include "arithmeticAverage.hpp"
+#include <functional>
+#include <numeric>
+#include <stdexcept>
 #include <vector>
+#include <cmath>
+#include <iostream>
 
 double ArithmeticAverage(const std::vector<int>& vec1, const std::vector<int>& vec2) {
-    return 0.00;
+    if (vec1.size() != vec2.size()) {
+        throw std::invalid_argument("ArithmeticAverage - arguments are not equal in size.");
+    }
+    return std::transform_reduce(vec1.cbegin(), vec1.cend(), vec2.cbegin(), 0, std::plus<>(), std::plus<>()) /
+           static_cast<double>(vec1.size() * 2);
 }
 
 double Distance(const std::vector<int>& vec1, const std::vector<int>& vec2) {
-    return 0.00;
+    if (vec1.size() != vec2.size()) {
+        throw std::invalid_argument("Distance - different dimension number of provided points.");
+    }
+    return std::sqrt(std::transform_reduce(vec1.cbegin(), vec1.cend(), vec2.cbegin(), 0, std::plus<>(),
+        [](const int a, const int b) {
+            return std::pow(a - b, 2);
+        }));
 }
--- homework/inner-product/test.cpp
+++ homework/inner-product/test.cpp
@@ -1,3 +1,4 @@
+#include <stdexcept>
 #include <vector>
 
 #include "arithmeticAverage.hpp"
@@ -7,18 +8,47 @@ TEST(arithmeticAverageTest, ShouldCalculateArithmeticAverage) {
     std::vector<int> first{-5, 4, 2, -5, 2, 4, 6, 7};
     std::vector<int> second{3, 5, -2, 6, 7, -3, 6, 9};
     EXPECT_DOUBLE_EQ(ArithmeticAverage(first, second), 2.875);
-    // Write more test case
+
+    first = {2, 2, 2};
+    second = {2, 2, 2};
+    EXPECT_DOUBLE_EQ(ArithmeticAverage(first, second), 2.0);
+
+    first = {0, 0};
+    second = {0, 0};
+    EXPECT_DOUBLE_EQ(ArithmeticAverage(first, second), 0.0);
+
+    first = {-5, 4, 2, -5, 2, 4, 6, 7};
+    second = {3, 5, -2, 6, 7, -3, 6, 9, 10};
+    try {
+        ArithmeticAverage(first, second);
+        FAIL() << "Expected std::invalid_argument";
+    } catch (const std::invalid_argument& err) {
+        EXPECT_EQ(err.what(), std::string("ArithmeticAverage - arguments are not equal in size."));
+    } catch (...) {
+        FAIL() << "Expected std::invalid_argument";
+    }
 }
 
 TEST(arithmeticAverageTest, ShouldCalculateDistance) {
     std::vector<int> first{7, 4, 3};
     std::vector<int> second{17, 6, 2};
-    double expected = 10.247;
+    // I had to change expected value to more precise one ( original one 10.247 )
+    double expected = 10.246950765959598;
     EXPECT_DOUBLE_EQ(Distance(first, second), expected);
-    
+
     first = {7, 4, 3, 13};
     second = {17, 6, 2, -2};
     expected = 18.16590212458495;
     EXPECT_DOUBLE_EQ(Distance(first, second), expected);
-    // Write more test case
+
+    first = {-5, 4, 2, -5, 2, 4, 6, 7};
+    second = {3, 5, -2, 6, 7, -3, 6, 9, 10};
+    try {
+        Distance(first, second);
+        FAIL() << "Expected std::invalid_argument";
+    } catch (const std::invalid_argument& err) {
+        EXPECT_EQ(err.what(), std::string("Distance - different dimension number of provided points."));
+    } catch (...) {
+        FAIL() << "Expected std::invalid_argument";
+    }
 }
