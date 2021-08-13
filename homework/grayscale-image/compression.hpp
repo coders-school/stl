b94db733c1f6c26d242ceb184ff4b04ac005b639
raxonpc <75646062+raxonpc@users.noreply.github.com>
2021-08-13 16:26:52 +0200

    Update hpp file

--- homework/grayscale-image/compression.hpp
+++ homework/grayscale-image/compression.hpp
@@ -1,19 +1,18 @@
 #pragma once
-#include <algorithm>
-#include <iostream>
-#include <iterator>
-#include <numeric>
-#include <vector>
+
 #include <array>
+#include <vector>
+#include <cstdint>
+#include <utility>
 
-inline constexpr size_t width{32};
-inline constexpr size_t height{32};
+using std::uint8_t;
+using std::size_t;
 
-using CompressedBitmap = std::vector<std::pair<uint8_t, uint8_t>>;
+inline constexpr size_t width{ 32 };
+inline constexpr size_t height{ 32 };
+
 using Bitmap = std::array<std::array<uint8_t, width>, height>;
+using CompressedBitmap = std::vector<std::pair<uint8_t, uint8_t>>;
 
 Bitmap decompressGrayscale(const CompressedBitmap&);
-
 CompressedBitmap compressGrayscale(const Bitmap&);
-char grayscaleToAscii(uint8_t grayscale);
-void printMap(const Bitmap&);
