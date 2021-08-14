8fc66b8dac8c5c63098edb02d9ca61f78a13c5df
Artur Michałowski <artur.michalowski93@gmail.com>
2021-08-14 19:11:16 +0200

    Add is_palindrome function

---
+++ homework/insensitive-palindrom/IsPalindrome.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <cwctype>
+
+bool is_palindrome(std::string word) {
+    std::transform(begin(word), end(word), begin(word), [](auto& el){
+        if (iswalnum(el)) {
+            el = std::tolower(el);
+            return el;
+        } 
+        el = 'A';
+        return el;
+    });
+    word.erase(std::remove(begin(word), end(word), 'A'), end(word));
+
+    return std::equal(begin(word), begin(word) + word.size()/2, rbegin(word));
+}
---
+++ homework/insensitive-palindrom/IsPalindrome.hpp
@@ -0,0 +1,1 @@
+bool is_palindrome(std::string word);
