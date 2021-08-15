fa96183120e3eaf903f73f770cd9823e410acdf1
Runer <runer53248@gmail.com>
2021-08-15 14:11:56 +0200

    Implement removeDuplicateAndTranformToMap.

---
+++ homework/transform-containers/transform-containers.cpp
@@ -0,0 +1,22 @@
+#include "transform-containers.hpp"
+#include <algorithm>
+
+std::map<int, std::string>
+removeDuplicateAndTranformToMap(std::list<std::string>& strings, std::deque<int>& ints) {
+    strings.sort();
+    auto stringsit = std::unique(begin(strings), end(strings));
+    strings.erase(stringsit, strings.end());
+
+    std::sort(begin(ints), end(ints));
+    auto intsit = std::unique(begin(ints), end(ints));
+    ints.erase(intsit, ints.end());
+
+    std::map<int, std::string> result;
+    std::transform(begin(strings), end(strings),
+                   begin(ints),
+                   std::inserter(result, begin(result)),
+                   [](const auto& text, auto value) {
+                       return std::make_pair(value, text);
+                   });
+    return result;
+}
