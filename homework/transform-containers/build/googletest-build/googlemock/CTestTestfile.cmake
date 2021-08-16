402569b61453e27d7976d435645ed2e7b264938b
lukasz-kukulka <qqla83@gmail.com>
2021-08-16 23:19:01 +0100

    Complete group task transorm containers

--- homework/transform-containers/build/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nanautzin/coders_school/stl/homework/transform-containers/build/googletest-src/googlemock
-# Build directory: /home/nanautzin/coders_school/stl/homework/transform-containers/build/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
