37c3d30fa56c3d872be1beccf63f92fe7b440ed2
marcin-dolowy <marcindolowy00@gmail.com>
2021-08-16 23:30:22 +0200

    added tests

--- homework/inner-product/test.cpp
+++ homework/inner-product/test.cpp
@@ -7,7 +7,18 @@ TEST(arithmeticAverageTest, ShouldCalculateArithmeticAverage) {
     std::vector<int> first{-5, 4, 2, -5, 2, 4, 6, 7};
     std::vector<int> second{3, 5, -2, 6, 7, -3, 6, 9};
     EXPECT_DOUBLE_EQ(ArithmeticAverage(first, second), 2.875);
-    // Write more test case
+    
+    first = {-5, -3};
+    second = {0, 12};
+    EXPECT_DOUBLE_EQ(ArithmeticAverage(first, second), 1);
+
+    first = {6, 2, 0};
+    second = {6, -2, 3};
+    EXPECT_DOUBLE_EQ(ArithmeticAverage(first, second), 2.5);
+
+    first = {3, 1, 7, 3, 0, -2, 5, 1, 5};
+    second = {5, 8, 2, 1, 1, -7, 3};
+    EXPECT_DOUBLE_EQ(ArithmeticAverage(first, second), 2.25);
 }
 
 TEST(arithmeticAverageTest, ShouldCalculateDistance) {
@@ -20,5 +31,19 @@ TEST(arithmeticAverageTest, ShouldCalculateDistance) {
     second = {17, 6, 2, -2};
     expected = 18.16590212458495;
     EXPECT_DOUBLE_EQ(Distance(first, second), expected);
-    // Write more test case
+    
+    first = {6, 2, 8};
+    second = {-1, 5, 0};
+    expected = 11.045361017187261;
+    EXPECT_DOUBLE_EQ(Distance(first, second), expected);
+
+    first = {1, 1, 1};
+    second = {1, 1, 1};
+    expected = 0;
+    EXPECT_DOUBLE_EQ(Distance(first, second), expected);
+
+    first = {5,1,-2};
+    second = {3,1,6};
+    expected = 8.2462112512353212;
+    EXPECT_DOUBLE_EQ(Distance(first, second), expected);
 }
