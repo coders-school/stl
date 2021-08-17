b079e8b5d8256d3ad4effe907f91b2fee99dea57
Maciej3206 <heilmanmaciej@gmail.com>
2021-08-17 19:38:45 +0200

    both functionalities

--- homework/inner-product/arithmeticAverage.cpp
+++ homework/inner-product/arithmeticAverage.cpp
@@ -4,28 +4,25 @@
 #include "arithmeticAverage.hpp"
 
 double ArithmeticAverage(const std::vector <int>& first, const std::vector <int>& second) {
-    int sum = 0;
+    double sum = 0;
     for (auto el : first) {
         sum += el;
     }
     for (auto el : second) {
         sum += el;
     }
-        return sum / (first.size() + second.size());
+    return sum / (first.size() + second.size());
 }
 
 double Distance(const std::vector <int>& first, const std::vector <int>& second) {
+    int size = first.size();
     float distA = 0;
-    float distB = 0;
-    //float distC = 0;
-    distA = std::sqrt (std::abs (first[1] - second[1]) * (first[1] - second[1]) + 
-                       std::abs (first[2] - second[2]) * (first[2] - second[2]));
-    distB = std::sqrt (std::abs (first[2] - second[2]) * (first[2] - second[2]) + 
-                       std::abs (first[3] - second[3]) * (first[3] - second[3]));
-                       
-
-    return std::sqrt ((distA * distA) + (distB * distB));
-}
-
-// std::vector<int> first{-5, 4, 2, -5, 2, 4, 6, 7};
-// std::vector<int> second{3, 5, -2, 6, 7, -3, 6, 9};
+    distA = std::sqrt (std::pow (std::abs (first[0] - second[0]), 2) + 
+                       std::pow (std::abs (first[1] - second[1]), 2));
+    while (size > 2) {
+        distA = std::sqrt (std::pow (distA, 2) +
+                std::pow (std::abs (first[size-1] - second[size-1]), 2));
+        --size;
+    }
+    return distA;
+} 
--- homework/inner-product/main.cpp
+++ homework/inner-product/main.cpp
@@ -3,8 +3,16 @@
 #include "arithmeticAverage.hpp"
 
 int main() {
-    std::vector <int> vec1 = {7, 4, 3};
-    std::vector <int> vec2 = {7, 4, 3};
+    // std::vector <int> vec1 = {7, 4, 3};
+    // std::vector <int> vec2 = {17, 6, 2};
+    // std::vector<int> vec1 = {-5, 4, 2, -5, 2, 4, 6, 7};
+    // std::vector<int> vec2 = {3, 5, -2, 6, 7, -3, 6, 9};
+    std::vector<int> vec1 = {7, 4, 3, 13};
+    std::vector<int> vec2 = {17, 6, 2, -2};
+    
+    //  first = {7, 4, 3, 13};
+    // second = {17, 6, 2, -2};
+    // expected = 18.16590212458495;
     std::cout << ArithmeticAverage (vec1, vec2);
     std::cout << '\n';
     std::cout << Distance (vec1, vec2);
