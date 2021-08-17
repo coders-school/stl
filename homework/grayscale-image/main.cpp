3d7cc8403f97730e6f3c515fde3ff86ebb500498
Gyrydymptum <gyrydymptum@gmail.com>
2021-08-17 12:28:16 +0200

    commenting on the parts to be changed

--- homework/grayscale-image/main.cpp
+++ homework/grayscale-image/main.cpp
@@ -43,7 +43,7 @@ int main() {
     auto ninja = generateNinja();
     // printMap(ninja);
     auto compressed = compressGrayscale(ninja);
-    auto decompressed = decompressGrayscale(compressed);
+    // auto decompressed = decompressGrayscale(compressed);
     // printMap(decompressed);
 
     return 0;
