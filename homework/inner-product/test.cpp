ae469779bb3f84fc71679ed85d1e9f2ed311b3e1
Piotr Rogalski <progalski89@gmail.com>
2021-08-20 23:46:02 +0200

    Suggestion implemented

--- homework/inner-product/test.cpp
+++ homework/inner-product/test.cpp
@@ -50,27 +50,16 @@ TEST(arithmeticAverageTest, ShouldCalculateDistance) {
     first = {7, 4, 3, 13};
     second = {17, 6, 2, -2};
     expected = 18.16590212458495;
+    
     EXPECT_DOUBLE_EQ(Distance(first, second), expected);
 
     first = {-5, 4, 2, -5, 2, 4, 6, 7};
     second = {3, 5, -2, 6, 7, -3, 6, 9, 10};
-    try {
-        Distance(first, second);
-        FAIL() << "Expected std::invalid_argument";
-    } catch (const std::invalid_argument& err) {
-        EXPECT_EQ(err.what(), std::string("Distance - points of different dimensions number provided."));
-    } catch (...) {
-        FAIL() << "Expected std::invalid_argument";
-    }
+
+    EXPECT_THROW(Distance(first, second), std::invalid_argument);
 
     first = {};
     second = {};
-    try {
-        Distance(first, second);
-        FAIL() << "Expected std::invalid_argument";
-    } catch (const std::invalid_argument& err) {
-        EXPECT_EQ(err.what(), std::string("Distance - empty vectors."));
-    } catch (...) {
-        FAIL() << "Expected std::invalid_argument";
-    }
+
+    EXPECT_THROW(Distance(first, second), std::invalid_argument);
 }
