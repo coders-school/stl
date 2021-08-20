525be18bd2e939d8319272d0f2a5a595b87b808a
Jakub Kubalka <jk.kubalka@gmail.com>
2021-08-20 13:45:30 +0200

    Getting rid of while loops

--- homework/grayscale-image/compression.cpp
+++ homework/grayscale-image/compression.cpp
@@ -10,9 +10,8 @@ std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(const std::array<std:
     //Two posibilities, only difference is that first is for_each and second is simple for row, so might say it's the same
     //First
     std::for_each(image_array.begin(), image_array.end(),[&compressed](auto& row){
-                    auto It = row.begin();
                     int vecPlace = 0;
-                    while(It != row.end()) {
+                    for(auto It = row.begin(); It != row.end();) {
                         compressed.push_back(std::make_pair(*It, std::count_if(It, row.end(), [&It](auto& el){return el == *It;})));
                         It += compressed[vecPlace].second;
                         vecPlace++;
@@ -38,8 +37,7 @@ std::array<std::array<uint8_t, width>, height> decompressGrayscale(const std::ve
     int vectorPlace = 0;
     std::generate(decompressed.begin(), decompressed.end(), [&](){
         std::array<uint8_t, width> row;
-        int amount = 0;
-        while (row.size() != amount) {
+        for (int amount = 0; row.size() != amount;) {
             std::fill_n(row.begin() + amount, image_vector[vectorPlace].second, image_vector[vectorPlace].first);
             amount += image_vector[vectorPlace].second;
             vectorPlace++;
