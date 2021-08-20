32d5f9a6cd5d63d26d082475a57e3ecdc89c8339
Jakub Kubalka <jk.kubalka@gmail.com>
2021-08-20 13:36:32 +0200

    Added some algorithms to grayscale image

--- homework/grayscale-image/compression.cpp
+++ homework/grayscale-image/compression.cpp
@@ -1,46 +1,51 @@
-#include "compression.hpp"
+#include <algorithm>
 #include <iostream>
+#include "compression.hpp"
+
 
 std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(const std::array<std::array<uint8_t, width>, height>& image_array) {
     std::vector<std::pair<uint8_t, uint8_t>> compressed;
     compressed.reserve(width * height);
-    for (auto row : image_array) {
-        int number_of_color = 0;
-        uint8_t previous_color = 0;
-        for (auto it = row.begin(); it != row.end(); it++) {
-            if (!number_of_color) {
-                previous_color = *it;
-            }
-            if (previous_color == *it) {
-                number_of_color++;
-            } else {
-                compressed.push_back(std::make_pair(previous_color, number_of_color));
-                number_of_color = 1;
-                previous_color = *it;
-            }
-            if (it == row.end() - 1) {
-                compressed.push_back(std::make_pair(previous_color, number_of_color));
-            }
-       }
-    }
+
+    //Two posibilities, only difference is that first is for_each and second is simple for row, so might say it's the same
+    //First
+    std::for_each(image_array.begin(), image_array.end(),[&compressed](auto& row){
+                    auto It = row.begin();
+                    int vecPlace = 0;
+                    while(It != row.end()) {
+                        compressed.push_back(std::make_pair(*It, std::count_if(It, row.end(), [&It](auto& el){return el == *It;})));
+                        It += compressed[vecPlace].second;
+                        vecPlace++;
+                    }
+    });
+
+    //Second
+   /* for (auto row : image_array) {
+        auto It = row.begin();
+        while (It != row.end()) {
+            auto amount = std::count_if(It, row.end(), [&It](auto& el1){ return el1 == *It;});
+            compressed.push_back(std::make_pair(*It, amount));
+            It += amount;
+        }
+    }*/
+
     compressed.shrink_to_fit();
     return compressed;
 }
 
 std::array<std::array<uint8_t, width>, height> decompressGrayscale(const std::vector<std::pair<uint8_t, uint8_t>>& image_vector) {
     std::array<std::array<uint8_t, width>, height> decompressed;
-    int position = 0;
-    int row = 0;
-    for (auto el : image_vector) {
-        for (int i = 0; i < el.second; i++) {
-            decompressed[row][i + position] = el.first;
+    int vectorPlace = 0;
+    std::generate(decompressed.begin(), decompressed.end(), [&](){
+        std::array<uint8_t, width> row;
+        int amount = 0;
+        while (row.size() != amount) {
+            std::fill_n(row.begin() + amount, image_vector[vectorPlace].second, image_vector[vectorPlace].first);
+            amount += image_vector[vectorPlace].second;
+            vectorPlace++;
         }
-        position += el.second;
-        if (position == width) {
-            row++;
-            position = 0;
-        }
-    }
+        return row;
+    });
     return decompressed;
 }
 
