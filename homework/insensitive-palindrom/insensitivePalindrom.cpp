2df27dda433d598c62b0e15860c0e951c8c90206
Jakub Kubalka <jk.kubalka@gmail.com>
2021-08-20 18:52:10 +0200

    Problems resolved, program working

--- homework/insensitive-palindrom/insensitivePalindrom.cpp
+++ homework/insensitive-palindrom/insensitivePalindrom.cpp
@@ -1,6 +1,13 @@
-#include <string>
 #include "insensitivePalindrom.hpp"
+#include <algorithm>
+#include <cctype>
 
-bool is_palidrome(const std::string& palindrome) {
-    return true;
-}   
+bool is_palindrome(const std::string& palindrome) {
+    std::string removedSigns;
+    std::copy_if(palindrome.begin(), palindrome.end(), std::back_inserter(removedSigns), [](unsigned char ch){ return std::isalnum(ch);});
+    std::transform(removedSigns.begin(), removedSigns.end(), removedSigns.begin(), [](auto& ch){return std::tolower(ch);});
+    if (std::mismatch(removedSigns.begin(), removedSigns.end(), removedSigns.rbegin()) == std::make_pair(removedSigns.end(), removedSigns.rend())) {
+        return true;
+    }
+    return false;
+}
--- homework/insensitive-palindrom/insensitivePalindrom.hpp
+++ homework/insensitive-palindrom/insensitivePalindrom.hpp
@@ -1,3 +1,4 @@
 #pragma once
+#include <string>
 
 bool is_palindrome(const std::string& palindrome);
