51f0c968e78ba976cc1b3e46b11bfafdee47e274
NikczemnyOkularnik <grzegorzbrodziakgd@gmail.com>
2021-08-21 12:47:07 +0200

    palindrome with passed test

--- homework/insensitive-palindrom/palindrom.cpp
+++ homework/insensitive-palindrom/palindrom.cpp
@@ -2,13 +2,13 @@
 #include <cctype>
 #include <iostream>
 
-int main()
+bool is_palindrome(std::string palindrome)
 {
-    std::string removeSpecialSigns {"kwwAAaaaAAakkkk"};
-    std::string palindrome {"kw%^#$!&#%&*@^%k!" "kak@" "kek" "kwik!!!@"};
+    std::string removeSpecialSigns;
     std::copy_if(palindrome.begin(), palindrome.end(), std::back_inserter(removeSpecialSigns), [](char ch){ return std::isalnum(ch);});
     std::cout << removeSpecialSigns << '\n';
     std::transform(removeSpecialSigns.begin(), removeSpecialSigns.end(), removeSpecialSigns.begin(), [](auto& ch){return std::tolower(ch);});
     std::cout << removeSpecialSigns << '\n';
-    
+    return(std::equal(removeSpecialSigns.begin(), removeSpecialSigns.begin() + removeSpecialSigns.size()/2, removeSpecialSigns.rbegin()));
+
 }
