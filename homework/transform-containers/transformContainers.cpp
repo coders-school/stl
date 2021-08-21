f872cbe398d8f7c7c135b4b93c4b90bf09174e9b
Łukasz Meisnerowski <lukasz.meisnerowski@gmail.com>
2021-08-21 22:16:41 +0200

    added function

---
+++ homework/transform-containers/transformContainers.cpp
@@ -0,0 +1,23 @@
+#pragma once
+#include <map>
+#include <string>
+#include <list>
+#include <deque>
+#include "transformContainters.hpp"
+
+std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string> strList, std::deque<int> deq) {
+
+    strList.sort();
+    strList.erase(std::unique(strList.begin(), strList.end()), strList.end());
+
+    std::sort(deq.begin(), deq.end());
+    deq.erase(std::unique(deq.begin(), deq.end()), deq.end());
+
+    std::map<int, std::string> newMap;
+    std::transform(deq.begin(),deq.end(), strList.begin(), std::inserter(newMap, newMap.begin()),
+                                                                        [] (const auto & number, const auto & str){
+                                                                            return std::make_pair(number, str);
+                                                                        });
+
+    return newMap;
+}
