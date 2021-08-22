a901b0b8ca1e2f762d41b2e5ca1d702eecc19093
BarTes8 <barbara.tessema@gmail.com>
2021-08-22 11:36:16 +0200

    changed formatting

--- homework/inner-product/arithmeticAverage.cpp
+++ homework/inner-product/arithmeticAverage.cpp
@@ -17,33 +17,15 @@ double ArithmeticAverage(const std::vector<int>& first, const std::vector<int>& 
 }
 
 double Distance(const std::vector<int>& first, const std::vector<int>& second) {
-    // int size = first.size();
-    // if (first.size() != second.size()) {
-    //     std::cout << "The both vectors must heave same size" << '\n';
-    //     return 0;
-    // }
-
-    // if (first.size() == 1) {
-    //     return std::abs(first[0] - second[0]);
-    // }
-
-    // double distA = std::sqrt(std::pow(std::abs(first[0] - second[0]), 2) +
-    //                          std::pow(std::abs(first[1] - second[1]), 2));
-    // while (size > 2) {
-    //     distA = std::sqrt(std::pow(distA, 2) +
-    //                       std::pow(std::abs(first[size - 1] - second[size - 1]), 2));
-    //     --size;
-    // }
-    // return distA;
     if (first.size() != second.size() && first.size() != 0) {
         return 0;
     }
     return std::sqrt(std::inner_product(first.begin(),
-                              first.end(),
-                              second.begin(),
-                              0,
-                              std::plus<>(),
-                              [](const auto lhs, const auto rhs) {
-                                  return std::pow(lhs - rhs, 2);
-                              }));
+                                        first.end(),
+                                        second.begin(),
+                                        0,
+                                        std::plus<>(),
+                                        [](const auto lhs, const auto rhs) {
+                                            return std::pow(lhs - rhs, 2);
+                                        }));
 }
