abad590cf14280de0b0f178b510c51ee05d19179
BarTes8 <barbara.tessema@gmail.com>
2021-08-22 11:52:34 +0200

    added new line

--- homework/inner-product/arithmeticAverage.hpp
+++ homework/inner-product/arithmeticAverage.hpp
@@ -2,5 +2,4 @@
 #include <vector>
 
 double ArithmeticAverage(const std::vector <int>& first, const std::vector <int>& second);
-
 double Distance(const std::vector <int>& first, const std::vector <int>& second);
