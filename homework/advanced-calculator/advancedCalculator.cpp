e7db82ca71a1e4bebdd517a1d44eca3870cb5b89
lukasz-kukulka <qqla83@gmail.com>
2021-08-24 00:19:29 +0100

    Add process function

--- homework/advanced-calculator/advancedCalculator.cpp
+++ homework/advanced-calculator/advancedCalculator.cpp
@@ -1,1 +1,6 @@
 #include "advancedCalculator.hpp"
+
+ErrorCode process(std::string input, double* out) {
+
+    return ErrorCode::OK;
+}
--- homework/advanced-calculator/advancedCalculator.hpp
+++ homework/advanced-calculator/advancedCalculator.hpp
@@ -1,11 +1,13 @@
+#include <string>
 
 
-
-enum class errorCode {
-    Ok,
+enum class ErrorCode {
+    OK,
     BadCharacter,                             // Jeżeli użytkownik poda znak inny niż liczbę.
     BadFormat,                                // Jeżeli użytkownik poda zły format komendy np. + 5 4, powinno być 4 + 5.
     DivideBy0,                                // dzielenie przez 0.
-    SqrtOfNagativeNumber,                     // pierwiastek z liczby ujemnej.
+    SqrtOfNegativeNumber,                     // pierwiastek z liczby ujemnej.
     ModuleOfNonIntegerValue                   // Póba obliczenia % na liczbie niecałkowitej.
 };
+
+ErrorCode process(std::string input, double* out);
