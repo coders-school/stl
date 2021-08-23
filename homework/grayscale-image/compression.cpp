e5b6c1b4f90a87bdba3584dc82965692d6c14f9b
Gyrydymptum <gyrydymptum@gmail.com>
2021-08-23 13:40:47 +0200

    add files

--- homework/grayscale-image/compression.cpp
+++ homework/grayscale-image/compression.cpp
@@ -1,64 +1,67 @@
 #include "compression.hpp"
 #include <algorithm>
-#include <iostream>
+#include <functional>
+#include <numeric>
 
-std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(std::array<std::array<uint8_t, width>, height> array_in) {
-    std::vector<std::pair<uint8_t, uint8_t>> vec;
-    uint8_t color = 0;
-    uint8_t count = 0;
-    for (size_t y = 0; y < height; ++y) {
-        color = array_in[y][0];
-        count = 0;
-        for (size_t x = 1; x <= width; ++x) {
-            if (color == array_in[y][x - 1]) {
-                ++count;
-                if (x == width) {
-                    vec.push_back(std::make_pair(color, count));
-                }
-            } else {
-                vec.push_back(std::make_pair(color, count));
-                color = array_in[y][x - 1];
-                count = 1;
-            }
-            if ((x == width) && (count == 1)) {
-                vec.push_back(std::make_pair(color, count));
-            }
-        }
-    }
-    return vec;
+std::vector<uint8_t> compress(std::vector<uint8_t> vec1d, std::vector<uint8_t> vecunique) {
+    std::vector<uint8_t> v3;
+
+    auto f = [vec1d](int x) {
+        auto i = std::find(vec1d.begin(), vec1d.end(), x);
+        int index = std::distance(vec1d.begin(), i);
+        return index;
+    };
+    std::transform(vecunique.begin(), vecunique.end(), std::back_inserter(v3), f);
+    v3.push_back(v3.size());
+
+    adjacent_difference(begin(v3), end(v3), begin(v3));
+    v3.erase(v3.begin());
+
+    return v3;
 }
 
-std::array<std::array<uint8_t, width>, height> decompressGrayscale(std::vector<std::pair<uint8_t, uint8_t>> vec) {
-    std::array<std::array<uint8_t, width>, height> array_out;
-    uint8_t x = 0;
-    uint8_t y = 0;
+pairUint mergeVector(std::vector<uint8_t> v1, std::vector<uint8_t> v2) {
+    pairUint vectorPair;
+    auto f = [](uint8_t x, uint8_t y) {
+        return std::make_pair(x, y);
+    };
 
-    for (auto& pair : vec) {
-        uint8_t color = pair.first;
-        uint8_t count = pair.second;
-        for (size_t i = 0; i < count; ++i) {
-            if (x < width) {
-                array_out[y][x] = color;
-                ++x;
-                if (x == width) {
-                    ++y;
-                    x = 0;
-                }
-            }
-        }
-    }
-    return array_out;
+    std::transform(v1.begin(), v1.end(), v2.begin(), std::back_inserter(vectorPair), f);
+    return vectorPair;
 }
 
-void printMap(const std::array<std::array<uint8_t, width>, height>& ninja) {
-    for (const auto& elem : ninja) {
-        for (const auto& el : elem) {
-            if (el < ' ') {
-                std::cout << ' ';
-            } else {
-                std::cout << el;
-            }
-        }
-        std::cout << '\n';
+pairUint compressGrayscale(board2d array2D) {
+    int vectorsize = width * height;
+
+    pairUint vecFinal;
+    if ((width == 0) || (height == 0)) {
+        return vecFinal;
+    } else {
+        std::vector<uint8_t> vec1D(vectorsize);
+        std::vector<uint8_t> vecUnique(vectorsize);
+        std::vector<uint8_t> vecCompres;
+
+        std::copy(array2D[0].begin(), array2D[height - 1].end(), vec1D.begin());
+        auto re = std::unique_copy(vec1D.begin(), vec1D.end(), vecUnique.begin());
+        vecUnique.erase(re, vecUnique.end());
+        vecCompres = compress(vec1D, vecUnique);
+        vecFinal = mergeVector(vecUnique, vecCompres);
+
+        return vecFinal;
     }
+}
+
+board2d decompressGrayscale(pairUint vecPair) {
+    board2d finalArray;
+    std::vector<uint8_t> vec;
+
+    auto func = [&](std::pair<uint8_t, uint8_t> p) {
+        return fill_n(std::back_inserter(vec), p.second, p.first);
+    };
+
+    transform(vecPair.begin(), vecPair.end(), std::back_inserter(vec), func);
+
+    std::copy(vec.begin(), vec.end(), finalArray[0].begin());
+
+    return finalArray;
 }
--- homework/grayscale-image/compression.hpp
+++ homework/grayscale-image/compression.hpp
@@ -1,12 +1,17 @@
+ 
 #pragma once
 #include <array>
+#include <iostream>
 #include <vector>
-#include <cinttypes>
+#include <utility>
+constexpr size_t width = 0;
+constexpr size_t height = 0;
 
-constexpr size_t width = 32;
-constexpr size_t height = 32;
+using board2d =  std::array<std::array<uint8_t, width>, height>;
+using pairUint = std::vector<std::pair<uint8_t, uint8_t>>;
 
+pairUint compressGrayscale(board2d);
+board2d decompressGrayscale(pairUint);
+pairUint mergeVector(std::vector<uint8_t>, std::vector<uint8_t>);
+std::vector<int> compress(std::vector<int>);
 
-std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(std::array<std::array<uint8_t, width>, height>);
-std::array<std::array<uint8_t, width>, height> decompressGrayscale(std::vector<std::pair<uint8_t, uint8_t>>);
-void printMap(const std::array<std::array<uint8_t, width>, height> &);
