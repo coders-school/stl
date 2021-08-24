a600b663ba21249f50974f004ae779a975480f9c
Marcin$ <marcin.s10335@gmail.com>
2021-08-24 21:31:31 +0200

    Fix all tests

--- homework/insensitive-palindrom/palindrom.hpp
+++ homework/insensitive-palindrom/palindrom.hpp
@@ -1,4 +1,24 @@
-#pragma once 
+#pragma once
+#include <algorithm>
 #include <string>
 
-bool is_palindrome(std::string&);
+void str_tolower(std::string& s) {
+    std::transform(begin(s), end(s), begin(s),
+                   [](unsigned char ch) { return std::tolower(ch); });
+}
+bool valid_char(unsigned char ch) {
+    return !((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'));
+}
+
+std::string prepare_string(const std::string& str) {
+    std::string retrunStrng = str;
+    retrunStrng.erase(std::remove_if(begin(retrunStrng), end(retrunStrng), valid_char), end(retrunStrng));
+    str_tolower(retrunStrng);
+    return retrunStrng;
+}
+
+bool is_palindrome(const std::string& str) {
+    std::string tempString = prepare_string(str);
+    auto pair = std::mismatch(begin(tempString), end(tempString), rbegin(tempString), rend(tempString));
+    return pair.first == end(tempString);
+}
