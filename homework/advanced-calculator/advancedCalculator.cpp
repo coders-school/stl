904c9665a0932df14b8c5c23c93052a0872f5719
BarTes8 <barbara.tessema@gmail.com>
2021-08-28 18:16:24 +0200

    passed all tests

--- homework/advanced-calculator/advancedCalculator.cpp
+++ homework/advanced-calculator/advancedCalculator.cpp
@@ -1,10 +1,10 @@
 #include "advancedCalculator.hpp"
 #include <algorithm>
-#include <iomanip>
 #include <cmath>
+#include <iomanip>
 #include <sstream>
 
-const std::map<const char, std::function<double(double, double)>> operations {
+const std::map<const char, std::function<double(double, double)>> operations{
     {'+', std::plus<double>()},
     {'-', std::minus<double>()},
     {'*', std::multiplies<double>()},
@@ -15,12 +15,12 @@ const std::map<const char, std::function<double(double, double)>> operations {
     {'!', [](auto value, [[maybe_unused]] auto unused) {if (value >= 0.0) {
              return std::tgamma(value + 1);
          }
-         return -std::tgamma(-value + 1);} }
+         return -std::tgamma(-value + 1); }}
 };
 
 bool isAllowedOperation(char operation) {
-    std::string allowedOperations {"+-*/%^$!"};
-    auto it = std::find_if(allowedOperations.begin(), 
+    std::string allowedOperations{"+-*/%^$!"};
+    auto it = std::find_if(allowedOperations.begin(),
                            allowedOperations.end(),
                            [&operation](auto el) {
                                return el == operation;
@@ -29,10 +29,10 @@ bool isAllowedOperation(char operation) {
 }
 
 bool isBadCharacter(const std::string& input) {
-    return std::any_of(input.begin(), 
+    return std::any_of(input.begin(),
                        input.end(),
                        [&input](char character) {
-                           return !isdigit(character) && !isAllowedOperation(character) && character != '.' && character != ' ' && character != ',' ;
+                           return !isdigit(character) && !isAllowedOperation(character) && character != '.' && character != ' ' && character != ',';
                        });
 }
 
@@ -41,10 +41,9 @@ bool isInteger(double number) {
 }
 
 bool isBadFormat(std::string& input, Data& data) {
-  
     input.erase(std::remove(begin(input), end(input), ' '), end(input));
     size_t minusCount = std::count(input.begin(), input.end(), '-');
-    if (minusCount > 3 ) {
+    if (minusCount > 3) {
         return true;
     }
     size_t dotCount = std::count(input.begin(), input.end(), '.');
@@ -56,26 +55,25 @@ bool isBadFormat(std::string& input, Data& data) {
         return true;
     }
     const std::string operations{"+*/%^$!"};
-    size_t operationCount {0};
-    for(int i = 0; i < operations.size(); i++) {
+    size_t operationCount{0};
+    for (int i = 0; i < operations.size(); i++) {
         operationCount += std::count(input.begin(), input.end(), operations[i]);
     }
     if (operationCount > 1) {
-      return true;
+        return true;
     }
     if (isBadNumber(data.firstValue_)) {
         return true;
     }
 
     if (data.operation_ == '!') {
-       if (input.size() != data.firstValue_.size() + 1) {
-         return true;
-       }
-    }
-    else {
-      if (isBadNumber(data.secondValue_)) {
-        return true;
-      }
+        if (input.size() != data.firstValue_.size() + 1) {
+            return true;
+        }
+    } else {
+        if (isBadNumber(data.secondValue_)) {
+            return true;
+        }
     }
     return false;
 }
@@ -89,28 +87,27 @@ double stringToDouble(std::string input) {
 }
 
 Data divideInput(std::string& input) {
-  if (input.empty()) {
-    return {};
-  }
-  input.erase(std::remove(begin(input), end(input), ' '), end(input));
-    std::string allowedOperations {"+-*/%^$!"};
-    auto it = std::find_first_of (input.begin() + 1,
-                                  input.end(),
-                                  allowedOperations.begin(), 
-                                  allowedOperations.end());
+    if (input.empty()) {
+        return {};
+    }
+    input.erase(std::remove(begin(input), end(input), ' '), end(input));
+    std::string allowedOperations{"+-*/%^$!"};
+    auto it = std::find_first_of(input.begin() + 1,
+                                 input.end(),
+                                 allowedOperations.begin(),
+                                 allowedOperations.end());
     Data data;
     if (it == input.end()) {
-       return {};   
+        return {};
     }
-   
+
     data.operation_ = *it;
     data.firstValue_ = input.substr(0, std::distance(input.begin(), it));
     if (data.operation_ == '!') {
-      data.secondValue_ = "";
-    }
-    else {
-      size_t sizeOfFirstValue = (data.firstValue_).size();
-      data.secondValue_ = input.substr(sizeOfFirstValue + 1, std::distance(it+1, input.end()));
+        data.secondValue_ = "";
+    } else {
+        size_t sizeOfFirstValue = (data.firstValue_).size();
+        data.secondValue_ = input.substr(sizeOfFirstValue + 1, std::distance(it + 1, input.end()));
     }
     return data;
 }
@@ -141,7 +138,7 @@ bool isBadNumber(std::string& input) {
 ErrorCode process(std::string input, double* out) {
     Data data;
     data = divideInput(input);
-    
+
     if (isBadCharacter(input)) {
         return ErrorCode::BadCharacter;
     }
@@ -150,16 +147,15 @@ ErrorCode process(std::string input, double* out) {
     }
     double firstNumber = stringToDouble(data.firstValue_);
     double secondNumber = stringToDouble(data.secondValue_);
-    if (data.operation_ == '/' &&  secondNumber == 0) {
+    if (data.operation_ == '/' && secondNumber == 0) {
         return ErrorCode::DivideBy0;
     }
-    if (data.operation_ == '%' && (!isInteger(firstNumber)) || !isInteger(secondNumber)) {
+    if (data.operation_ == '%' && (!isInteger(firstNumber) || !isInteger(secondNumber))) {
         return ErrorCode::ModuleOfNonIntegerValue;
     }
-    if (data.operation_ == '$' && ((firstNumber <= 0) || (secondNumber <= 0))) {
+    if (data.operation_ == '$' && firstNumber <= 0) {
         return ErrorCode::SqrtOfNegativeNumber;
     }
-
     *out = operations.at(data.operation_)(firstNumber, secondNumber);
     return ErrorCode::OK;
 }
