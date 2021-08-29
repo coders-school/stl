67100b9065099c8dfad04376579fc92fc17f67ee
kondorski <thegreatvaultofknowledge@gmail.com>
2021-08-29 15:46:10 +0200

    typo fixed

--- homework/insensitive-palindrom/checkPalindrome.cpp
+++ homework/insensitive-palindrom/checkPalindrome.cpp
@@ -8,4 +8,4 @@ bool is_palindrome(std::string stringToTest) {
     std::transform(begin(stringToTest), end(stringToTest), begin(stringToTest), [](char c){ return std::tolower(c); });
     stringToTest.erase(std::remove_if(stringToTest.begin(), stringToTest.end(), [](char c){ return !isalnum(c); }), stringToTest.end());
     return std::equal(stringToTest.begin(), stringToTest.begin() + stringToTest.size()/2, stringToTest.rbegin() );
-}git s
+}
