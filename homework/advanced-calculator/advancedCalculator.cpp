6fc01725ca2cfb089674aecf35faa95237af74e8
opiotrowsky <78661971+opiotrowsky@users.noreply.github.com>
2021-08-29 21:05:40 +0200

    Fixed modulo

--- homework/advanced-calculator/advancedCalculator.cpp
+++ homework/advanced-calculator/advancedCalculator.cpp
@@ -56,81 +56,13 @@ ErrorCode process(std::string input, double* out) {
             return ErrorCode::DivideBy0;
         } else if ((operation == "$") and (lhs < 0)) {
             return ErrorCode::SqrtOfNegativeNumber;
-        } else if ((operation == "%") and (isInteger(lhs) or isInteger(rhs))) {
+        } else if ((operation == "%") and (!isInteger(lhs) or !isInteger(rhs))) {
             return ErrorCode::ModuleOfNonIntegerValue;
         }
 
         *out = calc::calculations.at(operation[0])(lhs, rhs);
         return ErrorCode::OK;
     }
-    //  std::istringstream iss(input);
-    //     std::vector<std::string> expressions{std::istream_iterator<std::string>(iss), {}};
-
-    //     std::string LHS, RHS;
-    //     double lhs, rhs;
-    //     char operation;
-    //     int badchar = 0, badform = 0, divby0 = 0, modofnonint = 0, sqrtofneg = 0;
-    //     // parse vector (or string directly)
-    //     // to operation type and its arguments (lhs & rhs or lhs for factorial)
-    //     for(const auto & letter : input) {
-    //         if(operation == '\0'){
-    //             if((letter >= 48 && letter <= 57) || letter == '.') {
-    //                 LHS.push_back(letter);
-    //             }
-    //             // if((letter < 46 || letter == 47 || letter > 57) && letter != 32) {
-    //             //     return ErrorCode::BadCharacter;
-    //             // }
-    //         }
-    //         for(const auto & iter : calc::calculations) {
-    //             if(LHS.empty() == false && operation == '\0' && letter == iter.first) {
-    //                 operation = letter;
-    //             }
-    //             if(LHS.empty() == false && operation == '\0' && (letter != iter.first || letter != ' ' || !(letter >= 48 && letter <= 57) || letter != '.')) {
-    //                 badchar++;
-    //             }
-    //             if(operation != '\0' && letter == iter.first) {
-    //                 badform++;
-    //             }
-    //         }
-    //         if(operation != '\0') {
-    //             if((letter >= 48 && letter <= 57) || letter == '.') {
-    //                 RHS.push_back(letter);
-    //             }
-    //             // if((letter < 46 || letter == 47 || letter > 57) && letter != 32) {
-    //             //     return ErrorCode::BadCharacter;
-    //             // }
-    //         }
-    //     }
-
-    //     if(badform >= calc::calculations.size() || LHS.empty() == true) {
-    //         return ErrorCode::BadFormat;
-    //     }
-    //     if(badchar >= calc::calculations.size()) {
-    //         return ErrorCode::BadCharacter;
-    //     }
-
-    //     lhs = std::stod(LHS);
-    //     rhs = std::stod(RHS);
-
-    //     if(rhs == 0 && operation == '/') {
-    //         return ErrorCode::DivideBy0;
-    //     }
-
-    //     // char operation{ '+' };      // example
-    //     // double lhs{ 2 };            // example
-    //     // double rhs{ 3 };            // example
-
-    //     /* return error code if input cannot be parsed correctly
-    //     //return ErrorCodeBadFormat;
-    //     // return ErrorCodeBadCharacter;
-    //     return ErrorCodeDivideBy0;
-    //     return ErrorCodeModuleOfNonIntegerValue;
-    //     return ErrorCodeSqrtOfNegativeNumber;
-    //     */
-
-    //     // otherwise calculate and return OK
-    //     *out = calc::calculations.at(operation)(lhs, rhs);
-
-    //     return ErrorCode::OK;
+    
     return ErrorCode::BadFormat;
 }
