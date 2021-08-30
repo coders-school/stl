6025afa175b9893994ae6407811986d9573d4318
raxonpc <tomeczekorlowski1@gmail.com>
2021-08-30 15:10:02 +0200

    Format the files

--- homework/advanced-calculator/advancedCalculator.hpp
+++ homework/advanced-calculator/advancedCalculator.hpp
@@ -2,26 +2,26 @@
 
 #include <functional>
 #include <map>
+#include <optional>
 #include <string>
 #include <tuple>
-#include <optional>
 #include <vector>
 
 namespace AdvancedCalculator {
-    enum class ErrorCode {
-        OK,
-        BadCharacter,
-        BadFormat,
-        DivideBy0,                                
-        SqrtOfNegativeNumber,                     
-        ModuleOfNonIntegerValue,
-    };
-    
-    ErrorCode process(const std::string& input, double* out);
+enum class ErrorCode {
+    OK,
+    BadCharacter,
+    BadFormat,
+    DivideBy0,
+    SqrtOfNegativeNumber,
+    ModuleOfNonIntegerValue,
+};
 
-    using tuple_type = std::tuple<std::optional<double>, 
-          std::optional<char>, std::optional<double>>;  
-    tuple_type get_values(const std::string& input);
+ErrorCode process(const std::string &input, double *out);
 
-    std::vector<ErrorCode> get_errors(const tuple_type& expression);
-}
+using tuple_type = std::tuple<std::optional<double>, std::optional<char>,
+                              std::optional<double>>;
+tuple_type get_values(const std::string &input);
+
+std::vector<ErrorCode> get_errors(const tuple_type &expression);
+}  // namespace AdvancedCalculator
