044bdcb9927b9dd336df88f8ccb22b44ea0e3d7b
bartosz-gruszczyk <bartosz.gruszczyk@gmail.com>
2021-08-31 01:56:45 +0200

    Distance() seems to work too

--- homework/inner-product/arithmeticAverage.cpp
+++ homework/inner-product/arithmeticAverage.cpp
@@ -1,15 +1,27 @@
 #include "arithmeticAverage.hpp"
 #include <algorithm>
+#include <cmath>
+#include <iostream>
 #include <numeric>
 
 double ArithmeticAverage(const std::vector<int>& firstVector, const std::vector<int>& secondVector) {
-
-    return static_cast<double>(std::accumulate(firstVector.cbegin(), firstVector.cend(), 0)
-          + std::accumulate(secondVector.cbegin(), secondVector.cend(), 0))
-          / (firstVector.size() + secondVector.size());
+    return static_cast<double>(std::reduce(firstVector.cbegin(), firstVector.cend(), 0)
+           + std::reduce(secondVector.cbegin(), secondVector.cend(), 0))
+           / (firstVector.size() + secondVector.size());
 }
 
 double Distance(const std::vector<int>& firstVector, const std::vector<int>& secondVector) {
-
-    return 0.0;
+    if (firstVector.size() == secondVector.size()) {
+        std::vector<int> squaredDifferences(firstVector.size());
+        std::transform(firstVector.cbegin(),
+                       firstVector.cend(),
+                       secondVector.cbegin(),
+                       squaredDifferences.begin(),
+                       [](auto first, auto second){
+                           return std::pow(first - second, 2);
+                       });
+        return std::sqrt(std::reduce(squaredDifferences.cbegin(), squaredDifferences.cend(), 0));
+    }
+    std::cerr << "Wrong input data.\n";
+    return -1.0;
 }
--- homework/inner-product/main.cpp
+++ homework/inner-product/main.cpp
@@ -1,7 +1,12 @@
 #include "arithmeticAverage.hpp"
-
+#include <iostream>
+#include <vector>
 
 int main() {
+    std::vector<int> vec1{7, 4, 3};
+    std::vector<int> vec2{17, 6, 2};
+
+    std::cout << "Distance: " << Distance(vec1, vec2) << '\n';
     
     return 0;
 }
