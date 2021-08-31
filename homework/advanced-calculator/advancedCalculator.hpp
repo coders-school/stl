b59f6eda96614dbfec2f05c7526b82706cf0f8f9
NikczemnyOkularnik <grzegorzbrodziakgd@gmail.com>
2021-08-31 17:40:04 +0200

    changed % to $

--- homework/advanced-calculator/advancedCalculator.hpp
+++ homework/advanced-calculator/advancedCalculator.hpp
@@ -21,5 +21,5 @@ std::map<char, std::function<errorCode(double, double)>> kalkulator{
     {'/', [](double numOne, double numTwo){std::cout << numOne / numTwo << '\n'; return errorCode::Ok;}},
     {'-', [](double numOne, double numTwo){std::cout << numOne - numTwo << '\n'; return errorCode::Ok;}},
     {'^', [](double numOne, double numTwo){std::cout << pow(numOne, numTwo) << '\n'; return errorCode::Ok;}},
-    {'%', [](double numOne, double numTwo){std::cout << pow(numOne, 1/numTwo) << '\n'; return errorCode::Ok;}},
+    {'$', [](double numOne, double numTwo){std::cout << pow(numOne, 1/numTwo) << '\n'; return errorCode::Ok;}},
     };
