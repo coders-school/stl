423ada938602295806102b9dc765df3a926922d0
Mariusz Lisowski <mariusz.lisowski@tutanota.com>
2021-08-31 09:05:04 +0200

    Fix factorial calculation & improve code formatting

--- homework/advanced-calculator/advancedCalculator.cpp
+++ homework/advanced-calculator/advancedCalculator.cpp
@@ -17,50 +17,59 @@ std::map<char, std::function<double(double, double)>> calculations{
     {'%', std::modulus<int>()},
     {'^', [](auto base, auto exponent) { return std::pow(base, exponent); }},
     {'$', [](auto base, auto root) { return std::pow(base, 1.0 / root); }},
-    {'!', [](auto n, [[maybe_unused]] auto x) { return n <= 0 ? 1.0 : std::tgamma(n + 1.0); }}};
-}  // namespace calc
+    {'!', [](auto n, [[maybe_unused]] auto x) { return n < 0
+                                                       ? -(std::tgamma(-n + 1.0))
+                                                       : std::tgamma(n + 1.0); }
+    }
+};
+} // namespace calc
+
 bool isInteger(double number) {
     return std::floor(number) == number;
 }
+
 bool forbidenCharacter(const std::string& input) {
     const std::array<char, 21> legalCharacters{'+', '-', '*', '/', '%', '!', '^', '$', '.', ',',
                                                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' '};
-    return !std::none_of(input.begin(), input.end(), [legalCharacters](char ch) {
-        return std::find(legalCharacters.begin(), legalCharacters.end(), ch) == legalCharacters.end();
-    });
+    return std::any_of(input.begin(),
+                       input.end(),
+                       [legalCharacters](char ch) {
+                           return std::find(legalCharacters.begin(),
+                                            legalCharacters.end(),
+                                            ch) == legalCharacters.end();
+                       });
 }
 
 ErrorCode process(std::string input, double* out) {
     if (forbidenCharacter(input)) {
         return ErrorCode::BadCharacter;
     }
-
     if (std::find(input.begin(), input.end(), ',') != input.end()) {
         return ErrorCode::BadFormat;
     }
-
     std::smatch matcher;
-
     std::regex availableOperation(R"(([-]?\d+\.\d+|[-]?\d+)\s?([-+*\/%^$])\s?([-]?\d+\.\d+|[-]?\d+))");
-
-    if (std::regex_search(input, matcher, availableOperation)) {
+    std::regex factorial(R"(([-]?\d+.\d+|[-]?\d+)\s?(!))");
+    
+    if (std::regex_search(input, matcher, availableOperation) ||
+        std::regex_search(input, matcher, factorial) )
+    {
         if (matcher[0] != input) {
             return ErrorCode::BadFormat;
         }
-
-        double lhs = std::stod(matcher[1]);
-        double rhs = std::stod(matcher[3]);
         std::string operation = matcher[2];
+        double lhs = std::stod(matcher[1]);
+        double rhs = (operation != "!" ? std::stod(matcher[3]) : 0);
 
-        if ((operation == "/") and (rhs == 0)) {
+        if (operation == "/" and rhs == 0) {
             return ErrorCode::DivideBy0;
-        } else if ((operation == "$") and (lhs < 0)) {
+        } else if (operation == "$" and lhs < 0) {
             return ErrorCode::SqrtOfNegativeNumber;
-        } else if ((operation == "%") and (!isInteger(lhs) or !isInteger(rhs))) {
+        } else if (operation == "%" and (!isInteger(lhs) or !isInteger(rhs))) {
             return ErrorCode::ModuleOfNonIntegerValue;
         }
-
         *out = calc::calculations.at(operation[0])(lhs, rhs);
+
         return ErrorCode::OK;
     }
     
