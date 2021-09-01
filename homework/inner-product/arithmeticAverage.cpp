e2234a95a51540bdbaa0771f06f8f0cf5424b3a9
kondorski <thegreatvaultofknowledge@gmail.com>
2021-09-01 18:58:21 +0200

    working distance

--- homework/inner-product/arithmeticAverage.cpp
+++ homework/inner-product/arithmeticAverage.cpp
@@ -1,21 +1,21 @@
 #include "arithmeticAverage.hpp"
 
 #include <algorithm>
+#include <cmath>
 #include <functional>
 #include <numeric>
 
 double ArithmeticAverage(const std::vector<int>& first, const std::vector<int>& second) {
     double firstSum =  std::accumulate(first.begin(), first.end(), 0, std::plus<>{}); 
     double secondSum = std::accumulate(second.begin(), second.end(), 0, std::plus<>{});
-    return std::plus<>{}(firstSum, secondSum) / first.size() + second.size();
+    return std::plus<>{}(firstSum, secondSum) / (first.size() + second.size());
 }
 
 
 double Distance(const std::vector<int>& first, const std::vector<int>& second) {
-    if (first.size() == second.size()) {
-
-    }
-    else {
-        
-    }
+        double sum = 0;
+        for (size_t i {0}; i < first.size(); ++i) {
+            sum += std::pow(first[i] - second[i], 2);
+        }
+        return std::sqrt(sum);;
 }
--- homework/inner-product/main.cpp
+++ homework/inner-product/main.cpp
@@ -1,8 +1,10 @@
 #include "arithmeticAverage.hpp"
+#include <iostream>
+
 
 int main () {
-    std::vector<int> first{-5, 4, 2, -5, 2, 4, 6, 7};
-    std::vector<int> second{3, 5, -2, 6, 7, -3, 6, 9};
+    std::vector<int> first{7, 4, 3};
+    std::vector<int> second{17, 6, 2};
 
     ArithmeticAverage(first, second);
     Distance(first, second);
