2ca09c9f68ee03cc0454b99b76ead4a678b3aba4
Crazyayane07 <crazyayane07@gmail.com>
2021-09-01 10:05:57 +0200

    add missing hpp

---
+++ homework/inner-product/arithmeticAverage.hpp
@@ -0,0 +1,6 @@
+#pragma once
+#include <vector>
+
+float ArithmeticAverage(const std::vector<int>& v1, const std::vector<int>& v2);
+
+float Distance(const std::vector<int>& v1, const std::vector<int>& v2);
