7ac7914fbcb02726177f94d37da24a09908eb4c6
Jakub Kubalka <jk.kubalka@gmail.com>
2021-09-10 12:58:29 +0200

    Every test is passing

--- homework/advanced-calculator/advancedCalculator.cpp
+++ homework/advanced-calculator/advancedCalculator.cpp
@@ -15,26 +15,46 @@ std::map<char, std::function<double(double, double)>> possibilities = {
 };
 
 ErrorCode process(std::string input, double* out) {
-    auto itToRemove = std::copy_if(input.begin(), input.end(), input.begin(), [](const auto& ch){return !(std::isspace(ch)); });
+    auto itToRemove = std::copy_if(input.begin(), input.end(), input.begin(), [](const auto& ch){return !(std::isspace(ch)); });  
     if (itToRemove != input.end()) {
-        input.erase(itToRemove, input.end());
+        input.erase(itToRemove, input.end()); //Preparing string, removing extra spaces
     }
     auto symbolIt =  std::adjacent_find(input.begin(), 
                                         input.end(), 
                                         [](const auto& ch1, const auto& ch2){ 
                                             return ((std::isdigit(ch1) && std::ispunct(ch2)) && ch2 != '.');
-                                            }) + 1;
+                                            }) + 1; //Finding mathematical character, key to map
     std::string value;
-    std::copy(input.begin(), symbolIt, std::back_inserter(value));
-    if ( std::find_if(value.begin(), value.end(), [](const auto& ch){ return !std::isdigit(ch) && ch != '.' && ch != '-'; }) != value.end() ) {
+    std::copy(input.begin(), symbolIt, std::back_inserter(value)); //Finding first value
+    auto wrongCharacter = std::find_if(value.begin(), value.end(), [](const auto& ch){ return !std::isdigit(ch) && ch != '.' && ch != '-'; });
+    if ( wrongCharacter != value.end() ) { //Checking if in first value is any extra character
+        if (possibilities.find(*wrongCharacter) == possibilities.end()) {
+            return ErrorCode::BadCharacter;
+        }
         return ErrorCode::BadFormat;
+    }
+    if (std::count_if(value.begin(), value.end(), [](const auto& ch){ return ch == '.';}) > 1) { //Checking if number of dots is correct
+            return ErrorCode::BadFormat;
     }
     double val1 = std::stod(value);
     value.clear();
-    if (symbolIt + 1 != input.end()) {
-        std::copy(symbolIt + 1, input.end(), std::back_inserter(value));
-        if ( std::find_if(value.begin(), value.end(), [](const auto& ch){ return !std::isdigit(ch) && ch != '.' && ch != '-'; }) != value.end() ) {
+    if (symbolIt + 1 != input.end()) { 
+        if (*symbolIt == '!') { //If we want to calculate factorial and it isn't last sign, return BadFormat
             return ErrorCode::BadFormat;
+        }
+        std::copy(symbolIt + 1, input.end(), std::back_inserter(value));
+        wrongCharacter = std::find_if(value.begin(), 
+                                    value.end(), 
+                                    [](const auto& ch){ 
+                                        return (!std::isdigit(ch) && ch != '.' && ch != '-'); });
+        if ( wrongCharacter != value.end() ) { //Checking if in second value is any extra character
+            if (possibilities.find(*wrongCharacter) == possibilities.end()) {
+                return ErrorCode::BadCharacter;
+            }
+            return ErrorCode::BadFormat;
+        }
+        if (std::count_if(value.begin(), value.end(), [](const auto& ch){ return ch == '.';}) > 1) { //Checking if number of dots is correct
+                return ErrorCode::BadFormat;
         }
         double val2 = std::stod(value);
         if (*symbolIt == '/' && val2 == 0) {
@@ -46,13 +66,16 @@ ErrorCode process(std::string input, double* out) {
         if (possibilities.find(*symbolIt) == possibilities.end()) {
             return ErrorCode::BadCharacter;
         }
+        if (*symbolIt == '$' && val1 < 0) {
+            return ErrorCode::SqrtOfNegativeNumber;
+        }
         *out = possibilities[*symbolIt](val1, val2);
         return ErrorCode::OK;
     }
     if (possibilities.find(*symbolIt) == possibilities.end()) {
         return ErrorCode::BadCharacter;
     }
-    if (*symbolIt == '&' && val1 < 0) {
+    if (*symbolIt == '$' && val1 < 0) {
         return ErrorCode::SqrtOfNegativeNumber;
     }
     if (symbolIt == input.end()) {
