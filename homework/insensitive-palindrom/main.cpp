689fa28e3abc413694bdf7992eea67cbacb32793
bartosz-gruszczyk <bartosz.gruszczyk@gmail.com>
2021-09-14 14:16:23 +0200

    seems to work correctly

--- homework/insensitive-palindrom/main.cpp
+++ homework/insensitive-palindrom/main.cpp
@@ -1,8 +1,8 @@
 #include "palindrome.hpp"
+#include <iostream>
 
 int main() {
-
-    
-
+    std::string str = "Eva, can I see bees in a cave?";
+    std::cout << std::boolalpha << is_palindrome(str) << '\n';
     return 0;
 }
--- homework/insensitive-palindrom/palindrome.cpp
+++ homework/insensitive-palindrom/palindrome.cpp
@@ -1,6 +1,11 @@
 #include "palindrome.hpp"
+#include <algorithm>
+#include <cctype>
 
-bool is_palindrome(std::string str) {
-
-    return false;
+bool is_palindrome(std::string text) {
+    text.erase(std::remove_if(text.begin(), text.end(), [](unsigned char c)
+                              { return isspace(c) || !isalpha(c); }),
+               text.end());
+    std::transform(text.begin(), text.end(), text.begin(), tolower);
+    return std::equal(text.cbegin(), text.cend(), text.crbegin(), text.crend());
 }
