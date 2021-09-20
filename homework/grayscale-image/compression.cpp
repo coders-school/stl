c92dfe3632a3c4d2a75196cc01d9a24c447da0e2
PawelYak <pawelstojak@gmail.com>
2021-09-20 11:14:58 +0200

    compression implementation

--- homework/grayscale-image/compression.cpp
+++ homework/grayscale-image/compression.cpp
@@ -1,31 +1,38 @@
 #include "compression.hpp"
 #include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <iostream>
+#include <iterator>
 #include <utility>
 #include <vector>
 
 std::vector<std::pair<uint8_t, uint8_t>>
-compressGrayscale(std::array<std::array<uint8_t, 240>, 160> array) {
+compressGrayscale(std::array<std::array<uint8_t, width>, height> array) {
   std::vector<std::pair<uint8_t, uint8_t>> compressed{};
-  compressed.reserve(array.size());
+  compressed.reserve(width*height);
 
+  uint8_t counter{1};
   for (const auto &el : array) {
-    size_t counter{0};
-    for (auto e : el) {
-      while (e == e - 1) {
+    for (auto pixel = el.cbegin(); pixel != el.cend(); pixel++) {
+      if (*pixel != *(std::next(pixel)) || pixel == std::prev(el.end())) {
+        compressed.emplace_back(std::make_pair(static_cast<uint8_t>(*pixel), counter));
+        std::cout << "|" << int(*pixel) << ' ' << int(counter) << "|";
+        counter = 1;
+      } else {
         counter++;
       }
-      compressed.emplace_back(std::make_pair(e, counter));
-    };
-  };
+    }
+    std::cout << '\n';
+  }
   compressed.shrink_to_fit();
   return compressed;
 }
 
-
-  std::array<std::array<uint8_t, 240>, 160>
+std::array<std::array<uint8_t, width>, height>
 decompressGrayscale(std::vector<std::pair<uint8_t, uint8_t>> compressed) {
 
-    std::array<std::array<uint8_t, 240>, 160> decompressed{0};
+  std::array<std::array<uint8_t, width>, height> decompressed{0};
 
-    return decompressed;
-  };
+  return decompressed;
+};
--- homework/grayscale-image/compression.hpp
+++ homework/grayscale-image/compression.hpp
@@ -1,12 +1,12 @@
-//#include "compressGrayscale.cpp"
+
 #include <array>
 #include <cstdint>
 #include <utility>
 #include <vector>
-constexpr size_t width = 240;
-constexpr size_t height = 160;
+constexpr size_t width = 240; //240
+constexpr size_t height = 160; //160
 std::vector<std::pair<uint8_t, uint8_t>>
-compressGrayscale(std::array<std::array<uint8_t, 240>, 160> array);
+compressGrayscale(std::array<std::array<uint8_t, width>, height> array);
 
-std::array<std::array<uint8_t, 240>, 160>
+std::array<std::array<uint8_t, width>, height>
 decompressGrayscale(std::vector<std::pair<uint8_t, uint8_t>> compressed);
