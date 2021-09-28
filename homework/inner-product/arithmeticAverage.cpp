cbbe5db75b32826420c63599e0be2a7c66bc9220
miczel <michal.soloducha@gmail.com>
2021-09-28 19:29:24 +0200

    arithmeticAverage() works

--- homework/inner-product/arithmeticAverage.cpp
+++ homework/inner-product/arithmeticAverage.cpp
@@ -1,7 +1,12 @@
 #include "arithmeticAverage.hpp"
+// #include <algorithm>
+#include <iostream>
+#include <numeric>
 
 double ArithmeticAverage(const std::vector<int>& first, const std::vector<int>& second) {
-    return 0;
+    int sum1 = std::accumulate(begin(first), end(first), 0);
+    int sum2 = std::accumulate(begin(second), end(second), 0);
+    return double((sum1 + sum2) / double(first.size() + second.size()));
 }
 
 double Distance(const std::vector<int>& first, const std::vector<int>& second) {
