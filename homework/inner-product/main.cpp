2ab9dd8a4d4bea0976598d7e6a96a3de218fca6a
miczel <michal.soloducha@gmail.com>
2021-09-28 19:08:09 +0200

    Initialise project files

---
+++ homework/inner-product/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "arithmeticAverage.hpp"
+
+int main() {
+    std::cout << "Test\n";
+}
--- homework/inner-product/test.cpp
+++ homework/inner-product/test.cpp
@@ -1,5 +1,4 @@
 #include <vector>
-
 #include "arithmeticAverage.hpp"
 #include "gtest/gtest.h"
 
