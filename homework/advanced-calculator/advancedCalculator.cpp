2b3c7db851f649c4b52b61c106befa1d8e4c1882
miczel <michal.soloducha@gmail.com>
2021-09-29 14:53:02 +0200

    Add enum class ErrorCode

--- homework/advanced-calculator/advancedCalculator.cpp
+++ homework/advanced-calculator/advancedCalculator.cpp
@@ -1,1 +1,5 @@
 #include "advancedCalculator.hpp"
+
+ErrorCode process(std::string input, double* out) {
+    return ErrorCode::Ok;
+}
--- homework/advanced-calculator/advancedCalculator.hpp
+++ homework/advanced-calculator/advancedCalculator.hpp
@@ -1,1 +1,13 @@
 #pragma once
+#include <string>
+
+enum class ErrorCode {
+    Ok,
+    BadCharacter,
+    BadFormat,
+    DivideBy0,
+    SqrtOfNegativeNumber,
+    ModuleOfNonIntegerValue
+};
+
+ErrorCode process(std::string, double*);
