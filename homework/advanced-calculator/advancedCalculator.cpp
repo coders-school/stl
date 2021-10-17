e91e3e0accac179d34abdecbb4dfa41db7109b34
bartosz-gruszczyk <bartosz.gruszczyk@gmail.com>
2021-10-18 01:02:20 +0200

    work in progress...

--- homework/advanced-calculator/advancedCalculator.cpp
+++ homework/advanced-calculator/advancedCalculator.cpp
@@ -1,5 +1,6 @@
 #include "advancedCalculator.hpp"
 #include <algorithm>
+#include <cmath>
 #include <functional>
 #include <map>
 // #include <string>
@@ -14,17 +15,31 @@ ErrorCode process(std::string input, double* out) {
     // std::erase_if(input, std::isspace);
 
     std::erase(input, ' ');
-    std::cout << input << "after erase \n";
+    // std::cout << input << "after erase \n";
     // input.erase()
     char operation = input[0];  // wczesniej 1
     input.erase(0, 1); // wczensiej 0, 2
-    double secondNumber = std::stod(input);
+    double secondNumber = 1.0; 
+    if (operation != '!') {
+        secondNumber = std::stod(input);
+    }
 
-    std::cout << firstNumber << '_' << operation << '_' << secondNumber << '\n';
-    
+    // std::cout << firstNumber << '_' << operation << '_' << secondNumber << '\n';
+    // std::function<int(int, int)> factorial = [&factorial](int a, int b) -> int {return a < 2 ? 1 : (a * factorial(a - 1, a));};
+    std::function<double(double, double)> factorial = [&factorial](double a, double b) -> double {return a < 2 ? 1 : (a * factorial(a - 1, a));};
+
+    // std::cout << "uuuuu \n\n   : " << factorial(2, 3) << "\n----\n";
+
+    // std::map<char, std::function<double(double, double)>> operations {
     std::map<char, std::function<double(double, double)>> operations {
         {'+', [](double a, double b){return a + b;}},
         {'-', [](double a, double b){return a - b;}},
+        {'*', [](double a, double b){return a * b;}},
+        {'/', [](double a, double b){return a / b;}},
+        {'%', [](int a, int b){return a % b;}},
+        {'^', [](double a, double b){return std::pow(a, b);}},
+        {'$', [](double a, double b){return std::pow(a, 1/b);}},
+        {'!', factorial}
 
     };
 
