709908d7525cfa0576dcb95232ff7e830237244a
Michał Zdziech <m.zdziech82@gmail.com>
2021-11-25 22:08:36 +0100

    Added definitions funcion sortLength

---
+++ homework/length-sort/sort.cpp
@@ -0,0 +1,17 @@
+#include "sort.hpp"
+
+// funkcja ma przyjąć forward_list<std::string> i zwrócić deque<std::string> z posortowanymi słowami
+// od najkrótszego do najdłuższego. Jeżeli dwa lub więcej słów ma tyle samo znaków posortuj je leksykograficznie.
+
+std::deque<std::string>length_Sort(std::forward_list<std::string>& words) {
+
+    std::deque<std::string> word;
+    word.resize(100,0);
+    for(const auto& w : words) {
+
+        word.push_back( w);
+    }
+    std::sort(word.begin(), word.end());
+
+    return word;
+}
