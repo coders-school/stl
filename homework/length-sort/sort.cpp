d2312b792ce1ce8996799a71ede29f52fdccd2b9
Michał Zdziech <m.zdziech82@gmail.com>
2021-11-25 22:11:36 +0100

    rename name function with length_Sort to lengthSort

--- homework/length-sort/sort.cpp
+++ homework/length-sort/sort.cpp
@@ -3,7 +3,7 @@
 // funkcja ma przyjąć forward_list<std::string> i zwrócić deque<std::string> z posortowanymi słowami
 // od najkrótszego do najdłuższego. Jeżeli dwa lub więcej słów ma tyle samo znaków posortuj je leksykograficznie.
 
-std::deque<std::string>length_Sort(std::forward_list<std::string>& words) {
+std::deque<std::string>lengthSort(std::forward_list<std::string>& words) {
 
     std::deque<std::string> word;
     word.resize(100,0);
