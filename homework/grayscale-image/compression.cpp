31a52b86cdf6c30605e73b1f6d1fcf50b29fe805
Maciej Białczak <m.bialczak@protonmail.com>
2022-01-06 22:51:20 +0100

    compressGrayscale seems to work

--- homework/grayscale-image/compression.cpp
+++ homework/grayscale-image/compression.cpp
@@ -2,8 +2,34 @@
 
 // compresses a bitmap into compressedImage format
 CompressedImage compressGrayscale(const Image& bitmap)
-{}
+{
+    CompressedImage result;
+    for (const auto& row : bitmap) {
+        compressRow(row, result);
+    }
+
+    return result;
+}
 
 // decompresses a compressed image into a grayscale bitmap
 Image decompressGrayscale(const CompressedImage& compressed_bitmap)
-{}
+{
+    // REMOVE after implementation
+    return Image {};
+}
+
+// Helper function for compressGrayscale. Parses single row of a bitmap
+// and inserts compressed data into passed compressed_bitmap
+void compressRow(const BitmapRow& row, CompressedImage& compressed_bitmap)
+{
+    auto it = row.begin();
+    while (it != row.end()) {
+        uint8_t pixel_color = *it;
+        uint8_t repeat_count { 1 };
+        // check next element for same color occurences
+        while (++it != row.end() && *it == pixel_color) {
+            ++repeat_count;
+        }
+        compressed_bitmap.emplace_back(pixel_color, repeat_count);
+    }
+}
--- homework/grayscale-image/compression.hpp
+++ homework/grayscale-image/compression.hpp
@@ -8,9 +8,14 @@ constexpr size_t height = 32;
 
 using Image = std::array<std::array<uint8_t, width>, height>;
 using CompressedImage = std::vector<std::pair<uint8_t, uint8_t>>;
+using BitmapRow = std::array<uint8_t, width>;
 
 // compresses a grayscale bitmap into compressedImage format
 CompressedImage compressGrayscale(const Image& bitmap);
 
 // decompresses a compressed image into a grayscale bitmap
 Image decompressGrayscale(const CompressedImage& compressed_bitmap);
+
+// Helper function for compressGrayscale. Parses single row of a bitmap
+// and inserts compressed data into passed compressed_bitmap
+void compressRow(const BitmapRow& row, CompressedImage& compressed_bitmap);
