e4e96923ce1837a179a7b9b107538bd251031d34
Maciej Białczak <m.bialczak@protonmail.com>
2022-01-07 04:05:23 +0100

    all tests passed but needs refactoring

--- homework/grayscale-image/compression.cpp
+++ homework/grayscale-image/compression.cpp
@@ -4,6 +4,7 @@
 CompressedImage compressGrayscale(const Image& bitmap)
 {
     CompressedImage result;
+
     for (const auto& row : bitmap) {
         compressRow(row, result);
     }
@@ -14,8 +15,57 @@ CompressedImage compressGrayscale(const Image& bitmap)
 // decompresses a compressed image into a grayscale bitmap
 Image decompressGrayscale(const CompressedImage& compressed_bitmap)
 {
-    // REMOVE after implementation
-    return Image {};
+    Image result {};
+
+    auto it = compressed_bitmap.begin();
+
+    for (auto& row : result) {
+        std::size_t col { 0 };
+        while (col < row.size()) {
+            auto color = it->first;
+            auto count = it->second;
+            for (auto i = count; i > 0; --i) {
+                row[col] = color;
+                ++col;
+            }
+            ++it;
+        }
+        // for (std::size_t col = 0; col < row.size(); ) {
+        //     for(std::size_t repeat = *it->second ; repeat > 0 ; --repeat ){
+        //       row[col] = *it->first;
+        //     }
+        // }
+    }
+
+    // for (auto row : result) {
+    //     for (auto pixel : row) {
+    //         pixel = 0;
+    //     }
+    // }
+
+    // std::size_t row { 0 };
+    // std::size_t column { 0 };
+    // for (const auto& pair : compressed_bitmap) {
+    //     for (std::size_t i = 0; i < pair.second; ++i) {
+    //         result[row][column] = pair.first;
+    //         ++column;
+    //         if (column = width) {
+    //             column = 0;
+    //             ++row;
+    //         }
+    //     }
+    // }
+
+    // std::size_t el_count { 0 };
+    // // std::size_t column { 0 }; REMOVE
+    // for (const auto& pair : compressed_bitmap) {
+    //     for (std::size_t i = 0; i < pair.second, el_count < height * width; ++i, ++el_count) {
+    //         result[el_count / width][el_count % width] = pair.first;
+    //         // ++el_count; REMOVE
+    //     }
+    // }
+
+    return result;
 }
 
 // Helper function for compressGrayscale. Parses single row of a bitmap
@@ -33,3 +83,8 @@ void compressRow(const BitmapRow& row, CompressedImage& compressed_bitmap)
         compressed_bitmap.emplace_back(pixel_color, repeat_count);
     }
 }
+
+// // Helper function for decompressGrayscale. TODO:
+// // Decompresses a single row from the compressed bitmap
+// BitmapRow decompressRow(const CompressedImage& compressed_bitmap, BitmapRow::iterator& it)
+// {}
--- homework/grayscale-image/compression.hpp
+++ homework/grayscale-image/compression.hpp
@@ -3,8 +3,8 @@
 #include <array>
 #include <vector>
 
-constexpr size_t width = 32;
-constexpr size_t height = 32;
+constexpr size_t width = 240;
+constexpr size_t height = 160;
 
 using Image = std::array<std::array<uint8_t, width>, height>;
 using CompressedImage = std::vector<std::pair<uint8_t, uint8_t>>;
@@ -19,3 +19,8 @@ Image decompressGrayscale(const CompressedImage& compressed_bitmap);
 // Helper function for compressGrayscale. Parses single row of a bitmap
 // and inserts compressed data into passed compressed_bitmap
 void compressRow(const BitmapRow& row, CompressedImage& compressed_bitmap);
+
+// Helper function for decompressGrayscale.
+// Decompresses a single row from the compressed bitmap
+// Takes TODO: REMOVE INU
+BitmapRow decompressRow(const CompressedImage& compressed_bitmap, BitmapRow::iterator& it);
