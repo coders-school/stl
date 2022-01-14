0c2903a4f7162e8b47a91f00f7cc085f6ac08ea0
Aleksiiej <94867075+Aleksiiej@users.noreply.github.com>
2022-01-14 21:07:20 +0100

    Add files via upload

---
+++ homework/grayscale-image/sort.hpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <forward_list>
+
+constexpr size_t width = 32;
+constexpr size_t height = 32;
+
+std::vector<std::pair<uint8_t,uint8_t>>compressGrayscale(std::array<std::array<uint8_t,width>,height>pgm_arr);
+std::array<std::array<uint8_t,width>,height>decompressGrayscale(std::vector<std::pair<uint8_t,uint8_t>>compression_result);
