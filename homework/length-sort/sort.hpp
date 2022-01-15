0d20f60672907517a8a860d2025b11ec15310cf3
Kamil <kamil.fiz@gmail.com>
2022-01-15 16:31:40 +0100

    Create sort.hpp

---
+++ homework/length-sort/sort.hpp
@@ -0,0 +1,11 @@
+#ifndef SORT_HPP
+#define SORT_HPP
+
+#include <string>
+#include <map>
+#include <forward_list>
+#include <deque>
+
+std::deque<std::string> length_sort(std::forward_list<std::string> & word_list);
+
+#endif
