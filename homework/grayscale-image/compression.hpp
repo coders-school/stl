6bf11b624749cf3055720e915094de9695db406f
Kamil <kamil.fiz@gmail.com>
2022-01-18 15:56:51 +0100

    Update compression.hpp

--- homework/grayscale-image/compression.hpp
+++ homework/grayscale-image/compression.hpp
@@ -5,11 +5,16 @@
 #include <vector>
 #include <utility>
 
-constexpr size_t width = 32;
-constexpr size_t height = 32;
+constexpr size_t width = 240;
+constexpr size_t height = 160;
+
+constexpr size_t width2 = 32;
+constexpr size_t height2 = 32;
 
 std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(std::array<std::array<uint8_t, width>, height> to_compress);
-
 std::array<std::array<uint8_t, width>, height> decompressGrayscale (std::vector<std::pair<uint8_t, uint8_t>> to_decompress);
+
+std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(std::array<std::array<uint8_t, width2>, height2> to_compress);
+std::array<std::array<uint8_t, width2>, height2> decompressGrayscale2 (std::vector<std::pair<uint8_t, uint8_t>> to_decompress);
 
 #endif
