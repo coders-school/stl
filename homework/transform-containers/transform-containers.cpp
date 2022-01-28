0d05684465e13f70cf9a3dd51b2cc06119fd56ec
Maciej Białczak <m.bialczak@protonmail.com>
2022-01-28 08:44:12 +0100

    Added empty removeDuplicateAndTranformToMap() to pass initial build

--- homework/transform-containers/transform-containers.cpp
+++ homework/transform-containers/transform-containers.cpp
@@ -1,1 +1,6 @@
 #include "transform-containers.hpp"
+
+intStringMap removeDuplicateAndTranformToMap(const stringList& list, const intDeque& ints)
+{
+    return intStringMap {};
+}
--- homework/transform-containers/transform-containers.hpp
+++ homework/transform-containers/transform-containers.hpp
@@ -1,1 +1,13 @@
 #pragma once
+#include <algorithm>
+#include <deque>
+#include <list>
+#include <map>
+#include <string>
+
+// helper directives
+using stringList = std::list<std::string>;
+using intDeque = std::deque<int>;
+using intStringMap = std::map<int, std::string>;
+
+intStringMap removeDuplicateAndTranformToMap(const stringList& list, const intDeque& ints);
