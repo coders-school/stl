185599d438d3586e8fe6e4e188ddc547e8d77ac6
Maciej Białczak <m.bialczak@protonmail.com>
2022-01-28 12:42:13 +0100

    first template helper experiments

--- homework/transform-containers/transform-containers.cpp
+++ homework/transform-containers/transform-containers.cpp
@@ -1,30 +1,71 @@
 #include "transform-containers.hpp"
 
-#include <iostream>
-
 // removes duplicates from passed containers and returns
 // a map of <int,string> unique elements
 // does not modify original passed arguments
-intStringMap removeDuplicateAndTranformToMap(stringList strings, intDeque ints)
+intStringMap removeDuplicateAndTranformToMap(const stringList& strings, const intDeque& ints)
 {
     // remove duplicates from passed containers
-    strings.sort();
-    auto last_string = std::unique(strings.begin(), strings.end());
-    strings.erase(last_string, strings.end());
+    // strings.sort();
+    // auto last_string = std::unique(strings.begin(), strings.end());
+    // strings.erase(last_string, strings.end());
 
-    std::sort(ints.begin(), ints.end());
-    auto last_int = std::unique(ints.begin(), ints.end());
-    ints.erase(last_int, ints.end());
+    // std::sort(ints.begin(), ints.end());
+    // auto last_int = std::unique(ints.begin(), ints.end());
+    // ints.erase(last_int, ints.end());
+    auto unique_strings = uniqueCopy(strings);
+    auto unique_ints = uniqueCopy(ints);
 
     intStringMap result;
 
-    std::transform(strings.begin(),
-                   strings.end(),
-                   ints.begin(),
-                   std::inserter(result, result.end()),
-                   [](const auto& text, const auto& num) {
-                       return std::make_pair(num, text);
-                   });
-
+    // std::transform(strings.begin(),
+    //                strings.end(),
+    //                ints.begin(),
+    //                std::inserter(result, result.end()),
+    //                [](const auto& text, const auto& num) {
+    //                    return std::make_pair(num, text);
+    //                });
     return std::move(result);
 }
+
+// returns a copy of container storing only unique elements of the original  passed containar
+// takes container by copy in order to preserve orignal container unmodified (dessign choice)
+// primary template
+// template <typename value_type, template <typename> typename Container>
+// Container<value_type> uniqueCopy(Container<value_type> container)
+// {
+//     std::sort(begin(container), end(container));
+//     auto first_to_remove = std::unique(begin(container), end(container));
+//     container.erase(first_to_remove, end(container));
+
+//     return std::move(container);
+// }
+
+// partial specialisation for lists
+template <typename value_type>
+std::list<value_type> uniqueCopy(std::list<value_type> listContainer)
+{
+    listContainer.sort();
+    auto first_to_remove = std::unique(begin(listContainer), end(listContainer));
+    listContainer.erase(first_to_remove, end(listContainer));
+    return std::move(listContainer);
+}
+
+template <typename value_type>
+std::deque<value_type> uniqueCopy(std::deque<value_type> dequeContainer)
+{
+    //     std::deque<value_type> result { dequeContainer };
+    std::sort(begin(dequeContainer), end(dequeContainer));
+    auto first_to_remove = std::unique(begin(dequeContainer), end(dequeContainer));
+    dequeContainer.erase(first_to_remove, end(dequeContainer));
+    return std::move(dequeContainer);
+}
+
+// stringList uniqueCopy(const stringList& textList)
+// {
+//     stringList result { textList };
+//     result.sort();
+//     auto first_to_remove = std::unique(begin(result), end(result));
+//     result.erase(first_to_remove, end(result));
+//     return std::move(result);
+// }
--- homework/transform-containers/transform-containers.hpp
+++ homework/transform-containers/transform-containers.hpp
@@ -10,4 +10,22 @@ using stringList = std::list<std::string>;
 using intDeque = std::deque<int>;
 using intStringMap = std::map<int, std::string>;
 
-intStringMap removeDuplicateAndTranformToMap(stringList strings, intDeque ints);
+intStringMap removeDuplicateAndTranformToMap(const stringList& strings, const intDeque& ints);
+
+// returns a copy of container storing only unique elements of the original  passed containar
+// takes container by copy in order to preserve orignal container unmodified (dessign choice)
+// primary template
+// template <typename value_type, template <typename> typename Container>
+// Container<value_type> uniqueCopy(Container<value_type> container);
+
+// partial specialisation for lists
+template <typename value_type>
+std::list<value_type> uniqueCopy(std::list<value_type> listContainer);
+
+// partial specialisation for deques
+template <typename value_type>
+std::deque<value_type> uniqueCopy(std::deque<value_type> dequeContainer);
+
+// template <typename value_type>
+// typename std::list<value_type> uniqueCopy(typename std::list<value_type> container);
+// stringList uniqueCopy(const stringList& textList);
