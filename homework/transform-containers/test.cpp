d3f740d0cd8b704788f625b1b0052647edf8a167
Maciej Białczak <m.bialczak@protonmail.com>
2022-01-28 08:31:19 +0100

    Prepared empty files for building
    - Added empty source and header files.
    - Updated CMakeLists.txt
    - included proper header in test.cpp

--- homework/transform-containers/test.cpp
+++ homework/transform-containers/test.cpp
@@ -1,20 +1,48 @@
 #include "gtest/gtest.h"
+#include "transform-containers.hpp"
 
-// TODO: add proper includes
-
-TEST(transformContainerTests, ShouldReturnUniqueMap) {
-    std::map<int, std::string> expected_result{
-        {1, "Ala"},
-        {2, "Kot"},
-        {3, "Ma"},
-        {4, "Rysia"},
-        {5, "Sierotka"}};
-    std::list<std::string> list{
-        "Ala", "Kot", "Ma", "Rysia", "Ala",
-        "Sierotka", "Kot", "Ma", "Ala"};
-    std::deque<int> deque{
-        1, 2, 3, 4, 5, 3, 1, 2, 3, 4,
-        5, 2, 3, 1, 1, 2, 3, 2, 1, 4};
+TEST(transformContainerTests, ShouldReturnUniqueMap)
+{
+    std::map<int, std::string> expected_result {
+        { 1, "Ala" },
+        { 2, "Kot" },
+        { 3, "Ma" },
+        { 4, "Rysia" },
+        { 5, "Sierotka" }
+    };
+    std::list<std::string> list {
+        "Ala",
+        "Kot",
+        "Ma",
+        "Rysia",
+        "Ala",
+        "Sierotka",
+        "Kot",
+        "Ma",
+        "Ala"
+    };
+    std::deque<int> deque {
+        1,
+        2,
+        3,
+        4,
+        5,
+        3,
+        1,
+        2,
+        3,
+        4,
+        5,
+        2,
+        3,
+        1,
+        1,
+        2,
+        3,
+        2,
+        1,
+        4
+    };
     auto result = removeDuplicateAndTranformToMap(list, deque);
 
     ASSERT_TRUE(expected_result.size() == result.size());
