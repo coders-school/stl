2ee98127cd71b2ea38f03afe7a742f768be14e9e
Maciej Białczak <m.bialczak@protonmail.com>
2022-01-28 07:14:46 +0100

    introduced normalize() helper preparing string for comparison

--- homework/insensitive-palindrom/palindrome-check.cpp
+++ homework/insensitive-palindrom/palindrome-check.cpp
@@ -1,8 +1,32 @@
 #include "palindrome-check.hpp"
 
+#include <algorithm>
+#include <iostream>   // REMOVE
+
 // checks if the given text is a palindrome
 // Is case insensitive, ignores special chars, like ,.?()[] and white spaces
 bool is_palindrome(const std::string& text)
 {
+    // get a string ready for comparison
+    // (select only alpha chars and make them lower case)
+    std::string normalized = normalize(text);
+    std::cout << "text:\t" << text << 'n'
+              << "alpha only: " << normalized
+              << "\n---------------------\n";
     return false;
+}
+
+// returns string stripped of chars other than alphabethical
+// and lowercased
+std::string normalize(const std::string& text)
+{
+    std::string result;
+    result.reserve(text.size());
+    std::for_each(begin(text), end(text), [&result](auto ch) {
+        if (isalpha(ch)) {
+            result += tolower(ch);
+        }
+    });
+    result.shrink_to_fit();
+    return result;
 }
--- homework/insensitive-palindrom/palindrome-check.hpp
+++ homework/insensitive-palindrom/palindrome-check.hpp
@@ -5,3 +5,7 @@
 // checks if the given text is a palindrome
 // Is case insensitive, ignores special chars, like ,.?()[] and white spaces
 bool is_palindrome(const std::string& text);
+
+// returns string stripped of chars other than alphabethical
+// and lowercased
+std::string normalize(const std::string& text);
