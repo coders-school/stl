3d0b76741267303f9564247faddc793d91a3d63b
Maciej Białczak <m.bialczak@protonmail.com>
2022-01-28 06:21:42 +0100

    Introduced dummy is_palindrome() for passing initial project build

--- homework/insensitive-palindrom/palindrome-check.cpp
+++ homework/insensitive-palindrom/palindrome-check.cpp
@@ -1,1 +1,8 @@
 #include "palindrome-check.hpp"
+
+// checks if the given text is a palindrome
+// Is case insensitive, ignores special chars, like ,.?()[] and white spaces
+bool is_palindrome(const std::string& text)
+{
+    return false;
+}
--- homework/insensitive-palindrom/palindrome-check.hpp
+++ homework/insensitive-palindrom/palindrome-check.hpp
@@ -1,1 +1,7 @@
 #pragma once
+
+#include <string>
+
+// checks if the given text is a palindrome
+// Is case insensitive, ignores special chars, like ,.?()[] and white spaces
+bool is_palindrome(const std::string& text);
