8b848af717e3cdf269e62d04c90ed25ab50d88bc
Maciej Białczak <m.bialczak@protonmail.com>
2022-02-02 22:50:03 +0100

    Added simple user input receipt

--- homework/advanced-calculator/advancedCalculator.cpp
+++ homework/advanced-calculator/advancedCalculator.cpp
@@ -1,6 +1,11 @@
 #include "advancedCalculator.hpp"
 
+#include <iostream>
+
 ErrorCode process(std::string input, double* out)
 {
+    std::string user_input;
+    std::getline(std::cin, input);
+
     return ErrorCode::OK;
 }
