1285e07f566b8bdd44f07048fd7b236cd2309458
Maciej Białczak <m.bialczak@protonmail.com>
2022-02-03 00:20:53 +0100

    Started formatInput() implementation

--- homework/advanced-calculator/advancedCalculator.cpp
+++ homework/advanced-calculator/advancedCalculator.cpp
@@ -1,8 +1,10 @@
 #include "advancedCalculator.hpp"
 
 #include <algorithm>
+#include <array>
 #include <cctype>
 #include <iostream>
+#include <sstream>
 
 ErrorCode process(std::string input, double* out)
 {
@@ -20,4 +22,19 @@ void printInstructions()
 {
     std::cout << "-----Simple calculator-----\n"
               << "type 'quit' or enter a calculation to be performed\n";
+}
+
+FormatedInput formatInput(const std::string& line)
+{
+    //   * Dodawanie, mnożenie, dzielenie, odejmowanie (`+`,  `*` , `/` , `-`)
+    // * Modulo (`%`)
+    // * Obliczanie silni (`!`)
+    // * Podnoszenie liczby do potęgi (`^`)
+    // * Obliczanie pierwiastka (`$`)
+    static constexpr std::array<char, 8> allowed_operations { '+', '*' };
+    std::istringstream(line);
+    std::string lhs_operand;
+    std::string rhs_operand;
+    std::string operation;
+    line >> lhs_operand >> operation >> rhs_operand;
 }
