0a97fb9b08ef50700dc59a0c181436efbe6d997e
Maciej Białczak <m.bialczak@protonmail.com>
2022-02-02 22:39:37 +0100

    Added neccessary declarations, defs for passing initial build

--- homework/advanced-calculator/advancedCalculator.hpp
+++ homework/advanced-calculator/advancedCalculator.hpp
@@ -1,1 +1,14 @@
 #pragma once
+#include <string>
+
+enum class ErrorCode
+{
+    OK,
+    BadCharacter,             // operator other then a number
+    BadFormat,                // bad command format, e.g. "+ 5 4" instead of "4 + 5"
+    DivideBy0,                // attempt of division by zero
+    SqrtOfNegativeNumber,     // attempt of getting Sqrt of negative numbber
+    ModuleOfNonIntegerValue   // attempt of getting modulo of non integer value
+};
+
+ErrorCode process(std::string input, double* out);
