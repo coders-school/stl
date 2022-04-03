f5135d8cdfb1c89fbce1692491b74dae0da3b7fa
kogrodnik <94931123+kogrodnik@users.noreply.github.com>
2022-04-03 19:14:52 +0200

    Update vowel.hpp

--- homework/remove-vowels/vowel.hpp
+++ homework/remove-vowels/vowel.hpp
@@ -3,19 +3,4 @@
 #include <string> 
 #include <vector> 
 
-std::vector <std::string> removeVowels (std::vector<std::string> word){
-
-    std::vector<std::string> newVector;
-    
-    for (int i = 0; i < word.size(); i++){
-       std::string newString;
-        std::string newWord = word.at(i);
-        for (auto ele : newWord){
-        if (ele != 'a' && ele != 'e' && ele != 'i' && ele != 'o' && ele != 'u' && ele != 'y' && ele != 'A' && ele != 'E' && ele != 'I' && ele != 'O' && ele != 'U' && ele != 'Y'){
-        newString += ele;
-        }
-    }
-    newVector.push_back(newString);
-   }
-    return newVector;
-}
+std::vector <std::string> removeVowels (std::vector<std::string> word);
