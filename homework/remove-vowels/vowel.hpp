070865731d435e6071160a6a242a3304bd6f7367
kogrodnik <94931123+kogrodnik@users.noreply.github.com>
2022-04-12 18:56:27 +0200

    Update vowel.hpp

--- homework/remove-vowels/vowel.hpp
+++ homework/remove-vowels/vowel.hpp
@@ -3,4 +3,4 @@
 #include <string> 
 #include <vector> 
 
-std::vector <std::string> removeVowels (std::vector<std::string> word);
+std::vector <std::string> removeVowels (std::vector<std::string> &word);
