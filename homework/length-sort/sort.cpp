9c7744c65d130d751cd8e19c7852d8dd0b4b8ca9
Sl0mek <andrzejslomkapraca@gmail.com>
2022-05-14 22:23:27 +0200

    First implementation of lengthSort function

--- homework/length-sort/sort.cpp
+++ homework/length-sort/sort.cpp
@@ -1,6 +1,30 @@
 #include "sort.hpp"
 
-std::deque<std::string> lengthSort(std::forward_list<std::string>& list)
-{
-    
+
+bool mycomp(std::string a, std::string b){
+	//returns 1 if string a is alphabetically 
+	//less than string b
+	//quite similar to strcmp operation
+	return a<b;
 }
+
+void alphabaticallySort(std::deque<std::string>& a){
+	int n=a.size();
+	//mycomp function is the defined function which 
+	//sorts the strings in alphabatical order
+	sort(a.begin(),a.end(),mycomp);
+}
+
+std::deque<std::string> lengthSort(std::forward_list<std::string> list)
+{
+    //std::sort(list.begin(), list.end());
+    std::deque<std::string> deq;
+    for(auto word : list)
+    {
+        deq.push_back(word);
+    }
+    //std::sort(deq.begin(), deq.end());
+    alphabaticallySort(deq);
+    return deq;
+}
+	
--- homework/length-sort/sort.hpp
+++ homework/length-sort/sort.hpp
@@ -1,5 +1,6 @@
 #include <deque>
 #include <forward_list>
 #include <string>
+#include <algorithm>
 
-std::deque<std::string> lengthSort(std::forward_list<std::string>&);
+std::deque<std::string> lengthSort(std::forward_list<std::string>);
