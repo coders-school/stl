f95837892de2dd8ac8973a82ecd8beef5516426e
Sl0mek <92469177+Sl0mek@users.noreply.github.com>
2022-05-16 21:49:00 +0200

    Update compress.cpp

--- homework/grayscale-image/compress.cpp
+++ homework/grayscale-image/compress.cpp
@@ -1,25 +1,25 @@
 #include "compress.hpp"
 
-std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(std::array<std::array<uint8_t, 240>, 160> input)
+std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(std::array<std::array<uint8_t, 32>, 32> input)
 {
     std::vector<std::pair<uint8_t, uint8_t>> vec;
-    for (size_t i = 0; i < 160; i++)
+    for (size_t i = 0; i < input.size(); i++)
     {
         auto start = 0;
         int count = 0;
         int val = 0;
         int tmp = 0;
-        while(start < 240)
+        while (start < input[0].size() - 1)
         {
             val = input[i][start];
             count++;
-            while(start < 240)
+            while (start < 31)
             {
                 start++;
-                if(input[i][start] == val)
+                if (input[i][start] == val)
                 {
                     count++;
-                    if(start == 239)
+                    if (start == input[0].size() - 1)
                     {
                         break;
                     }
@@ -27,11 +27,12 @@ std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(std::array<std::array
                 else
                 {
                     vec.push_back(std::pair<uint8_t, uint8_t>{val, count});
+                    count = 0;
                     break;
                 }
             }
         }
-        
+        vec.push_back(std::pair<uint8_t, uint8_t>{val, count});
     }
-    
+    return vec;
 }
