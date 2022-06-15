80aa133fe59e8f78787cdcf18588d44511289565
Sl0mek <andrzejslomkapraca@gmail.com>
2022-06-15 12:23:14 +0200

    Adding is_high_letter and is_low_letter functions

--- homework/insensitive-palindrom/palindrom.cpp
+++ homework/insensitive-palindrom/palindrom.cpp
@@ -1,6 +1,25 @@
 #include"palindrom.hpp"
 
-bool is_palindrome(std::string str)
+bool is_high_letter(char a)
 {
 
+}
+
+bool is_low_letter(char a)
+{
+    
+}
+
+bool is_palindrome(std::string str)
+{
+     auto p = str.size()/2;
+     auto q = str.size()/2;
+     if(str.size() % 2 == 0) q++;
+     while(p != -1 && q == str.size())
+     {
+         if(str[p] != str[q])
+         {
+
+         }
+     }
 }
