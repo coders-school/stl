2972d7c473b4e9fef373b24b6b0bf5f0f88773bd
Sl0mek <andrzejslomkapraca@gmail.com>
2022-06-27 23:29:47 +0200

    Adding operations map

--- homework/advanced-calculator/advancedCalculator.cpp
+++ homework/advanced-calculator/advancedCalculator.cpp
@@ -1,46 +1,52 @@
-#include"advancedCalculator.hpp"
+#include "advancedCalculator.hpp"
 
-bool isMinus(char a)
-{
-    if(a == '-') return true;
-    else return false;
+std::map<char, std::function<double(double, double)>> operations{
+    {'+', [](double a, double b) { return a + b; }},
+    {'-', [](double a, double b) { return a - b; }},
+    {'*', [](double a, double b) { return a * b; }},
+    {'/', [](double a, double b) { return a / b; }},
+    {'%', [](double a, double b) { return (int)a % (int)b; }},
+    {'!', [](double a, double b) { return (sqrt(2 * M_PI * a)) * pow(a / E, a); }},
+    {'^', [](double a, double b) { return pow(a, b); }},
+    {'$', [](double a, double b) { return pow(a, 1 / double(b)); }}};
+
+bool isMinus(char a) {
+  if (a == '-')
+    return true;
+  else
+    return false;
 }
 
-bool isComma(char a)
-{
-    if(a == 44) return true;
-    else return false;
+bool isComma(char a) {
+  if (a == 44)
+    return true;
+  else
+    return false;
 }
 
-bool isDigit(char a)
-{
-    if(a >= 48 && a <= 57) return true;
-    else return false;
+bool isDigit(char a) {
+  if (a >= 48 && a <= 57)
+    return true;
+  else
+    return false;
 }
 
-ErrorCode isValidInput(std::string input)
-{
-    if(input.empty()) return ErrorCode::BadCharacter;
-    remove(input.begin(), input.end(), ' ');
-    int count = 0;
-    bool firstMinus = false;
-    bool secondMinus = false;
-    for (size_t i = 0; i < input.size(); i++)
-    {
-        if(i == 0)
-        {
-            if(isMinus(input[i])) firstMinus = true;
-            continue;
-        }
-        if (!isDigit(input[i]))
-        {
-
-        }
+ErrorCode isValidInput(std::string input) {
+  if (input.empty())
+    return ErrorCode::BadCharacter;
+  remove(input.begin(), input.end(), ' ');
+  int count = 0;
+  bool firstMinus = false;
+  bool secondMinus = false;
+  for (size_t i = 0; i < input.size(); i++) {
+    if (i == 0) {
+      if (isMinus(input[i]))
+        firstMinus = true;
+      continue;
     }
-    
+    if (!isDigit(input[i])) {
+    }
+  }
 }
 
-ErrorCode process(std::string input, double* out)
-{
-
-}
+ErrorCode process(std::string input, double *out) {}
--- homework/advanced-calculator/advancedCalculator.hpp
+++ homework/advanced-calculator/advancedCalculator.hpp
@@ -1,14 +1,19 @@
-#include<iostream>
-#include<map>
-#include<algorithm>
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <math.h>
 
-enum ErrorCode {
-    Ok,
-    BadCharacter,
-    BadFormat,
-    DivideBy0,
-    SqrtOfNegativeNumber,
-    ModuleOfNonIntegerValue
-};
+#define M_PI 3.14159265358979323846 /* pi */
+#define E 2.71828182845904523536 /* e */
 
-ErrorCode process(std::string input, double* out);
+    enum ErrorCode {
+      Ok,
+      BadCharacter,
+      BadFormat,
+      DivideBy0,
+      SqrtOfNegativeNumber,
+      ModuleOfNonIntegerValue
+    };
+
+ErrorCode process(std::string input, double *out);
