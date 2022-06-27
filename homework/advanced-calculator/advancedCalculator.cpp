340836885ad7c671c66cd4408065db5caa02f009
Sl0mek <andrzejslomkapraca@gmail.com>
2022-06-27 15:57:57 +0200

    Function implementation

--- homework/advanced-calculator/advancedCalculator.cpp
+++ homework/advanced-calculator/advancedCalculator.cpp
@@ -1,6 +1,46 @@
 #include"advancedCalculator.hpp"
 
+bool isMinus(char a)
+{
+    if(a == '-') return true;
+    else return false;
+}
+
+bool isComma(char a)
+{
+    if(a == 44) return true;
+    else return false;
+}
+
+bool isDigit(char a)
+{
+    if(a >= 48 && a <= 57) return true;
+    else return false;
+}
+
+ErrorCode isValidInput(std::string input)
+{
+    if(input.empty()) return ErrorCode::BadCharacter;
+    remove(input.begin(), input.end(), ' ');
+    int count = 0;
+    bool firstMinus = false;
+    bool secondMinus = false;
+    for (size_t i = 0; i < input.size(); i++)
+    {
+        if(i == 0)
+        {
+            if(isMinus(input[i])) firstMinus = true;
+            continue;
+        }
+        if (!isDigit(input[i]))
+        {
+
+        }
+    }
+    
+}
+
 ErrorCode process(std::string input, double* out)
 {
-    
+
 }
--- homework/advanced-calculator/advancedCalculator.hpp
+++ homework/advanced-calculator/advancedCalculator.hpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<map>
+#include<algorithm>
 
 enum ErrorCode {
     Ok,
