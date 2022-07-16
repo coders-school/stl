9068130301b14953d1833c7acf1ce42f54da7b2d
kogrodnik <94931123+kogrodnik@users.noreply.github.com>
2022-07-16 19:12:23 +0200

    Update vowel.cpp

--- homework/remove-vowels/vowel.cpp
+++ homework/remove-vowels/vowel.cpp
@@ -4,12 +4,12 @@
 #include <string> 
 #include <vector> 
 
-std::vector<std::string> removeVowels(std::vector<std::string> &word) {
+std::vector<std::string> removeVowels(std::vector<std::string>& word) {
     std::vector<char> vowels{'A', 'E', 'I', 'O', 'U', 'Y', 'a', 'e', 'i', 'o', 'u', 'y'};
     for (auto& el : word) {
         for (const auto& ele : vowels) {
             el.erase(std::remove(el.begin(), el.end(), ele), el.end());
-            }
+        }
     }
     return word;
 }
