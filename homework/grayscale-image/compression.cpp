6c2f321efd1f13bf51a19a483ab8c46fe73fb536
Dario Github <dario.github@wp.pl>
2022-09-16 00:41:18 +0200

    Prepare files to start working

---
+++ homework/grayscale-image/compression.cpp
@@ -0,0 +1,13 @@
+#include "compression.hpp"
+
+std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(std::array<std::array<uint8_t, width>, height>)
+{
+    std::vector<std::pair<uint8_t, uint8_t>> pp{{1,1}};
+    return pp;
+}
+
+std::array<std::array<uint8_t, width>, height> decompressGrayscale(std::vector<std::pair<uint8_t, uint8_t>> input)
+{
+     std::array<std::array<uint8_t, width>, height> output{};
+     return output;
+}
---
+++ homework/grayscale-image/compression.hpp
@@ -0,0 +1,12 @@
+
+#include <vector>
+#include <array>
+#include <cstdint>
+#include <cwchar>
+
+constexpr size_t width = 32;
+constexpr size_t height = 32;
+
+std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(std::array<std::array<uint8_t, width>, height>);
+std::array<std::array<uint8_t, width>, height> decompressGrayscale(std::vector<std::pair<uint8_t, uint8_t>> input);
+
