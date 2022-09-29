61a97dc463750e57ea4ad838e5b72bb0c462e470
adrian <7adrian.mail@gmail.com>
2022-09-29 23:30:53 +0200

    Implement of lengthSort (pure optimalization)

--- homework/length-sort/sort.cpp
+++ homework/length-sort/sort.cpp
@@ -1,6 +1,33 @@
+#include <algorithm>
+#include <utility>
+
 #include "sort.hpp"
 
-std::deque<std::string> lengthSort(const std::forward_list<std::string>& fList) {
-    // TODO Implement
-    return std::deque<std::string>();
+void sortByLength(std::deque<std::string>& deque, std::forward_list<std::string>& fList, unsigned int size) {
+    std::vector<std::pair<unsigned int, unsigned int>> sizeVector;
+    sizeVector.reserve(size);
+    unsigned int order = 0;
+    for (const std::string& el : fList) {
+        sizeVector.emplace_back(std::pair(el.length(), order++));
+    }
+    std::sort(sizeVector.begin(), sizeVector.end());
+    order = 0;
+    for (const std::string& el : fList) {
+        int index = 0;
+        while (order != sizeVector[index].second) {
+            index++;
+        }
+        deque[index] = el;
+        ++order;
+    }
+}
+
+std::deque<std::string> lengthSort(std::forward_list<std::string>& fList) {
+    std::deque<std::string> deque;
+    unsigned int listSize = std::distance(fList.cbegin(), fList.cend());
+    deque.resize(listSize);
+    fList.sort();
+    sortByLength(deque, fList, listSize);
+
+    return deque;
 }
--- homework/length-sort/sort.hpp
+++ homework/length-sort/sort.hpp
@@ -4,4 +4,4 @@
 #include <forward_list>
 #include <string>
 
-std::deque<std::string> lengthSort(const std::forward_list<std::string>& fList);
+std::deque<std::string> lengthSort(std::forward_list<std::string>& fList);
