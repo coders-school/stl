004fd86ab88f2ffd8e0e20609c313b06354b7540
adrian <7adrian.mail@gmail.com>
2022-09-30 13:16:48 +0200

    Prepare for implementation cmpressGrayscale() and decompressGrayscale()

---
+++ homework/grayscale-image/compression.cpp
@@ -0,0 +1,9 @@
+#include "compression.hpp"
+
+std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(const std::array<std::array<uint8_t, width>, height>& arr) {
+    return std::vector<std::pair<uint8_t, uint8_t>>();
+}
+
+std::array<std::array<uint8_t, width>, height> decompressGrayscale(const std::vector<std::pair<uint8_t, uint8_t>>& bitmap) {
+    return std::array<std::array<uint8_t, width>, height>();
+}
