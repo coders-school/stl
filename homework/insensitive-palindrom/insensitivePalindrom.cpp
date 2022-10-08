a6a4b167e69d30560799a5fe5a74365e7e69da13
adrian <7adrian.mail@gmail.com>
2022-10-08 23:03:22 +0200

    Implementation is_palindrome optimize for better code

--- homework/insensitive-palindrom/insensitivePalindrom.cpp
+++ homework/insensitive-palindrom/insensitivePalindrom.cpp
@@ -8,9 +8,7 @@ bool is_palindrome(const std::string& word) {
     std::copy_if(word.cbegin(), word.cend(), std::back_inserter(wordAlphabetic), [](char c) {
         return isalpha(c);
     });
-    std::transform(wordAlphabetic.begin(), wordAlphabetic.end(), wordAlphabetic.begin(), [](char c) {
-        return tolower(c);
-    });
+    std::transform(wordAlphabetic.begin(), wordAlphabetic.end(), wordAlphabetic.begin(), tolower);
     auto itMiddle = std::next(wordAlphabetic.cbegin(), (wordAlphabetic.size() + 1) / 2);
     auto revMiddle = std::make_reverse_iterator(std::next(wordAlphabetic.cbegin(), wordAlphabetic.size() / 2));
     auto it = std::search(wordAlphabetic.cbegin(), itMiddle, wordAlphabetic.crbegin(), revMiddle);
