67b3a508b6ad4e74976ebd1693d58e5dc966a364
adrian <7adrian.mail@gmail.com>
2022-10-08 21:14:56 +0200

    Decompress Grayscale in STL

--- homework/grayscale-image/compression.cpp
+++ homework/grayscale-image/compression.cpp
@@ -6,7 +6,6 @@
 std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(const std::array<std::array<uint8_t, width>, height>& arr) {
     std::vector<std::pair<uint8_t, uint8_t>> vector;
     vector.reserve(width * height);
-
     std::for_each(arr.cbegin(), arr.cend(), [&vector](const std::array<uint8_t, width>& widthArr) {
         auto it = widthArr.cbegin();
         while (it != widthArr.cend()) {
@@ -28,15 +27,15 @@ std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(const std::array<std:
 
 std::array<std::array<uint8_t, width>, height> decompressGrayscale(const std::vector<std::pair<uint8_t, uint8_t>>& bitmap) {
     std::array<std::array<uint8_t, width>, height> arr{};
-    int index = 0;
-    for (const std::pair<uint8_t, uint8_t>& pair : bitmap) {
-        for (int i = 0; i < pair.second; i++) {
-            arr[index / width][index % height] = pair.first;
-            ++index;
-        }
-    }
+    auto it = arr.begin()->begin();
+    std::for_each(bitmap.cbegin(), bitmap.cend(), [&arr, &it](const std::pair<uint8_t, uint8_t>& vec) {
+        it = std::generate_n(it, vec.second, [&vec]() {
+            return vec.first;
+        });
+    });
     return arr;
 }
+
 void printMap(const std::array<std::array<uint8_t, width>, height>& map) {
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
