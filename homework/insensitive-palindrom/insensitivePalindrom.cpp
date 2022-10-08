884933d9d382a66a944b24ee1403fe9c5c757b6b
adrian <7adrian.mail@gmail.com>
2022-10-08 22:16:18 +0200

    Implementation is_palindrome

--- homework/insensitive-palindrom/insensitivePalindrom.cpp
+++ homework/insensitive-palindrom/insensitivePalindrom.cpp
@@ -1,5 +1,14 @@
 #include "insensitivePalindrom.hpp"
 
+#include <algorithm>
+
 bool is_palindrome(const std::string& word) {
-    return false;
+    std::string wordAlphabetic;
+    std::copy_if(word.cbegin(), word.cend(), std::back_inserter(wordAlphabetic), [](char c) {
+        return isalpha(c);
+    });
+    std::transform(wordAlphabetic.begin(), wordAlphabetic.end(), wordAlphabetic.begin(), [](char c) {
+        return tolower(c);
+    });
+    return std::equal(wordAlphabetic.cbegin(), wordAlphabetic.cend(), wordAlphabetic.crbegin());
 }
