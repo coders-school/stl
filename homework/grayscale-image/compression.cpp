c9b150b289b077ea7a3e47d0bd2ddb4534c7ccfc
Dario Github <dario.github@wp.pl>
2022-10-09 22:09:15 +0200

    Refactor for grayscale-image-algo

--- homework/grayscale-image/compression.cpp
+++ homework/grayscale-image/compression.cpp
@@ -1,17 +1,17 @@
 #include "compression.hpp"
 #include <iostream>
 #include <tuple>
-
+#include <algorithm>
 #include <queue>
 
 void print_compressed_line_output(const std::vector<std::pair<uint8_t, uint8_t>>& v)
 {
-    //std::cout << "[";
+    std::cout << "[";
     for(const auto& compressed_line_chunk : v)
     {
-        //std::cout << "{" << unsigned(compressed_line_chunk.first) << ", " << unsigned(compressed_line_chunk.second) << "}";
+        std::cout << "{" << unsigned(compressed_line_chunk.first) << ", " << unsigned(compressed_line_chunk.second) << "}";
     }
-    //std::cout << "]\n";
+    std::cout << "]\n";
 
 }
 
@@ -24,62 +24,57 @@ void print_line(const std::array<uint8_t, width>& v)
 }
 
 
-std::vector<std::pair<uint8_t, uint8_t>> compress_single_line(const std::array<uint8_t, width>& v)
+std::vector<std::pair<uint8_t, uint8_t>> compress_single_line(const std::array<uint8_t, width>& line)
 {
     std::vector<std::pair<uint8_t, uint8_t>>  output;
-    uint8_t current_value = v[0];
-    uint8_t old_value = v[0];
-    int same_value_cnt = 0;
-    ////std::cout << " s " << std::endl;
-    for (size_t i = 0; i < std::size(v); ++i) {
-           current_value = v[i];
-           ////std::cout << " c = " << unsigned(current_value) << "\t";
-           ////std::cout << " i = " << i << std::endl;
-           if(current_value == old_value)
+    uint8_t character = *line.begin();
+    uint8_t old_character = *line.begin();
+    int number_of_same_characters = 0;
+    std::for_each(line.begin(), line.end(), [&](uint8_t character) mutable {
+           if(character == old_character)
            {
-                ++same_value_cnt;
+                ++number_of_same_characters;
            }
            else
            {
-                output.push_back({old_value, same_value_cnt});
-                old_value = current_value;
-                same_value_cnt = 1;
+                auto chunk = std::make_pair(old_character, number_of_same_characters);
+                output.push_back(chunk);
+                number_of_same_characters = 1;
            }
-    }
+           old_character = character;
+    });
 
-    auto all_output_values_are_the_same = [&](){
+    auto all_characters_are_the_same = [&](){
         return output.empty();
     };
 
-    auto are_still_unsaved_values = [&](){
-        return same_value_cnt != 1;
+    auto are_still_any_unsaved_values = [&](){
+        return number_of_same_characters != 1;
     };
 
-    if(all_output_values_are_the_same() or are_still_unsaved_values())
+    if(all_characters_are_the_same() or are_still_any_unsaved_values())
     {
-        output.push_back({old_value, same_value_cnt});
+        auto chunk = std::make_pair(old_character, number_of_same_characters);
+        output.push_back(chunk);
     }
-    ////std::cout << output.size() << std::endl;
-    return output;
+
+    return output;    
 }
 
 std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(std::array<std::array<uint8_t, width>, height> input) {
-    std::vector<std::pair<uint8_t, uint8_t>> pp;
+    std::vector<std::pair<uint8_t, uint8_t>> compressed_bitmap;
 
-    for (size_t h = 0; h < height; ++h) {
-        auto line = input[h];
-        //print_line(line);
+    std::for_each(input.begin(), input.end(), [&](auto line){
         auto compressed_line = compress_single_line(line);
-        //print_compressed_line_output(compressed_line);
+        std::transform(compressed_line.begin(), 
+                        compressed_line.end(), 
+                        std::back_inserter(compressed_bitmap),
+                        [](auto chunk){ 
+                            return chunk;
+        });
+    });
 
-        for(const auto& chunk : compressed_line)
-        {
-            pp.push_back(chunk);
-        }
-    }
-
-
-    return pp;
+    return compressed_bitmap;
 }
 
 std::array<std::array<uint8_t, width>, height> decompressGrayscale(std::vector<std::pair<uint8_t, uint8_t>> input) {
