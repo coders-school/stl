08c866f08e8f0e1b12827f5720da8717d427c968
adrian <7adrian.mail@gmail.com>
2022-10-10 19:52:02 +0200

    Distance implementation

--- homework/inner-product/arithmeticAverage.cpp
+++ homework/inner-product/arithmeticAverage.cpp
@@ -1,7 +1,18 @@
 #include "arithmeticAverage.hpp"
 
 double ArithmeticAverage(const std::vector<int>& first, const std::vector<int>& second) {
-    return 0;
+    if (first.empty() && second.empty()) {
+        return 0.0;
+    }
+    int size = first.size() + second.size();
+    double average = 0.0;
+    for (auto num: first) {
+        average += num;
+    }
+    for (auto num: second) {
+        average += num;
+    }
+    return average / size;
 }
 
 double Distance(const std::vector<int>& first, const std::vector<int>& second) {
