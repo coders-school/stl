d6cda1b702759ec01a791aca923f6743c986fe86
adrian <7adrian.mail@gmail.com>
2022-10-10 19:56:47 +0200

    Distance implementation refactor

--- homework/inner-product/arithmeticAverage.cpp
+++ homework/inner-product/arithmeticAverage.cpp
@@ -5,11 +5,10 @@ double ArithmeticAverage(const std::vector<int>& first, const std::vector<int>& 
     if (first.empty() && second.empty()) {
         return 0.0;
     }
-    int size = first.size() + second.size();
-    double average = 0.0;
-    average += std::reduce(first.cbegin(), first.cend(), 0);
-    average += std::reduce(second.cbegin(), second.cend(), 0);
-    return average / size;
+
+    double average = std::reduce(first.cbegin(), first.cend(),
+                                 std::reduce(second.cbegin(), second.cend(), 0));
+    return average / static_cast<double>(first.size() + second.size());
 }
 
 double Distance(const std::vector<int>& first, const std::vector<int>& second) {
