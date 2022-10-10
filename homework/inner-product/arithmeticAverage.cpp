e382fb243c54dc6de35181ada1f736d6d982bdec
adrian <7adrian.mail@gmail.com>
2022-10-10 19:46:02 +0200

    Prepare to implementation

---
+++ homework/inner-product/arithmeticAverage.cpp
@@ -0,0 +1,9 @@
+#include "arithmeticAverage.hpp"
+
+double ArithmeticAverage(const std::vector<int>& first, const std::vector<int>& second) {
+    return 0;
+}
+
+double Distance(const std::vector<int>& first, const std::vector<int>& second) {
+    return 0;
+}
--- homework/inner-product/test.cpp
+++ homework/inner-product/test.cpp
@@ -9,7 +9,31 @@ TEST(arithmeticAverageTest, ShouldCalculateArithmeticAverage) {
     std::vector<int> first{-5, 4, 2, -5, 2, 4, 6, 7};
     std::vector<int> second{3, 5, -2, 6, 7, -3, 6, 9};
     EXPECT_NEAR(ArithmeticAverage(first, second), 2.875, epsilon);
-    // Write more test case
+
+    first = {};
+    second = {};
+    double expected = 0.0;
+    EXPECT_NEAR(ArithmeticAverage(first, second), expected, epsilon);
+
+    first = {1};
+    second = {5};
+    expected = 3.0;
+    EXPECT_NEAR(ArithmeticAverage(first, second), expected, epsilon);
+
+    first = {1, 3};
+    second = {5, 3};
+    expected = 3.0;
+    EXPECT_NEAR(ArithmeticAverage(first, second), expected, epsilon);
+
+    first = {1, 3, -6};
+    second = {5, 3, -6};
+    expected = 0.0;
+    EXPECT_NEAR(ArithmeticAverage(first, second), expected, epsilon);
+
+    first = {1, 3, -6, 10, 5, -7};
+    second = {5, 3, -6};
+    expected = 0.888;
+    EXPECT_NEAR(ArithmeticAverage(first, second), expected, epsilon);
 }
 
 TEST(arithmeticAverageTest, ShouldCalculateDistance) {
@@ -22,5 +46,24 @@ TEST(arithmeticAverageTest, ShouldCalculateDistance) {
     second = {17, 6, 2, -2};
     expected = 18.16590212458495;
     EXPECT_NEAR(Distance(first, second), expected, 0.00001);
-    // Write more test case
+
+    first = {};
+    second = {};
+    expected = 0.0;
+    EXPECT_NEAR(Distance(first, second), expected, epsilon);
+
+    first = {5};
+    second = {3};
+    expected = 2.0;
+    EXPECT_NEAR(Distance(first, second), expected, epsilon);
+
+    first = {5};
+    second = {5};
+    expected = 0.0;
+    EXPECT_NEAR(Distance(first, second), expected, epsilon);
+
+    first = {4, 5};
+    second = {1, 1};
+    expected = 5.0;
+    EXPECT_NEAR(Distance(first, second), expected, epsilon);
 }
