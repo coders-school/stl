fda67d20b0b582f1a90236ab15acab6cdec17c49
adrian <7adrian.mail@gmail.com>
2022-10-11 16:01:24 +0200

    Refactor function "check if function has bad command"

--- homework/advanced-calculator/advancedCalculator.cpp
+++ homework/advanced-calculator/advancedCalculator.cpp
@@ -1,5 +1,6 @@
 #include "advancedCalculator.hpp"
 
+#include <algorithm>
 #include <cmath>
 #include <numbers>
 #include <stdexcept>
@@ -7,52 +8,52 @@
 #include <string_view>
 
 namespace calculator {
-    using std::abs;
-    using std::pow;
-    using std::sqrt;
-    using std::numbers::pi_v;
+using std::abs;
+using std::pow;
+using std::sqrt;
+using std::numbers::pi_v;
 
-    double addiction(const double lhs, const double rhs) {
-        return lhs + rhs;
-    }
+double addiction(const double lhs, const double rhs) {
+    return lhs + rhs;
+}
 
-    double multiply(const double lhs, const double rhs) {
-        return lhs * rhs;
-    }
+double multiply(const double lhs, const double rhs) {
+    return lhs * rhs;
+}
 
-    double divide(const double lhs, const double rhs) {
-        return (lhs / rhs);
-    }
+double divide(const double lhs, const double rhs) {
+    return (lhs / rhs);
+}
 
-    double substract(const double lhs, const double rhs) {
-        return lhs - rhs;
-    }
+double substract(const double lhs, const double rhs) {
+    return lhs - rhs;
+}
 
-    double modulo(const double lhs, const double rhs) {
-        return static_cast<int>(lhs) % static_cast<int>(rhs);
-    }
+double modulo(const double lhs, const double rhs) {
+    return static_cast<int>(lhs) % static_cast<int>(rhs);
+}
 
-    double factorial(double lhs, const double) {
-        return std::tgamma(abs(lhs)) * lhs;
-    }
+double factorial(double lhs, const double) {
+    return std::tgamma(abs(lhs)) * lhs;
+}
 
-    double power(const double lhs, const double rhs) {
-        return pow(lhs, rhs);
-    }
+double power(const double lhs, const double rhs) {
+    return pow(lhs, rhs);
+}
 
-    double sqrtRoot(const double lhs, const double rhs) {
-        return pow(lhs, 1.0 / rhs);
-    }
+double sqrtRoot(const double lhs, const double rhs) {
+    return pow(lhs, 1.0 / rhs);
+}
 
-    std::map<char, std::function<double(const double, const double)>> commands{
-            {'+', addiction},
-            {'*', multiply},
-            {'/', divide},
-            {'-', substract},
-            {'%', modulo},
-            {'!', factorial},
-            {'^', power},
-            {'$', sqrtRoot}};
+std::map<char, std::function<double(const double, const double)>> commands{
+    {'+', addiction},
+    {'*', multiply},
+    {'/', divide},
+    {'-', substract},
+    {'%', modulo},
+    {'!', factorial},
+    {'^', power},
+    {'$', sqrtRoot}};
 }  // namespace calculator
 
 bool checkIfCharIsACommand(const char com) {
@@ -85,11 +86,11 @@ bool isFactiorialSingleNumber(auto commandIterator, const std::string& inputData
 
 char findCommand(const std::string& inputData, auto& beginIterator) {
     auto commandIterator =
-            std::find_first_of(beginIterator, inputData.cend(),
-                               calculator::commands.cbegin(), calculator::commands.cend(),
-                               [](auto inputChar, auto commandChar) {
-                                   return inputChar == commandChar.first;
-                               });
+        std::find_first_of(beginIterator, inputData.cend(),
+                           calculator::commands.cbegin(), calculator::commands.cend(),
+                           [](auto inputChar, auto commandChar) {
+                               return inputChar == commandChar.first;
+                           });
     if (commandIterator == inputData.cend() || commandIterator == inputData.cbegin()) {
         return 0;
     }
@@ -121,20 +122,35 @@ ErrorCode findFunction(const std::string& inputData, char& command) {
     return ErrorCode::OK;
 }
 
-ErrorCode checkIfFunctionHasBadCommand(const std::string& inputData, char command, double& lhs, double& rhs) {
+bool isMoreThanOneDotInNumber(const std::string& number) {
+    return std::ranges::count(number, '.') > 1;
+}
+
+bool trySplitInputDataToTwoStringOfNumbers(const std::string& inputData, char command, std::string& leftSide, std::string& rightSide) {
     size_t distance = inputData.find_first_of(command);
-    if (inputData[0] == '-' && command == '-') {
+    if (inputData[0] == command) {
         distance = inputData.find_first_of('-', 1);
     }
-    std::string lValue = inputData.substr(0, distance);
-    std::string rValue = inputData.substr(distance + 1, inputData.size());
-    if (std::count(lValue.cbegin(), lValue.cend(), '.') > 1 || std::count(rValue.cbegin(), rValue.cend(), '.') > 1) {
+    leftSide = inputData.substr(0, distance);
+    rightSide = inputData.substr(distance + 1, inputData.size());
+    if (isMoreThanOneDotInNumber(leftSide) || isMoreThanOneDotInNumber(rightSide)) {
+        return false;
+    }
+
+    return true;
+}
+
+ErrorCode checkIfFunctionHasBadCommand(const std::string& inputData, char command, double& lhs, double& rhs) {
+    std::string leftSideOfInputData;
+    std::string rightSideOfInputData;
+    if (!trySplitInputDataToTwoStringOfNumbers(inputData, command, leftSideOfInputData, rightSideOfInputData)) {
         return ErrorCode::BadFormat;
     }
+
     try {
-        lhs = std::stod(lValue);
+        lhs = std::stod(leftSideOfInputData);
         if (command != '!') {
-            rhs = std::stod(rValue);
+            rhs = std::stod(rightSideOfInputData);
         }
     } catch (std::invalid_argument&) {
         return ErrorCode::BadFormat;
