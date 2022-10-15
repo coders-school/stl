6e154d750de8decabc4e3f70786750dc3aabd03f
automationdream <automationdream@gmail.com>
2022-10-15 14:43:01 +0200

    remove const

--- homework/remove-vowels/vowel.cpp
+++ homework/remove-vowels/vowel.cpp
@@ -5,7 +5,7 @@ std::vector<std::string> removeVowels(std::vector<std::string>& vec) {
     std::array<char, 12> vowels{'a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U', 'y', 'Y'};
 
     for (auto& element : vec) {
-        for (auto const vowel : vowels) {
+        for (auto vowel : vowels) {
             auto i = std::remove(element.begin(), element.end(), vowel);
             element.erase(i, element.end());
         }
