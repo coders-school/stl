0166d1605a5984ed6c9e0709292ecf7128673029
automationdream <automationdream@gmail.com>
2022-10-15 14:38:50 +0200

    fix linting

--- homework/length-sort/sort.hpp
+++ homework/length-sort/sort.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include "deque"
-#include "string"
 #include "forward_list"
 #include "list"
+#include "string"
 
 std::deque<std::string> lengthSort(std::forward_list<std::string>& list);
