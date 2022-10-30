62c192d0921c26cd05a868bdfc1306be1be7f8b3
Dario Github <dario.github@wp.pl>
2022-10-30 18:31:47 +0100

    Make project compilable

---
+++ homework/inner-product/arithmeticAverage.cpp
@@ -0,0 +1,12 @@
+#include "arithmeticAverage.hpp"
+
+
+double ArithmeticAverage(std::vector<int>& first, std::vector<int>& second)
+{
+    return 0.0;
+}
+
+double Distance(std::vector<int>& first, std::vector<int>& second)
+{
+    return 0.0;
+}
