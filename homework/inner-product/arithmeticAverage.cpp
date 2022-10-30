05df8ebff924a09c6d44355f5f21b2be1e1f8e83
Dario Github <dario.github@wp.pl>
2022-10-30 19:33:03 +0100

    All tests passed

--- homework/inner-product/arithmeticAverage.cpp
+++ homework/inner-product/arithmeticAverage.cpp
@@ -1,12 +1,28 @@
 #include "arithmeticAverage.hpp"
-
+#include <iostream>
+#include <numeric>
+#include <functional>
+#include <cmath>
 
 double ArithmeticAverage(std::vector<int>& first, std::vector<int>& second)
 {
-    return 0.0;
+    auto r1 = std::accumulate(first.begin(), first.end(), 0.0);
+    auto sum = std::accumulate(second.begin(), second.end(), r1);
+    auto result = sum / (second.size() + first.size());
+    return result;
 }
 
 double Distance(std::vector<int>& first, std::vector<int>& second)
 {
-    return 0.0;
+    std::vector<double> factors;
+    auto first_it = first.begin();
+    auto second_it = second.begin();
+    for(; first_it != first.end(); ++first_it, ++second_it)
+    {
+        auto factor = pow(*second_it - *first_it, 2);      
+        factors.push_back(factor);
+    }
+    auto sum = std::accumulate(factors.begin(), factors.end(), 0.0); 
+    auto result = sqrt(sum); 
+    return result;
 }
