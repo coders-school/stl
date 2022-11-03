47415881b5bc3d4990dc194045d40e5fbcf5b0c0
Dario Github <dario.github@wp.pl>
2022-11-03 22:12:51 +0100

    Start from scratch, implemented BadCharacter

--- homework/advanced-calculator/advancedCalculator.cpp
+++ homework/advanced-calculator/advancedCalculator.cpp
@@ -20,7 +20,7 @@ std::map<char, std::function<double(double, double)>> operations
 };
 
 
-std::string allowed_operations()
+std::string give_allowed_operations()
 {
     std::string allowed_operation = "";
     std::for_each(operations.begin(), operations.end(), [&allowed_operation](auto operation){
@@ -29,103 +29,27 @@ std::string allowed_operations()
     return allowed_operation;
 }
 
+bool badCharacterIsIn(std::string input)
+{
+    const std::string allowed_operations = give_allowed_operations();
+    auto result = std::find_if_not(input.begin(), input.end(), [&allowed_operations](auto input_char){
+        return allowed_operations.find(input_char) != std::string::npos;
+    });
+
+    return (result != std::end(input));
+}
+
 enum ErrorCode process(std::string oparation, double* x)
 {
-    const std::string allowed = allowed_operations();
+    
     auto operation_iterator = oparation.begin();
 
+    oparation.erase(std::remove_if(oparation.begin(), 
+                              oparation.end(),
+                              [](unsigned char x){return std::isspace(x);}), oparation.end());
     std::cout << "\noparation = "<< oparation << std::endl;
-    std::stringstream ss;
-    ss.str(oparation);
-    double lhs;
-    std::string oper;
-    double rhs;
-    std::string anything_more;
-    ss >> lhs >> oper >> rhs >> anything_more;
-    std::cout << "----------------\n";
-    std::cout << "lhs = " << lhs << std::endl;
-    std::cout << "oper = " << oper << std::endl;
-    std::cout << "rhs = " << rhs << std::endl;
-    std::cout << "anything_more = " << anything_more << std::endl;
-    auto disallowed_operator = std::none_of(allowed.begin(), allowed.end(), [&oper](auto allowed_operator){ return oper == std::to_string(allowed_operator);});
-    if(disallowed_operator)
-    {
-        std::cout << "disallowed_operator\n";
-        auto has_comma = oper.find(",")!=std::string::npos;
-        auto has_semicolon = oper.find(";")!=std::string::npos;
-        auto has_allowed_operator = std::any_of(allowed.begin(), allowed.end(), [&oper](auto allowed_operator){ return oper.find(allowed_operator)!=std::string::npos; });
-        auto has_digit = std::any_of(oper.begin(), oper.end(), [](auto x ){ return std::isdigit(x);});
-        auto whole_operation_has_semicolon = oparation.find(";")!=std::string::npos;
-        auto anything_more_has_correct_operator = std::any_of(allowed.begin(), allowed.end(), [&anything_more](auto allowed_operator){ return anything_more.find(allowed_operator)!=std::string::npos; });
-        if(anything_more_has_correct_operator)
-        {
-            std::cout << "anything_more_has_correct_operator\n";
-            return ErrorCode::BadFormat;
-        }
-        if(has_semicolon)
-        {
-            std::cout << "has_semicolon\n";
-            std::cout << "ErrorCode::BadCharacter\n";
-            return ErrorCode::BadCharacter;
-        }
-        if(has_comma and has_digit and has_allowed_operator)
-        {
-            std::cout << "has_comma\n";
-            std::cout << "has_digit\n";
-            std::cout << "has_allowed_operator\n";
-            return ErrorCode::BadFormat;
-        }
-        if(has_allowed_operator)
-        {
-            std::cout << "has_allowed_operator\n";
-           
-            auto has_more_allowed_operators_than_one = [&oper](){ 
-                    auto it = std::adjacent_find (oper.begin(), oper.end());
-                    return it!=oper.end();
-            };
-
-            if(has_more_allowed_operators_than_one())
-            {
-                std::cout << "has_more_allowed_operators_than_one\n";
-                return ErrorCode::BadFormat;
-            }
-            
-        }
-        if(has_comma and has_digit and rhs == 0.0)
-        {
-            std::cout << "has_comma\n";
-            std::cout << "has_digit\n";
-            std::cout << "has rhs == 0.0\n";
-            if(whole_operation_has_semicolon)
-            {
-                std::cout << "whole_operation_has_semicolon\n";
-                return ErrorCode::BadCharacter;
-            }
-            return ErrorCode::BadFormat;
-        }
-        if(has_comma)
-        {
-            std::cout << "has_comma\n";
-            std::cout << "has_digit\n";
-            return ErrorCode::BadFormat;
-        }
-        std::cout << "ErrorCode::BadCharacter\n";
-        return ErrorCode::BadCharacter;
-    }
-    if(not anything_more.empty())
-    {
-        std::cout << "not anything_more.empty\n";
-        std::cout << "ErrorCode::BadCharacter\n";
-        return ErrorCode::BadCharacter;
-    }
-    if(rhs == 0.0)
-    {
-        std::cout << "rhs == 0.0\n";
-        std::cout << "ErrorCode::BadCharacter\n";
-        return ErrorCode::BadCharacter;
-    }
-
-
+    
+    if(badCharacterIsIn(oparation)) return  ErrorCode::BadCharacter;
 
     return ErrorCode::BadFormat;
 }
