1b4a05ae8bb7149cbb018f1046eb35fe5a15d89e
Dario Github <dario.github@wp.pl>
2022-11-07 17:24:44 +0100

    Refactored and tidyied up

--- homework/advanced-calculator/advancedCalculator.cpp
+++ homework/advanced-calculator/advancedCalculator.cpp
@@ -42,31 +42,16 @@ bool badCharacterIsIn(std::string input)
     return false;
 }
 
-//   ASSERT_EQ(process("5,1!", &result), ErrorCode::BadFormat);
-//     ASSERT_EQ(process("11,3 + 12.4", &result), ErrorCode::BadFormat);
-//     ASSERT_EQ(process("13.4 ++ 12.43", &result), ErrorCode::BadFormat);
-//     ASSERT_EQ(process("13.2 + 12.3 + 13.0", &result), ErrorCode::BadFormat);
-//     ASSERT_EQ(process("-12.4! + 2", &result), ErrorCode::BadFormat);
-//     ASSERT_EQ(process("11 / 13.4+", &result), ErrorCode::BadFormat);
-//     ASSERT_EQ(process("+8 - 32.1", &result), ErrorCode::BadFormat);
-//     ASSERT_EQ(process("12 ^% 3", &result), ErrorCode::BadFormat);
-//     ASSERT_EQ(process("+ 3 4", &result), ErrorCode::BadFormat);
-//     ASSERT_EQ(process("12.4.3 + 12.3", &result), ErrorCode::BadFormat);
-//     ASSERT_EQ(process("123.4 ! 345", &result), ErrorCode::BadFormat);
-
-
-
 bool badFormatIsIn(std::string input)
 {
     std::cout << "badFormatIsIn invoked" << std::endl;
-    bool result = false;
+    bool bad_format = false;
 
-    const std::string allowed_operations = give_allowed_operations();
     auto has_at_least_one_coma = (std::count(input.begin(), input.end(), ',') > 0) ? true : false;
     
     auto has_doubled_plus_characters = [&input](){
         auto first_adjacent = std::adjacent_find(input.begin(), 
-                                                    input.end()); 
+                                                 input.end()); 
         auto second_adjacent = first_adjacent + 1;
         if (*first_adjacent == '+' && *second_adjacent == '+') return true;
         return false; 
@@ -93,22 +78,21 @@ bool badFormatIsIn(std::string input)
     auto has_adjacent_power_and_modulo_characters = [&input](){
         auto contains = [](const auto& cont, std::string_view s)
         {
-            // str.find() (or str.contains(), since C++23) can be used as well
             return std::search(cont.begin(), cont.end(), s.begin(), s.end()) != cont.end();
         };
         return contains(input, "^%");
     };
 
-    if(has_at_least_one_coma){result = true; std::cout << "has_at_least_one_coma" << std::endl;} 
-    if(has_doubled_plus_characters()){result = true;  std::cout << "has_doubled_plus_characters" << std::endl;} 
-    if(starts_with_plus){result = true; std::cout << "starts_with_plus" << std::endl;} 
-    if(has_more_than_two_dots){result = true; std::cout << "has_more_than_two_dots" << std::endl;}
-    if(has_factorial_character_not_last()){result = true; std::cout << "has_factorial_character_not_last" << std::endl;} 
-    if(has_last_character_plus){result = true; std::cout << "has_last_character_plus" << std::endl;}
-    if(has_adjacent_power_and_modulo_characters()){result = true; std::cout << "has_adjacent_power_and_modulo_characters" << std::endl;} 
+    if(has_at_least_one_coma)                                    {bad_format = true; std::cout << "has_at_least_one_coma" << std::endl;} 
+    else if(has_doubled_plus_characters())                       {bad_format = true; std::cout << "has_doubled_plus_characters" << std::endl;} 
+    else if(starts_with_plus)                                    {bad_format = true; std::cout << "starts_with_plus" << std::endl;} 
+    else if(has_more_than_two_dots)                              {bad_format = true; std::cout << "has_more_than_two_dots" << std::endl;}
+    else if(has_factorial_character_not_last())                  {bad_format = true; std::cout << "has_factorial_character_not_last" << std::endl;} 
+    else if(has_last_character_plus)                             {bad_format = true; std::cout << "has_last_character_plus" << std::endl;}
+    else if(has_adjacent_power_and_modulo_characters())          {bad_format = true; std::cout << "has_adjacent_power_and_modulo_characters" << std::endl;} 
 
-    std::cout << "badFormatIsIn result: " << std::boolalpha << result <<  std::endl;
-    return result;
+    std::cout << "badFormatIsIn result: " << std::boolalpha << bad_format <<  std::endl;
+    return bad_format;
 }
 
 bool DivideBy0IsIn(std::string input)
@@ -116,36 +100,25 @@ bool DivideBy0IsIn(std::string input)
     auto has_divide_character = (std::count_if(input.begin(), input.end(), [](char character){ return character == '/';})) > 0 ? true : false;
     if(has_divide_character)
     {
-        auto has_zero_after_divide_character = [&input](){
-            
-                                                        std::size_t pos_divide = input.find('/');
-                                                        std::size_t pos_zero = input.find('0');
-                                                        std::cout << "pos_divide: " << pos_divide << std::endl;
-                                                        std::cout << "pos_zero: " << pos_zero << std::endl;
-                                                        if(pos_zero != std::string::npos)
-                                                        {
-                                                            std::cout << "if(pos_zero != std::string::npos): " << std::endl;
-                                                            return ((pos_zero > pos_divide) or (input.back() == '0')) ? true : false;
-
-                                                        }
-                                                        std::cout << "result false: " << std::endl;
-                                                        return false;
-                                                        };
-
-        return has_zero_after_divide_character();
+        auto has_zero_as_last_character = input.back() == '0';
+        if(has_zero_as_last_character)
+        {
+            std::cout << "has_zero_as_last_character" << std::endl;
+            return true;
+        }
     }
     return false;
-
 }
 
 enum ErrorCode process(std::string oparation, double* x)
 {
     
-    auto operation_iterator = oparation.begin();
-
-    oparation.erase(std::remove_if(oparation.begin(), 
+    auto remove_whitespaces_from_input = [&oparation](){oparation.erase(std::remove_if(oparation.begin(), 
                               oparation.end(),
                               [](unsigned char x){return std::isspace(x);}), oparation.end());
+                              };
+
+    remove_whitespaces_from_input();
     std::cout << "\noparation = \""<< oparation << "\"" << std::endl;
     
     if(badCharacterIsIn(oparation)) return  ErrorCode::BadCharacter;
