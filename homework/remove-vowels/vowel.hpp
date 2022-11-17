dbac7faccf9ab29ff2be4f61a011d8df57db423d
KrzucH <115778673+KrzucH@users.noreply.github.com>
2022-11-17 14:55:59 +0100

    Update vowel.hpp

--- homework/remove-vowels/vowel.hpp
+++ homework/remove-vowels/vowel.hpp
@@ -1,9 +1,8 @@
 #pragma once
 #include <algorithm>
-#include <vector>
 #include <iostream>
 #include <string>
-
+#include <vector>
 
 void printVector(const std::vector<std::string>& vec);
 void removeVowels(std::vector<std::string>& vec);
