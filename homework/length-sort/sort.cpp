68cc2002258b800470c1838ab8d03417be1a790e
KrzucH <115778673+KrzucH@users.noreply.github.com>
2022-11-18 13:10:15 +0100

    Update sort.cpp

--- homework/length-sort/sort.cpp
+++ homework/length-sort/sort.cpp
@@ -4,12 +4,10 @@ bool Compare(std::string& word1, std::string& word2) {
     return word1.length() < word2.length();
 }
 bool Compare1(std::string& word1, std::string& word2) {
-    return std::lexicographical_compare(word1.begin(), word1.end(), 
-                                        word2.begin(), word2.end());
+    return std::lexicographical_compare(word1.begin(), word1.end(), word2.begin(), word2.end());
 }
 std::deque<std::string> lengthSort(std::forward_list<std::string>& for_lis) { 
     std::deque<std::string> deq;
-    
     for_lis.sort(Compare1);
     for_lis.sort(Compare);
     for (auto n : for_lis) {
