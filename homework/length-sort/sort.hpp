955676e0fa2b4dc46610404cb0a0074eecb3da71
Krzysiek Nowak <krzynowak93@gmail.com>
2022-11-18 13:04:40 +0100

    Length-vowels Zadanko

---
+++ homework/length-sort/sort.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <algorithm>
+#include <iostream>
+#include <deque>
+#include <forward_list>
+#include <string>
+#include <cstring>
+#include <vector>
+
+std::deque<std::string> lengthSort(std::forward_list<std::string>& for_lis); 
