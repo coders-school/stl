349df64a7c01b3f19f44826ef5e422b92ca6d397
Łukasz Konieczny <ftefrjbhfvasf32@o2.pl>
2022-12-04 19:30:17 +0100

    decompressGrayscale implemented

--- homework/grayscale-image/compression.hpp
+++ homework/grayscale-image/compression.hpp
@@ -12,5 +12,5 @@ constexpr size_t height = 32;
 std::vector<std::pair<uint8_t, uint8_t>>
 compressGrayscale(const std::array<std::array<uint8_t, width>, height>&);
 
-std::array<std::array<uint8_t, 240>, 160>
+std::array<std::array<uint8_t, width>, height>
 decompressGrayscale(const std::vector<std::pair<uint8_t, uint8_t>>&);
