508767556a174e35773f780bba194677f8a89d24
Łukasz Konieczny <ftefrjbhfvasf32@o2.pl>
2022-12-04 15:27:37 +0100

    Fix formatting

--- homework/remove-vowels/vowel.cpp
+++ homework/remove-vowels/vowel.cpp
@@ -4,7 +4,7 @@
 
 void removeVowels(std::vector<std::string>& words) {
     std::set<char> vowels = {'a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y'};
-    for (auto& word: words) {
+    for (auto& word : words) {
         for (int i = 0; i < word.size(); i++) {
             if (vowels.count(word[i]) == 1) {
                 word.erase(i--, 1);
