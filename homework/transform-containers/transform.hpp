eb6ed903077b48b8b61370b4e867a2cf3816a3a1
KrzucH <115778673+KrzucH@users.noreply.github.com>
2022-12-05 16:18:48 +0100

    Update transform.hpp

--- homework/transform-containers/transform.hpp
+++ homework/transform-containers/transform.hpp
@@ -1,9 +1,9 @@
 #include <algorithm>
+#include <deque>
 #include <iostream>
 #include <list>
-#include <deque>
-#include <string>
 #include <map>
+#include <string>
 #include <vector>
 
 std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string> lis, std::deque<int> deq);
