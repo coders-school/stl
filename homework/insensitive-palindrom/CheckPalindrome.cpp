cfb3c477387f17269c1665b83361a98946e5a4d1
Krzysiek Nowak <krzynowak93@gmail.com>
2022-12-05 18:12:50 +0100

    Check-Palindrome

--- homework/insensitive-palindrom/CheckPalindrome.cpp
+++ homework/insensitive-palindrom/CheckPalindrome.cpp
@@ -1,8 +1,39 @@
 #include "CheckPalindrome.hpp"
 
-
-bool is_palindrome(std::string str) {
-    std::for_each(str.begin(), str.end(), [](char & c){c = ::tolower(c);});
-    str.resize(std::remove_if(str.begin(), str.end(),[](char x){return !isalnum(x);})-str.begin());
-    return std::equal(str.begin(), str.end(), str.rbegin());
+bool is_palindrome(std::string & word) {
+    for(auto n : word) {
+        if (n == '\'') {
+            word.erase(std::remove(word.begin(), word.end(), n), word.end());
+        }
+        if (n == ' ') {
+            word.erase(std::remove(word.begin(), word.end(), n), word.end());
+        }
+        if (n == ',') {
+            word.erase(std::remove(word.begin(), word.end(), n), word.end());
+        }
+        if (n == '!') {
+            word.erase(std::remove(word.begin(), word.end(), n), word.end());
+        }
+        if (n == '?') {
+            word.erase(std::remove(word.begin(), word.end(), n), word.end());
+        }
+        if (n == '-') {
+            word.erase(std::remove(word.begin(), word.end(), n), word.end());
+        }
+        if (n == '.') {
+            word.erase(std::remove(word.begin(), word.end(), n), word.end());
+        }
+        std::transform(word.begin(), word.end(), word.begin(), ::tolower);   
+    }
+    std::reverse(std::begin(word), std::end(word));
+    std::string word2 {};
+    for(auto n : word) {
+        word2.push_back(n);
+    }
+    std::reverse(std::begin(word), std::end(word));
+    if(word == word2) {
+        return true;
+    }
+    else
+        return false;
 }
