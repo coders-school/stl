a618a30eb961383b5c19419d28c46ede6a1579c9
KrzucH <115778673+KrzucH@users.noreply.github.com>
2022-12-05 16:18:03 +0100

    Update transform.cpp

--- homework/transform-containers/transform.cpp
+++ homework/transform-containers/transform.cpp
@@ -1,8 +1,8 @@
 #include "transform.hpp"
 
 std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string> lis, std::deque<int> deq) {
-    std::map<int,std::string> map {};
-    std::vector<std::string> vec {};
+    std::map<int,std::string> map{};
+    std::vector<std::string> vec{};
 
     lis.sort();
     auto word = std::unique(lis.begin(), lis.end());
@@ -10,11 +10,10 @@ std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string
     std::sort(deq.begin(), deq.end());
     auto number = std::unique(deq.begin(), deq.end());
     deq.erase(number, deq.end());
-    for(auto n : lis) {
+    for (auto n : lis) {
         vec.push_back(n);
     }
     std::transform(deq.begin(), deq.end(), vec.begin(), std::inserter(map, map.end()),
-                  [](const auto i,const auto s) {return std::make_pair(i, s);});
+                   [](const auto i,const auto s) {return std::make_pair(i, s);});
     return map;
-
 }
