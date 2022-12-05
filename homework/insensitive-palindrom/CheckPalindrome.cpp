8d76126f297fd01196e6c4cff8da1e466ebcde0e
KrzucH <115778673+KrzucH@users.noreply.github.com>
2022-12-05 19:10:14 +0100

    Update CheckPalindrome.cpp

--- homework/insensitive-palindrom/CheckPalindrome.cpp
+++ homework/insensitive-palindrom/CheckPalindrome.cpp
@@ -1,8 +1,7 @@
 #include "CheckPalindrome.hpp"
 
 bool is_palindrome(std::string word) {
-    
     std::transform(word.begin(), word.end(), word.begin(), ::tolower);
-    word.erase(std::remove_if(word.begin(), word.end(), [](auto x) {return !isalnum(x);})-word.begin());
+    word.erase(std::remove_if(word.begin(), word.end(), [](auto x) { return !isalnum(x); }) - word.begin());
     return std::equal(word.begin(), word.end(), word.rbegin());
 }
