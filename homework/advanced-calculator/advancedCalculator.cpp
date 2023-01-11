bedf1c884649104506605a08ef74f597dbd0c4da
Krzysztof Goldon <krzysztofgoldon@gmail.com>
2023-01-11 21:42:09 +0100

    Initial commit

---
+++ homework/advanced-calculator/advancedCalculator.cpp
@@ -0,0 +1,5 @@
+#include "advancedCalculator.hpp"
+
+ErrorCode process(std::string, double*) {
+    return ErrorCode::OK;
+}
---
+++ homework/advanced-calculator/advancedCalculator.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+
+enum class ErrorCode {
+    OK,
+    BadCharacter,
+    BadFormat,
+    DivideBy0,
+    SqrtOfNegativeNumber,
+    ModuleOfNonIntegerValue
+};
+
+ErrorCode process(std::string, double*);
