5c8d86f1bd66ce2287d26cc9c5ca5b764fc8328f
Krzysztof Goldon <krzysztofgoldon@gmail.com>
2023-01-13 08:58:43 +0100

    Add map for binding cmd with functions

--- homework/advanced-calculator/advancedCalculator.cpp
+++ homework/advanced-calculator/advancedCalculator.cpp
@@ -1,5 +1,24 @@
 #include "advancedCalculator.hpp"
 
-ErrorCode process(std::string, double*) {
+std::function<void()> fPlus = []() {};
+std::function<void()> fMinus = []() {};
+std::function<void()> fMulti = []() {};
+std::function<void()> fDiv = []() {};
+std::function<void()> fMod = []() {};
+std::function<void()> fFactorial = []() {};
+std::function<void()> fPower = []() {};
+std::function<void()> fSqr = []() {};
+
+std::unordered_map<char, std::function<void()>> cmdFunction = {
+    {'+', fPlus},
+    {'-', fMinus},
+    {'*', fMulti},
+    {'/', fDiv},
+    {'%', fMod},
+    {'!', fFactorial},
+    {'^', fPower},
+    {'$', fSqr}};
+
+ErrorCode process(std::string cmd, double* out) {
     return ErrorCode::OK;
 }
