13ca0e66f6f148a2bad5a1f5f32717ba6ae1768a
Adam Pietrzyk <apietrzyk92@gmail.com>
2023-02-01 21:59:06 +0100

    Initial implementation

---
+++ homework/remove-vowels/vowel.cpp
@@ -0,0 +1,14 @@
+#include "vowel.hpp"
+void removeVowels(std::vector<std::string>& vec) {
+    std::string vowels = "aeiouyAEIOUY";
+    for (auto& el : vec) {
+        for (size_t i = 0; i < el.size(); i++) {
+            for (size_t j = 0; j < vowels.size(); j++) {
+                if (el[i] == vowels[j]) {
+                    el.erase(i,1);
+                    i--;
+                }
+            }
+        }
+    }
+}
