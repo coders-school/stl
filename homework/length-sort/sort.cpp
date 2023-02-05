61d9114e652006f32a4597ae6928bf23da5dee49
PaSawi <sawicki.patryk@gmail.com>
2023-02-05 20:19:02 +0100

    length-sort start

---
+++ homework/length-sort/sort.cpp
@@ -0,0 +1,5 @@
+#include "sort.hpp"
+#include <string>
+
+std::deque<std::string> lengthSort(std::forward_list<std::string> deq) {
+}
---
+++ homework/length-sort/sort.hpp
@@ -0,0 +1,4 @@
+#include <deque>
+#include <forward_list>
+
+std::deque<std::string> lengthSort(std::forward_list<std::string> deq);
