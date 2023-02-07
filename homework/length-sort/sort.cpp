c5f68cdd96f83c1a5eccd79420151f34d55d63a3
PaSawi <sawicki.patryk@gmail.com>
2023-02-07 13:55:06 +0100

    sorted list of strings

--- homework/length-sort/sort.cpp
+++ homework/length-sort/sort.cpp
@@ -1,13 +1,29 @@
 #include "sort.hpp"
 #include <algorithm>
+#include <functional>
+#include <iostream>
+#include <iterator>
 #include <string>
 
 std::deque<std::string> lengthSort(std::forward_list<std::string> fli) {
     std::deque<std::string> deq;
     fli.sort();
 
-    for (auto it = fli.begin(); it != fli.end(); ++it) {
+    int i{0};
+
+    for (auto it = fli.begin(); it != fli.end(); ++it, i++) {
+        for (int j = 0; j < (*it).length() - 1; j++) {
+            auto it1 = fli.begin();
+            auto it2 = next(fli.begin(), j);
+
+            std::cout << " it1: " << *it1 << "it2: " << *it2 << " ";
+        }
+        std::cout << (*it).size() << " ";
+
+        // if(*it < *(it).be )
+
         deq.emplace_back(*it);
     }
+
     return deq;
 }
