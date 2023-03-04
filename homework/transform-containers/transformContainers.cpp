635a85380c14727cb67cb75fae34e9551986b40a
Roman Nawrot <romug1337@gmail.com>
2023-03-04 10:12:29 +0100

    implementation

--- homework/transform-containers/transformContainers.cpp
+++ homework/transform-containers/transformContainers.cpp
@@ -3,8 +3,8 @@
 std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string> list,
                                                            std::deque<int> deque) {
     std::map<int, std::string> finalMap;
-    std::unique(list.begin(), list.end());
-    std::unique(deque.begin(), deque.end());
+    list.erase(std::unique(list.begin(), list.end()), list.end());
+    deque.erase(std::unique(deque.begin(), deque.end()), deque.end());
 
     std::transform(list.begin(), list.end(), deque.begin(), std::inserter(finalMap, finalMap.end()),
                    [](const auto& listArg, const auto& dequeArg) { return std::make_pair(dequeArg, listArg); });
