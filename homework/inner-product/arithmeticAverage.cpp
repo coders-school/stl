bbb92588017da1aef2d083c328b75c8a01fec878
Roman Nawrot <romug1337@gmail.com>
2023-03-04 11:10:51 +0100

    implementation

---
+++ homework/inner-product/arithmeticAverage.cpp
@@ -0,0 +1,12 @@
+#include "arithmeticAverage.hpp"
+
+double ArithmeticAverage(const std::vector<int>& first, const std::vector<int>& second) {
+    double result = std::inner_product(
+        first.begin(), first.end(), second.begin(), 0, std::plus<>{}, std::plus<>{});
+    result /= (first.size() + second.size());
+    return result;
+}
+double Distance(const std::vector<int>& first, const std::vector<int>& second) {
+    double result = std::inner_product(first.begin(), first.end(), second.begin(), 0, std::plus<>{}, [](const auto& a, const auto& b) { return sqrt(a * a + b * b); });
+    return result;
+}
---
+++ homework/inner-product/arithmeticAverage.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <algorithm>
+#include <cmath>
+#include <numeric>
+#include <vector>
+
+double ArithmeticAverage(const std::vector<int>& first, const std::vector<int>& second);
+double Distance(const std::vector<int>& first, const std::vector<int>& second);
