57bb596f909b89f43c933601b9205c4ea5b8e003
Benia <beniab333@gmail.com>
2023-03-04 15:58:32 +0100

    made sort.hpp and sort.cpp

---
+++ homework/length-sort/sort.cpp
@@ -0,0 +1,9 @@
+#include "sort.hpp"
+#include <string>
+#include <forward_list>
+
+auto lengthSort(std::forward_list<std::string> &list) -> std::deque<std::string>
+{
+    std::deque<std::string> result;
+
+}
---
+++ homework/length-sort/sort.hpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <deque>
+
+
+
+auto lengthSort(std::forward_list<std::string> &list) -> std::deque<std::string>;
