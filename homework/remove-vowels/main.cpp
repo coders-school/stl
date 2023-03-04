2b8bfd350e715a5e2899063fb2a0bfb854770632
kostuch76 <piotr.kosc@op.pl>
2023-03-04 13:05:07 -0500

    Reinstatement orginal main function

--- homework/remove-vowels/main.cpp
+++ homework/remove-vowels/main.cpp
@@ -17,7 +17,7 @@ void printVector(const std::vector<T>& collection) {
 
 int main() {
     std::vector<std::string> words = {
-        "Somebody once told me the world is gonna rollaa me",
+        "Somebody once told me the world is gonna rolla me",
         "I ain't the sharpest tool in the shed",
         "She was looking kind of dumb with her finger and her thumb",
         "In the shape of an L on her forehead"};
