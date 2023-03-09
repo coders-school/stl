3fdc1207525592827e366b17354abda4bbfe74f6
Benia <beniab333@gmail.com>
2023-03-09 18:05:59 +0100

    done?

--- homework/length-sort/sort.cpp
+++ homework/length-sort/sort.cpp
@@ -1,20 +1,14 @@
 #include "sort.hpp"
 
-auto lengthSort(std::forward_list<std::string> &list) -> std::deque<std::string>
-{
+auto lengthSort(std::forward_list<std::string>& list) -> std::deque<std::string> {
     std::deque<std::string> result;
 
-    for(auto &word : list)
-    {
-        if(result.empty())
-        {
+    for (auto& word : list) {
+        if (result.empty()) {
             result.push_back(word);
-        }
-        else 
-        {
+        } else {
             auto it = result.begin();
-            while(it != result.end() && (word.size() > it->size() || (word.size() == it->size() && word > *it)))
-            {
+            while (it != result.end() && (word.size() > it->size() || (word.size() == it->size() && word > *it))) {
                 ++it;
             }
             result.insert(it, word);
