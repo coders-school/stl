9793a7493ec54172d59085e21687440db71d4616
Benia <beniab333@gmail.com>
2023-03-09 17:52:50 +0100

    again format

--- homework/remove-vowels/vowel.cpp
+++ homework/remove-vowels/vowel.cpp
@@ -2,16 +2,13 @@
 #include <algorithm>
 #include <cctype>
 
-auto isVowel(char c, const std::array<char, 6>& vowels)
-{
+auto isVowel(char c, const std::array<char, 6>& vowels) {
     c = std::tolower(c);
     return std::find(vowels.begin(), vowels.end(), c) != vowels.end();
 }
 
-auto removeVowels(std::vector<std::string>& list) -> void
-{
+auto removeVowels(std::vector<std::string>& list) -> void {
     std::array<char, 6> arr = { 'a', 'e', 'i', 'o', 'u', 'y' };
-
     for (auto& str : list) {
         str.erase(std::remove_if(str.begin(), str.end(),
                       [&](char c) { return isVowel(c, arr); }),
