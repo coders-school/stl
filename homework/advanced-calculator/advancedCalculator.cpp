6cc13183f61c46f82a2e698ed4e53e198c535e5d
Roman Nawrot <romug1337@gmail.com>
2023-03-09 22:45:16 +0100

    advanced-calculator

--- homework/advanced-calculator/advancedCalculator.cpp
+++ homework/advanced-calculator/advancedCalculator.cpp
@@ -2,14 +2,14 @@
 
 std::map<char, std::function<double(const double&, const double&)>>
     mapOfType = {
-        {'+', [](const auto& a, const auto& b) -> double { return a + b; }},
-        {'*', [](const auto& a, const auto& b) -> double { return a * b; }},
-        {'/', [](const auto& a, const auto& b) -> double { return a / b; }},
-        {'-', [](const auto& a, const auto& b) -> double { return a - b; }},
-        {'%', [](const auto& a, const auto& b) -> double { return std::fmod(a, b); }},
-        {'!', [](const auto& a, const auto& b) -> double { return std::tgamma(a + 1); }},
-        {'^', [](const auto& a, const auto& b) -> double { return std::pow(a, b); }},
-        {'$', [](const auto& a, const auto& b) -> double { return std::pow(a, 1.0 / b); }},
+        {'+', [](const double& a, const double& b) -> double { return a + b; }},
+        {'*', [](const double& a, const double& b) -> double { return a * b; }},
+        {'/', [](const double& a, const double& b) -> double { return a / b; }},
+        {'-', [](const double& a, const double& b) -> double { return a - b; }},
+        {'%', [](const double& a, const double& b) -> double { return std::fmod(a, b); }},
+        {'!', [](const double& a, const double& b) -> double { if (a < 0.f) { double o = a; o*=(-1); return -(std::tgamma(o + 1.f));} return std::tgamma(a + 1.f); }},
+        {'^', [](const double& a, const double& b) -> double { return std::pow(a, b); }},
+        {'$', [](const double& a, const double& b) -> double { return std::pow(a, 1.f / b); }},
 };
 
 ErrorCode process(std::string input, double* out) {
@@ -25,33 +25,65 @@ ErrorCode process(std::string input, double* out) {
         std::string b_string = input.substr(find_string_it + 1, input.size());
         try {
             a = std::stod(a_string);
-            b = std::stod(b_string);
-            std::cout << a << b;
+            if (type != '!') {
+                b = std::stod(b_string);
+            }
         } catch (std::invalid_argument) {
-            return ErrorCode::BadCharacter;
+            std::cout << "error \n";
+            return ErrorCode::BadFormat;
         }
+        std::cout << "a: " << a << ", b: " << b << ", type: " << type << "." << std::endl;
     } else {
-        return ErrorCode::BadFormat;
+        return ErrorCode::BadCharacter;
     }
+
+    std::cout << "\n1\n";
 
     switch (type) {
     case '/':
-        if (b == 0) {
+        if (b == 0.f) {
             return ErrorCode::DivideBy0;
         }
+        break;
     case '%':
-        if (a == 0 || b == 0) {
+        if (a == 0.f || b == 0.f || b != static_cast<int>(b)) {
             return ErrorCode::ModuleOfNonIntegerValue;
         }
+        break;
     case '$':
-        if (a < 0) {
+        if (a < 0.f) {
             return ErrorCode::SqrtOfNegativeNumber;
         }
+        break;
     }
+    std::cout << "\n2\n";
 
-    std::for_each(mapOfType.begin(), mapOfType.end(), [&](const auto& el) {
-        if (el.first == type) 
-        { *out = el.second(a, b);
-     } });
+    auto it = mapOfType.find(type);
+    if (it != mapOfType.end()) {
+        *out = it->second(a, b);
+        std::cout << "Wynik 1: " << it->second(a, b) << std::endl;
+    }
     return ErrorCode::OK;
 }
+
+// D:\Projekty\Coders_School\STL\stl\homework\advanced-calculator\test.cpp:139: Failure
+// Expected equality of these values:
+//   process("5,1!", &result)
+//     Which is: 4-byte object <00-00 00-00>
+//   ErrorCode::BadFormat
+//     Which is: 4-byte object <02-00 00-00>
+// [  FAILED  ] advancedCalculatorTest.ShouldReturnBadFormat (22 ms)
+// [ RUN      ] advancedCalculatorTest.ShouldReturnBadCharacter
+// a: 123, b: 123, type: -.
+
+// 1
+
+// 2
+// Wynik 1: 0
+// D:\Projekty\Coders_School\STL\stl\homework\advanced-calculator\test.cpp:155: Failure
+// Expected equality of these values:
+//   process("123 #- 123", &result)
+//     Which is: 4-byte object <00-00 00-00>
+//   ErrorCode::BadCharacter
+//     Which is: 4-byte object <01-00 00-00>
+// [  FAILED  ] advancedCalculatorTest.ShouldReturnBadCharacter (21 ms)
--- homework/advanced-calculator/main.cpp
+++ homework/advanced-calculator/main.cpp
@@ -4,9 +4,10 @@ int main() {
     std::string input;
     double result;
     while (true) {
+        result = 0;
         std::cout << "Podaj wyrazenie do obliczenia " << std::endl;
         std::cin >> input;
         process(input, &result);
-        std::cout << "Wynik:" << result << std::endl;
+        std::cout << std::setprecision(10) << "Wynik: " << result << std::endl;
     }
 }
