84aa89ec03a8cf287ea1b82507c5c693e8c13387
Benia <beniab333@gmail.com>
2023-03-09 17:57:17 +0100

    ayay formatting

--- homework/remove-vowels/vowel.cpp
+++ homework/remove-vowels/vowel.cpp
@@ -7,7 +7,7 @@ auto isVowel(char c, const std::array<char, 6>& vowels) {
     return std::find(vowels.begin(), vowels.end(), c) != vowels.end();
 }
 
-void removeVowels(std::vector<std::string>& list)  {
+void removeVowels(std::vector<std::string>& list) {
     std::array<char, 6> arr = { 'a', 'e', 'i', 'o', 'u', 'y' };
     for (auto& str : list) {
         str.erase(std::remove_if(str.begin(), str.end(),
