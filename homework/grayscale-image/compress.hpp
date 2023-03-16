25e989945a5a901c1dbd5b5ee4cd79c3df869a20
Krzysiek Nowak <krzynowak93@gmail.com>
2023-03-16 16:12:19 +0100

    greyscale-algo done 2

--- homework/grayscale-image/compress.hpp
+++ homework/grayscale-image/compress.hpp
@@ -1,10 +1,9 @@
 #include <algorithm>
-#include <iostream>
 #include <array>
+#include <iostream>
 #include <vector>
-
 
 const int width = 32;
 const int height = 32;
-std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale (std::array<std::array<uint8_t, width>, height>& );
-// std::array<std::array<uint8_t, width>, height> decompressGrayscale(std::vector<std::pair<uint8_t, uint8_t>>& );
+std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(std::array<std::array<uint8_t, width>, height>&);
+std::array<std::array<uint8_t, width>, height> decompressGrayscale(std::vector<std::pair<uint8_t, uint8_t>>&);
