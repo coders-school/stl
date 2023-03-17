384fc05a04d7a50af7ac5d43f9bd2cd6b5c02cdd
Roman Nawrot <romug1337@gmail.com>
2023-03-17 18:07:20 +0100

    insensitive-palindrom

--- homework/insensitive-palindrom/PalindromTest.cpp
+++ homework/insensitive-palindrom/PalindromTest.cpp
@@ -1,6 +1,6 @@
 #include "PalindromTest.hpp"
 
-bool PalindromTest::is_palindrome(const std::string& input) {
+bool is_palindrome(const std::string& input) {
     std::string removeChars = ",.?()[] ";
     std::string newString = input;
     newString.erase(std::remove_if(newString.begin(), newString.end(), [&](const auto& el) { if(removeChars.find(el) != std::string::npos){ return true; } return false; }));
