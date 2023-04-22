cb99ddc896ca2e3d1ad45cc4d9fcb1965e66ebdb
dominikpalatynski <palatynski.dominik@gmail.com>
2023-04-22 11:01:17 +0200

    all clear

--- homework/length-sort/sort.hpp
+++ homework/length-sort/sort.hpp
@@ -2,4 +2,5 @@
 #include <deque>
 #include <forward_list>
 #include <iostream>
+bool sortB(const std::string& s1, const std::string& s2);
 std::deque<std::string> lengthSort(const std::forward_list<std::string>& fl);
