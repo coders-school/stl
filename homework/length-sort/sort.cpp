7de7588c847a6f69f72efd5d0b541561c6d12d1e
0x90akomenda <104097329+0x90akomenda@users.noreply.github.com>
2023-05-23 22:11:21 +0200

    Update sort.cpp

--- homework/length-sort/sort.cpp
+++ homework/length-sort/sort.cpp
@@ -6,6 +6,6 @@
 std::deque<std::string> lengthSort(std::forward_list<std::string>& list)
 {
     std::deque<std::string> deq(list.begin(), list.end());
-    sort(deq.begin(), deq.end(), [](std::string& a, std::string& b) {if (a.size() == b.size()) { return a < b; } else { return a.size() < b.size(); }});
+    sort(deq.begin(), deq.end(), [](std::string& a, std::string& b) {if (a.size() == b.size()) { return a < b; } else { return a.size() < b.size(); } });
     return deq;
 }
