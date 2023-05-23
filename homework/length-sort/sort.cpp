4a38857d56307c3311261a260fcf70ca09290e52
0x90akomenda <104097329+0x90akomenda@users.noreply.github.com>
2023-05-23 22:19:02 +0200

    Update sort.cpp
    Fixing formatting

--- homework/length-sort/sort.cpp
+++ homework/length-sort/sort.cpp
@@ -4,7 +4,7 @@
 #include <string>
 
 std::deque<std::string> lengthSort(std::forward_list<std::string>& list) {
-     std::deque<std::string> deq(list.begin(), list.end());
-     sort(deq.begin(), deq.end(), [](std::string& a, std::string& b) {if (a.size() == b.size()) { return a < b; } else { return a.size() < b.size(); } });
-     return deq;
+    std::deque<std::string> deq(list.begin(), list.end());
+    sort(deq.begin(), deq.end(), [](std::string& a, std::string& b) {if (a.size() == b.size()) { return a < b; } else { return a.size() < b.size(); } });
+    return deq;
 }
