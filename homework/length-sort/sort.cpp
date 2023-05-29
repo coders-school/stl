05cd8cd8a4841163ab647bdbbd847f97183f1e08
Klaudiusz Ogrodnik <ogrodnik.klaudiusz@gmail.com>
2023-05-29 18:19:52 +0200

    Fix some format issues

--- homework/length-sort/sort.cpp
+++ homework/length-sort/sort.cpp
@@ -6,7 +6,7 @@
 #include <string>
 
 std::deque<std::string> lengthSort(std::forward_list<std::string> words) {
-    std::deque<std::string> sortedWords {words.begin(), words.end()};
+    std::deque<std::string> sortedWords{words.begin(), words.end()};
     std::sort(sortedWords.begin(), sortedWords.end(), [](const auto& first, const auto& second) {
         if (first.length() == second.length()) {
             return first < second;
