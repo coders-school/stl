ae2fb2b90b1c09d85599a36e731a9d17110c6940
Klaudiusz Ogrodnik <ogrodnik.klaudiusz@gmail.com>
2023-05-29 18:16:56 +0200

    Adding sort.cpp and sort.hpp functions

---
+++ homework/length-sort/sort.cpp
@@ -0,0 +1,17 @@
+#include "sort.hpp"
+#include <algorithm>
+#include <deque>
+#include <forward_list>
+#include <functional>
+#include <string>
+
+std::deque<std::string> lengthSort(std::forward_list<std::string> words) {
+    std::deque<std::string> sortedWords {words.begin(), words.end()};
+    std::sort(sortedWords.begin(), sortedWords.end(), [](const auto& first, const auto& second) {
+        if (first.length() == second.length()) {
+            return first < second;
+        }
+        return first.length() < second.length();
+    });
+    return sortedWords;
+}
