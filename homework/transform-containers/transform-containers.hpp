ce5f2cb2318907e56101b1fa17a97fa4f3d16049
0x90akomenda <104097329+0x90akomenda@users.noreply.github.com>
2023-06-22 19:54:11 +0200

    Add files via upload

---
+++ homework/transform-containers/transform-containers.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <algorithm>
+#include <list>
+#include <deque>
+#include <map>
+#include <string>
+
+std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string>& list, std::deque<int>& deque);
