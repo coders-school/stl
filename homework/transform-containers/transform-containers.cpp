e2cf6de332fb2baa69f0d84c2422c6eefadd77f6
0x90akomenda <104097329+0x90akomenda@users.noreply.github.com>
2023-06-22 20:14:44 +0200

    Update transform-containers.cpp

--- homework/transform-containers/transform-containers.cpp
+++ homework/transform-containers/transform-containers.cpp
@@ -2,20 +2,19 @@
 #include <iterator>
 
 std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string>& list, std::deque<int>& deque) {
-	
     std::map<int, std::string> result;
-    //Removing duplicates from list
+    // Removing duplicates from list
     list.sort();
     auto p = std::unique(list.begin(), list.end());
     list.erase(p, list.end());
 
-    //Removing duplicates from deque
+    // Removing duplicates from deque
     std::sort(deque.begin(), deque.end());
     auto pd = std::unique(deque.begin(), deque.end());
     deque.erase(pd, deque.end());
 
-    //Transforming two containers to the output container
-    std::transform(deque.begin(), deque.end(), list.begin(), std::inserter(result, result.end()), [](auto first, auto seond) {return std::make_pair(first, seond); });
+    // Transforming two containers to the output container
+    std::transform(deque.begin(), deque.end(), list.begin(), std::inserter(result, result.end()), [](auto first, auto seond) { return std::make_pair(first, seond); });
 
     return result;
 }
