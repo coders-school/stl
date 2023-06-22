20157c1f6d87e77745660310434ee31b53da215e
0x90akomenda <104097329+0x90akomenda@users.noreply.github.com>
2023-06-22 20:12:02 +0200

    Update transform-containers.cpp
    Fixing again c style format document

--- homework/transform-containers/transform-containers.cpp
+++ homework/transform-containers/transform-containers.cpp
@@ -1,20 +1,21 @@
-﻿#include "transform-containers.hpp"
+#include "transform-containers.hpp"
 #include <iterator>
 
-std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string>& list, std::deque<int>& deque)
-{
-	std::map<int, std::string> result;
-	//Removing duplicates from list
-	list.sort();
-	auto p = std::unique(list.begin(), list.end());
-	list.erase(p, list.end());
+std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string>& list, std::deque<int>& deque) {
+	
+    std::map<int, std::string> result;
+    //Removing duplicates from list
+    list.sort();
+    auto p = std::unique(list.begin(), list.end());
+    list.erase(p, list.end());
 
-	//Removing duplicates from deque
-	std::sort(deque.begin(), deque.end());
-	auto pd = std::unique(deque.begin(), deque.end());
-	deque.erase(pd, deque.end());
+    //Removing duplicates from deque
+    std::sort(deque.begin(), deque.end());
+    auto pd = std::unique(deque.begin(), deque.end());
+    deque.erase(pd, deque.end());
 
-	std::transform(deque.begin(), deque.end(), list.begin(), std::inserter(result, result.end()), [](auto first, auto seond) {return std::make_pair(first, seond); });
+    //Transforming two containers to the output container
+    std::transform(deque.begin(), deque.end(), list.begin(), std::inserter(result, result.end()), [](auto first, auto seond) {return std::make_pair(first, seond); });
 
-	return result;
+    return result;
 }
