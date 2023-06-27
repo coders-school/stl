8121990a7c69481e44aac05bbab25bbd9940a3d7
0x90akomenda <104097329+0x90akomenda@users.noreply.github.com>
2023-06-27 21:43:02 +0200

    Update is_palindrome.cpp
    Fixing c style format

--- homework/insensitive-palindrom/is_palindrome.cpp
+++ homework/insensitive-palindrom/is_palindrome.cpp
@@ -1,14 +1,14 @@
-﻿#include "is_palindrome.hpp"
+#include "is_palindrome.hpp"
 bool is_palindrome(std::string text ) {
-	std::string temp = "";
-	text.erase(std::remove_if(text.begin(), text.end(), [](unsigned char c) { return !isalnum(c); }), text.end());
-	std::transform(text.begin(), text.end(), text.begin(), [](unsigned char c) { return std::tolower(c); });
-	temp = text;
-	std::reverse(temp.begin(), temp.end());
+    std::string temp = "";
+    text.erase(std::remove_if(text.begin(), text.end(), [](unsigned char c) { return !isalnum(c); }), text.end());
+    std::transform(text.begin(), text.end(), text.begin(), [](unsigned char c) { return std::tolower(c); });
+    temp = text;
+    std::reverse(temp.begin(), temp.end());
 
-	if (temp == text) {
-		return true;
-	}
+    if (temp == text) {
+    	return true;
+    }
 
-	return false;
+    return false;
 }
