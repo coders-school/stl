4f8f15b9aa19716f4ccd628e535c598f44556fcf
Patryk Sawicki <patryk.sawicki@polon-alfa.pl>
2023-07-12 12:11:23 +0200

    gray

--- homework/grayscale-image/compression.cpp
+++ homework/grayscale-image/compression.cpp
@@ -23,28 +23,6 @@ std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(const std::array<std:
     return xbMap;
 }
 
-/*
-std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(std::array<std::array<uint8_t, width>, height> &arr) {
-    std::vector<std::pair<uint8_t, uint8_t>> compression{std::make_pair(arr[0][0], 0)};
-    int iv = 0;
-    for(std::size_t i = 0; i < height; i++) {
-        for(std::size_t j = 0; j < width; j++) {
-            if (compression[iv].first == arr[i][j]) {
-                compression[iv].second +=1;
-            } else {
-              compression.push_back(std::make_pair(arr[i][j], 1));
-              ++iv;
-            }
-            if (j == width - 1 && i != height - 1) {
-              compression.push_back(std::make_pair(arr[i+1][0], 0));
-              iv++;
-            }
-        }
-    }
-    return compression;
-    
-}*/
-
 std::array<std::array<uint8_t, width>, height> decompressGrayscale(std::vector<std::pair<uint8_t, uint8_t>> &vec) {
     std::array<std::array<uint8_t, width>, height> decompression;
     std::size_t k = 0;
