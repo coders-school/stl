1c61e831d1cf627fa0224a205f194af483bdf4ce
AdamWiercioch95 <adamwiercioch95@gmail.com>
2023-07-14 09:25:26 +0200

    remove-vowels works

---
+++ homework/remove-vowels/vowel.cpp
@@ -0,0 +1,12 @@
+#include "vowel.hpp"
+
+bool isVowel(char c) {
+    c = std::tolower(c);
+    return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y');
+}
+
+void removeVowels (std::vector<std::string>& vec) {
+    for (auto& el : vec) {
+        el.erase(remove_if(el.begin(), el.end(), isVowel), el.end());
+    }
+}
---
+++ homework/remove-vowels/vowel.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <algorithm>
+
+bool isVowel (char c);
+
+void removeVowels (std::vector<std::string>& vec);
