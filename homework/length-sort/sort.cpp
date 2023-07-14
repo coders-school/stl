816d566de8e173030f007cd31d6526501b220dc8
AdamWiercioch95 <adamwiercioch95@gmail.com>
2023-07-14 11:05:15 +0200

    length-sort works

---
+++ homework/length-sort/sort.cpp
@@ -0,0 +1,21 @@
+#include "sort.hpp"
+
+std::deque<std::string> lengthSort (const std::forward_list<std::string> fl) {
+    std::deque<std::string> deq;
+
+    for (const auto& el : fl) {
+        deq.push_back(el);
+    }
+
+    std::sort(deq.begin(), deq.end());
+
+    for (int i = 0; i < deq.size(); i++) {
+        for (int j = 0; j < deq.size() - 1; j++) {
+            if (deq[j].length() > deq[j + 1].length()) {
+                std::swap(deq[j], deq[j + 1]);
+            }
+        }
+    }
+
+    return deq;
+}
---
+++ homework/length-sort/sort.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <algorithm>
+#include <deque>
+#include <forward_list>
+#include <string>
+
+std::deque<std::string> lengthSort (const std::forward_list<std::string> fl);
