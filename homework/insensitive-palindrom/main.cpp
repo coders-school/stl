ca4b505a8b51ef1f5745cce8f6d2d9738c8519dd
PaSawi <sawicki.patryk@gmail.com>
2023-08-04 17:21:57 +0200

    intensitive-palindrome ALL TEST PASSED

--- homework/insensitive-palindrom/main.cpp
+++ homework/insensitive-palindrom/main.cpp
@@ -3,17 +3,16 @@
 
 int main()
 {
-
-    std::string s;
-    std::cout << "Specify string of charakter: ";
-    std::getline(std::cin, s);
+    std::string s = "Are we not pure? “No, sir!” Panama’s moody Noriega brags."
+                       "It is garbage!” Irony dooms a man—a prisoner up to new era.";
+    //std::cout << "Specify string of character: ";
+    //std::getline(std::cin, s);
 
     if (is_palindrome(s)) {
         std::cout << "The given string is a palindrome. \n";
     } else {
         std::cout << "The given string isn't a palindrome. \n";
     }
-
     std::cout << std::endl;
 
     return 0;
--- homework/insensitive-palindrom/palindrome.cpp
+++ homework/insensitive-palindrom/palindrome.cpp
@@ -1,11 +1,24 @@
-//ignore special chars like ,.?()[], upper letters, spaces or endl
+// ignore special chars like ,.?()[], upper letters, spaces or endl
 #include "palindrome.hpp"
 #include <algorithm>
+#include <cctype>
 #include <iostream>
 #include <ranges>
 
-bool is_palindrome(std::string word_to_check){
-    std::string reversed_word = word_to_check;
-    std::ranges::reverse(word_to_check);
-    return word_to_check == reversed_word;
+bool is_palindrome(std::string word_to_check) {
+
+    std::ranges::for_each(word_to_check, [](char & letter) {
+        letter = std::tolower(letter);
+    });
+    std::string w = word_to_check; 
+    w.erase(std::remove_if(w.begin(), w.end(),
+                                       [](char & letter) {
+                                           return !(std::isalnum(letter));
+                                       }),
+                        w.end());
+    std::string r_w = w;
+    std::ranges::reverse(r_w);
+    //std::cout<<"w  : " << w << std::endl;
+    //std::cout<<"r-w: " << r_w << std::endl;
+    return w == r_w;
 }
