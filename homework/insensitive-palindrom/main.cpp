7ebd02d501c560778675841d48d4d86b1bd713c3
PaSawi <sawicki.patryk@gmail.com>
2023-08-04 17:28:46 +0200

    insensitive-palindrom - corect formating

--- homework/insensitive-palindrom/main.cpp
+++ homework/insensitive-palindrom/main.cpp
@@ -1,12 +1,12 @@
-#include "palindrome.hpp"
 #include <iostream>
+#include "palindrome.hpp"
 
-int main()
-{
-    std::string s = "Are we not pure? “No, sir!” Panama’s moody Noriega brags."
-                       "It is garbage!” Irony dooms a man—a prisoner up to new era.";
-    //std::cout << "Specify string of character: ";
-    //std::getline(std::cin, s);
+int main() {
+    std::string s =
+        "Are we not pure? “No, sir!” Panama’s moody Noriega brags."
+        "It is garbage!” Irony dooms a man—a prisoner up to new era.";
+    // std::cout << "Specify string of character: ";
+    // std::getline(std::cin, s);
 
     if (is_palindrome(s)) {
         std::cout << "The given string is a palindrome. \n";
--- homework/insensitive-palindrom/palindrome.cpp
+++ homework/insensitive-palindrom/palindrome.cpp
@@ -1,4 +1,3 @@
-// ignore special chars like ,.?()[], upper letters, spaces or endl
 #include "palindrome.hpp"
 #include <algorithm>
 #include <cctype>
@@ -6,19 +5,18 @@
 #include <ranges>
 
 bool is_palindrome(std::string word_to_check) {
-
-    std::ranges::for_each(word_to_check, [](char & letter) {
+    std::ranges::for_each(word_to_check, [](char& letter) {
         letter = std::tolower(letter);
     });
-    std::string w = word_to_check; 
+    std::string w = word_to_check;
     w.erase(std::remove_if(w.begin(), w.end(),
-                                       [](char & letter) {
-                                           return !(std::isalnum(letter));
-                                       }),
-                        w.end());
+                           [](char& letter) {
+                               return !(std::isalnum(letter));
+                           }),
+            w.end());
     std::string r_w = w;
     std::ranges::reverse(r_w);
-    //std::cout<<"w  : " << w << std::endl;
-    //std::cout<<"r-w: " << r_w << std::endl;
+    // std::cout<<"w  : " << w << std::endl;
+    // std::cout<<"r-w: " << r_w << std::endl;
     return w == r_w;
 }
