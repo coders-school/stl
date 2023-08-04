702a0e46abd373f16edc1d8163e855227e21c46c
PaSawi <sawicki.patryk@gmail.com>
2023-08-04 17:47:23 +0200

    form

--- homework/insensitive-palindrom/palindrome.cpp
+++ homework/insensitive-palindrom/palindrome.cpp
@@ -4,19 +4,18 @@
 #include <iostream>
 #include <ranges>
 
-bool is_palindrome(std::string word_to_check) {
+bool is_palindrome(std::string word_to_check)
+{
     std::ranges::for_each(word_to_check, [](char& letter) {
         letter = std::tolower(letter);
     });
     std::string w = word_to_check;
     w.erase(std::remove_if(w.begin(), w.end(),
-                           [](char& letter) {
-                               return !(std::isalnum(letter));
-                           }),
-            w.end());
+                [](char& letter) {
+                    return !(std::isalnum(letter));
+                }),
+        w.end());
     std::string r_w = w;
     std::ranges::reverse(r_w);
-    std::cout<<"w  : " << w << std::endl;
-    std::cout<<"r-w: " << r_w << std::endl;
     return w == r_w;
 }
