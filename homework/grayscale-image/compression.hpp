da716cd9248bced300140ce97fddd2abc00e9189
Jacek Prętki <jacekpiotrpretki@gmail.com>
2023-09-02 21:56:18 +0200

    adding works

--- homework/grayscale-image/compression.hpp
+++ homework/grayscale-image/compression.hpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <array>
 #include <cstdint>
+#include <cstddef>
 constexpr size_t width = 32;
 constexpr size_t height = 32;
 std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(std::array<std::array<uint8_t, width>, height>);
