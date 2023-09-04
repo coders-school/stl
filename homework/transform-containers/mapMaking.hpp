dbc87217e499e2612e9a1f53d8ea6ada4a4c94de
JacekPretki <jacekpiotrpretki@gmail.com>
2023-09-04 17:57:09 +0200

    adding works

--- homework/transform-containers/mapMaking.hpp
+++ homework/transform-containers/mapMaking.hpp
@@ -1,7 +1,8 @@
 #pragma once
-#include <map>
-#include <list>
 #include <deque>
+#include <list>
+#include <map>
 #include <string>
 
-std::map<int, std::string>removeDuplicateAndTranformToMap(std::list<std::string>, std::deque<int>);
+
+std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string>, std::deque<int>);
