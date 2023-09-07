ccf76b82674633e9573c79f1a05d51cf679d5d37
JacekPretki <jacekpiotrpretki@gmail.com>
2023-09-07 22:51:16 +0200

    adding works

--- homework/inner-product/arithmeticAverage.hpp
+++ homework/inner-product/arithmeticAverage.hpp
@@ -1,5 +1,6 @@
 #pragma once
-#include <vector>
 #include <numeric>
+#include <vector>
+
 double ArithmeticAverage(const std::vector<int>& first, const std::vector<int>& second);
 double Distance(const std::vector<int>& first, const std::vector<int>& second);
