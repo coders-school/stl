c796bf1b8148e08ee74017cb76ae9fcf8fa13fff
JacekPretki <jacekpiotrpretki@gmail.com>
2023-09-10 08:45:27 +0200

    removing the recommended code

--- homework/append-new-recipe/main.cpp
+++ homework/append-new-recipe/main.cpp
@@ -9,15 +9,11 @@ int main() {
         {1, 's'},
         {40, 'm'}};
     std::vector<std::string> result = FormatIngredients(ingredients, amount);
-    // for (auto x : result) {
-    //     std::cout << x << "\n";
-    // }
     std::vector<std::string> steps{"Wsypac do miski 20 gram cukru",
                                    "Dorzucic 1 szklanke maki",
                                    "Dokladnie wymieszac",
                                    "Nalac 40ml rumu do kieliszka",
                                    "Wypic kieliszek",
                                    "Wysypac zawartosc miski"};
-    //AppendNewRecipe(steps, ingredients, amount);
     FormatRecipit(steps, ingredients, amount);
 }
