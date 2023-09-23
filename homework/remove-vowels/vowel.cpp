fa1dc046a5ff425e2744cb1118a6ee9a4e77d8fb
Marcin <marcinjagielak90@gmail.com>
2023-09-23 04:20:38 +0200

    remove-vowels completed

---
+++ homework/remove-vowels/vowel.cpp
@@ -0,0 +1,12 @@
+#include "vowel.hpp"
+#include <algorithm>
+#include <cctype>
+
+void removeVowels(std::vector<std::string>& vec) {
+    std::vector<char> vowels = {'a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y'};
+    for(auto &v : vec){
+        for(auto vow : vowels){
+            v.erase(std::remove(v.begin(), v.end(), vow), v.end());
+        }
+    }
+}
---
+++ homework/remove-vowels/vowel.hpp
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+void removeVowels(std::vector<std::string>& vec);
