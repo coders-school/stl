a819cad23d58f7fe3908b1eaa19a5ce994a038d2
Maciej Dawiec <dmaciek85@gmail.com>
2023-10-20 23:09:45 +0200

    adding formating2

--- homework/remove-vowels/vowel.cpp
+++ homework/remove-vowels/vowel.cpp
@@ -5,9 +5,9 @@ std::string allVow{"aeiouyAEIOUY"};
 std::vector<std::string> removeVowels(std::vector<std::string>& vow) {
     std::for_each(vow.begin(), vow.end(), [](std::string& str) {
         str.erase(std::remove_if(str.begin(), str.end(), [](const char& ch) {
-                        return allVow.find(ch) != std::string::npos;
-                    }),
-                    str.end());
+                      return allVow.find(ch) != std::string::npos;
+                  }),
+                  str.end());
     });
     return vow;
 }
