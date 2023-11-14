517720411c3b10ebdee9547eb4f83d8a8afe7232
Michał Maj <47927347+michalmaj@users.noreply.github.com>
2023-11-14 18:43:10 +0100

    Create vowel.cpp

---
+++ homework/remove-vowels/vowel.cpp
@@ -0,0 +1,14 @@
+#include "vowel.hpp"
+#include <algorithm>
+
+bool isVowel(const char& ch){
+    char c = static_cast<char>(std::tolower(ch));
+    if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+        return true;
+    return false;
+}
+
+void removeVowels(std::vector<std::string>& vec){
+    for (auto& e : vec)
+        e.erase(std::remove_if(e.begin(), e.end(), isVowel), e.end());
+}
