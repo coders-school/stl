cc449d6a6851e7571101179befe32902a4329ed7
Michał Maj <47927347+michalmaj@users.noreply.github.com>
2023-11-14 20:02:32 +0100

    Update sort.cpp

--- homework/length-sort/sort.cpp
+++ homework/length-sort/sort.cpp
@@ -2,6 +2,6 @@
 #include <utility>
 
 std::deque<std::string> lengthSort(std::forward_list<std::string>& fl) {
-    fl.sort([](const std::string& s1, const std::string& s2) {return std::make_pair(s1.size(), s1) < std::make_pair(s2.size(), s2);});
+    fl.sort([](const std::string& s1, const std::string& s2) { return std::make_pair(s1.size(), s1) < std::make_pair(s2.size(), s2); });
     return {fl.begin(), fl.end()};
 }
