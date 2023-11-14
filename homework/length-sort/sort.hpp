f9c9ac2eddb74d53dced7c2cabdef483587a0a0c
Michał Maj <47927347+michalmaj@users.noreply.github.com>
2023-11-14 19:57:43 +0100

    Create sort.hpp

---
+++ homework/length-sort/sort.hpp
@@ -0,0 +1,10 @@
+#ifndef SORT_HPP
+#define SORT_HPP
+
+#include <forward_list>
+#include <string>
+#include <deque>
+
+std::deque<std::string> lengthSort(std::forward_list<std::string>& fl);
+
+#endif
