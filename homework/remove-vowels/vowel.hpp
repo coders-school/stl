c3524f4374feff64cd63fcdc383af9ed68f3e529
Michał Maj <47927347+michalmaj@users.noreply.github.com>
2023-11-14 18:41:32 +0100

    Create vowel.hpp

---
+++ homework/remove-vowels/vowel.hpp
@@ -0,0 +1,10 @@
+#ifndef VOWEL_HPP
+#define VOWEL_HPP
+
+#include <string>
+#include <vector>
+
+bool isVowel(const char& c);
+void removeVowels(std::vector<std::string>& vec);
+
+#endif // VOWEL_HPP
