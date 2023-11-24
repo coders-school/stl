4a89e145896c5f26c8f4c4e28e50e22edba01dda
Michał Maj <47927347+michalmaj@users.noreply.github.com>
2023-11-24 18:02:54 +0100

    Create main.cpp

---
+++ homework/insensitive-palindrom/main.cpp
@@ -0,0 +1,18 @@
+#include "palindrome.hpp"
+#include <iostream>
+
+int main()
+{
+
+    std::string s{"helleh"};
+
+    if (is_palindrome(s)) {
+        std::cout << "The given string is a palindrome. \n";
+    } else {
+        std::cout << "The given string isn't a palindrome. \n";
+    }
+
+    std::cout << std::endl;
+
+    return 0;
+}
