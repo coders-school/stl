764b43ef4b9e6a0c255c6bf6231ea51c0576fec7
Michał Maj <47927347+michalmaj@users.noreply.github.com>
2023-11-24 19:08:53 +0100

    Create transform.h

---
+++ homework/transform-containers/transform.h
@@ -0,0 +1,15 @@
+#ifndef TRANSFORM_H
+#define TRANSFORM_H
+
+#include <algorithm>
+#include <deque>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <string>
+#include <set>
+
+std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string> l, std::deque<int> d);
+
+#endif
