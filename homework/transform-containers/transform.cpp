6b0ee2babe24a572d3c2979c23a0647f180f202c
Michał Maj <47927347+michalmaj@users.noreply.github.com>
2023-11-24 19:26:59 +0100

    Update transform.cpp

--- homework/transform-containers/transform.cpp
+++ homework/transform-containers/transform.cpp
@@ -5,6 +5,7 @@ std::map<int, std::string> removeDuplicateAndTranformToMap(std::list<std::string
     std::set<int> s1{d.begin(), d.end()};
     std::set<std::string> s2{l.begin(), l.end()};
 
+    
     std::transform(s1.begin(), s1.end(), s2.begin(), std::inserter(m, m.begin()), [](const auto &key, const auto &value) {
         return std::make_pair(key, value);
     });
