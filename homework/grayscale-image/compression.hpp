4866f02eecabc1d8b9bf96c246a6eeef06e4d359
Michał Maj <47927347+michalmaj@users.noreply.github.com>
2023-11-25 18:53:49 +0100

    Update compression.hpp

--- homework/grayscale-image/compression.hpp
+++ homework/grayscale-image/compression.hpp
@@ -1,1 +1,11 @@
+#ifndef COMPRESSION_HPP
+#define COMPRESSION_HPP
 
+#include <array>
+#include <cstdint>
+#include <utility>
+#include <vector>
+
+std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(std::array<std::array<uint8_t, 32>, 32>& img);
+
+#endif
