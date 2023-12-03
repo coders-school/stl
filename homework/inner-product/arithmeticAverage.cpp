ccc3e98a1f8935072d2e1f24bd903aa4992c3152
Michał Maj <47927347+michalmaj@users.noreply.github.com>
2023-12-03 12:25:19 +0100

    Update arithmeticAverage.cpp

--- homework/inner-product/arithmeticAverage.cpp
+++ homework/inner-product/arithmeticAverage.cpp
@@ -2,11 +2,11 @@
 #include <cmath>
 #include <numeric>
 
-double ArithmeticAverage(const std::vector<int> &first, const std::vector<int> &second) {
+double ArithmeticAverage(const std::vector<int>& first, const std::vector<int>& second) {
     return (std::reduce(first.begin(), first.end(), 0) + std::reduce(second.begin(), second.end(), 0)) / static_cast<double>(first.size() + second.size());
 }
 
-double Distance(const std::vector<int> &first, const std::vector<int> &second) {
+double Distance(const std::vector<int>& first, const std::vector<int>& second) {
     double sum = 0.0;
     for (size_t i = 0; i < first.size(); ++i) {
         sum += std::pow(first[i] - second[i], 2);
