afef527170dfd5e7ea2d1d078d4417d8c7bcb503
Michał Maj <47927347+michalmaj@users.noreply.github.com>
2023-12-03 12:19:52 +0100

    Create arithmeticAverage.hpp

---
+++ homework/inner-product/arithmeticAverage.hpp
@@ -0,0 +1,8 @@
+#ifndef ARITHMETIC_AVERAGE_HPP
+#define ARITHMETIC_AVERAGE_HPP
+#include <vector>
+
+double ArithmeticAverage(const std::vector<int> &first, const std::vector<int> &second);
+double Distance(const std::vector<int> &first, const std::vector<int> &second);
+
+#endif
