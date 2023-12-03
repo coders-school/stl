939060cf81adf5945e29c1e8896980023fbcece4
Michał Maj <47927347+michalmaj@users.noreply.github.com>
2023-12-03 12:24:47 +0100

    Update arithmeticAverage.hpp

--- homework/inner-product/arithmeticAverage.hpp
+++ homework/inner-product/arithmeticAverage.hpp
@@ -2,7 +2,7 @@
 #define ARITHMETIC_AVERAGE_HPP
 #include <vector>
 
-double ArithmeticAverage(const std::vector<int> &first, const std::vector<int> &second);
-double Distance(const std::vector<int> &first, const std::vector<int> &second);
+double ArithmeticAverage(const std::vector<int>& first, const std::vector<int>& second);
+double Distance(const std::vector<int>& first, const std::vector<int>& second);
 
 #endif
