3e9a17e690e40f30501d4a93785b3fc0f4f0f4ba
Michał Maj <47927347+michalmaj@users.noreply.github.com>
2023-12-03 12:24:20 +0100

    Create main.cpp

---
+++ homework/inner-product/main.cpp
@@ -0,0 +1,10 @@
+#include "arithmeticAverage.hpp"
+#include <vector>
+
+int main() {
+    auto a = Distance({7, 4, 3}, {17, 6, 2});
+    std::cout << a << std::endl;
+    a = ArithmeticAverage({1, 2, 3, 4}, {1, 2, 3, 4});
+    std::cout << a << std::endl;
+    return 0;
+}
