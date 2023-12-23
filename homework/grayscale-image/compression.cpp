36e6aba2ca2f557514c0225b04ab56f335215a07
Maciej Dawiec <dmaciek85@gmail.com>
2023-12-23 14:45:55 +0100

    adding orking task

---
+++ homework/grayscale-image/compression.cpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "compression.hpp"
+
+std::vector<std::pair<uint8_t, uint8_t>>
+    compressGrayscale(const std::array<std::array<uint8_t, width>, height>& pixelArray) {
+        std::vector<std::pair<uint8_t, uint8_t>> forCompresion;
+        auto counter{1};
+        for (auto i = pixelArray.begin(); i != pixelArray.end(); ++i) {
+            for (auto j = i->begin(); j != i->end(); ++j) {
+                auto it = std::next(j);
+                if (*j == *it) {
+                    counter++;
+                    if (it == i->end()) {
+                        forCompresion.push_back(std::make_pair(*j, counter - 1));
+                        counter = 1;
+                    }
+                } else {
+                    forCompresion.push_back(std::make_pair(*j, counter));
+                    counter = 1;
+                }
+            }
+            std::cout << std::endl;
+        }
+    return forCompresion;
+    }
+
+std::array<std::array<uint8_t, width>, height>
+    decompressGrayscale(const std::vector<std::pair<uint8_t, uint8_t>>& pixelVector) {
+        std::array<std::array<uint8_t, width>, height> deCompresion;
+        std::vector<uint8_t> temVec;
+        for (auto const& element : pixelVector) {
+            std::fill_n(std::back_inserter(temVec), element.second, element.first);
+        }
+        auto it = temVec.begin();
+        for (auto i = 0; i != height; ++i) {
+            for (auto j = 0; j != width; ++j) {
+                deCompresion[i][j] = *it;
+                it++;
+            }
+        }
+        return deCompresion;
+    }
+
+void printMap(std::array<std::array<uint8_t, width>, height>& pixelPrint) {
+    for (auto i = 0; i != height; ++i) {
+        for (auto j = 0; j != width; ++j) {
+            std::cout << pixelPrint[i][j];
+        }
+    }
+}
---
+++ homework/grayscale-image/compression.hpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <utility>
+
+constexpr size_t height = 32;
+constexpr size_t width = 32;
+
+std::vector<std::pair<uint8_t, uint8_t>>
+    compressGrayscale(const std::array<std::array<uint8_t, width>, height>& pixelArray);
+std::array<std::array<uint8_t, width>, height>
+    decompressGrayscale(const std::vector<std::pair<uint8_t, uint8_t>>& pixelVector);
+
+void printMap(std::array<std::array<uint8_t, width>, height>& pixelPrint);
