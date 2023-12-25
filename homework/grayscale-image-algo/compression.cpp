cec5b79534d33113fd6f406d050bef032b507be0
Łukasz Konieczny <ftefrjbhfvasf32@o2.pl>
2023-12-26 00:33:30 +0100

    Changed compression to use for_each

--- homework/grayscale-image-algo/compression.cpp
+++ homework/grayscale-image-algo/compression.cpp
@@ -1,26 +1,50 @@
 #include "compression.hpp"
 
+#include <algorithm>
+
 std::vector<std::pair<uint8_t, uint8_t>>
 compressGrayscale(const std::array<std::array<uint8_t, width>, height>& image) {
     std::vector<std::pair<uint8_t, uint8_t>> result;
-    for (size_t i = 0; i < height; i++) {
+    std::for_each(image.cbegin(), image.cend(), [&result](const auto& elem1){
         uint8_t count = 1;
         uint8_t prevColor = 0;
-        for (size_t j = 0; j < width; j++) {
+        std::for_each(elem1.cbegin(),
+                      elem1.cend(),
+                      [&result, &count, &prevColor, &elem1, j = 0](const auto& elem2) mutable {
             if (j == 0) {
-                prevColor = image[i][j];
+                prevColor = elem1[j];
             } else {
-                if (prevColor != image[i][j]) {
+                if (prevColor != elem1[j]) {
                     result.push_back(std::make_pair(prevColor, count));
                     count = 1;
-                    prevColor = image[i][j];
+                    prevColor = elem1[j];
                 } else {
                     count++;
                 }
             }
-        }
+            j++;
+        });
         result.push_back(std::make_pair(prevColor, count));
-    }
+    });
+
+    // for (size_t i = 0; i < height; i++) {
+    //     uint8_t count = 1;
+    //     uint8_t prevColor = 0;
+    //     for (size_t j = 0; j < width; j++) {
+    //         if (j == 0) {
+    //             prevColor = image[i][j];
+    //         } else {
+    //             if (prevColor != image[i][j]) {
+    //                 result.push_back(std::make_pair(prevColor, count));
+    //                 count = 1;
+    //                 prevColor = image[i][j];
+    //             } else {
+    //                 count++;
+    //             }
+    //         }
+    //     }
+    //     result.push_back(std::make_pair(prevColor, count));
+    // }
     return result;
 }
 
