23e3d8c450acf74b7cb4b187f0156574de40d9e4
Łukasz Konieczny <ftefrjbhfvasf32@o2.pl>
2023-12-26 16:28:48 +0100

    Generating map implemented

---
+++ homework/transform-containers/generate_map.cpp
@@ -0,0 +1,27 @@
+#include "generate_map.hpp"
+
+#include <algorithm>
+#include <iterator>
+#include <utility>
+#include <vector>
+
+std::map<int, std::string>
+removeDuplicateAndTranformToMap(std::list<std::string> l, std::deque<int> d) {
+    std::sort(d.begin(), d.end());
+    l.sort();
+    d.erase(std::unique(d.begin(), d.end()), d.end());
+    l.unique();
+    std::vector<std::pair<int, std::string>> temp;
+    std::map<int, std::string> result;
+    std::transform(d.cbegin(),
+                   d.cend(),
+                   l.cbegin(),
+                   std::back_inserter(temp),
+                   [](const auto& item1, const auto& item2) {
+                       return std::make_pair(item1, item2);
+                   });
+    std::for_each(temp.cbegin(), temp.cend(), [&result](const auto& elem) {
+        result[elem.first] = elem.second;
+    });
+    return result;
+}
