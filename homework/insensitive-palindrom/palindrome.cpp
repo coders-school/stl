5dc363c6cc31538057707b450c5e57eacfaadad5
Łukasz Konieczny <ftefrjbhfvasf32@o2.pl>
2023-12-26 14:05:37 +0100

    Formatting fix

--- homework/insensitive-palindrom/palindrome.cpp
+++ homework/insensitive-palindrom/palindrome.cpp
@@ -6,20 +6,21 @@
 
 bool is_palindrome(std::string words) {
     words.erase(std::remove_if(words.begin(), words.end(), [](const auto& elem){
-        if (std::tolower(elem) < 'a' || std::tolower(elem) > 'z') {
-            return true;
-        } else {
-            return false;
-        }
-    }), words.end());
+                    if (std::tolower(elem) < 'a' || std::tolower(elem) > 'z') {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }),
+                words.end());
     return std::equal(words.cbegin(),
                       words.cbegin() + words.size() / 2,
                       words.crbegin(),
                       [](const auto& letter1, const auto& letter2){
-        if (std::tolower(letter1) == std::tolower(letter2)) {
-            return true;
-        } else {
-            return false;
-        }
-    });
+                          if (std::tolower(letter1) == std::tolower(letter2)) {
+                              return true;
+                          } else {
+                              return false;
+                          }
+                    });
 }
