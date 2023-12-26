f84eece3d4256b4280e40468df7849c1806ec70f
Łukasz Konieczny <ftefrjbhfvasf32@o2.pl>
2023-12-26 01:10:12 +0100

    Changed decompression to use algorithms

--- homework/grayscale-image-algo/compression.cpp
+++ homework/grayscale-image-algo/compression.cpp
@@ -26,25 +26,6 @@ compressGrayscale(const std::array<std::array<uint8_t, width>, height>& image) {
         });
         result.push_back(std::make_pair(prevColor, count));
     });
-
-    // for (size_t i = 0; i < height; i++) {
-    //     uint8_t count = 1;
-    //     uint8_t prevColor = 0;
-    //     for (size_t j = 0; j < width; j++) {
-    //         if (j == 0) {
-    //             prevColor = image[i][j];
-    //         } else {
-    //             if (prevColor != image[i][j]) {
-    //                 result.push_back(std::make_pair(prevColor, count));
-    //                 count = 1;
-    //                 prevColor = image[i][j];
-    //             } else {
-    //                 count++;
-    //             }
-    //         }
-    //     }
-    //     result.push_back(std::make_pair(prevColor, count));
-    // }
     return result;
 }
 
@@ -53,15 +34,17 @@ decompressGrayscale(const std::vector<std::pair<uint8_t, uint8_t>>& compressed) 
     std::array<std::array<uint8_t, width>, height> result;
     uint8_t lineLength = 0;
     size_t i = 0;
-    for (const auto& elem : compressed) {
-        for (size_t j = lineLength; j < lineLength + elem.second; j++) {
-            result[i][j] = elem.first;
-        }
+    std::for_each(compressed.cbegin(),
+                  compressed.cend(),
+                  [&i, &result, &lineLength](const auto& elem) {
+        std::fill(result[i].begin() + lineLength,
+                  result[i].begin() + lineLength + elem.second,
+                  elem.first);
         lineLength += elem.second;
         if (lineLength == width) {
             i++;
             lineLength = 0;
         }
-    }
+    });
     return result;
 }
