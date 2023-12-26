2d25a47cd92f4c36ed2bbda4ec051f42e05879a9
Łukasz Konieczny <ftefrjbhfvasf32@o2.pl>
2023-12-26 16:33:30 +0100

    Formatting fix 2

--- homework/transform-containers/generate_map.hpp
+++ homework/transform-containers/generate_map.hpp
@@ -6,4 +6,4 @@
 #include <string>
 
 std::map<int, std::string>
-removeDuplicateAndTranformToMap(std::list<std::string>, std::deque<int>);
+    removeDuplicateAndTranformToMap(std::list<std::string>, std::deque<int>);
