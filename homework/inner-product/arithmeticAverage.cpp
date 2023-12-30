0fed6050644f6fbddf13e81fc472ba3e412736da
Łukasz Konieczny <ftefrjbhfvasf32@o2.pl>
2023-12-30 12:49:37 +0100

    Formatting fix 2

--- homework/inner-product/arithmeticAverage.cpp
+++ homework/inner-product/arithmeticAverage.cpp
@@ -10,17 +10,17 @@ double ArithmeticAverage(const std::vector<int>& first, const std::vector<int>& 
                               second.cbegin(),
                               0.0,
                               std::plus<int>{},
-                              std::plus<int>{})
-        / (first.size() + second.size());
+                              std::plus<int>{}) /
+           (first.size() + second.size());
 }
 
 double Distance(const std::vector<int>& first, const std::vector<int>& second) {
     return std::sqrt(std::inner_product(first.cbegin(),
-                              first.cend(),
-                              second.cbegin(),
-                              0.0,
-                              std::plus<int>{},
-                              [](const auto& f, const auto& s) {
-                                  return (f - s) * (f - s);
-                              }));
+                                        first.cend(),
+                                        second.cbegin(),
+                                        0.0,
+                                        std::plus<int>{},
+                                        [](const auto& f, const auto& s) {
+                                            return (f - s) * (f - s);
+                                        }));
 }
