e412dc7e02344b46a9a97b626ddf7038c6161db0
baldingTomato <tomek.kluba12@gmail.com>
2024-01-31 20:00:35 +0100

    Prepared header file with declarations

---
+++ homework/remove-vowels/vowel.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+
+void printWords(const std::vector<std::string>& v);
+
+bool isVowel(char c);
+
+void removeVowels(std::vector<std::string>& v);
