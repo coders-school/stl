86a1d013a650c52d03771dafa86827800b839759
MaciekDavv <dmaciek85@gmail.com>
2024-02-25 22:18:10 +0100

    adding formating 2

--- homework/insensitive-palindrom/palindrom.hpp
+++ homework/insensitive-palindrom/palindrom.hpp
@@ -5,6 +5,6 @@
 #include <string>
 #include <vector>
 
-const std::string charsToRemove = { " “ ” ’ —" };
+const std::string charsToRemove = {" “ ” ’ —"};
 std::string removeSpecificChars(std::string& str, const std::string& charsToRemove);
 bool is_palindrome(const std::string& neo);
